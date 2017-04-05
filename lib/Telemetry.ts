96dd207ec85ff6d4e075b4f2d32625bc50cd04e4
shivbijlani <shivbijlani@users.noreply.github.com>
2017-04-05 12:00:59 -0400

    Fix for issue #3: Implement Telemetry public API surface (#8)
    * Fix for issue #3: Implement Telemetry public API surface
    
    * Fix typo

---
+++ lib/Telemetry.ts
@@ -0,0 +1,17 @@
+namespace MSAL {
+    export class Telemetry {
+        private static instance: Telemetry;
+        private receiverCallback: (r: Array<Object>) => void;
+
+        private constructor() {
+        }
+
+        RegisterReceiver(receiverCallback: (receiver: Array<Object>) => void): void {
+            this.receiverCallback = receiverCallback;
+        }
+
+        static GetInstance(): Telemetry {
+            return this.instance || (this.instance = new this());
+        }
+    }
+}
