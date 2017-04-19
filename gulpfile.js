a67104a6c58e98a73b9445645a5278a6cd8b3758
Shiv Bijlani <shivb@microsoft.com>
2017-04-18 18:35:31 -0700

    Enables tslint in visual studio code and 2015/2017

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+﻿/// <binding AfterBuild='TSLint:All' ProjectOpened='TSLint:Watch' />
+//Node packages
+var gulp = require("gulp");
+var plumber = require("gulp-plumber");
+var tslint = require("gulp-tslint");
+
+//Paths to include/exclude
+var TYPE_SCRIPT_FILES = ["lib/**/*.ts", "!node_modules/**"];
+
+//Our TSLint settings
+var TYPE_SCRIPT_REPORT = tslint.report({ summarizeFailureOutput: true });
+
+//The actual task to run
+gulp.task("TSLint:All", function () {
+    return gulp.src(TYPE_SCRIPT_FILES)
+        .pipe(plumber())
+        .pipe(tslint({ formatter: "msbuild" }))
+        .pipe(TYPE_SCRIPT_REPORT);
+});
