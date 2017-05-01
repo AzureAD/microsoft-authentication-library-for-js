679e2e67c4671e39aa2573f803e5b415787d0909
Shiv Bijlani <shivb@microsoft.com>
2017-05-01 16:38:45 -0700

    switch to FF

--- karma.conf.js
+++ karma.conf.js
@@ -36,7 +36,7 @@ module.exports = function (config) {
         // - Safari (only Mac)
         // - PhantomJS
         // - IE (only Windows)
-        browsers: ['Chrome'],
+        browsers: ['Firefox'],
 
         // Continuous Integration mode
         // if true, it capture browsers, run tests and exit
