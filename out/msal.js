42ef9914c90ffdcd4eb12193a6d9ae7cbe647284
Rohit Narula <ronaru@microsoft.com>
2017-10-06 16:13:34 -0700

    adding clientId as default parameter for loginPopup

--- out/msal.js
+++ out/msal.js
@@ -1,16 +1,8 @@
-/*! msal v0.1.2 2017-09-29 */
-
-'use strict';
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = Object.setPrototypeOf ||
-        ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-        function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
@@ -1232,6 +1224,9 @@ var Msal;
                         return;
                     }
                     scopes = _this.filterScopes(scopes);
+                }
+                else {
+                    scopes = [_this.clientId];
                 }
                 var scope = scopes.join(" ").toLowerCase();
                 var popUpWindow = _this.openWindow('about:blank', '_blank', 1, _this, resolve, reject);
