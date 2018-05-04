b7c37f8c193fa8b48ec8d9c295a89653237fb5f5
nehaagrawal <nehaagrawal30@gmail.com>
2018-05-04 13:48:18 -0700

    moving files

--- lib-commonjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { UserAgentApplication } from "./UserAgentApplication";
-export { Logger } from "./Logger";
-export { LogLevel } from "./Logger";
-export { User } from "./User";
-export { Constants } from "./Constants";
