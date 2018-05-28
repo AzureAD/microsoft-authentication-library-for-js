4a914943b523ba644713ef1efe7ffcf86a124c3e
nehaagrawal <nehaagrawal30@gmail.com>
2018-05-28 16:45:50 -0700

    updated angular sample

--- lib/msalAngular/devApps/MSALAngularDemoApp/src/app/msGraph.service.ts
+++ lib/msalAngular/devApps/MSALAngularDemoApp/src/app/msGraph.service.ts
@@ -7,15 +7,17 @@ export class MsGraphService {
 
   private startDate = new Date();
   private endDate = new Date();
-  private url : string;
 
   constructor(private http: HttpClient) {
-    this.endDate.setDate(this.startDate.getDate()+7);
-     this.url = "https://graph.microsoft.com/beta/me/calendarview?startdatetime=" +this.startDate.toLocaleDateString("en-US") + "&enddatetime="+ this.endDate.toLocaleDateString("en-US")+ "&$select=subject,start,end&$orderBy=start/dateTime";
+    this.endDate.setDate(this.startDate.getDate() + 7);
+  }
+
+  getUrl(): string {
+    return "https://graph.microsoft.com/beta/me/calendarview?startdatetime=" + this.startDate.toLocaleDateString("en-US") + "&enddatetime=" + this.endDate.toLocaleDateString("en-US") + "&$select=subject,start,end&$orderBy=start/dateTime";
   }
 
   public httpGetRequest() {
-    return this.http.get(this.url)
+    return this.http.get(this.getUrl())
       .map(response => {
         return response;
       })
