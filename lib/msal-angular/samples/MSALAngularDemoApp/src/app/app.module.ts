cc39c114bf3cf94b86d36ec1cd9d3ce78e8bb2fd
nehaagrawal <nehaagrawal30@gmail.com>
2018-08-14 14:27:58 -0700

    removed unused code

--- lib/msal-angular/samples/MSALAngularDemoApp/src/app/app.module.ts
+++ lib/msal-angular/samples/MSALAngularDemoApp/src/app/app.module.ts
@@ -11,7 +11,6 @@ import {ProductDetailComponent} from './product/product-detail.component'
 import {ProductService} from './product/product.service';
 import {appRoutes} from './app.routes';
 import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-//import {MsalModule, MsalInterceptor} from "@azure/msal-angular";
 import {MsalModule} from "../../../../dist/msal.module";
 import { MsalInterceptor} from "../../../../dist/msal.interceptor";
 import {LogLevel} from "msal";
