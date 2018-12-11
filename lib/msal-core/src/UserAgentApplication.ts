f80448fbe4a92bf1d83fe5c3e61bceeb95796346
Sameera Gajjarapu <sameera.gajjarapu@microsoft.com>
2018-12-10 17:23:43 -0800

    Remove prompt=select_account from all login functions

--- lib/msal-core/src/UserAgentApplication.ts
+++ lib/msal-core/src/UserAgentApplication.ts
@@ -410,7 +410,7 @@ export class UserAgentApplication {
               this._cacheStorage.setItem(Constants.msalErrorDescription, "");
               const authorityKey = Constants.authority + Constants.resourceDelimeter + authenticationRequest.state;
               this._cacheStorage.setItem(authorityKey, this.authority, this.storeAuthStateInCookie);
-              const urlNavigate = authenticationRequest.createNavigateUrl(scopes) + Constants.prompt_select_account + Constants.response_mode_fragment;
+              const urlNavigate = authenticationRequest.createNavigateUrl(scopes)  + Constants.response_mode_fragment;
               this.promptUser(urlNavigate);
           });
   }
@@ -495,7 +495,7 @@ export class UserAgentApplication {
           this._cacheStorage.setItem(Constants.msalErrorDescription, "");
           const authorityKey = Constants.authority + Constants.resourceDelimeter + authenticationRequest.state;
           this._cacheStorage.setItem(authorityKey, this.authority, this.storeAuthStateInCookie);
-          const urlNavigate = authenticationRequest.createNavigateUrl(scopes) + Constants.prompt_select_account + Constants.response_mode_fragment;
+          const urlNavigate = authenticationRequest.createNavigateUrl(scopes)  + Constants.response_mode_fragment;
           window.renewStates.push(authenticationRequest.state);
           window.requestType = Constants.login;
           this.registerCallback(authenticationRequest.state, scope, resolve, reject);
