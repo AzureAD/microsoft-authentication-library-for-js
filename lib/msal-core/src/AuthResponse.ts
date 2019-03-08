9a200bebf2a31898e158ae435d1f7bac8a1ae27e
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-03-08 15:01:08 -0800

    Adding response object

---
+++ lib/msal-core/src/AuthResponse.ts
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+export type AuthResponse = {
+    uniqueId: string;
+    tenantId: string;
+    tokenType: string;
+    idToken: object;
+    accessToken: object;
+    scopes: Array<string>;
+    expiresOn: Date;
+    account: Account;
+    userState: string;
+}
