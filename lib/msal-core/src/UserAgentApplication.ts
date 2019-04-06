628601604067e85c4d0545b55aa44afcd5ae94ad
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-04-05 18:22:19 -0700

    Updating tests

--- lib/msal-core/src/UserAgentApplication.ts
+++ lib/msal-core/src/UserAgentApplication.ts
@@ -142,7 +142,7 @@ const resolveTokenOnlyIfOutOfIframe = (target: any, propertyKey: string, descrip
 export class UserAgentApplication {
 
   // input Configuration by the user
-  private config: Configuration;
+  public config: Configuration;
 
   // TODO: This will be extracted with Response Changes
   private tokenReceivedCallback: tokenReceivedCallback = null;
@@ -209,9 +209,9 @@ export class UserAgentApplication {
 
     // cache keys msal - typescript throws an error if any value other than "localStorage" or "sessionStorage" is passed
     try {
-        this.cacheStorage = new Storage(this.config.cache.cacheLocation);
+      this.cacheStorage = new Storage(this.config.cache.cacheLocation);
     } catch (e) {
-        this.config.system.logger.error("CacheLocation can be set only to 'localStorage' or 'sessionStorage' ");
+      this.config.system.logger.error("CacheLocation can be set only to 'localStorage' or 'sessionStorage' ");
     }
 
     // Initialize window handling code
@@ -356,7 +356,7 @@ export class UserAgentApplication {
     this.userLoginInProgress = true;
 
     // TODO: Make this more readable - is authorityInstance changed, what is happening with the return for AuthorityKey?
-    this.authorityInstance.ResolveEndpointsAsync().then(() => {
+    this.authorityInstance.resolveEndpointsAsync().then(() => {
 
       // create the Request to be sent to the Server
       let serverAuthenticationRequest = new ServerRequestParameters(
@@ -396,7 +396,7 @@ export class UserAgentApplication {
 
       // build URL to navigate to proceed with the login
       let urlNavigate = serverAuthenticationRequest.createNavigateUrl(scopes)  + Constants.response_mode_fragment;
-
+      console.log(urlNavigate);
       // Redirect user to login URL
       this.promptUser(urlNavigate);
     });
@@ -446,7 +446,7 @@ export class UserAgentApplication {
     this.acquireTokenInProgress = true;
 
     // TODO: Set response type here
-    acquireTokenAuthority.ResolveEndpointsAsync().then(() => {
+    acquireTokenAuthority.resolveEndpointsAsync().then(() => {
       // On Fulfillment
       const responseType = this.getTokenType(userObject, request.scopes, false);
       serverAuthenticationRequest = new ServerRequestParameters(
@@ -597,7 +597,7 @@ export class UserAgentApplication {
     this.userLoginInProgress = true;
 
     // Resolve endpoint
-    this.authorityInstance.ResolveEndpointsAsync().then(() => {
+    this.authorityInstance.resolveEndpointsAsync().then(() => {
       let serverAuthenticationRequest = new ServerRequestParameters(this.authorityInstance, this.clientId, scopes, ResponseTypes.id_token, this.getRedirectUri(), this.config.auth.state);
 
       // populate QueryParameters (sid/login_hint/domain_hint) and any other extraQueryParameters set by the developer
@@ -701,7 +701,7 @@ export class UserAgentApplication {
         return;
       }
 
-      acquireTokenAuthority.ResolveEndpointsAsync().then(() => {
+      acquireTokenAuthority.resolveEndpointsAsync().then(() => {
         // On fullfillment
         const responseType = this.getTokenType(userObject, request.scopes, false);
         serverAuthenticationRequest = new ServerRequestParameters(
@@ -985,7 +985,7 @@ export class UserAgentApplication {
             serverAuthenticationRequest.authorityInstance = request.authority ? AuthorityFactory.CreateInstance(request.authority, this.config.auth.validateAuthority) : this.authorityInstance;
         }
         // cache miss
-        return serverAuthenticationRequest.authorityInstance.ResolveEndpointsAsync()
+        return serverAuthenticationRequest.authorityInstance.resolveEndpointsAsync()
         .then(() => {
           // refresh attempt with iframe
           // Already renewing for this scope, callback when we get the token.
@@ -2202,7 +2202,7 @@ export class UserAgentApplication {
   /**
    * tracks if login is in progress
    */
-  loginInProgress(): boolean {
+  protected getLoginInProgress(): boolean {
     const pendingCallback = this.cacheStorage.getItem(Constants.urlHash);
     if (pendingCallback) {
         return true;
@@ -2213,7 +2213,7 @@ export class UserAgentApplication {
   /**
    * @param userLoginInProgress
    */
-  protected setuserLoginInProgress(userLoginInProgress : boolean) {
+  protected setUserLoginInProgress(userLoginInProgress : boolean) {
     this.userLoginInProgress = userLoginInProgress;
   }
 
@@ -2247,7 +2247,7 @@ export class UserAgentApplication {
    * @ignore
    * @hidden
    */
-  private getRedirectUri(): string {
+  public getRedirectUri(): string {
     if (typeof this.config.auth.redirectUri === "function") {
       return this.config.auth.redirectUri();
     }
@@ -2259,7 +2259,7 @@ export class UserAgentApplication {
    * @ignore
    * @hidden
    */
-  private getPostLogoutRedirectUri(): string {
+  public getPostLogoutRedirectUri(): string {
     if (typeof this.config.auth.postLogoutRedirectUri === "function") {
       return this.config.auth.postLogoutRedirectUri();
     }
---
+++ lib/msal-core/tests/mochaTests/genericTests.ts
@@ -0,0 +1,123 @@
+import * as Mocha from "mocha";
+import * as chai from "chai";
+import { UserAgentApplication, AuthError, ClientConfigurationError, ClientAuthError, Authority } from '../../src/index';
+import { buildConfiguration } from "../../src/Configuration";
+import sinon from "sinon";
+import { XhrClient } from "../../src/XHRClient";
+import { ITenantDiscoveryResponse } from "../../src/ITenantDiscoveryResponse";
+const expect = chai.expect;
+chai.config.includeStack = false;
+
+describe("Redirect Flow Unit Tests", function () {
+    var msal: UserAgentApplication;
+
+    var DEFAULT_INSTANCE = "https://login.microsoftonline.com/";
+    var TEST_REDIR_URI = "https://localhost:8081/redirect.html"
+    var TENANT = 'common';
+    var validAuthority = DEFAULT_INSTANCE + TENANT;
+    const validOpenIdConfigString = `{"authorization_endpoint":"${validAuthority}/oauth2/v2.0/authorize","token_endpoint":"https://token_endpoint","issuer":"https://fakeIssuer", "end_session_endpoint":"https://end_session_endpoint"}`;
+    const validOpenIdConfigurationResponse: ITenantDiscoveryResponse = {
+        AuthorizationEndpoint: `${validAuthority}/oauth2/v2.0/authorize`,
+        EndSessionEndpoint: `https://end_session_endpoint`,
+        Issuer: `https://fakeIssuer`
+    }
+
+    beforeEach(function() {
+        this.jsdom = require("jsdom-global")("", { url: TEST_REDIR_URI });
+
+        let config = buildConfiguration({clientId: "0813e1d1-ad72-46a9-8665-399bba48c201" }, {}, {}, {});
+        msal = new UserAgentApplication(config);
+    });
+
+    it("throws error if loginRedirect is called without calling setRedirectCallbacks", function (done) {
+        expect(msal.getRedirectUri()).to.be.equal(window.location.href);
+        // expect(msal.loginRedirect.bind(msal)).to.throw(ClientConfigurationError);
+        done();
+    });
+
+    it('throws error if null or non-function argument is passed to either argument of setRedirectCallbacks', (done) => {
+        // expect(() => msal.setRedirectCallbacks(function(token, tokenType, state) {}, null)).to.throw(ClientConfigurationError);
+        // expect(() => msal.setRedirectCallbacks(null, function(err, state) {})).to.throw(ClientConfigurationError);
+        done();
+    });
+
+    it('navigates user to login and prompt parameter is not passed by default', (done) => {
+        let resolveEndptsStub = sinon.stub(msal.authorityObj, "resolveEndpointsAsync").callsFake(function () {
+            return new Promise((resolve, reject) => {
+                resolve(msal.authority);
+            });
+        });
+        let authorityAuthEndptStub = sinon.stub(msal.authorityObj, "AuthorizationEndpoint").callsFake( function () {
+            return validOpenIdConfigurationResponse.AuthorizationEndpoint;
+        });
+        let authorityEndSessionEndptStub = sinon.stub(msal.authorityObj, "EndSessionEndpoint").callsFake( function () {
+            return validOpenIdConfigurationResponse.EndSessionEndpoint;
+        });;
+        let authorityIssuerStub = sinon.stub(msal.authorityObj, "SelfSignedJwtAudience").callsFake( function () {
+            return validOpenIdConfigurationResponse.Issuer;
+        });;
+        msal.handleRedirectCallbacks(function(response) {}, function (error) {});
+        expect(msal.getRedirectUri()).to.be.equal(window.location.href);
+        // msal.promptUser = function (args: string) {
+        //     expect(args).toContain(DEFAULT_INSTANCE + TENANT + '/oauth2/v2.0/authorize?response_type=id_token&scope=openid%20profile');
+        //     expect(args).toContain('&client_id=' + msal.clientId);
+        //     expect(args).toContain('&redirect_uri=' + encodeURIComponent(msal.getRedirectUri()));
+        //     expect(args).toContain('&state');
+        //     expect(args).toContain('&client_info=1');
+        //     expect(args).not.toContain(Constants.prompt_select_account);
+        //     expect(args).not.toContain(Constants.prompt_none);
+        //     done();
+        // };
+        msal.loginRedirect({});
+        // console.log(global.window.location);
+        done();
+    });
+
+    // it('navigates user to login and prompt parameter is passed as extraQueryParameter', (done) => {
+    //     msal.setRedirectCallbacks(function(token, tokenType, state) {}, function (error) {});
+    //     expect(msal.getRedirectUri()).toBe(global.window.location.href);
+    //     msal.promptUser = function (args: string) {
+    //         expect(args).toContain(DEFAULT_INSTANCE + TENANT + '/oauth2/v2.0/authorize?response_type=id_token&scope=openid%20profile');
+    //         expect(args).toContain('&client_id=' + msal.clientId);
+    //         expect(args).toContain('&redirect_uri=' + encodeURIComponent(msal.getRedirectUri()));
+    //         expect(args).toContain('&state');
+    //         expect(args).toContain('&client_info=1');
+    //         expect(args).toContain(Constants.prompt_select_account);
+    //         expect(args).not.toContain(Constants.prompt_none);
+    //         done();
+    //     };
+
+    //     msal.loginRedirect(null, Constants.prompt_select_account);
+    // });
+
+    // it('navigates user to redirectURI passed as extraQueryParameter', (done) => {
+    //     msal = new UserAgentApplication("0813e1d1-ad72-46a9-8665-399bba48c201", null, { redirectUri: TEST_REDIR_URI });
+    //     msal.setRedirectCallbacks(function(token, tokenType, state) {}, function (error) {});
+    //     msal._user = null;
+    //     msal._renewStates = [];
+    //     msal._activeRenewals = {};
+    //     msal._cacheStorage = storageFake;
+    //     expect(msal._redirectUri).toBe(TEST_REDIR_URI);
+    //     msal.promptUser = function (args: string) {
+    //         expect(args).toContain(DEFAULT_INSTANCE + TENANT + '/oauth2/v2.0/authorize?response_type=id_token&scope=openid%20profile');
+    //         expect(args).toContain('&client_id=' + msal.clientId);
+    //         expect(args).toContain('&redirect_uri=' + encodeURIComponent(msal._redirectUri));
+    //         expect(args).toContain('&state');
+    //         expect(args).toContain('&client_info=1');
+    //         done();
+    //     };
+
+    //     msal.loginRedirect();
+    // });
+
+    // it('uses current location.href as returnUri by default, even if location changed after UserAgentApplication was instantiated', (done) => {
+    //     history.pushState(null, null, '/new_pushstate_uri');
+    //     msal.setRedirectCallbacks(function(token, tokenType, state) {}, function (error) {});
+    //     msal.promptUser = function (args: string) {
+    //         expect(args).toContain('&redirect_uri=' + encodeURIComponent('http://localhost:8080/new_pushstate_uri'));
+    //         done();
+    //     };
+    //     msal.loginRedirect();
+    // });
+
+});
