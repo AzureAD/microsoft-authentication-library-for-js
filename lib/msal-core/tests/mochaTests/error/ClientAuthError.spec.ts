ab7da39ee13b5c54fdbe232177d136093c84429d
sameerag <sameera.gajjarapu@microsoft.com>
2019-04-08 17:18:28 -0700

    All Error classes - UnitTest files

---
+++ lib/msal-core/tests/mochaTests/error/ClientAuthError.spec.ts
@@ -0,0 +1,307 @@
+import * as chai from "chai";
+import { ClientAuthError, ClientAuthErrorMessage } from "../../../src/error/ClientAuthError";
+import { IdToken } from "../../../src/IdToken";
+
+const expect = chai.expect;
+chai.config.includeStack = false;
+
+// TODO: Should we test the type of object created? Also setPrototypeOf() related test to be added if needed.
+describe("ClientAuthError", () => {
+
+  it("ClientAuthError object can be created", () => {
+    const TEST_ERROR_CODE: string = "test";
+    const TEST_ERROR_MSG: string = "This is a test error";
+    const clientAuthError = new ClientAuthError(TEST_ERROR_CODE, TEST_ERROR_MSG);
+    let err: ClientAuthError;
+
+    try {
+      throw clientAuthError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(TEST_ERROR_CODE);
+    expect(err.errorMessage).to.equal(TEST_ERROR_MSG);
+    expect(err.message).to.equal(TEST_ERROR_MSG);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createEndpointResolutionError creates a ClientAuthError object", () => {
+
+    const ERROR_DETAIL = "This is the error detail";
+    const endPtReslnError = ClientAuthError.createEndpointResolutionError(ERROR_DETAIL);
+    let err: ClientAuthError;
+
+    try {
+      throw endPtReslnError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.endpointResolutionError.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.endpointResolutionError.desc);
+    expect(err.errorMessage).to.contain(ERROR_DETAIL);
+    expect(err.message).to.contain(ClientAuthErrorMessage.endpointResolutionError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+
+  });
+
+  it("createMultipleMatchingTokensInCacheError creates a ClientAuthError object", () => {
+
+    const scope: string = "user.read";
+    const errorDetail: string = "Cache error for scope";
+    const multipleMatchingTokensError = ClientAuthError.createMultipleMatchingTokensInCacheError(scope);
+    let err: ClientAuthError;
+
+    try {
+      throw multipleMatchingTokensError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.multipleMatchingTokens.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.multipleMatchingTokens.desc);
+    expect(err.errorMessage).to.contain(`${errorDetail} ${scope}`);
+    expect(err.message).to.contain(ClientAuthErrorMessage.multipleMatchingTokens.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createMultipleAuthoritiesInCacheError creates a ClientAuthError object", () => {
+
+    const scope: string = "user.read";
+    const errorDetail: string = "Cache error for scope";
+    const multipleAuthoritiesError = ClientAuthError.createMultipleAuthoritiesInCacheError(scope);
+    let err: ClientAuthError;
+
+    try {
+      throw multipleAuthoritiesError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.multipleMatchingAuthorities.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.multipleMatchingAuthorities.desc);
+    expect(err.errorMessage).to.contain(`${errorDetail} ${scope}`);
+    expect(err.message).to.contain(ClientAuthErrorMessage.multipleMatchingAuthorities.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createPopupWindowError creates a ClientAuthError object", () => {
+
+    const ERROR_DETAIL = "Details:";
+    const popupWindowError = ClientAuthError.createPopupWindowError(ERROR_DETAIL);
+    let err: ClientAuthError;
+
+    try {
+      throw popupWindowError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.popUpWindowError.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.popUpWindowError.desc);
+    expect(err.errorMessage).to.contain(ERROR_DETAIL);
+    expect(err.message).to.contain(ClientAuthErrorMessage.popUpWindowError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createTokenRenewalTimeoutError creates a ClientAuthError object", () => {
+
+    const tokenRenewalTimeOutError = ClientAuthError.createTokenRenewalTimeoutError();
+    let err: ClientAuthError;
+
+    try {
+      throw tokenRenewalTimeOutError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.tokenRenewalError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.tokenRenewalError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.tokenRenewalError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  // TODO: Cklarify if we want to throw decoded idToken back to the client?
+  it("createInvalidIdTokenError creates a ClientAuthError object", () => {
+
+    // Chose not to spy/stub as the idToken functionality needed in error class tests
+    // is more depedent on already pried open idToken in IDToken Class
+    const idToken: IdToken = null;
+
+    const invalidTokenError = ClientAuthError.createInvalidIdTokenError(idToken);
+    let err: ClientAuthError;
+
+    try {
+      throw invalidTokenError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.invalidIdToken.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.invalidIdToken.desc);
+    expect(err.message).to.contain(ClientAuthErrorMessage.invalidIdToken.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createInvalidStateError creates a ClientAuthError object", () => {
+
+    const state = "12345";
+    const invalidState = "67890";
+    const invalidStateError = ClientAuthError.createInvalidStateError(invalidState, state);
+    let err: ClientAuthError;
+
+    try {
+      throw invalidStateError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.invalidStateError.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.invalidStateError.desc);
+    expect(err.errorMessage).to.contain(`${invalidState}, state expected : ${state}.`);
+    expect(err.message).to.contain(ClientAuthErrorMessage.invalidStateError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createNonceMismatchError creates a ClientAuthError object", () => {
+
+    const nonce = "12345";
+    const invalidNonce = "67890";
+    const invalidNonceError = ClientAuthError.createNonceMismatchError(invalidNonce, nonce);
+    let err: ClientAuthError;
+
+    try {
+      throw invalidNonceError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.nonceMismatchError.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.nonceMismatchError.desc);
+    expect(err.errorMessage).to.contain(`${invalidNonce}, nonce expected : ${nonce}.`);
+    expect(err.message).to.contain(ClientAuthErrorMessage.nonceMismatchError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createLoginInProgressError creates a ClientAuthError object", () => {
+
+    const loginInProgressError = ClientAuthError.createLoginInProgressError();
+    let err: ClientAuthError;
+
+    try {
+      throw loginInProgressError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.loginProgressError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.loginProgressError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.loginProgressError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createAcquireTokenInProgressError creates a ClientAuthError object", () => {
+
+    const acqTokenInProgressError = ClientAuthError.createAcquireTokenInProgressError();
+    let err: ClientAuthError;
+
+    try {
+      throw acqTokenInProgressError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.acquireTokenProgressError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.acquireTokenProgressError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.acquireTokenProgressError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createUserCancelledError creates a ClientAuthError object", () => {
+
+    const userCancelledError = ClientAuthError.createUserCancelledError();
+    let err: ClientAuthError;
+
+    try {
+      throw userCancelledError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.userCancelledError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.userCancelledError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.userCancelledError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createErrorInCallbackFunction creates a ClientAuthError object", () => {
+
+    const ERROR_DESC = "There is a callback error";
+    const errorInCallback = ClientAuthError.createErrorInCallbackFunction(ERROR_DESC);
+    let err: ClientAuthError;
+
+    try {
+      throw errorInCallback;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.callbackError.code);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.callbackError.desc);
+    expect(err.errorMessage).to.contain(ClientAuthErrorMessage.callbackError.desc);
+    expect(err.message).to.contain(`${ERROR_DESC}`);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createUserLoginRequiredError creates a ClientAuthError object", () => {
+
+    const userLoginRequiredError = ClientAuthError.createUserLoginRequiredError();
+    let err: ClientAuthError;
+
+    try {
+      throw userLoginRequiredError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.userLoginRequiredError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.userLoginRequiredError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.userLoginRequiredError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+
+  it("createUserDoesNotExistError creates a ClientAuthError object", () => {
+
+    const userDoesNotExistError = ClientAuthError.createUserDoesNotExistError();
+    let err: ClientAuthError;
+
+    try {
+      throw userDoesNotExistError;
+    } catch (error) {
+      err = error;
+    }
+
+    expect(err.errorCode).to.equal(ClientAuthErrorMessage.userDoesNotExistError.code);
+    expect(err.errorMessage).to.equal(ClientAuthErrorMessage.userDoesNotExistError.desc);
+    expect(err.message).to.equal(ClientAuthErrorMessage.userDoesNotExistError.desc);
+    expect(err.name).to.equal("ClientAuthError");
+    expect(err.stack).to.include("ClientAuthError.spec.js");
+  });
+});
+
+
