9eef85515b60716351fd1af8940a5a95a55e7dda
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-04-22 19:49:26 -0700

    Update AccessTokenKey.ts

--- lib/msal-core/src/AccessTokenKey.ts
+++ lib/msal-core/src/AccessTokenKey.ts
@@ -14,7 +14,6 @@ export class AccessTokenKey {
   homeAccountIdentifier: string;
 
   constructor(authority: string, clientId: string, scopes: string, uid: string, utid: string) {
-    // TODO: Canonicalize authority URI
     this.authority = Utils.CanonicalizeUri(authority);
     this.clientId = clientId;
     this.scopes = scopes;
