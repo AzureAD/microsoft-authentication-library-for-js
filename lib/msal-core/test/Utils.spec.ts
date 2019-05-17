e2e40cdad22bebdac5c50e0808973f15f5e10e57
sameerag <sameera.gajjarapu@microsoft.com>
2019-05-17 15:09:33 -0500

    Adding tests

--- lib/msal-core/test/Utils.spec.ts
+++ lib/msal-core/test/Utils.spec.ts
@@ -1,5 +1,6 @@
 import { expect } from "chai";
 import { Utils } from "../src/Utils";
+import { IdToken } from "../src/IdToken";
 
 describe("Utils.ts class", () => {
     it("get getLibraryVersion()", () => {
@@ -13,5 +14,23 @@ describe("Utils.ts class", () => {
         console.log(Utils.replaceTenantPath("http://a.com/common/d?e=f", "1234-5678"));
         console.log(Utils.replaceTenantPath("http://a.com/common/", "1234-56778"));
         console.log(Utils.replaceTenantPath("http://a.com/common", "1234-5678"));
+    });
+
+    it("test Base64 encode decode", () => {
+        // english
+        expect(Base64.encode("msaljs")).to.be.equal("bXNhbGpz");
+        expect(Base64.decode("bXNhbGpz")).to.be.equal("msaljs");
+
+        // Icelandic
+        expect(Base64.encode("Björn Ironside")).to.be.equal("QmrDtnJuIElyb25zaWRl");
+        expect(Base64.decode("QmrDtnJuIElyb25zaWRl")).to.be.equal("Björn Ironside");
+
+        // hebrew
+        expect(Base64.encode("בְּצַלְאֵל")).to.be.equal("15HWvNaw16bWt9ec1rDXkNa115w=");
+        expect(Base64.decode("15HWvNaw16bWt9ec1rDXkNa115w=")).to.be.equal("בְּצַלְאֵל");
+
+         // spanish
+         expect(Base64.encode("Avrán")).to.be.equal("QXZyw6Fu");
+         expect(Base64.decode("QXZyw6Fu")).to.be.equal("Avrán");
     });
 });
