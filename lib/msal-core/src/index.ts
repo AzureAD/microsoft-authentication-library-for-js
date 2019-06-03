817632bc675cdce9abe1275e7289af90b71eeda8
sameerag <sameera.gajjarapu@microsoft.com>
2019-06-03 09:36:37 -0700

    publish idToken to the developers

--- lib/msal-core/src/index.ts
+++ lib/msal-core/src/index.ts
@@ -8,6 +8,7 @@ export { CacheResult } from "./UserAgentApplication";
 export { CacheLocation, Configuration } from "./Configuration";
 export { AuthenticationParameters } from "./AuthenticationParameters";
 export { AuthResponse } from "./AuthResponse";
+export { IdToken } from "./IdToken";
 
 // Errors
 export { AuthError } from "./error/AuthError";
