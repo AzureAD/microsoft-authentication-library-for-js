c418cb64b51cb9ad648f26aeed7b12d47d8e8da1
Hector Morales <hectormgdev@gmail.com>
2019-07-01 16:25:26 -0700

    Add sample app main files

---
+++ lib/msal-electron-proof-of-concept/samples/msal-electron-sample-app/src/Main.ts
@@ -0,0 +1,37 @@
+import { app, BrowserWindow } from "electron";
+import * as path from "path";
+
+export default class Main {
+    static mainWindow: Electron.BrowserWindow;
+    static application: Electron.App;
+
+    static main(): void {
+        Main.application = app;
+        Main.application.on("window-all-closed", Main.onWindowAllClosed);
+        Main.application.on("ready", Main.onReady);
+    }
+
+    private static onWindowAllClosed(): void {
+        Main.application.quit();
+    }
+
+    private static onClose(): void {
+        Main.mainWindow = null;
+    }
+
+    private static onReady(): void {
+        Main.createMainWindow();
+        Main.mainWindow.loadFile(path.join(__dirname, "../index.html"));
+        Main.mainWindow.on("closed", Main.onClose);
+    }
+
+    private static createMainWindow(): void {
+        this.mainWindow = new BrowserWindow({
+            width: 1000,
+            height: 800,
+            webPreferences: {
+                nodeIntegration: true,
+            },
+        });
+    }
+}
