9a7b1daabca073f8445ca826a6fc66292146feae
tbudukh <Tanmay.Budukh@cbre.com>
2019-07-09 09:59:43 -0400

    changes to remove dependency on router while booting up msal service

--- lib/msal-angular/src/index.ts
+++ lib/msal-angular/src/index.ts
@@ -5,3 +5,4 @@ export { MsalService } from './msal.service';
 export { MsalGuard } from './msal-guard.service';
 export { MsalInterceptor } from './msal.interceptor';
 export { MsalModule } from './msal.module';
+export { MsalRouter } from './msal.router';
