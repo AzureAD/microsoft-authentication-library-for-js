620498b4c54d9196eec1aef700b1e670358b9006
Hector Morales <hectormgdev@gmail.com>
2019-07-11 13:32:22 -0700

    Fix redaction errors in documentation comments

--- lib/msal-electron-proof-of-concept/src/appConfig/applicationConfiguration.ts
+++ lib/msal-electron-proof-of-concept/src/appConfig/applicationConfiguration.ts
@@ -3,9 +3,9 @@
 
 /**
  * @type AuthOptions: Used to configure the auth options in the ApplicationConfiguration object.
- * - clientId       - Client ID of your app registered with our Application registration portal : https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredAppsPreview in Microsoft Identity Platform
+ * - clientId       - Client ID of your app registered in the application registration portal : https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredAppsPreview in Microsoft Identity Platform
  * - authority      - You can configure a specific authority, defaults to " " or "https://login.microsoftonline.com/common"
- * - redirectUri    - The redirect URI of the application, this should be same as the value in the application registration portal. Defaults to `msal://auth`.
+ * - redirectUri    - The redirect URI of the application, this should be the same as the value in the application registration portal. Defaults to `msal://auth`.
  */
 export type AuthOptions = {
     clientId: string;
