4610b84e39ba996d829434b58bc5edadf452ddc0
Hector Morales <hectormgdev@gmail.com>
2019-07-11 13:51:27 -0700

    Remove capitalization from non-proper nouns in Application Config documenting comments

--- lib/msal-electron-proof-of-concept/src/appConfig/applicationConfiguration.ts
+++ lib/msal-electron-proof-of-concept/src/appConfig/applicationConfiguration.ts
@@ -17,7 +17,7 @@ export type AuthOptions = {
  * Use the ApplicationConfiguration object to configure MSAL and initialize your PublicClientApplication.
  *
  * This object allows you to configure your application to use important elements of MSAL functionality.
- * - auth: this object holds elements that deal with authentication (clientID, Microsoft Identity Platform authority, etc.)
+ * - auth: this object holds elements that deal with authentication (clientID, Microsoft identity platform authority, etc.)
  */
 
 export type ApplicationConfiguration = {
