331f228aacd2228be58f2d7aa75e5fcfa7c9ed64
Hector Morales <hectormgdev@gmail.com>
2019-07-15 10:14:49 -0700

    Update file export statements to match new filename casing

--- lib/msal-electron-proof-of-concept/src/index.ts
+++ lib/msal-electron-proof-of-concept/src/index.ts
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License
 
-export { PublicClientApplication } from './AppConfig/publicClientApplication';
-export { ApplicationConfiguration } from './AppConfig/applicationConfiguration';
+export { PublicClientApplication } from './AppConfig/PublicClientApplication';
+export { ApplicationConfiguration } from './AppConfig/ApplicationConfiguration';
