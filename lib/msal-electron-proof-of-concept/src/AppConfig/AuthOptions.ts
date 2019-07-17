372b4f2f53c725d8e90f3262fdce866888ddb749
Hector Morales <hectormgdev@gmail.com>
2019-07-17 16:35:41 -0700

    Add tenant/tenantId as a configurable options for the Authority class in msal-electron

--- lib/msal-electron-proof-of-concept/src/AppConfig/AuthOptions.ts
+++ lib/msal-electron-proof-of-concept/src/AppConfig/AuthOptions.ts
@@ -6,9 +6,11 @@
  * - clientId       - Client ID of your app registered in the application registration portal : https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredAppsPreview in Microsoft Identity Platform
  * - authority      - You can configure a specific authority, defaults to " " or "https://login.microsoftonline.com/common"
  * - redirectUri    - The redirect URI of the application, this should be the same as the value in the application registration portal. Defaults to `msal://auth`.
+ * - tenantId       - The Tenant ID of the application registered in the application registration portal.
  */
 export type AuthOptions = {
     clientId: string;
     authority?: string;
     redirectUri?: string;
+    tenantId: string;
 };
