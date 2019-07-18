364f2f3c55e8455b9007f38b9923f600c290a4a0
Hector Mg <hectormgwake@gmail.com>
2019-07-18 16:07:51 -0700

    Update default redirect URI

--- lib/msal-electron-proof-of-concept/src/AppConfig/DefaultConstants.ts
+++ lib/msal-electron-proof-of-concept/src/AppConfig/DefaultConstants.ts
@@ -2,7 +2,7 @@
 // Licensed under the MIT License//
 
 // Default Redirect URI corresponds to a custom file protocol
-export const DEFAULT_REDIRECT_URI = 'msalElectron://auth/';
+export const DEFAULT_REDIRECT_URI = 'msal://auth/';
 
 // Default AAD Authority
 export const DEFAULT_AUTHORITY = 'https://login.microsoftonline.com/common/';
