ab65cfe2daca21dde6ed44905ee16447b4c2363a
Hector Morales <hectormgdev@gmail.com>
2019-07-25 10:30:31 -0700

    Remove unnecessary trailing comma in request-promise params

--- lib/msal-electron-proof-of-concept/src/AppConfig/ClientApplication/PublicClientApplication.ts
+++ lib/msal-electron-proof-of-concept/src/AppConfig/ClientApplication/PublicClientApplication.ts
@@ -107,7 +107,7 @@ export class PublicClientApplication extends ClientApplication {
     private tradeAuthCodeForAccessToken(authorityInstance: Authority, scopes: string[], authCode: string): Promise<string> {
         // Build token request URL
         const tokenRequest = this.buildTokenRequest(authorityInstance, scopes, authCode);
-        return rp(tokenRequest.body, ).then((body) => {
+        return rp(tokenRequest.body).then((body) => {
             const tokenResponse = new TokenResponse(body);
             return tokenResponse.accessToken;
         }).catch((responseError) => {
