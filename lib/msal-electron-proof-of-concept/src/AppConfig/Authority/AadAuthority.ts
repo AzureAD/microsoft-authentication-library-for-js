612a96e8ba68ef04e3591ceda6fba3334f0d9dfb
Hector Morales <hectormgdev@gmail.com>
2019-07-29 13:47:13 -0700

    Update sample style for msal-electron

--- lib/msal-electron-proof-of-concept/src/AppConfig/Authority/AadAuthority.ts
+++ lib/msal-electron-proof-of-concept/src/AppConfig/Authority/AadAuthority.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+import { DEFAULT_AUTH_ENDPOINT_PATH, DEFAULT_TOKEN_ENDPOINT_PATH } from '../DefaultConstants';
 import { Authority } from './Authority';
 
 export class AadAuthority extends Authority {
@@ -9,10 +10,10 @@ export class AadAuthority extends Authority {
     }
 
     public get authorizationEndpoint(): string {
-       return `${this.canonicalAuthority}oauth2/v2.0/authorize?`;
+       return `${this.canonicalAuthority}${DEFAULT_AUTH_ENDPOINT_PATH}`;
     }
 
     public get tokenEndpoint(): string {
-        return `${this.canonicalAuthority}oauth2/v2.0/token?`;
+        return `${this.canonicalAuthority}${DEFAULT_TOKEN_ENDPOINT_PATH}`;
     }
 }
