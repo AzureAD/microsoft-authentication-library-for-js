f4721a573e5a4d0080dec0fbde75f11a5c585017
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-08-29 15:35:21 -0700

    adding docs, testing claims

--- lib/msal-core/src/AuthenticationParameters.ts
+++ lib/msal-core/src/AuthenticationParameters.ts
@@ -28,6 +28,8 @@ export function validateClaimsRequest(request: AuthenticationParameters) {
         return;
     }
     let claims;
+    console.log("Claims Req: ");
+    console.log(request.claimsRequest);
     try {
         claims = JSON.parse(request.claimsRequest);
     } catch (e) {
