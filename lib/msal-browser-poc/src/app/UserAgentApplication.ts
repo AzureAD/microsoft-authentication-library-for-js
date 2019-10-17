60b4bb97691f81c67f6cfe692b8d63e4f4f22e31
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-10-16 18:41:47 -0700

    Initial code for browser package

---
+++ lib/msal-browser-poc/src/app/UserAgentApplication.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Configuration } from "./Configuration";
+
+/**
+ * UserAgentApplication class
+ *
+ * Object Instance that the developer can use to make loginXX OR acquireTokenXX functions
+ */
+export class UserAgentApplication {
+
+    private config: Configuration;
+    
+    /**
+     * @constructor
+     * Constructor for the UserAgentApplication used to instantiate the UserAgentApplication object
+     *
+     * Important attributes in the Configuration object for auth are:
+     * - clientID: the application ID of your application.
+     * You can obtain one by registering your application with our Application registration portal : https://portal.azure.com/#blade/Microsoft_AAD_IAM/ActiveDirectoryMenuBlade/RegisteredAppsPreview
+     * - authority: the authority URL for your application.
+     *
+     * In Azure AD, authority is a URL indicating the Azure active directory that MSAL uses to obtain tokens.
+     * It is of the form https://login.microsoftonline.com/&lt;Enter_the_Tenant_Info_Here&gt;.
+     * If your application supports Accounts in one organizational directory, replace "Enter_the_Tenant_Info_Here" value with the Tenant Id or Tenant name (for example, contoso.microsoft.com).
+     * If your application supports Accounts in any organizational directory, replace "Enter_the_Tenant_Info_Here" value with organizations.
+     * If your application supports Accounts in any organizational directory and personal Microsoft accounts, replace "Enter_the_Tenant_Info_Here" value with common.
+     * To restrict support to Personal Microsoft accounts only, replace "Enter_the_Tenant_Info_Here" value with consumers.
+     *
+     *
+     * In Azure B2C, authority is of the form https://&lt;instance&gt;/tfp/&lt;tenant&gt;/&lt;policyName&gt;/
+     *
+     * @param {@link (Configuration:type)} configuration object for the MSAL UserAgentApplication instance
+     */
+    constructor(configuration: Configuration) {
+        this.config = configuration;
+    }
+    
+}
