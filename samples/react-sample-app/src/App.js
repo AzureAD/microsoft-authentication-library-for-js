c09fe5328df4fe009fc204e23d943ea5abfec2c7
James.Innes <James.Innes@asos.com>
2019-10-20 20:06:19 +0100

    refactor into func with hooks

--- samples/react-sample-app/src/App.js
+++ samples/react-sample-app/src/App.js
@@ -6,64 +6,58 @@ import "./App.css";
 
 const Json = ({ data }) => <pre>{JSON.stringify(data, null, 4)}</pre>;
 
-class App extends React.Component {
-    static propTypes = {
-        account: PropTypes.object,
-        emailMessages: PropTypes.object,
-        error: PropTypes.string,
-        graphProfile: PropTypes.object,
-        onSignIn: PropTypes.func.isRequired,
-        onSignOut: PropTypes.func.isRequired,
-        onRequestEmailToken: PropTypes.func.isRequired
-    };
+const App = props => {
+  return (
+    <div>
+      <section>
+        <h1>
+          Welcome to the Microsoft Authentication Library For Javascript - React
+          Quickstart
+        </h1>
+        {!props.account ? (
+          <button onClick={props.onSignIn}>Sign In</button>
+        ) : (
+          <>
+            <button onClick={props.onSignOut}>Sign Out</button>
+            <button onClick={props.onRequestEmailToken}>
+              * Request Email Permission
+            </button>
+          </>
+        )}
+        {props.error && <p className="error">Error: {props.error}</p>}
+      </section>
+      <section className="data">
+        {props.account && (
+          <div className="data-account">
+            <h2>Session Account Data</h2>
+            <Json data={props.account} />
+          </div>
+        )}
+        {props.graphProfile && (
+          <div className="data-graph">
+            <h2>Graph Profile Data</h2>
+            <Json data={props.graphProfile} />
+          </div>
+        )}
+        {props.emailMessages && (
+          <div className="data-graph">
+            <h2>Messages Data</h2>
+            <Json data={props.emailMessages} />
+          </div>
+        )}
+      </section>
+    </div>
+  );
+};
 
-    render() {
-        return (
-            <div>
-                <section>
-                    <h1>
-                        Welcome to the Microsoft Authentication Library For
-                        Javascript - React Quickstart
-                    </h1>
-                    {!this.props.account ? (
-                        <button onClick={this.props.onSignIn}>Sign In</button>
-                    ) : (
-                        <>
-                            <button onClick={this.props.onSignOut}>
-                                Sign Out
-                            </button>
-                            <button onClick={this.props.onRequestEmailToken}>
-                                Request Email Permission
-                            </button>
-                        </>
-                    )}
-                    {this.props.error && (
-                        <p className="error">Error: {this.props.error}</p>
-                    )}
-                </section>
-                <section className="data">
-                    {this.props.account && (
-                        <div className="data-account">
-                            <h2>Session Account Data</h2>
-                            <Json data={this.props.account} />
-                        </div>
-                    )}
-                    {this.props.graphProfile && (
-                        <div className="data-graph">
-                            <h2>Graph Profile Data</h2>
-                            <Json data={this.props.graphProfile} />
-                        </div>
-                    )}
-                    {this.props.emailMessages && (
-                        <div className="data-graph">
-                            <h2>Messages Data</h2>
-                            <Json data={this.props.emailMessages} />
-                        </div>
-                    )}
-                </section>
-            </div>
-        );
-    }
-}
+App.propTypes = {
+  account: PropTypes.object,
+  emailMessages: PropTypes.object,
+  error: PropTypes.string,
+  graphProfile: PropTypes.object,
+  onSignIn: PropTypes.func.isRequired,
+  onSignOut: PropTypes.func.isRequired,
+  onRequestEmailToken: PropTypes.func.isRequired
+};
 
 export default AuthProvider(App);
