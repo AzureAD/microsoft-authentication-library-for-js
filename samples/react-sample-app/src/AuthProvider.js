54b3be95631b435452938ad51d3d464bc08f3d08
James.Innes <James.Innes@asos.com>
2019-10-20 20:06:42 +0100

    refactor into func hoc with hooks

--- samples/react-sample-app/src/AuthProvider.js
+++ samples/react-sample-app/src/AuthProvider.js
@@ -1,188 +1,164 @@
-import React, { Component } from "react";
+import React, { useEffect, useState } from "react";
 import {
-    msalApp,
-    requiresInteraction,
-    fetchMsGraph,
-    isIE,
-    GRAPH_ENDPOINTS,
-    GRAPH_SCOPES,
-    GRAPH_REQUESTS
+  msalApp,
+  requiresInteraction,
+  fetchMsGraph,
+  isIE,
+  GRAPH_ENDPOINTS,
+  GRAPH_SCOPES,
+  GRAPH_REQUESTS
 } from "./auth-utils";
 
 // If you support IE, our recommendation is that you sign-in using Redirect APIs
 const useRedirectFlow = isIE();
 // const useRedirectFlow = true;
 
-export default C =>
-    class AuthProvider extends Component {
-        constructor(props) {
-            super(props);
+const AuthProvider = Component => {
+  const WrappedComponent = props => {
+    const [account, setAccount] = useState(null);
+    const [error, setError] = useState(null);
+    const [emailMessages, setEmailMessages] = useState(null);
+    const [graphProfile, setGraphProfile] = useState(null);
 
-            this.state = {
-                account: null,
-                error: null,
-                emailMessages: null,
-                graphProfile: null
-            };
+    const acquireToken = async (request, redirect) => {
+      return msalApp.acquireTokenSilent(request).catch(error => {
+        // Call acquireTokenPopup (popup window) in case of acquireTokenSilent failure
+        // due to consent or interaction required ONLY
+        if (requiresInteraction(error.errorCode)) {
+          return redirect
+            ? msalApp.acquireTokenRedirect(request)
+            : msalApp.acquireTokenPopup(request);
         }
-
-        async acquireToken(request, redirect) {
-            return msalApp.acquireTokenSilent(request).catch(error => {
-                // Call acquireTokenPopup (popup window) in case of acquireTokenSilent failure
-                // due to consent or interaction required ONLY
-                if (requiresInteraction(error.errorCode)) {
-                    return redirect
-                        ? msalApp.acquireTokenRedirect(request)
-                        : msalApp.acquireTokenPopup(request);
-                }
-            });
-        }
-
-        async onSignIn(redirect) {
-            if (redirect) {
-                return msalApp.loginRedirect(GRAPH_REQUESTS.LOGIN);
-            }
-
-            const loginResponse = await msalApp
-                .loginPopup(GRAPH_REQUESTS.LOGIN)
-                .catch(error => {
-                    this.setState({
-                        error: error.message
-                    });
-                });
-
-            if (loginResponse) {
-                this.setState({
-                    account: loginResponse.account,
-                    error: null
-                });
-
-                const tokenResponse = await this.acquireToken(
-                    GRAPH_REQUESTS.LOGIN
-                ).catch(error => {
-                    this.setState({
-                        error: error.message
-                    });
-                });
-
-                if (tokenResponse) {
-                    const graphProfile = await fetchMsGraph(
-                        GRAPH_ENDPOINTS.ME,
-                        tokenResponse.accessToken
-                    ).catch(() => {
-                        this.setState({
-                            error: "Unable to fetch Graph profile."
-                        });
-                    });
-
-                    if (graphProfile) {
-                        this.setState({
-                            graphProfile
-                        });
-                    }
-
-                    if (tokenResponse.scopes.indexOf(GRAPH_SCOPES.MAIL_READ) > 0) {
-                        return this.readMail(tokenResponse.accessToken);
-                    }
-                }
-            }
-        }
-
-        onSignOut() {
-            msalApp.logout();
-        }
-
-        async onRequestEmailToken() {
-            const tokenResponse = await this.acquireToken(
-                GRAPH_REQUESTS.EMAIL,
-                useRedirectFlow
-            ).catch(e => {
-                this.setState({
-                    error: "Unable to acquire access token for reading email."
-                });
-            });
-
-            if (tokenResponse) {
-                return this.readMail(tokenResponse.accessToken);
-            }
-        }
-
-        async readMail(accessToken) {
-            const emailMessages = await fetchMsGraph(
-                GRAPH_ENDPOINTS.MAIL,
-                accessToken
-            ).catch(() => {
-                this.setState({
-                    error: "Unable to fetch email messages."
-                });
-            });
-
-            if (emailMessages) {
-                this.setState({
-                    emailMessages,
-                    error: null
-                });
-            }
-        }
-
-        async componentDidMount() {
-            msalApp.handleRedirectCallback(error => {
-                if (error) {
-                    const errorMessage = error.errorMessage ? error.errorMessage : "Unable to acquire access token.";
-                    // setState works as long as navigateToLoginRequestUrl: false
-                    this.setState({
-                        error: errorMessage
-                    });
-                }
-            });
-
-            const account = msalApp.getAccount();
-
-            this.setState({
-                account
-            });
-
-            if (account) {
-                const tokenResponse = await this.acquireToken(
-                    GRAPH_REQUESTS.LOGIN,
-                    useRedirectFlow
-                );
-
-                if (tokenResponse) {
-                    const graphProfile = await fetchMsGraph(
-                        GRAPH_ENDPOINTS.ME,
-                        tokenResponse.accessToken
-                    ).catch(() => {
-                        this.setState({
-                            error: "Unable to fetch Graph profile."
-                        });
-                    });
-
-                    if (graphProfile) {
-                        this.setState({
-                            graphProfile
-                        });
-                    }
-
-                    if (tokenResponse.scopes.indexOf(GRAPH_SCOPES.MAIL_READ) > 0) {
-                        return this.readMail(tokenResponse.accessToken);
-                    }
-                }
-            }
-        }
-
-        render() {
-            return (
-                <C
-                    {...this.props}
-                    account={this.state.account}
-                    emailMessages={this.state.emailMessages}
-                    error={this.state.error}
-                    graphProfile={this.state.graphProfile}
-                    onSignIn={() => this.onSignIn(useRedirectFlow)}
-                    onSignOut={() => this.onSignOut()}
-                    onRequestEmailToken={() => this.onRequestEmailToken()}
-                />
-            );
-        }
+      });
     };
+
+    const onSignIn = async redirect => {
+      if (redirect) {
+        return msalApp.loginRedirect(GRAPH_REQUESTS.LOGIN);
+      }
+
+      const loginResponse = await msalApp
+        .loginPopup(GRAPH_REQUESTS.LOGIN)
+        .catch(error => {
+          setError(error.message);
+        });
+
+      if (loginResponse) {
+        setAccount(loginResponse.account);
+        setError(null);
+
+        const tokenResponse = await acquireToken(GRAPH_REQUESTS.LOGIN).catch(
+          error => {
+            setError(error.message);
+          }
+        );
+
+        if (tokenResponse) {
+          const graphProfile = await fetchMsGraph(
+            GRAPH_ENDPOINTS.ME,
+            tokenResponse.accessToken
+          ).catch(() => {
+            setError("Unable to fetch Graph profile.");
+          });
+
+          if (graphProfile) {
+            setGraphProfile(graphProfile);
+          }
+
+          if (tokenResponse.scopes.indexOf(GRAPH_SCOPES.MAIL_READ) > 0) {
+            return readMail(tokenResponse.accessToken);
+          }
+        }
+      }
+    };
+
+    const onSignOut = () => {
+      msalApp.logout();
+    };
+
+    const onRequestEmailToken = async () => {
+      const tokenResponse = await acquireToken(
+        GRAPH_REQUESTS.EMAIL,
+        useRedirectFlow
+      ).catch(e => {
+        setError("Unable to acquire access token for reading email.");
+      });
+
+      if (tokenResponse) {
+        return readMail(tokenResponse.accessToken);
+      }
+    };
+
+    const readMail = async accessToken => {
+      const emailMessages = await fetchMsGraph(
+        GRAPH_ENDPOINTS.MAIL,
+        accessToken
+      ).catch(() => {
+        setError("Unable to fetch email messages.");
+      });
+
+      if (emailMessages) {
+        setEmailMessages(emailMessages);
+        setError(null);
+      }
+    };
+
+    useEffect(() => {
+      async function performAsync() {
+        msalApp.handleRedirectCallback(error => {
+          if (error) {
+            const errorMessage = error.errorMessage
+              ? error.errorMessage
+              : "Unable to acquire access token.";
+            // setState works as long as navigateToLoginRequestUrl: false
+            setError(errorMessage);
+          }
+        });
+
+        setAccount(msalApp.getAccount(), [account]);
+
+        if (account) {
+          const tokenResponse = await acquireToken(
+            GRAPH_REQUESTS.LOGIN,
+            useRedirectFlow
+          );
+
+          if (tokenResponse) {
+            const graphProfile = await fetchMsGraph(
+              GRAPH_ENDPOINTS.ME,
+              tokenResponse.accessToken
+            ).catch(() => {
+              setError("Unable to fetch Graph profile.");
+            });
+
+            if (graphProfile) {
+              setGraphProfile(graphProfile);
+            }
+
+            if (tokenResponse.scopes.indexOf(GRAPH_SCOPES.MAIL_READ) > 0) {
+              return readMail(tokenResponse.accessToken);
+            }
+          }
+        }
+      }
+
+      performAsync();
+    }, [account]);
+
+    return (
+      <Component
+        {...props}
+        account={account}
+        emailMessages={emailMessages}
+        error={error}
+        graphProfile={graphProfile}
+        onSignIn={() => onSignIn(useRedirectFlow)}
+        onSignOut={() => onSignOut()}
+        onRequestEmailToken={() => onRequestEmailToken()}
+      />
+    );
+  };
+  return WrappedComponent;
+};
+
+export default AuthProvider;
