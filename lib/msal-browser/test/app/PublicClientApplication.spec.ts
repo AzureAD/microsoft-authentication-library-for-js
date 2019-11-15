d6471324a7eada4f8396b56c725464c1a6986ef0
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-11-15 13:57:23 -0800

    Added test and code coverage frameworks

---
+++ lib/msal-browser/test/app/PublicClientApplication.spec.ts
@@ -0,0 +1,13 @@
+import * as Mocha from "mocha";
+import sinon from "sinon";
+import { expect } from "chai";
+
+describe("PublicClientApplication.ts Class Unit Tests", () => {
+
+    describe("Constructor tests", () => {
+
+        it("passes null check", () => {
+            expect(null).to.be.null;
+        });
+    });
+});
