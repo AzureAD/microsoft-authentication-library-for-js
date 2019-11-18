10eb628c101ce85ede0bc6f3397fb7ae44fff47d
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-11-18 13:58:34 -0800

    added coverage and tests for msal-browser package

--- test/mochaSetup.js
+++ test/mochaSetup.js
@@ -1,1 +1,5 @@
 require("@babel/register")({ extensions: ['.js', '.jsx', '.ts', '.tsx'] });
+require("jsdom-global")("", {
+    url: "https://localhost:8081/index.html"
+});
+require("msal-common");
---
+++ test/utils/BrowserUtils.spec.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { BrowserUtils } from "../../src/utils/BrowserUtils"
+import { TEST_URIS } from "./StringConstants";
+
+describe("BrowserUtils.ts Function Unit Tests", () => {
+    
+    it("getDefaultRedirectUri returns current location uri of browser", () => {
+        expect(BrowserUtils.getDefaultRedirectUri()).to.be.eq(TEST_URIS.TEST_REDIR_URI);
+    });
+});
