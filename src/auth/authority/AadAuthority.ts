75cb352cbdd2ed180b05414042cc26a42005d689
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-11-23 22:07:12 -0800

    Adding authority and protocol classes

---
+++ src/auth/authority/AadAuthority.ts
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { Authority, AuthorityType } from "./Authority";
+import { AADTrustedHostList } from "../../utils/Constants";
+import { INetworkModule } from "../../network/INetworkModule";
+
+/**
+ * @hidden
+ */
+export class AadAuthority extends Authority {
+    public get authorityType(): AuthorityType {
+        return AuthorityType.Aad;
+    }
+
+    public get isValidationEnabled(): boolean {
+        // Hardcoded to true for now - will change depending on requirements
+        return true;
+    }
+
+    private static readonly aadInstanceDiscoveryEndpoint: string = "https://login.microsoftonline.com/common/discovery/instance";
+
+    private get aadInstanceDiscoveryEndpointUrl(): string {
+        return `${AadAuthority.aadInstanceDiscoveryEndpoint}?api-version=1.0&authorization_endpoint=${this.canonicalAuthority}oauth2/v2.0/authorize`;
+    }
+
+    public constructor(authority: string, networkInterface: INetworkModule) {
+        super(authority, networkInterface);
+    }
+
+    /**
+     * Returns a promise which resolves to the OIDC endpoint
+     * Only responds with the endpoint
+     */
+
+    public async getOpenIdConfigurationAsync(): Promise<string> {
+        if (!this.isValidationEnabled || this.isInTrustedHostList(this.canonicalAuthorityUrlComponents.HostNameAndPort)) {
+            return this.defaultOpenIdConfigurationEndpoint;
+        }
+
+        // for custom domains in AAD where we query the service for the Instance discovery
+        const response = await this.networkInterface.sendRequestAsync(this.aadInstanceDiscoveryEndpointUrl, { method: "GET" }, true);
+        return response.tenant_discovery_endpoint;
+    }
+
+    /**
+     * Checks to see if the host is in a list of trusted hosts
+     * @param {string} The host to look up
+     */
+    public isInTrustedHostList(host: string): boolean {
+        return AADTrustedHostList[host.toLowerCase()];
+    }
+}
---
+++ src/url/UrlString.ts
@@ -0,0 +1,178 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { AADAuthorityConstants, AADServerHashParamKeys } from "../utils/Constants";
+import { CryptoUtils } from "../utils/CryptoUtils";
+import { StringUtils } from "../utils/StringUtils";
+import { IUri } from "./IUri";
+import { ClientAuthError } from "../error/ClientAuthError";
+
+/**
+ * Url object class which can perform various transformations on url strings.
+ */
+export class UrlString {
+
+    private urlString: string;
+    
+    constructor(url: string) {
+        this.urlString = url;
+        if(StringUtils.isEmpty(this.getHash())) {
+            this.urlString = this.canonicalizeUri(url);
+        }
+    }
+
+    /**
+     * Ensure urls are lower case and end with a / character.
+     * @param url 
+     */
+    private canonicalizeUri(url: string): string {
+        if (url) {
+            url = url.toLowerCase();
+        }
+
+        if (url && !url.endsWith("/")) {
+            url += "/";
+        }
+
+        return url;
+    }
+
+    /**
+     * Throws if urlString passed is not a valid authority URI string.
+     */
+    validateAsUri(): void {
+        let components;
+        try {
+            components = this.getUrlComponents();
+        } catch (e) {
+            throw ClientAuthError.createUrlSegmentError(this.urlString);
+        }
+
+        if(!components.Protocol || components.Protocol.toLowerCase() !== "https:") {
+            throw ClientAuthError.createInsecureAuthorityUriError(this.urlString);
+        }
+
+        if (!components.PathSegments || components.PathSegments.length < 1) {
+            throw ClientAuthError.createInsecureAuthorityUriError(this.urlString);
+        }
+    }
+
+    /**
+     * Function to remove query string params from url. Returns the new url.
+     * @param url
+     * @param name
+     */
+    urlRemoveQueryStringParameter(name: string): string {
+        if (StringUtils.isEmpty(this.urlString)) {
+            return this.urlString;
+        }
+
+        let regex = new RegExp("(\\&" + name + "=)[^\&]+");
+        this.urlString = this.urlString.replace(regex, "");
+        // name=value&
+        regex = new RegExp("(" + name + "=)[^\&]+&");
+        this.urlString = this.urlString.replace(regex, "");
+        // name=value
+        regex = new RegExp("(" + name + "=)[^\&]+");
+        this.urlString = this.urlString.replace(regex, "");
+        return this.urlString;
+    }
+
+    /**
+     * Given a url like https://a:b/common/d?e=f#g, and a tenantId, returns https://a:b/tenantId/d
+     * @param href The url
+     * @param tenantId The tenant id to replace
+     */
+    replaceTenantPath(tenantId: string): UrlString {
+        const urlObject = this.getUrlComponents();
+        const pathArray = urlObject.PathSegments;
+        if (tenantId && (pathArray.length !== 0 && (pathArray[0] === AADAuthorityConstants.COMMON || pathArray[0] === AADAuthorityConstants.ORGANIZATIONS))) {
+            pathArray[0] = tenantId;
+        }
+        return UrlString.constructAuthorityUriFromObject(urlObject, pathArray);
+    }
+
+    /**
+     * @hidden
+     * @ignore
+     *
+     * Returns the anchor part(#) of the URL
+     */
+    getHash(): string {
+        const hashIndex1 = this.urlString.indexOf("#");
+        const hashIndex2 = this.urlString.indexOf("#/");
+        if (hashIndex2 > -1) {
+            return this.urlString.substring(hashIndex2 + 2);
+        } else if (hashIndex1 > -1) {
+            return this.urlString.substring(hashIndex1 + 1);
+        }
+        return "";
+    }
+
+    /**
+     * @hidden
+     * Returns deserialized portion of URL hash
+     * @ignore
+     */
+    getDeserializedHash() {
+        const hash = this.getHash();
+        return CryptoUtils.deserialize(hash);
+    }
+
+    /**
+     * Parses out the components from a url string.
+     * @returns An object with the various components. Please cache this value insted of calling this multiple times on the same url.
+     */
+    getUrlComponents(): IUri {
+        if (!this.urlString) {
+            throw ClientAuthError.createUrlSegmentError(this.urlString);
+        }
+
+        // https://gist.github.com/curtisz/11139b2cfcaef4a261e0
+        const regEx = RegExp("^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\\?([^#]*))?(#(.*))?");
+
+        const match = this.urlString.match(regEx);
+
+        if (!match || match.length < 6) {
+            throw ClientAuthError.createUrlSegmentError(this.urlString);
+        }
+
+        const urlComponents = {
+            Protocol: match[1],
+            HostNameAndPort: match[4],
+            AbsolutePath: match[5]
+        } as IUri;
+
+        let pathSegments = urlComponents.AbsolutePath.split("/");
+        pathSegments = pathSegments.filter((val) => val && val.length > 0); // remove empty elements
+        urlComponents.PathSegments = pathSegments;
+        return urlComponents;
+    }
+
+    getUrlString(): string {
+        return this.urlString;
+    }
+
+    static constructAuthorityUriFromObject(urlObject: IUri, pathArray: string[]) {
+        return new UrlString(urlObject.Protocol + "//" + urlObject.HostNameAndPort + "/" + pathArray.join("/"));
+    }
+
+    /**
+     * @hidden
+     * Check if the hash of the URL string contains known properties
+     * @ignore
+     */
+    static hashContainsKnownProperties(url: string): boolean {
+        const urlString = new UrlString(url);
+        const parameters = urlString.getDeserializedHash();
+        return (
+            parameters.hasOwnProperty(AADServerHashParamKeys.ERROR_DESCRIPTION) ||
+            parameters.hasOwnProperty(AADServerHashParamKeys.ERROR) ||
+            parameters.hasOwnProperty(AADServerHashParamKeys.ACCESS_TOKEN) ||
+            parameters.hasOwnProperty(AADServerHashParamKeys.ID_TOKEN) ||
+            parameters.hasOwnProperty(AADServerHashParamKeys.CODE)
+        );
+    }
+}
