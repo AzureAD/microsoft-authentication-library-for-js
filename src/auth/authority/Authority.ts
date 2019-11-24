1b8e9ca9b5aa1f0550c0ae19e27dd6487d93a1a0
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-11-23 22:12:11 -0800

    Fixing build

--- src/auth/authority/Authority.ts
+++ src/auth/authority/Authority.ts
@@ -120,3 +120,4 @@ export abstract class Authority {
 
         return this;
     }
+}
--- src/error/ClientAuthError.ts
+++ src/error/ClientAuthError.ts
@@ -16,7 +16,7 @@ export const ClientAuthErrorMessage = {
  * Error thrown when there is an error in the client code running on the browser.
  */
 export class ClientAuthError extends AuthError {
-    
+        
     constructor(errorCode: string, errorMessage?: string) {
         super(errorCode, errorMessage);
         this.name = "ClientAuthError";
@@ -24,23 +24,27 @@ export class ClientAuthError extends AuthError {
         Object.setPrototypeOf(this, ClientAuthError.prototype);
     }
 
-    static createClientInfoDecodingError(err: string) {
-
-    }
-
-    static createIdTokenParsingError(err: string) {
+    static createClientInfoDecodingError(err: string): ClientAuthError {
         throw new Error("Method not implemented.");
     }
 
-    static createIdTokenNullOrEmptyError(rawIdToken: string) {
+    static createIdTokenParsingError(err: string): ClientAuthError {
         throw new Error("Method not implemented.");
     }
 
-    static createInsecureAuthorityUriError(urlString: string) {
+    static createIdTokenNullOrEmptyError(rawIdToken: string): ClientAuthError {
+        throw new Error("Method not implemented.");
+    }
+
+    static createInsecureAuthorityUriError(urlString: string): ClientAuthError {
         throw new Error("Method not implemented.");
     }
     
-    static createUrlSegmentError(urlString: string) {
+    static createUrlSegmentError(urlString: string): ClientAuthError {
+        throw new Error("Method not implemented.");
+    }
+
+    static createEndpointDiscoveryIncompleteError(): ClientAuthError {
         throw new Error("Method not implemented.");
     }
 }
