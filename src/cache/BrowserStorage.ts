ae8c6ee3d1b1ae0bdc38a53e981549b1f9074381
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2019-12-03 15:12:23 -0800

    Adding browser implementation of cache storage

---
+++ src/cache/BrowserStorage.ts
@@ -0,0 +1,239 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { ICacheStorage, Constants, PersistentCacheKeys, TemporaryCacheKeys } from "msal-common";
+import { CacheOptions } from "../app/Configuration";
+import { BrowserAuthError } from "../error/BrowserAuthError";
+import { BrowserConfigurationAuthError } from "../error/BrowserConfigurationAuthError";
+import { BrowserConstants } from "../utils/BrowserConstants";
+import { ErrorCacheKeys } from "msal-common/dist/utils/Constants";
+
+export class BrowserStorage implements ICacheStorage {
+
+    private cacheConfig: CacheOptions;
+    private windowStorage: Storage;
+
+    private clientId: string;
+    private rollbackEnabled: boolean;
+
+    constructor(clientId: string, cacheConfig: CacheOptions) {
+        if (!window) {
+            throw BrowserAuthError.createNoWindowObjectError();
+        }
+
+        const storageSupported = typeof window[cacheConfig.cacheLocation] !== "undefined" && window[cacheConfig.cacheLocation] !== null;
+        if (!storageSupported) {
+            throw BrowserConfigurationAuthError.createStorageNotSupportedError();
+        }
+
+        this.cacheConfig = cacheConfig;
+        this.windowStorage = window[this.cacheConfig.cacheLocation];
+        this.clientId = clientId;
+
+        // This is hardcoded to true for now, we will roll this out as a configurable option in the future.
+        this.rollbackEnabled = true;
+
+        this.migrateCacheEntries();
+    }
+
+    /**
+     * Support roll back to old cache schema until the next major release: true by default now
+     * @param storeAuthStateInCookie
+     */
+    private migrateCacheEntries() {
+
+        const idTokenKey = `${Constants.CACHE_PREFIX}.${PersistentCacheKeys.ID_TOKEN}`;
+        const clientInfoKey = `${Constants.CACHE_PREFIX}.${PersistentCacheKeys.CLIENT_INFO}`;
+        const errorKey = `${Constants.CACHE_PREFIX}.${ErrorCacheKeys.ERROR}`;
+        const errorDescKey = `${Constants.CACHE_PREFIX}.${ErrorCacheKeys.ERROR_DESC}`;
+
+        const idTokenValue = this.getItem(idTokenKey);
+        const clientInfoValue = this.getItem(clientInfoKey);
+        const errorValue = this.getItem(errorKey);
+        const errorDescValue = this.getItem(errorDescKey);
+
+        const values = [idTokenValue, clientInfoValue, errorValue, errorDescValue];
+        const keysToMigrate = [PersistentCacheKeys.ID_TOKEN, PersistentCacheKeys.CLIENT_INFO, ErrorCacheKeys.ERROR, ErrorCacheKeys.ERROR_DESC];
+
+        keysToMigrate.forEach((cacheKey, index) => this.createCacheEntry(cacheKey, values[index]));
+    }
+
+    /**
+     * Utility function to help with rollback keys
+     * @param newKey
+     * @param value
+     * @param storeAuthStateInCookie
+     */
+    private createCacheEntry(newKey: string, value: string) {
+        if (newKey && value) {
+            this.setItem(newKey, value);
+        }
+    }
+
+    /**
+     * Prepend msal.<client-id> to each key; Skip for any JSON object as Key (defined schemas do not need the key appended: AccessToken Keys or the upcoming schema)
+     * @param key
+     * @param addInstanceId
+     */
+    private generateCacheKey(key: string, addInstanceId: boolean): string {
+        try {
+            // Defined schemas do not need the key appended
+            JSON.parse(key);
+            return key;
+        } catch (e) {
+            if (key.startsWith(`${Constants.CACHE_PREFIX}`) || key.startsWith(PersistentCacheKeys.ADAL_ID_TOKEN)) {
+                return key;
+            }
+            return addInstanceId ? `${Constants.CACHE_PREFIX}.${this.clientId}.${key}` : `${Constants.CACHE_PREFIX}.${key}`;
+        }
+    }
+
+    setItem(key: string, value: string): void {
+        const msalKey = this.generateCacheKey(key, true);
+        this.windowStorage.setItem(msalKey, value);
+        if (this.rollbackEnabled) {
+            this.windowStorage.setItem(this.generateCacheKey(key, false), value);
+        }
+        if (this.cacheConfig.storeAuthStateInCookie) {
+            this.setItemCookie(msalKey, value);
+        }
+        return;
+    }
+    
+    getItem(key: string): string {
+        const msalKey = this.generateCacheKey(key, true);
+        const itemCookie = this.getItemCookie(msalKey);
+        if (this.cacheConfig.storeAuthStateInCookie && itemCookie) {
+            return itemCookie;
+        }
+        return this.windowStorage.getItem(msalKey);
+    }
+    
+    removeItem(key: string): void {
+        const msalKey = this.generateCacheKey(key, true);
+        this.windowStorage.removeItem(msalKey);
+        if (this.rollbackEnabled) {
+            this.windowStorage.removeItem(this.generateCacheKey(key, false));
+        }
+        if (this.cacheConfig.storeAuthStateInCookie) {
+            this.clearItemCookie(msalKey);
+        }
+    }
+    
+    containsKey(key: string): boolean {
+        const msalKey = this.generateCacheKey(key, true);
+        return this.windowStorage.hasOwnProperty(msalKey) || this.windowStorage.hasOwnProperty(key);
+    }
+    
+    getKeys(): string[] {
+        return Object.keys(this.windowStorage);
+    }
+
+    clear(): void {
+        let key: string;
+        for (key in this.windowStorage) {
+            // Check if key contains msal prefix; For now, we are clearing all the cache items created by MSAL.js
+            if (this.windowStorage.hasOwnProperty(key) && (key.indexOf(Constants.CACHE_PREFIX) !== -1) && (key.indexOf(this.clientId) !== -1)) {
+                this.removeItem(key);
+            }
+        }
+    }
+
+    /**
+     * Reset all temporary cache items
+     * @param state 
+     */
+    resetTempCacheItems(state: string): void {
+        const storage = window[this.cacheConfig.cacheLocation];
+        let key: string;
+        // check state and remove associated cache
+        for (key in storage) {
+            if (!state || key.indexOf(state) !== -1) {
+                const splitKey = key.split(Constants.RESOURCE_DELIM);
+                const keyState = splitKey.length > 1 ? splitKey[splitKey.length-1]: null;
+                if (keyState === state && !this.tokenRenewalInProgress(keyState)) {
+                    this.removeItem(key);
+                    this.setItemCookie(key, "", -1);
+                    this.clearMsalCookie(state);
+                }
+            }
+        }
+        // delete the interaction status cache
+        this.removeItem(TemporaryCacheKeys.INTERACTION_STATUS);
+        this.removeItem(TemporaryCacheKeys.REDIRECT_REQUEST);
+    }
+
+    /**
+     * Return if the token renewal is still in progress
+     * @param stateValue
+     */
+    private tokenRenewalInProgress(stateValue: string): boolean {
+        const renewStatus = this.getItem(`${TemporaryCacheKeys.RENEW_STATUS}|${stateValue}`);
+        return !!(renewStatus && renewStatus === BrowserConstants.inProgress);
+    }
+
+    /**
+     * add value to cookies
+     * @param cName
+     * @param cValue
+     * @param expires
+     */
+    setItemCookie(cName: string, cValue: string, expires?: number): void {
+        let cookieStr = cName + "=" + cValue + ";";
+        if (expires) {
+            const expireTime = this.getCookieExpirationTime(expires);
+            cookieStr += "expires=" + expireTime + ";";
+        }
+
+        document.cookie = cookieStr;
+    }
+
+    /**
+     * get one item by key from cookies
+     * @param cName
+     */
+    getItemCookie(cName: string): string {
+        const name = cName + "=";
+        const ca = document.cookie.split(";");
+        for (let i = 0; i < ca.length; i++) {
+            let c = ca[i];
+            while (c.charAt(0) === " ") {
+                c = c.substring(1);
+            }
+            if (c.indexOf(name) === 0) {
+                return c.substring(name.length, c.length);
+            }
+        }
+        return "";
+    }
+
+    /**
+     * Clear an item in the cookies by key
+     * @param cName
+     */
+    clearItemCookie(cName: string) {
+        this.setItemCookie(cName, "", -1);
+    }
+
+    /**
+     * Clear all msal cookies
+     */
+    public clearMsalCookie(state?: string): void {
+        const nonceKey = state ? `${TemporaryCacheKeys.NONCE_IDTOKEN}|${state}` : TemporaryCacheKeys.NONCE_IDTOKEN;
+        this.clearItemCookie(nonceKey);
+        this.clearItemCookie(TemporaryCacheKeys.REQUEST_STATE);
+        this.clearItemCookie(TemporaryCacheKeys.ORIGIN_URI);
+    }
+
+    /**
+     * Get cookie expiration time
+     * @param cookieLifeDays
+     */
+    getCookieExpirationTime(cookieLifeDays: number): string {
+        const today = new Date();
+        const expr = new Date(today.getTime() + cookieLifeDays * 24 * 60 * 60 * 1000);
+        return expr.toUTCString();
+    }
+}
---
+++ src/error/BrowserAuthError.ts
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+import { AuthError, ClientAuthError } from "msal-common";
+
+/**
+ * BrowserAuthErrorMessage class containing string constants used by error codes and messages.
+ */
+export const BrowserAuthErrorMessage = {
+    noWindowObjectError: {
+        code: "no_window_object",
+        desc: "No window object detected."
+    }
+};
+
+/**
+ * Browser library error class thrown by the MSAL.js library for SPAs
+ */
+export class BrowserAuthError extends ClientAuthError {
+
+    constructor(errorCode: string, errorMessage?: string) {
+        super(errorCode, errorMessage);
+        this.name = "BrowserAuthError";
+
+        Object.setPrototypeOf(this, BrowserAuthError.prototype);
+    }
+
+    static createNoWindowObjectError(): BrowserAuthError {
+        return new BrowserAuthError(BrowserAuthErrorMessage.noWindowObjectError.code, BrowserAuthErrorMessage.noWindowObjectError.desc);
+    }
+}
