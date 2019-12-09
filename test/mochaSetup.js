c42ffc0d06f21d8be4864edbdfd8324c64005082
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-12-09 13:54:24 -0800

    Fixing tests

--- test/mochaSetup.js
+++ test/mochaSetup.js
@@ -3,4 +3,5 @@ require("jsdom-global")("", {
     url: "https://localhost:8081/index.html"
 });
 window.crypto = require("@trust/webcrypto");
+window["msCrypto"] = require("@trust/webcrypto");
 require("msal-common");
