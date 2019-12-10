3c82f5c68155ef920dba8458a480b7dafb5d0da2
Prithvi Kanherkar <prithviraj.kanherkar@microsoft.com>
2019-12-10 15:10:01 -0800

    removing mocha setup for mscrypto

--- test/mochaSetup.js
+++ test/mochaSetup.js
@@ -3,5 +3,4 @@ require("jsdom-global")("", {
     url: "https://localhost:8081/index.html"
 });
 window.crypto = require("@trust/webcrypto");
-window["msCrypto"] = require("@trust/webcrypto");
 require("msal-common");
