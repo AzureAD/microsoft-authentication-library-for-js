d4c981cd4fb9ed947d31c38ee9f519d00a839d87
Jason Nutter <janutter@microsoft.com>
2020-01-21 13:30:17 -0800

    Add Material, profile page to Angular 7 sample

--- samples/angular7-sample-app/src/app/app.component.ts
+++ samples/angular7-sample-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BroadcastService, MsalService } from '@azure/msal-angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,46 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular7-sample-app';
+  title = 'MSAL Angular - v7 Sample';
+  isIframe = false;
+  loggedIn = false;
+
+  constructor(private broadcastService: BroadcastService, private authService: MsalService) { }
+
+  ngOnInit() {
+    this.isIframe = window !== window.parent && !window.opener;
+
+    this.checkoutAccount();
+
+    this.broadcastService.subscribe('msal:loginSuccess', () => {
+      this.checkoutAccount();
+    });
+
+    this.authService.handleRedirectCallback((authError, response) => {
+      if (authError) {
+        console.error('Redirect Error: ', authError.errorMessage);
+        return;
+      }
+
+      console.log('Redirect Success: ', response.accessToken);
+    });
+  }
+
+  checkoutAccount() {
+    this.loggedIn = !!this.authService.getAccount();
+  }
+
+  login() {
+    const isIE = window.navigator.userAgent.indexOf('MSIE ') > -1 || window.navigator.userAgent.indexOf('Trident/') > -1;
+
+    if (isIE) {
+      this.authService.loginRedirect();
+    } else {
+      this.authService.loginPopup();
+    }
+  }
+
+  logout() {
+    this.authService.logout();
+  }
 }
