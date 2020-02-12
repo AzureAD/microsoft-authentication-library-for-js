3effe4bc5429c7883701de8131570bebb8c25f34
Jason Nutter <janutter@microsoft.com>
2020-02-12 10:20:58 -0800

    Initial Angular 9 sample app

---
+++ samples/angular9-sample-app/src/app/app-routing.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core/core';
+import { Routes, RouterModule } from '@angular/router/router';
+
+
+const routes: Routes = [];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
