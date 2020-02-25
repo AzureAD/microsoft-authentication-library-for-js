d6c3eed14e51c5cf37d4cbe37b65c77b638e51ba
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-02-25 10:47:47 -0800

    scaffolding unit tests

--- lib/msal-common/test/server/ServerCodeRequestParameters.spec.ts
+++ lib/msal-common/test/server/ServerCodeRequestParameters.spec.ts
@@ -2,4 +2,18 @@ import { expect } from "chai";
 
 describe("ServerCodeRequestParameters.ts Class Unit Tests", () => {
 
+    describe("Constructor", () => {
+
+        it("correctly assigns request parameter values", () => {
+
+        });
+
+        it("appends extraScopesToConsent if isLoginCall is true", () => {
+
+        });
+
+        it("throws error if scopes are required and not provided", () => {
+
+        });
+    });
 });
--- lib/msal-common/test/utils/ProtocolUtils.spec.ts
+++ lib/msal-common/test/utils/ProtocolUtils.spec.ts
@@ -2,4 +2,19 @@ import { expect } from "chai";
 
 describe("ProtocolUtils.ts Class Unit Tests", () => {
 
+    it("setRequestState() appends random GUID to given state", () => {
+
+    });
+
+    it("setRequestState() only creates random GUID", () => {
+
+    });
+
+    it("getUserRequestState() returns blank string if serverResponseState is null or empty", () => {
+
+    });
+
+    it("getUserRequestState() correctly splits the state by the resource delimiter", () => {
+
+    });
 });
