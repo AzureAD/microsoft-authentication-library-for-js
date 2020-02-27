65e2a0bb82cc429de1b4353509a1eebcde269637
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-02-27 12:22:49 -0800

    Removed old sample files, Updated README, added 2.0 sample

---
+++ samples/VanillaJSTestApp2.0/JavascriptSPA/authConfig.js
@@ -0,0 +1,18 @@
+// Config object to be passed to Msal on creation
+const msalConfig = {
+    auth: {
+        clientId: "Enter_the_Application_Id_Here",
+        authority: "Enter_the_Cloud_Instance_Id_HereEnter_the_Tenant_Info_Here",
+        redirectUri: "Enter_the_Redirect_Uri_Here",
+        tmp_clientSecret: "Enter_your_client_secret"
+    },
+    cache: {
+        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
+    }
+};  
+
+// Add here scopes for id token to be used at MS Identity Platform endpoints.
+const loginRequest = {
+    scopes: ["openid", "profile", "User.Read"],
+};
