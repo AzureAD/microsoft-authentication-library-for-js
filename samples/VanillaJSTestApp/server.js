b182fe69c8ba35beac4e2e71e9e89fe3b8f8e419
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-02-27 11:52:24 -0800

    Adding sample

--- samples/VanillaJSTestApp/server.js
+++ samples/VanillaJSTestApp/server.js
@@ -3,27 +3,27 @@
 *  See LICENSE in the source repository root for complete license information.
 */
 
-var express = require('express');
-var app = express();
-var morgan = require('morgan');
-var path = require('path');
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+//initialize express.
+const app = express();
 
 // Initialize variables.
-var PORT = 30662;
+const port = 30662; // process.env.PORT || 30662;
 
 // Configure morgan module to log all requests.
 app.use(morgan('dev'));
 
 // Set the front-end folder to serve public assets.
-app.use("/lib", express.static(path.join(__dirname, "../../lib/msal-browser/lib")));
+app.use(express.static('JavaScriptSPA'))
 
-// Set up our one route to the index.html file.
+// Set up a route for index.html.
 app.get('*', function (req, res) {
-    const reqPath = req.path === "/" ? "/index_authCode.html" : req.path;
-    res.sendFile(path.join(__dirname + reqPath));
+    res.sendFile(path.join(__dirname + '/index.html'));
 });
 
 // Start the server.
-app.listen(PORT, function() {
-    console.log('Listening on port ' + PORT + '...');
-});
+app.listen(port);
+console.log('Listening on port ' + port + '...');
