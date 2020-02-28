471f9a8225e7b4ff119e9882723365c20305a73c
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-02-27 17:26:45 -0800

    Removed parseExpiresinSeconds since expiresIn and extExpiresIn are already numbers

--- lib/msal-common/src/utils/TimeUtils.ts
+++ lib/msal-common/src/utils/TimeUtils.ts
@@ -7,15 +7,6 @@
  * Utility class which exposes functions for managing date and time operations.
  */
 export class TimeUtils {
-    /**
-     * Returns time in seconds for expiration based on string value passed in.
-     *
-     * @param expiresIn
-     */
-    static parseExpiresInSeconds(expiresIn: number): number {
-        // if AAD did not send "expires_in" property, use default expiration of 3599 seconds, for some reason AAD sends 3599 as "expires_in" value instead of 3600
-        return expiresIn || 3599;
-    }
 
     /**
      * return the current time in Unix time (seconds).
--- lib/msal-common/test/utils/TimeUtils.spec.ts
+++ lib/msal-common/test/utils/TimeUtils.spec.ts
@@ -1,16 +1,11 @@
 import { expect } from "chai";
+import { TimeUtils } from "../../src/utils/TimeUtils";
 
 describe("TimeUtils.ts Class Unit Tests", () => {
 
-    it("parseExpiresInSeconds returns 3599 by default", () => {
-
-    });
-
-    it("parseExpiresInSeconds parses string into a number", () => {
-
-    });
-
     it("nowSeconds() gets the current Unix time in seconds", () => {
-
+        const currSeconds = TimeUtils.nowSeconds();
+        expect(typeof currSeconds).to.be.eq("number");
+        expect(currSeconds).to.be.at.most(TimeUtils.nowSeconds());
     });
 });
