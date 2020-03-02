caad937c8919627f9f7dc32a2266d130c6dcceec
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-03-02 13:02:44 -0800

    Adding changes from comments
    refactored handleRedirectCallbacks, renamed it to better reflect the feature, added utils for navigating without history.

--- src/utils/BrowserUtils.ts
+++ src/utils/BrowserUtils.ts
@@ -5,20 +5,37 @@
 import { INetworkModule } from "@azure/msal-common";
 import { FetchClient } from "../network/FetchClient";
 import { XhrClient } from "../network/XhrClient";
+import { BrowserAuthError } from "../error/BrowserAuthError";
 
 /**
  * Utility class for browser specific functions
  */
 export class BrowserUtils {
 
-    // #region Window Navigation
+    // #region Window Navigation and URL management
 
     /**
      * Used to redirect the browser to the STS authorization endpoint
      * @param {string} urlNavigate - URL of the authorization endpoint
      */
-    static navigateWindow(urlNavigate: string): void {
-        window.location.assign(urlNavigate);
+    static navigateWindow(urlNavigate: string, noHistory?: boolean): void {
+        const windowParentCheck = (window.parent !== window);
+        if (windowParentCheck) {
+            // If we are not in top frame, we shouldn't redirect. This is also handled by the service.
+            throw BrowserAuthError.createRedirectInIframeError(windowParentCheck);
+        }
+        if (noHistory) {
+            window.location.replace(urlNavigate);
+        } else {
+            window.location.assign(urlNavigate);
+        }
+    }
+
+    /**
+     * Clears hash from window url.
+     */
+    static clearHash(): void {
+        window.location.hash = "";
     }
 
     // #endregion
