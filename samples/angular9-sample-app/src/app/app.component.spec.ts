6cd60abd6297bcb90bc96c00c51acc73a229f53a
Jason Nutter <janutter@microsoft.com>
2020-03-02 14:29:32 -0800

    Fix tests for Angular 9 sample

--- samples/angular9-sample-app/src/app/app.component.spec.ts
+++ samples/angular9-sample-app/src/app/app.component.spec.ts
@@ -1,16 +1,53 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MsalService, MSAL_CONFIG, MSAL_CONFIG_ANGULAR, MsalAngularConfiguration, BroadcastService } from '@azure/msal-angular';
+import { Configuration } from 'msal';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatListModule,
       ],
       declarations: [
         AppComponent
       ],
+      providers: [
+        MsalService,
+        {
+          provide: MSAL_CONFIG,
+          useValue: {
+            auth: {
+              clientId: 'Enter_the_Application_Id_here', // This is your client ID
+              authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // This is your tenant info
+              redirectUri: 'Enter_the_Redirect_Uri_Here' // This is your redirect URI
+            },
+            cache: {
+              cacheLocation: 'localStorage',
+              storeAuthStateInCookie: false
+            },
+          } as Configuration
+        },
+        {
+          provide: MSAL_CONFIG_ANGULAR,
+          useValue: {
+            popUp: false,
+            consentScopes: [ 'user.read' ],
+            unprotectedResources: [],
+            protectedResourceMap: [
+              ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+            ]
+          } as MsalAngularConfiguration
+        },
+        BroadcastService
+      ]
     }).compileComponents();
   }));
 
@@ -20,16 +57,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular9-sample-app'`, () => {
+  it(`should have as title 'MSAL - Angular 9 Sample App'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular9-sample-app');
+    expect(app.title).toEqual('MSAL - Angular 9 Sample App');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('angular9-sample-app app is running!');
+    expect(compiled.querySelector('.title').textContent).toContain('MSAL - Angular 9 Sample App');
   });
 });
--- samples/angular9-sample-app/src/app/profile/profile.component.spec.ts
+++ samples/angular9-sample-app/src/app/profile/profile.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProfileComponent } from './profile.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { AppComponent } from '../app.component';
+import { MsalService, MSAL_CONFIG, MSAL_CONFIG_ANGULAR, MsalAngularConfiguration, BroadcastService } from '@azure/msal-angular';
+import { Configuration } from 'msal';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
@@ -8,9 +16,43 @@ describe('ProfileComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule,
+        HttpClientModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+      providers: [
+        MsalService,
+        {
+          provide: MSAL_CONFIG,
+          useValue: {
+            auth: {
+              clientId: 'Enter_the_Application_Id_here', // This is your client ID
+              authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // This is your tenant info
+              redirectUri: 'Enter_the_Redirect_Uri_Here' // This is your redirect URI
+            },
+            cache: {
+              cacheLocation: 'localStorage',
+              storeAuthStateInCookie: false
+            },
+          } as Configuration
+        },
+        {
+          provide: MSAL_CONFIG_ANGULAR,
+          useValue: {
+            popUp: false,
+            consentScopes: [ 'user.read' ],
+            unprotectedResources: [],
+            protectedResourceMap: [
+              ['https://graph.microsoft.com/v1.0/me', ['user.read']]
+            ]
+          } as MsalAngularConfiguration
+        },
+        BroadcastService
+      ]
+    }).compileComponents();
   }));
 
   beforeEach(() => {
