53738f1d48a734fc28ef8f0b0a3d96d160569e87
Jason Nutter <janutter@microsoft.com>
2020-03-02 14:32:33 -0800

    IE11 support for Angular 9 sample

--- samples/angular9-sample-app/src/polyfills.ts
+++ samples/angular9-sample-app/src/polyfills.ts
@@ -55,6 +55,7 @@
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
+import 'core-js'; // Needed for IE11
 import 'zone.js/dist/zone';  // Included with Angular CLI.
 
 
