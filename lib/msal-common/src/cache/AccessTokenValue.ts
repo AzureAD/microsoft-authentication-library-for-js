10c844f6476b746f8b8409b0be2bc53e7c25f54a
Jake McKennon <jamckenn@microsoft.com>
2020-03-05 15:36:46 -0800

    Fix access tokens being overwritten on bad case

--- lib/msal-common/src/cache/AccessTokenValue.ts
+++ lib/msal-common/src/cache/AccessTokenValue.ts
@@ -7,20 +7,30 @@
  * Value of access token cache item which is stored in cache. Helps manage token renewal.
  */
 export class AccessTokenValue {
+    // Success cases
+    tokenType?: string;
+    accessToken?: string;
+    idToken?: string;
+    refreshToken?: string;
+    expiresOnSec?: string;
+    extExpiresOnSec?: string;
 
-    tokenType: string;
-    accessToken: string;
-    idToken: string;
-    refreshToken: string;
-    expiresOnSec: string;
-    extExpiresOnSec: string;
+    // These fields exist when a thumbprint is being throttled
+    error?: string;
+    errorDescription?: string;
+    errorCodes?: string;         // delimited by " "
+    throttleTime?: number;       // in ms
 
-    constructor(tokenType: string, accessToken: string, idToken: string, refreshToken: string, expiresOn: string, extExpiresOn: string) {
+    constructor(tokenType?: string, accessToken?: string, idToken?: string, refreshToken?: string, expiresOn?: string, extExpiresOn?: string, error?: string, errorDescription?: string, errorCodes?: string, throttleTime?: number) {
         this.tokenType = tokenType;
         this.accessToken = accessToken;
         this.idToken = idToken;
         this.refreshToken = refreshToken;
         this.expiresOnSec = expiresOn;
         this.extExpiresOnSec = extExpiresOn;
+        this.error = error;
+        this.errorDescription = errorDescription;
+        this.errorCodes = errorCodes;
+        this.throttleTime = throttleTime;
     }
 }
--- lib/msal-common/src/network/NetworkManager.ts
+++ lib/msal-common/src/network/NetworkManager.ts
@@ -7,11 +7,11 @@ import { INetworkModule } from "./INetworkModule";
 import { ServerTokenRequestParameters } from "../server/ServerTokenRequestParameters";
 import { ICacheStorage } from "../cache/ICacheStorage";
 import { AccessTokenKey } from "../cache/AccessTokenKey";
+import { AccessTokenValue } from '../cache/AccessTokenValue';
 import { Constants } from "../utils/Constants";
 import { ICrypto } from "../crypto/ICrypto";
 import { TokenExchangeParameters } from "../request/TokenExchangeParameters";
 import { ClientInfo } from "../auth/ClientInfo";
-import { ErrorValue } from "../cache/ErrorValue";
 
 export type NetworkResponse<T> = {
     headers: Map<string, string>;
@@ -56,17 +56,27 @@ export class NetworkManager {
     }
 
     private postProcess(response: NetworkResponse<ServerAuthorizationTokenResponse>, thumbprint: AccessTokenKey): void {
-        if (response.status >= 500 || response.status == 429 || response.status >= 300 && response.headers.has("Retry-After")) {
-            const throttleTime = response.headers.get("Retry-After") || Date.now() + Constants.DEFAULT_THROTTLE_TIME_MS;
+        if (response.status >= 500 || response.status == 429 || response.status < 200 && response.status >= 300 && response.headers.has("Retry-After")) {
+            const throttleTime = parseInt(response.headers.get("Retry-After")) || Date.now() + Constants.DEFAULT_THROTTLE_TIME_MS;
 
-            const errorValue = new ErrorValue(
+            const cacheKey = JSON.stringify(thumbprint);
+            const currentCacheValue = this.cacheStorage.getItem(cacheKey);
+            const parsedValue = JSON.parse(currentCacheValue);
+
+            const accessTokenValue = new AccessTokenValue(
+                parsedValue.tokenType,
+                parsedValue.accessToken,
+                parsedValue.idToken,
+                parsedValue.refreshToken,
+                parsedValue.expiresOn,
+                parsedValue.extExpiresOn,
                 response.body.error,
                 response.body.error_description,
                 response.body.error_codes.join(" "),
                 throttleTime
             );
 
-            this.cacheStorage.setItem(JSON.stringify(thumbprint), JSON.stringify(errorValue));
+            this.cacheStorage.setItem(cacheKey, JSON.stringify(accessTokenValue));
         }
     }
 
