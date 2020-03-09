fca363af7d5fe1374fa515c03c1cd01912709671
Jake McKennon <jamckenn@microsoft.com>
2020-03-09 13:32:39 -0700

    Add preProcess function and cleanup other functions

--- lib/msal-common/src/network/NetworkManager.ts
+++ lib/msal-common/src/network/NetworkManager.ts
@@ -7,11 +7,12 @@ import { INetworkModule } from "./INetworkModule";
 import { ServerTokenRequestParameters } from "../server/ServerTokenRequestParameters";
 import { ICacheStorage } from "../cache/ICacheStorage";
 import { AccessTokenKey } from "../cache/AccessTokenKey";
-import { AccessTokenValue } from '../cache/AccessTokenValue';
+import { AccessTokenValue } from "../cache/AccessTokenValue";
 import { Constants } from "../utils/Constants";
 import { ICrypto } from "../crypto/ICrypto";
 import { TokenExchangeParameters } from "../request/TokenExchangeParameters";
 import { ClientInfo } from "../auth/ClientInfo";
+import { ServerError } from "../error/ServerError";
 
 export type NetworkResponse<T> = {
     headers: Map<string, string>;
@@ -31,22 +32,14 @@ export class NetworkManager {
     }
 
     async sendPostRequest(tokenEndpoint: string, tokenReqParams: ServerTokenRequestParameters, tokenRequest: TokenExchangeParameters, clientInfo: ClientInfo, clientId: string): Promise<ServerAuthorizationTokenResponse> {
-        const thumbprint = new AccessTokenKey(
-            tokenRequest.authority, 
-            clientId, 
-            tokenRequest.scopes.join(" "), 
-            tokenRequest.resource, 
-            clientInfo && clientInfo.uid, 
-            clientInfo && clientInfo.utid, 
-            this.cryptoObj
-        );
+        const thumbprint = this.createThumbprint(clientId, tokenRequest, clientInfo);
 
         const options = {
             body: tokenReqParams.createRequestBody(),
             headers: tokenReqParams.createRequestHeaders()
         };
 
-        // this.preProcess();
+        this.preProcess(thumbprint);
 
         const networkResponse = await this.networkClient.sendPostRequestAsync(tokenEndpoint, options);
 
@@ -59,28 +52,66 @@ export class NetworkManager {
         if (response.status >= 500 || response.status == 429 || (response.status < 200 || response.status >= 300) && response.headers.has("Retry-After")) {
             const throttleTime = parseInt(response.headers.get("Retry-After")) || Date.now() + Constants.DEFAULT_THROTTLE_TIME_MS;
 
-            const cacheKey = JSON.stringify(thumbprint);
-            const currentCacheValue = this.cacheStorage.getItem(cacheKey);
-            const parsedValue = JSON.parse(currentCacheValue);
+            const accessTokenValue = this.getThumbprintValueFromCache(thumbprint);
 
-            const accessTokenValue = new AccessTokenValue(
-                parsedValue.tokenType,
-                parsedValue.accessToken,
-                parsedValue.idToken,
-                parsedValue.refreshToken,
-                parsedValue.expiresOn,
-                parsedValue.extExpiresOn,
+            const newAccessTokenValue = new AccessTokenValue(
+                accessTokenValue.tokenType,
+                accessTokenValue.accessToken,
+                accessTokenValue.idToken,
+                accessTokenValue.refreshToken,
+                accessTokenValue.expiresOnSec,
+                accessTokenValue.extExpiresOnSec,
                 response.body.error,
                 response.body.error_description,
                 response.body.error_codes.join(" "),
                 throttleTime
             );
 
-            this.cacheStorage.setItem(cacheKey, JSON.stringify(accessTokenValue));
+            this.cacheStorage.setItem(JSON.stringify(thumbprint), JSON.stringify(newAccessTokenValue));
         }
     }
 
-    // private preProcess() {
+    private preProcess(thumbprint: AccessTokenKey): void {
+        const accessTokenValue = this.getThumbprintValueFromCache(thumbprint);
+        const currentTime = Date.now();
 
-    // }
+        if (accessTokenValue.throttleTime > currentTime) {
+            throw new ServerError(accessTokenValue.error, accessTokenValue.errorDescription);
+        }
+        else if (accessTokenValue.throttleTime <= currentTime) {
+            if (accessTokenValue.idToken) {
+                const newAccessTokenValue = new AccessTokenValue(
+                    accessTokenValue.tokenType,
+                    accessTokenValue.accessToken,
+                    accessTokenValue.idToken,
+                    accessTokenValue.refreshToken,
+                    accessTokenValue.expiresOnSec,
+                    accessTokenValue.extExpiresOnSec
+                );
+    
+                this.cacheStorage.setItem(JSON.stringify(thumbprint), JSON.stringify(newAccessTokenValue));
+            }
+            else {
+                this.cacheStorage.removeItem(JSON.stringify(thumbprint));
+            }
+        }
+    }
+
+    private createThumbprint(clientId: string, tokenRequest: TokenExchangeParameters, clientInfo: ClientInfo): AccessTokenKey {
+        return new AccessTokenKey(
+            tokenRequest.authority, 
+            clientId, 
+            tokenRequest.scopes.join(" "), 
+            tokenRequest.resource, 
+            clientInfo && clientInfo.uid, 
+            clientInfo && clientInfo.utid, 
+            this.cryptoObj
+        );
+    }
+
+    private getThumbprintValueFromCache(thumbprint: AccessTokenKey): AccessTokenValue {
+        const cacheKey = JSON.stringify(thumbprint);
+        const currentCacheValue = this.cacheStorage.getItem(cacheKey);
+        return JSON.parse(currentCacheValue) as AccessTokenValue;
+    }
 }
