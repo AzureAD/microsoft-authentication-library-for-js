7d8b154662e69eec39c3601848b61f9064e1cf62
Thomas Norling <thomas.l.norling@gmail.com>
2020-04-09 12:25:09 -0700

    Move B2CTrustedHostList to B2CAuthority class

--- lib/msal-common/src/auth/authority/B2CTrustedHostList.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
-/**
- * Initializes the B2C Trusted Host List. Populated by AuthorityFactory.setKnownAuthorities(). Used by the B2C authority class to validate authorities.
- */
-export const B2CTrustedHostList: string[] = [];
