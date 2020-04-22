29ef76aa0decd74d7d696fcf6d711163eeba9e5c
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-04-22 11:55:55 -0700

    Fixing interaction handler tests

--- lib/msal-browser/src/interaction_handler/SilentHandler.ts
+++ lib/msal-browser/src/interaction_handler/SilentHandler.ts
@@ -27,7 +27,7 @@ export class SilentHandler extends InteractionHandler {
             this.authModule.logger.info("Navigate url is empty");
             throw BrowserAuthError.createEmptyNavigationUriError();
         }
-        const frameName = `msalTokenFrame${userRequestScopes}`;
+        const frameName = userRequestScopes ? `msalTokenFrame${userRequestScopes}` : `msalTokenFrame`;
         return this.loadFrameTimeout ? await this.loadFrame(requestUrl, frameName) : this.loadFrameSync(requestUrl, frameName);
     }
 
