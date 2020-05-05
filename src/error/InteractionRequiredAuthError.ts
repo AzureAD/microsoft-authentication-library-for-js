29a063ab28647e0961fa92907e62ca11b70b2439
Thomas Norling <thomas.l.norling@gmail.com>
2020-05-05 11:53:15 -0700

    Address IE11 compat

--- src/error/InteractionRequiredAuthError.ts
+++ src/error/InteractionRequiredAuthError.ts
@@ -9,19 +9,19 @@ import { ServerError } from "./ServerError";
 /**
  * InteractionRequiredAuthErrorMessage class containing string constants used by error codes and messages.
  */
-export const InteractionRequiredAuthErrorMessage = {
-    interactionRequired: "interaction_required",
-    consentRequired: "consent_required",
-    loginRequired: "login_required"
-};
+export const InteractionRequiredAuthErrorMessage = [
+    "interaction_required",
+    "consent_required",
+    "login_required"
+];
 
-export const InteractionRequiredAuthSubErrorMessage = {
-    messageOnly: "message_only",
-    additionalAction: "additional_action",
-    basicAction: "basic_action",
-    userPasswordExpired: "user_password_expired",
-    consentRequired: "consent_required"
-};
+export const InteractionRequiredAuthSubErrorMessage = [
+    "message_only",
+    "additional_action",
+    "basic_action",
+    "user_password_expired",
+    "consent_required"
+];
 
 /**
  * Error thrown when user interaction is required at the auth server.
@@ -36,9 +36,9 @@ export class InteractionRequiredAuthError extends ServerError {
     }
 
     static isInteractionRequiredError(errorCode: string, errorString: string, subError?: string) : boolean {
-        const isInteractionRequiredErrorCode = !StringUtils.isEmpty(errorCode) && Object.values(InteractionRequiredAuthErrorMessage).includes(errorCode);
-        const isInteractionRequiredSubError = !StringUtils.isEmpty(subError) && Object.values(InteractionRequiredAuthSubErrorMessage).includes(subError);
-        const isInteractionRequiredErrorDesc = !StringUtils.isEmpty(errorString) && Object.values(InteractionRequiredAuthErrorMessage).some((irErrorCode) => {
+        const isInteractionRequiredErrorCode = !StringUtils.isEmpty(errorCode) && InteractionRequiredAuthErrorMessage.indexOf(errorCode) > -1;
+        const isInteractionRequiredSubError = !StringUtils.isEmpty(subError) && InteractionRequiredAuthSubErrorMessage.indexOf(subError) > -1;
+        const isInteractionRequiredErrorDesc = !StringUtils.isEmpty(errorString) && InteractionRequiredAuthErrorMessage.some((irErrorCode) => {
             return errorString.indexOf(irErrorCode) > -1;
         });
 
