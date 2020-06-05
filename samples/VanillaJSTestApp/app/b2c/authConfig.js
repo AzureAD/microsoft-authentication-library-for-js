35ad895d4885e3caf9cc7c3073c78bd2c13ad32d
Thomas Norling <thomas.l.norling@gmail.com>
2020-06-05 11:21:29 -0700

    Update with lab app registration

--- samples/VanillaJSTestApp/app/b2c/authConfig.js
+++ samples/VanillaJSTestApp/app/b2c/authConfig.js
@@ -1,9 +1,9 @@
 // Config object to be passed to Msal on creation
 const msalConfig = {
     auth: {
-        clientId: "e760cab2-b9a1-4c0d-86fb-ff7084abd902",
-        authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_susi",
-        knownAuthorities: ["fabrikamb2c.b2clogin.com"]
+        clientId: "5dac5d6d-225c-4a98-a5e4-e29c82c0c4c9",
+        authority: "https://public.msidlabb2c.com/tfp/cpimtestpartners.onmicrosoft.com/b2c_1_signupsignin_userflow",
+        knownAuthorities: ["public.msidlabb2c.com"]
     },
     cache: {
         cacheLocation: "localStorage", // This configures where your cache will be stored
@@ -22,12 +22,12 @@ const msalConfig = {
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 const loginRequest = {
-    scopes: ["https://fabrikamb2c.onmicrosoft.com/helloapi/demo.read"],
+    scopes: ["openid", "profile"],
     forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
 };
 
 const editProfileRequest = {
     scopes: ["openid"],
-    authority: "https://fabrikamb2c.b2clogin.com/fabrikamb2c.onmicrosoft.com/b2c_1_edit_profile",
+    authority: "https://public.msidlabb2c.com/tfp/cpimtestpartners.onmicrosoft.com/b2c_1_editprofile_userflow",
     forceRefresh: false // Set this to "true" to skip a cached token and go to the server to get a new token
 }
