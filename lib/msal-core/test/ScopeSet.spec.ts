e00bfd9fc07620b312b96919a8da222ca141d8e4
Hector Mg <hectormgdev@gmail.com>
2020-06-08 15:17:07 -0700

    Add initial tests for ScopeSet.appendScopes

--- lib/msal-core/test/ScopeSet.spec.ts
+++ lib/msal-core/test/ScopeSet.spec.ts
@@ -3,9 +3,20 @@ import { expect } from "chai";
 import { ClientConfigurationError, ClientConfigurationErrorMessage } from "../src/error/ClientConfigurationError";
 
 describe.only("ScopeSet.ts", () => {
+    describe.only("appendScopes", function() {
+        it("should return null if no scopes are passed into the first argument", function() {
+            expect(ScopeSet.appendScopes(null, ["S1"])).to.eq(null);
+        });
+    });
+
     describe("validateInputScope", function() {
-        it("should not throw any errors when", function () {
+        it("should not throw any errors when a single scope is passed in scopes array", function () {
             const scopes = ["S1"];
+            expect(() => ScopeSet.validateInputScope(scopes)).to.not.throw();
+        });
+
+        it("should not throw any errors when multiple scopes are passed in scopes array", function () {
+            const scopes = ["S1", "S2"];
             expect(() => ScopeSet.validateInputScope(scopes)).to.not.throw();
         });
 
@@ -22,8 +33,6 @@ describe.only("ScopeSet.ts", () => {
             expect(clientConfigError instanceof ClientConfigurationError).to.eq(true);
             expect(clientConfigError.errorCode).to.equal(ClientConfigurationErrorMessage.scopesRequired.code);
             expect(clientConfigError.message).to.contain(ClientConfigurationErrorMessage.scopesRequired.desc);
-            expect(clientConfigError.name).to.equal("ClientConfigurationError");
-            expect(clientConfigError.stack).to.include("ScopeSet.spec.ts");
         });
         
         it("should throw createScopesNonArrayError if scopes is not an array object", function() {
@@ -31,7 +40,8 @@ describe.only("ScopeSet.ts", () => {
             let clientConfigError;
 
             try {
-                ScopeSet.validateInputScope(scopes as any);
+                // @ts-ignore
+                ScopeSet.validateInputScope(scopes);
             } catch (e) {
                 clientConfigError = e;
             }
@@ -39,8 +49,6 @@ describe.only("ScopeSet.ts", () => {
             expect(clientConfigError instanceof ClientConfigurationError).to.eq(true);
             expect(clientConfigError.errorCode).to.equal(ClientConfigurationErrorMessage.nonArrayScopes.code);
             expect(clientConfigError.message).to.contain(ClientConfigurationErrorMessage.nonArrayScopes.desc);
-            expect(clientConfigError.name).to.equal("ClientConfigurationError");
-            expect(clientConfigError.stack).to.include("ScopeSet.spec.ts");
         });
 
         it("should throw createEmptyScopesArrayError if scopes is an empty array", function() {
@@ -56,8 +64,6 @@ describe.only("ScopeSet.ts", () => {
             expect(clientConfigError instanceof ClientConfigurationError).to.eq(true);
             expect(clientConfigError.errorCode).to.equal(ClientConfigurationErrorMessage.emptyScopes.code);
             expect(clientConfigError.message).to.contain(ClientConfigurationErrorMessage.emptyScopes.desc);
-            expect(clientConfigError.name).to.equal("ClientConfigurationError");
-            expect(clientConfigError.stack).to.include("ScopeSet.spec.ts");
         });
     });
 });
