e64aa035414ae60e8a524d73a3d493579035520b
Thomas Norling <thomas.l.norling@gmail.com>
2020-06-12 11:16:34 -0700

    Add acquireToken tests

--- samples/VanillaJSTestApp/app/b2c/test/browser.spec.ts
+++ samples/VanillaJSTestApp/app/b2c/test/browser.spec.ts
@@ -53,6 +53,50 @@ async function enterCredentials(page: puppeteer.Page, testName: string): Promise
     await page.click("#idSIButton9");
 }
 
+async function loginRedirect(page: puppeteer.Page, testName: string): Promise<void> {
+    // Home Page
+    await takeScreenshot(page, testName, `samplePageInit`);
+    // Click Sign In
+    await page.click("#SignIn");
+    await takeScreenshot(page, testName, `signInClicked`);
+    // Click Sign In With Redirect
+    await page.click("#loginRedirect");
+    await page.waitForSelector("#MSIDLAB4_AzureAD");
+    await takeScreenshot(page, testName, "b2cSignInPage");
+    // Select Lab Provider
+    await page.click("#MSIDLAB4_AzureAD");
+
+    // Enter credentials
+    await enterCredentials(page, testName);
+    // Wait for return to page
+    await page.waitForNavigation({ waitUntil: "networkidle0"});
+    await takeScreenshot(page, testName, `samplePageLoggedIn`);
+}
+
+async function loginPopup(page: puppeteer.Page, testName: string): Promise<void> {
+    // Home Page
+    await takeScreenshot(page, testName, `samplePageInit`);
+    // Click Sign In
+    await page.click("#SignIn");
+    await takeScreenshot(page, testName, `signInClicked`);
+    // Click Sign In With Popup
+    const newPopupWindowPromise = new Promise<puppeteer.Page>(resolve => page.once('popup', resolve));
+    await page.click("#loginPopup");
+    const popupPage = await newPopupWindowPromise;
+    const popupWindowClosed = new Promise<void>(resolve => popupPage.once("close", resolve));
+
+    await popupPage.waitForSelector("#MSIDLAB4_AzureAD");
+    await takeScreenshot(popupPage, testName, "b2cSignInPage");
+    // Select Lab Provider
+    await popupPage.click("#MSIDLAB4_AzureAD");
+
+    // Enter credentials
+    await enterCredentials(popupPage, testName);
+    // Wait until popup window closes and see that we are logged in
+    await popupWindowClosed;
+    await takeScreenshot(page, testName, `samplePageLoggedIn`);
+}
+
 describe("Browser tests", function () {
     this.timeout(8000);
     this.retries(1);
@@ -85,54 +129,56 @@ describe("Browser tests", function () {
         await browser.close();
     });
 
-    it("Performs loginRedirect", async () => {
-        const testName = "redirectBaseCase";
-        // Home Page
-        await takeScreenshot(page, testName, `samplePageInit`);
-        // Click Sign In
-        await page.click("#SignIn");
-        await takeScreenshot(page, testName, `signInClicked`);
-        // Click Sign In With Redirect
-        await page.click("#loginRedirect");
-        await page.waitForSelector("#MSIDLAB4_AzureAD");
-        await takeScreenshot(page, testName, "b2cSignInPage");
-        // Select Lab Provider
-        await page.click("#MSIDLAB4_AzureAD");
-
-        // Enter credentials
-        await enterCredentials(page, testName);
-        // Wait for return to page
-        await page.waitForNavigation({ waitUntil: "networkidle0"});
-        await takeScreenshot(page, testName, `samplePageLoggedIn`);
-        const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
-        expect(Object.keys(localStorage).length).to.be.eq(5);
+    describe("Test Login functions", async () => {  
+        it("Performs loginRedirect", async () => {
+            const testName = "redirectBaseCase";
+            await loginRedirect(page, testName);
+            
+            const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+            expect(Object.keys(localStorage).length).to.be.eq(5);
+        });
+        
+        it("Performs loginPopup", async () => {
+            const testName = "popupBaseCase";
+            await loginPopup(page, testName);
+            
+            const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+            expect(Object.keys(localStorage).length).to.be.eq(5);
+        });
     });
 
-    it("Performs loginPopup", async () => {
-        const testName = "popupBaseCase";
-        // Home Page
-        await takeScreenshot(page, testName, `samplePageInit`);
-        // Click Sign In
-        await page.click("#SignIn");
-        await takeScreenshot(page, testName, `signInClicked`);
-        // Click Sign In With Popup
-        const newPopupWindowPromise = new Promise<puppeteer.Page>(resolve => page.once('popup', resolve));
-        await page.click("#loginPopup");
-        const popupPage = await newPopupWindowPromise;
-        const popupWindowClosed = new Promise<void>(resolve => popupPage.once("close", resolve));
+    describe("Test AcquireToken functions", async () => {
+        it("Test acquireTokenRedirect", async () => {
+            const testName = "acquireTokenRedirectBaseCase";
+            await loginPopup(page, testName);
+            await page.click("#getAccessTokenRedirect");
+            await page.waitForSelector("#access-token-info");
+            await takeScreenshot(page, testName, "accessTokenAcquired");
 
-        await popupPage.waitForSelector("#MSIDLAB4_AzureAD");
-        await takeScreenshot(popupPage, testName, "b2cSignInPage");
-        // Select Lab Provider
-        await popupPage.click("#MSIDLAB4_AzureAD");
+            const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+            expect(Object.keys(localStorage).length).to.be.eq(6);
+        }); 
 
-        // Enter credentials
-        await enterCredentials(popupPage, testName);
-        // Wait until popup window closes and see that we are logged in
-        await popupWindowClosed;
-        await takeScreenshot(page, testName, `samplePageLoggedIn`);
-        const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
-        expect(Object.keys(localStorage).length).to.be.eq(5);
-        expect(popupPage.isClosed()).to.be.true;
+        it("Test acquireTokenPopup", async () => {
+            const testName = "acquireTokenPopupBaseCase";
+            await loginPopup(page, testName);
+            await page.click("#getAccessTokenPopup");
+            await page.waitForSelector("#access-token-info");
+            await takeScreenshot(page, testName, "accessTokenAcquired");
+
+            const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+            expect(Object.keys(localStorage).length).to.be.eq(6);
+        }); 
+
+        it("Test acquireTokenSilent", async () => {
+            const testName = "acquireTokenSilentBaseCase";
+            await loginPopup(page, testName);
+            await page.click("#getAccessTokenSilent");
+            await page.waitForSelector("#access-token-info");
+            await takeScreenshot(page, testName, "accessTokenAcquired");
+
+            const localStorage = await page.evaluate(() =>  Object.assign({}, window.localStorage));
+            expect(Object.keys(localStorage).length).to.be.eq(6);
+        }); 
     });
 });
