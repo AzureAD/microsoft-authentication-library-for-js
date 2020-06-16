09000ca9fa8a22b9dc0a1a70a4af03b17f8b265d
ahnmelis <t-meahn@microsoft.com>
2020-06-15 17:26:28 -0700

    copied over module

--- lib/msal-react-native-android-poc/MSALModule/android/src/main/java/com/reactlibrary/MSALModule.java
+++ lib/msal-react-native-android-poc/MSALModule/android/src/main/java/com/reactlibrary/MSALModule.java
@@ -1,3 +1,10 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+/**
+ * MSALModule.java functions as a wrapper around MSAL for Android's signing in, signing out, and acquiring tokens functionality.
+ * It is a proof of concept, focusing on single account mode and AAD.
+ */
+
 package com.reactlibrary;
 
 import com.facebook.react.bridge.ReactApplicationContext;
@@ -8,20 +15,97 @@ import com.facebook.react.bridge.Callback;
 public class MSALModule extends ReactContextBaseJavaModule {
 
     private final ReactApplicationContext reactContext;
+    private ISingleAccountPublicClientApplication IPublicClientApplication;
+    private IAccount mAccount;
 
     public MSALModule(ReactApplicationContext reactContext) {
         super(reactContext);
         this.reactContext = reactContext;
+
+        try{
+            //initialize IPublicClientApplication with config file, located in main/java/res/raw/auth_config_single_account.json
+            IPublicClientApplication = PublicClientApplication.createSingleAccountPublicClientApplication(
+                reactContext,
+                reactContext.getResources().getIdentifier("auth_config_single_account", "raw", reactContext.getPackageName()));
+        } catch (Exception e) {
+            //will handle this
+            Log.d(TAG, "Something wrong with initialization of IPublicClientApplication: " + e.toString());
+        }
     }
 
+     /**
+   * getName(): RNMsalPoc is the name used when importing the module from native modules
+   */
     @Override
     public String getName() {
         return "MSAL";
     }
 
+    /**
+     * SignIn: calls the signIn method of ISingleAccountPublicClientApplication.
+     * Parameters: string scopesValue (string containing scopes separated by a " "), Promise promise (returned to an async function)
+     */
     @ReactMethod
-    public void sampleMethod(String stringArgument, int numberArgument, Callback callback) {
-        // TODO: Implement some actually useful functionality
-        callback.invoke("Received numberArgument: " + numberArgument + " stringArgument: " + stringArgument);
+    public void signIn(String scopesValue, Promise promise) {
+        if (!scopesValue.isEmpty()) {
+            IPublicClientApplication.signIn(getCurrentActivity(), null, scopesValue.toLowerCase().split(" "), getLoginCallback(promise));
+        } else {
+            //using the code, message parameters; will change code in future when we know what that is
+            promise.reject("scopescode", "Scopes is empty.");
+        }
+    }
+
+    /**
+     * getLoginCallback should log messages of a successful authentication, update the current account, and then
+     * Based on getAuthCallback()
+     */
+
+    private AuthenticationCallback getLoginCallback(final Promise promise) {
+        return new AuthenticationCallback() {
+
+            @Override
+            public void onSuccess(IAuthenticationResult authenticationResult) {
+                Log.d(TAG, "Successfully authenticated");
+
+            //update account
+                Log.d(TAG, "Account: " + authenticationResult.getAccount().getUsername());
+                mAccount = authenticationResult.getAccount();
+                promise.resolve(mapMSALResult(authenticationResult));
+            }
+
+            @Override
+            public void onError(MsalException exception) {
+                /* Failed to acquireToken */
+                Log.d(TAG, "Authentication failed: " + exception.toString());
+                promise.reject("Authentication failed: " + exception.toString(), exception);
+            }
+
+            @Override
+            public void onCancel() {
+                /* User canceled the authentication */
+                Log.d(TAG, "User cancelled login.");
+                promise.reject("userCancel", "User cancelled login.");
+            }
+        };
+    }
+
+    /*
+    * Helper functions:
+    * mapMSALResult(IAuthenticationResult result): used to convert AuthenticationResult into a map
+    * so that it can be passed through a promise on the JS side
+    * */
+
+    private WritableNativeMap mapMSALResult (IAuthenticationResult result) {
+        WritableNativeMap map = new WritableNativeMap();
+        //add attributes from account
+        map.putString("authority", result.getAccount().getAuthority());
+        map.putString("accountId", result.getAccount().getId());
+        map.putString("tenantId", result.getAccount().getTenantId());
+        map.putString("username", result.getAccount().getUsername());
+        map.putString("idToken", (String) result.getAccount().getClaims().get("id_token"));
+        //add attributes from result
+        //access token?
+
+        return map;
     }
 }
