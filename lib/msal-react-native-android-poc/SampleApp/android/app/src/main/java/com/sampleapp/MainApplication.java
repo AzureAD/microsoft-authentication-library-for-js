41893006a116d38551d069f982e986b9a40580cc
ahnmelis <t-meahn@microsoft.com>
2020-06-17 15:51:29 -0700

    reinit app with getAccount functionality

--- lib/msal-react-native-android-poc/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
+++ lib/msal-react-native-android-poc/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -11,7 +11,6 @@ import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
-
 public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost =
