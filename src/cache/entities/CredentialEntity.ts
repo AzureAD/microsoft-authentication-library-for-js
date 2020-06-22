60315c2c634a91997cd969a91e63c2e1d1a79f65
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-06-22 13:10:00 -0700

    Fixing merge issues, naming

--- src/cache/entities/CredentialEntity.ts
+++ src/cache/entities/CredentialEntity.ts
@@ -98,11 +98,11 @@ export class CredentialEntity {
      * @param key
      */
     static getCredentialType(key: string): string {
-        if (key.indexOf(CredentialType.ACCESS_TOKEN) !== -1) {
+        if (key.indexOf(CredentialType.ACCESS_TOKEN.toLowerCase()) !== -1) {
             return CredentialType.ACCESS_TOKEN;
-        } else if (key.indexOf(CredentialType.ID_TOKEN) !== -1) {
+        } else if (key.indexOf(CredentialType.ID_TOKEN.toLowerCase()) !== -1) {
             return CredentialType.ID_TOKEN;
-        } else if (key.indexOf(CredentialType.REFRESH_TOKEN) !== -1) {
+        } else if (key.indexOf(CredentialType.REFRESH_TOKEN.toLowerCase()) !== -1) {
             return CredentialType.REFRESH_TOKEN;
         }
 
