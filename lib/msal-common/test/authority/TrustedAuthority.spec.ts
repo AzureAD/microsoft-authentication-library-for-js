91cb96ad41284583ac4d5d741ce4eddfeb18e579
Thomas Norling <thomas.l.norling@gmail.com>
2020-06-23 17:53:31 -0700

    Fix tests

--- lib/msal-common/test/authority/TrustedAuthority.spec.ts
+++ lib/msal-common/test/authority/TrustedAuthority.spec.ts
@@ -14,6 +14,8 @@ describe("TrustedAuthority.ts Class", function () {
         aliases: ["fabrikamb2c.b2clogin.com"]
     }];
 
+    const fakeCloudDiscoveryResponse = JSON.stringify({tenant_discovery_endpoint: "not relevant", metadata: cloudDiscoveryMetadata})
+
     afterEach(function() {
         sinon.restore();
     });
@@ -21,11 +23,11 @@ describe("TrustedAuthority.ts Class", function () {
     describe("setTrustedAuthoritiesFromConfig", () => {
         it("Sets TrustedHostList with knownAuthorities", () => {
             sinon.stub(TrustedAuthority, "getTrustedHostList").returns([]);
-            TrustedAuthority.setTrustedAuthoritiesFromConfig(knownAuthorities, []);
+            TrustedAuthority.setTrustedAuthoritiesFromConfig(knownAuthorities, "");
 
             expect(TrustedAuthority.IsInTrustedHostList(knownAuthorities[0])).to.be.true;
 
-            const savedcloudDiscoveryMetadata = TrustedAuthority.getcloudDiscoveryMetadata(knownAuthorities[0]);
+            const savedcloudDiscoveryMetadata = TrustedAuthority.getCloudDiscoveryMetadata(knownAuthorities[0]);
             savedcloudDiscoveryMetadata.aliases.forEach((alias) => {
                 expect(alias).to.eq(knownAuthorities[0]);
             });
@@ -35,19 +37,21 @@ describe("TrustedAuthority.ts Class", function () {
 
         it("Sets TrustedHostList with cloudDiscoveryMetadata", () => {
             sinon.stub(TrustedAuthority, "getTrustedHostList").returns([]);
-            TrustedAuthority.setTrustedAuthoritiesFromConfig([], cloudDiscoveryMetadata);
+            TrustedAuthority.setTrustedAuthoritiesFromConfig([], fakeCloudDiscoveryResponse);
 
             expect(TrustedAuthority.IsInTrustedHostList(knownAuthorities[0])).to.be.true;
 
-            const savedcloudDiscoveryMetadata = TrustedAuthority.getcloudDiscoveryMetadata(knownAuthorities[0]);
-            expect(savedcloudDiscoveryMetadata.aliases).to.eq(cloudDiscoveryMetadata[0].aliases);
+            const savedcloudDiscoveryMetadata = TrustedAuthority.getCloudDiscoveryMetadata(knownAuthorities[0]);
+            savedcloudDiscoveryMetadata.aliases.forEach(alias => {
+                expect(cloudDiscoveryMetadata[0].aliases).to.include(alias);
+            });
             expect(savedcloudDiscoveryMetadata.preferred_cache).to.eq(cloudDiscoveryMetadata[0].preferred_cache);
             expect(savedcloudDiscoveryMetadata.preferred_network).to.eq(cloudDiscoveryMetadata[0].preferred_network);
         });
 
         it("Do not add additional authorities to trusted host list if it has already been populated", () => {
             sinon.stub(TrustedAuthority, "getTrustedHostList").returns(["login.microsoftonline.com"]);
-            TrustedAuthority.setTrustedAuthoritiesFromConfig(["contoso.b2clogin.com"], []);
+            TrustedAuthority.setTrustedAuthoritiesFromConfig(["contoso.b2clogin.com"], "");
 
             expect(TrustedAuthority.IsInTrustedHostList("contoso.b2clogin.com")).to.be.false;
         });
@@ -56,11 +60,11 @@ describe("TrustedAuthority.ts Class", function () {
             sinon.stub(TrustedAuthority, "getTrustedHostList").returns([]);
             
             try {
-                TrustedAuthority.setTrustedAuthoritiesFromConfig(knownAuthorities, cloudDiscoveryMetadata);
+                TrustedAuthority.setTrustedAuthoritiesFromConfig(knownAuthorities, fakeCloudDiscoveryResponse);
             } catch (e) {
                 expect(e).to.be.instanceOf(ClientConfigurationError);
-                expect(e.errorCode).to.eq(ClientConfigurationErrorMessage.knownAuthoritiesAndcloudDiscoveryMetadata.code);
-                expect(e.errorMessage).to.eq(ClientConfigurationErrorMessage.knownAuthoritiesAndcloudDiscoveryMetadata.desc);
+                expect(e.errorCode).to.eq(ClientConfigurationErrorMessage.knownAuthoritiesAndCloudDiscoveryMetadata.code);
+                expect(e.errorMessage).to.eq(ClientConfigurationErrorMessage.knownAuthoritiesAndCloudDiscoveryMetadata.desc);
                 done();
             }
         });
@@ -73,14 +77,14 @@ describe("TrustedAuthority.ts Class", function () {
                 preferred_network: "test.b2clogin.com",
                 aliases: ["test.b2clogin.com", "test2.b2clogin.com"]
             }];
-            TrustedAuthority.savecloudDiscoveryMetadata(testMetadata);
+            TrustedAuthority.saveCloudDiscoveryMetadata(testMetadata);
 
             let savedcloudDiscoveryMetadata;
 
             testMetadata[0].aliases.forEach(alias => {
                 expect(TrustedAuthority.IsInTrustedHostList(alias)).to.be.true;
 
-                savedcloudDiscoveryMetadata = TrustedAuthority.getcloudDiscoveryMetadata(alias);
+                savedcloudDiscoveryMetadata = TrustedAuthority.getCloudDiscoveryMetadata(alias);
                 expect(savedcloudDiscoveryMetadata.aliases).to.eq(testMetadata[0].aliases);
                 expect(savedcloudDiscoveryMetadata.preferred_cache).to.eq(testMetadata[0].preferred_cache);
                 expect(savedcloudDiscoveryMetadata.preferred_network).to.eq(testMetadata[0].preferred_network);
@@ -98,14 +102,14 @@ describe("TrustedAuthority.ts Class", function () {
                 preferred_network: "test.b2clogin.com",
                 aliases: ["test4.b2clogin.com"]
             }];
-            TrustedAuthority.savecloudDiscoveryMetadata(testMetadata);
+            TrustedAuthority.saveCloudDiscoveryMetadata(testMetadata);
 
             let savedcloudDiscoveryMetadata;
 
             testMetadata.forEach(instance => {
                 expect(TrustedAuthority.IsInTrustedHostList(instance.aliases[0])).to.be.true;
 
-                savedcloudDiscoveryMetadata = TrustedAuthority.getcloudDiscoveryMetadata(instance.aliases[0]);
+                savedcloudDiscoveryMetadata = TrustedAuthority.getCloudDiscoveryMetadata(instance.aliases[0]);
                 expect(savedcloudDiscoveryMetadata.aliases).to.eq(instance.aliases);
                 expect(savedcloudDiscoveryMetadata.preferred_cache).to.eq(instance.preferred_cache);
                 expect(savedcloudDiscoveryMetadata.preferred_network).to.eq(instance.preferred_network);
@@ -114,11 +118,11 @@ describe("TrustedAuthority.ts Class", function () {
 
         it("createcloudDiscoveryMetadataFromKnownAuthorities creates metadata object for each host passed in", () => {
             const testAuthorities = ["contoso.b2clogin.com"]
-            TrustedAuthority.createcloudDiscoveryMetadataFromKnownAuthorities(testAuthorities);
+            TrustedAuthority.createCloudDiscoveryMetadataFromKnownAuthorities(testAuthorities);
 
             expect(TrustedAuthority.IsInTrustedHostList(testAuthorities[0])).to.be.true;
 
-            const savedcloudDiscoveryMetadata = TrustedAuthority.getcloudDiscoveryMetadata(testAuthorities[0]);
+            const savedcloudDiscoveryMetadata = TrustedAuthority.getCloudDiscoveryMetadata(testAuthorities[0]);
             savedcloudDiscoveryMetadata.aliases.forEach((alias) => {
                 expect(alias).to.eq(testAuthorities[0]);
             });
