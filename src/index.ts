952d87f75ffa65552619fdf61fcde09ed014020f
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-06-24 11:21:22 -0700

    Updating code before bug bash

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,8 @@ export type { AuthCallback } from "./types/AuthCallback";
 
 // Common Object Formats
 export {
+    // Account
+    AccountInfo,
     // Request
     AuthorizationUrlRequest,
     SilentFlowRequest,
