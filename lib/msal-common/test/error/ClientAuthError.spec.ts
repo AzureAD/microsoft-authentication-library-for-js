79ea69fc82383c5aa1bbd77513ed4f2c248de8eb
Jo Arroyo <joliuac@gmail.com>
2020-06-24 10:45:07 -0700

    Add test for invalidCacheType

--- lib/msal-common/test/error/ClientAuthError.spec.ts
+++ lib/msal-common/test/error/ClientAuthError.spec.ts
@@ -304,4 +304,17 @@ describe("ClientAuthError.ts Class Unit Tests", () => {
         expect(err.name).to.equal("ClientAuthError");
         expect(err.stack).to.include("ClientAuthError.spec.ts");
     });
+
+    it("createInvalidCacheTypeError creates a ClientAuthError object", () => {
+        const err: ClientAuthError = ClientAuthError.createInvalidCacheTypeError();
+
+        expect(err instanceof ClientAuthError).to.be.true;
+        expect(err instanceof AuthError).to.be.true;
+        expect(err instanceof Error).to.be.true;
+        expect(err.errorCode).to.equal(ClientAuthErrorMessage.invalidCacheType.code);
+        expect(err.errorMessage).to.include(ClientAuthErrorMessage.invalidCacheType.desc);
+        expect(err.message).to.include(ClientAuthErrorMessage.invalidCacheType.desc);
+        expect(err.name).to.equal("ClientAuthError");
+        expect(err.stack).to.include("ClientAuthError.spec.ts");
+    });
 });
