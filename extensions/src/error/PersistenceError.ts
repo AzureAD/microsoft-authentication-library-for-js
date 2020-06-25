c6aed94d03c8daf2475ba87f690ac42e52330b75
Santiago Gonzalez <sagonzal@microsoft.com>
2020-06-24 18:22:57 -0700

    Add error and index.ts

---
+++ extensions/src/error/PersistenceError.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * Error thrown when trying to write MSAL cache to persistence.
+ */
+export class PersistenceError extends Error {
+
+    // Short string denoting error
+    errorCode: string;
+    // Detailed description of error
+    errorMessage: string;
+
+    constructor(errorCode: string, errorMessage: string) {
+        const errorString = errorMessage ? `${errorCode}: ${errorMessage}` : errorCode;
+        super(errorString);
+        Object.setPrototypeOf(this, PersistenceError.prototype);
+
+        this.errorCode = errorCode;
+        this.errorMessage = errorMessage;
+        this.name = "PersistenceError";
+    }
+
+    static createFileSystemError(errorCode: string, errorMessage: string): PersistenceError {
+        return new PersistenceError(errorCode, errorMessage);
+    }
+
+    static createLibSecretError(errorCode: string, errorMessage: string): PersistenceError {
+        const updatedErrorMessage = `Error accessing Gnome Keyring: ${errorCode}- ${errorMessage}`;
+        return new PersistenceError("GnomeKeyringError", updatedErrorMessage);
+    }
+
+    static createKeychainPersistenceError(errorCode: string, errorMessage: string): PersistenceError {
+        const updatedErrorMessage = `Error accessing Keychain: ${errorCode}- ${errorMessage}`;
+        return new PersistenceError("KeychainError", updatedErrorMessage);
+    }
+
+    static createFilePersistenceWithDPAPIError(errorCode: string,errorMessage: string): PersistenceError {
+        const updatedErrorMessage = `Error accessing DPAPI encrypted file: ${errorCode}- ${errorMessage}`;
+        return new PersistenceError("DPAPIEncryptedFileError", updatedErrorMessage);
+    }
+
+    static createCrossPlatformLockError(errorCode: string, errorMessage: string): PersistenceError {
+        const updatedErrorMessage = `Error acquiring lock: ${errorCode}- ${errorMessage}`;
+        return new PersistenceError("CrossPlatformLockError", updatedErrorMessage);
+    }
+}
