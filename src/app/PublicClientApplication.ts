9dc71c2372e9d7c2ba269a51ada0ea249869a030
Prithvi Kanherkar <prithvi.kanherkar@gmail.com>
2020-06-29 14:25:18 -0700

    Revert "Fixing silent request error cleanup"
    This reverts commit cfaabe5b83af7c493694263e4a12c51f5becee50.

--- src/app/PublicClientApplication.ts
+++ src/app/PublicClientApplication.ts
@@ -448,7 +448,6 @@ export class PublicClientApplication {
             // Handle response from hash string.
             return await silentHandler.handleCodeResponse(hash);
         } catch (e) {
-            this.browserStorage.cleanRequest();
             throw e;
         }
     }
