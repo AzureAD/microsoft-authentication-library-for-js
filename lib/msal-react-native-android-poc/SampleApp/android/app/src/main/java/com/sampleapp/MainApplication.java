a81daea7ccf3ae646e208aae3aca5ca66d4528ea
ahnmelis <t-meahn@microsoft.com>
2020-06-29 16:01:34 -0700

    deleted line from mainapplication, edited app.js

--- lib/msal-react-native-android-poc/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
+++ lib/msal-react-native-android-poc/SampleApp/android/app/src/main/java/com/sampleapp/MainApplication.java
@@ -26,7 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new MSALPackage());
           return packages;
         }
 
