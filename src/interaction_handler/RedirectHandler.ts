e46bb9022155ae99b6a235da4d40d6cf0bb28d1e
Thomas Norling <thomas.l.norling@gmail.com>
2020-06-30 16:52:27 -0700

    Move caching of startPage to acquireTokenRedirect

--- src/interaction_handler/RedirectHandler.ts
+++ src/interaction_handler/RedirectHandler.ts
@@ -18,12 +18,6 @@ export class RedirectHandler extends InteractionHandler {
         // Navigate if valid URL
         if (!StringUtils.isEmpty(requestUrl)) {
             // Set interaction status in the library.
-            const loginStartPage = authCodeRequest.redirectStartPage || window.location.href;
-            this.browserStorage.setItem(
-                this.browserStorage.generateCacheKey(TemporaryCacheKeys.ORIGIN_URI), 
-                loginStartPage, 
-                CacheSchemaType.TEMPORARY
-            );
             this.browserStorage.setItem(this.browserStorage.generateCacheKey(BrowserConstants.INTERACTION_STATUS_KEY), BrowserConstants.INTERACTION_IN_PROGRESS_VALUE, CacheSchemaType.TEMPORARY);
             this.browserStorage.cacheCodeRequest(authCodeRequest, browserCrypto);
             this.authModule.logger.infoPii("Navigate to:" + requestUrl);
