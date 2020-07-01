baee171933435b92a8e7230da2194f8370877ad6
ahnmelis <t-meahn@microsoft.com>
2020-06-30 20:38:41 -0700

    removed local module files for testing

--- samples/msal-react-native-android-poc-sample/android/app/src/main/java/com/sampleapp/Constants.java
+++
@@ -1,9 +0,0 @@
-//Constants class which will be imported by MSALModule
-package com.sampleapp;
-
-public final class Constants {
-
-    private Constants() {}
-
-    public static final String[] DEFAULT_SCOPES = {"User.Read"};
-}
