051ed05df632cd446afe47b9ea72fd157c87f93b
Jason Nutter <janutter@microsoft.com>
2020-07-02 13:52:47 -0700

    Instantiate Logger instance for PublicClientApplication

--- src/config/Configuration.ts
+++ src/config/Configuration.ts
@@ -82,25 +82,7 @@ const DEFAULT_CACHE_OPTIONS: CacheOptions = {
 
 // Default logger options for browser
 const DEFAULT_LOGGER_OPTIONS: LoggerOptions = {
-    loggerCallback: (level: LogLevel, message: string, containsPii: boolean): void => {
-        if (containsPii) {
-            return;
-        }
-        switch (level) {
-            case LogLevel.Error:
-                console.error(message);
-                return;
-            case LogLevel.Info:
-                console.info(message);
-                return;
-            case LogLevel.Verbose:
-                console.debug(message);
-                return;
-            case LogLevel.Warning:
-                console.warn(message);
-                return;
-        }
-    },
+    loggerCallback: (level: LogLevel, message: string, containsPii: boolean): void => {},
     piiLoggingEnabled: false
 };
 
