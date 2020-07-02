5fbea2d5ff1d8e736cd78d84490d307df63cf35a
Jo Arroyo <joliuac@gmail.com>
2020-07-02 10:57:36 -0700

    Additional type comments for msal-common

--- src/request/SilentFlowRequest.ts
+++ src/request/SilentFlowRequest.ts
@@ -8,9 +8,12 @@ import { BaseAuthRequest } from "./BaseAuthRequest";
 
 /**
  * SilentFlow parameters passed by the user to retrieve credentials silently
- * - account:               Account entity to lookup the credentials
- * - forceRefresh:          Forces silent requests to make network calls if true
- * - correlationId:         GUID set by the user to trace the request
+ * - scopes                 - Scopes the application is requesting access to
+ * - authority              - Url of the authority which the application acquires tokens from
+ * - account                - Account entity to lookup the credentials
+ * - forceRefresh           - Forces silent requests to make network calls if true
+ * - correlationId          - GUID set by the user to trace the request
+ * - redirectUri            - The redirect URI where authentication responses can be received by your application. It must exactly match one of the redirect URIs registered in the Azure portal.
  */
 export type SilentFlowRequest = BaseAuthRequest & {
     account: AccountInfo;
