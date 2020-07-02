528b8aef6bb54a93201e7e0768256a97bb51ed0f
technical-boy <hectormgdev@gmail.com>
2020-07-02 09:36:25 -0700

    Add auth code fallback to silent flow sample

--- samples/msal-node-samples/msal-node-silent-flow/index.js
+++ samples/msal-node-samples/msal-node-silent-flow/index.js
@@ -8,6 +8,7 @@ const { promises: fs } = require("fs");
 
 const SERVER_PORT = process.env.PORT || 3000;
 
+// Cache Plugin Configuration
 const readFromStorage = () => {
     return fs.readFile("./data/cache.json", "utf-8");
 };
@@ -24,6 +25,7 @@ const cachePlugin = {
     writeToStorage
 };
 
+// Public Client Application Configuration
 const publicClientConfig = {
     auth: {
         clientId: "99cab759-2aab-420b-91d8-5e3d8d4f063b",
@@ -34,12 +36,22 @@ const publicClientConfig = {
         cachePlugin
     },
 };
+
+// Acquire Token Request Configuration
+const scopes = ["user.read"];
+
+const authCodeUrlParameters = {
+    scopes: scopes,
+    redirectUri: ["http://localhost:3000/redirect"],
+};
+
 const pca = new msal.PublicClientApplication(publicClientConfig);
 const msalCacheManager = pca.getCacheManager();
 let accounts;
 
 // Create Express App and Routes
 const app = express();
+
 
 app.get('/', (req, res) => {
 
@@ -50,20 +62,53 @@ app.get('/', (req, res) => {
     // Build silent request
     const silentRequest = {
         account: accounts[1],
-        redirectUri: "http://localhost:3000/redirect",
+        scopes: scopes,
     };
 
-    // get url to sign user in and consent to scopes needed for application
+    // Acquire Token Silent Request
+    console.log("Attempting silent token acquisition.");
     pca.acquireTokenSilent(silentRequest)
         .then((response) => {
-            console.log("\nResponse: \n:", response);
-            res.send(200);
+            // Successful silent request
+            console.log("\nSuccessful silent token acquisition:\nResponse: \n:", response);
+            res.sendStatus(200);
             return msalCacheManager.writeToPersistence();
         })
         .catch((error) => {
-            console.log(error);
-            res.status(500).send(error);
+            // No access tokens in cache
+            if(error.errorCode === "no_tokens_found") {
+                console.log("No tokens in cache, falling back to auth code.");
+                // get url to sign user in and consent to scopes needed for application
+                pca.getAuthCodeUrl(authCodeUrlParameters)
+                    .then((response) => {
+                        console.log(response);
+                        res.redirect(response);
+                })
+                .catch((error) => console.log(JSON.stringify(error)));
+            } else {
+                // Other errors
+                console.log(error);
+                res.status(500).send(error);
+            }
         });
+});
+
+// Second leg of Auth Code grant
+app.get('/redirect', (req, res) => {
+    const tokenRequest = {
+        code: req.query.code,
+        redirectUri: "http://localhost:3000/redirect",
+        scopes: scopes,
+    };
+
+    pca.acquireTokenByCode(tokenRequest).then((response) => {
+        console.log("\nResponse: \n:", response);
+        res.sendStatus(200);
+        return msalCacheManager.writeToPersistence();
+    }).catch((error) => {
+        console.log(error);
+        res.status(500).send(error);
+    });
 });
 
 msalCacheManager.readFromPersistence().then(() => {
