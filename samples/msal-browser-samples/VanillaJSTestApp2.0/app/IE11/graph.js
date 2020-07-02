cadd59dfe33bea736db18a63f73768ce8f8eff87
Thomas Norling <thomas.l.norling@gmail.com>
2020-07-02 09:52:48 -0700

    Initial IE11 sample

---
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/IE11/graph.js
@@ -0,0 +1,33 @@
+// Helper function to call MS Graph API endpoint 
+// using authorization bearer token scheme
+function callMSGraph(endpoint, accessToken, callback) {
+    const headers = new Headers();
+    const bearer = "Bearer " + accessToken;
+
+    headers.append("Authorization", bearer);
+
+    const options = {
+        method: "GET",
+        headers: headers
+    };
+
+    console.log('request made to Graph API at: ' + new Date().toString());
+
+    fetch(endpoint, options)
+        .then(function (response){ response.json() })
+        .then(function (response) { callback(response) })
+        .catch(function (error) { console.log(error) });
+}
+
+function seeProfileRedirect() {
+    const currentAcc = myMSALObj.getAccountByUsername(username);
+    if (currentAcc) {
+        getTokenRedirect(loginRequest, currentAcc).then(function (response) {
+            // Will only execute if token was acquired silently
+            callMSGraph(graphConfig.graphMeEndpoint, response.accessToken, updateUI);
+            profileButton.style.display = 'none';
+        }).catch(function (error) {
+            console.log(error);
+        });
+    }
+}
