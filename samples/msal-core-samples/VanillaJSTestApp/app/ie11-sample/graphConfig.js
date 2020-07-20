94414d4c0bf152020fb196d0b42dead27f96b052
dogan.erisen@gmail.com <dogan.erisen@gmail.com>
2020-07-20 10:29:10 -0700

    fix commit mistake

--- samples/msal-core-samples/VanillaJSTestApp/app/ie11-sample/graphConfig.js
+++
@@ -1,5 +0,0 @@
-// Add here the endpoints for MS Graph API services you would like to use.
-var graphConfig = {
-    graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
-    graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages"
-};
