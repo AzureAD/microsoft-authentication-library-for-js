ac24cf935e576af6a8e92e4c5b5348f1874458e3
Thomas Norling <thomas.l.norling@gmail.com>
2020-07-24 17:06:04 -0700

    Add customizable sample

---
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/ui.js
@@ -0,0 +1,21 @@
+// Select DOM elements to work with
+const welcomeDiv = document.getElementById("WelcomeMessage");
+const signInButton = document.getElementById("SignIn");
+const cardDiv = document.getElementById("card-div");
+const profileDiv = document.getElementById("profile-div");
+
+function showWelcomeMessage(account) {
+    // Reconfiguring DOM elements
+    cardDiv.style.display = 'initial';
+    welcomeDiv.innerHTML = `Welcome ${username}`;
+    signInButton.nextElementSibling.style.display = 'none';
+    signInButton.setAttribute("onclick", "signOut();");
+    signInButton.setAttribute('class', "btn btn-success")
+    signInButton.innerHTML = "Sign Out";
+}
+
+function updateUI(data) {
+    const scopes = document.createElement('p');
+    scopes.innerHTML = "<strong>Access Token Acquired for Scopes: </strong>" + data.scopes.join(" ");
+    profileDiv.appendChild(scopes);
+}
