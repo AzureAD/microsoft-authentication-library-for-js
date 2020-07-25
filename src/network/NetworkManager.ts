c35634c2d4854f83d35ce8482f3648b5d541ebe0
Thomas Norling <thomas.l.norling@gmail.com>
2020-07-24 17:18:24 -0700

    Clean up unused changes

--- src/network/NetworkManager.ts
+++ src/network/NetworkManager.ts
@@ -2,7 +2,6 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
-import { INetworkModule, NetworkRequestOptions } from "./INetworkModule";
 
 export type NetworkResponse<T> = {
     headers: Map<string, string>;
@@ -10,21 +9,5 @@ export type NetworkResponse<T> = {
     status: number;
 };
 
-export class NetworkManager {
-    private networkClient: INetworkModule;
-
-    constructor(networkClient: INetworkModule) {
-        this.networkClient = networkClient;
-    }
-
-    /**
-     * Wraps sendPostRequestAsync with necessary preflight and postflight logic
-     * @param tokenEndpoint
-     * @param options
-     */
-    async sendPostRequest<T>(tokenEndpoint: string, options: NetworkRequestOptions): Promise<NetworkResponse<T>> {
-        const response = await this.networkClient.sendPostRequestAsync<T>(tokenEndpoint, options);
-
-        return response;
-    }
-}
+// TODO placeholder: this will be filled in by the throttling PR
+export class NetworkManager {}
