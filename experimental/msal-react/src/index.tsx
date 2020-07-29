fa610aad6c9937d34b0e7dea24833e7503b851ba
Jason Nutter <janutter@microsoft.com>
2020-07-29 11:27:54 -0700

    Initial tsdx bootstrap

---
+++ experimental/msal-react/src/index.tsx
@@ -0,0 +1,11 @@
+import React, { FC, HTMLAttributes, ReactChild } from 'react';
+
+export interface Props extends HTMLAttributes<HTMLDivElement> {
+    children?: ReactChild;
+}
+
+// Please do not use types off of a default export module or else Storybook Docs will suffer.
+// see: https://github.com/storybookjs/storybook/issues/9556
+export const Thing: FC<Props> = ({ children }) => {
+    return <div>{children || `the snozzberries taste like snozzberries`}</div>;
+};
