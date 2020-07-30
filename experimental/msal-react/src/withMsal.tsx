b6879c323284b4a44477d2d1a636764ced05f81a
Andrew Craswell <ajcraswell@gmail.com>
2020-07-30 15:38:29 -0700

    Improve typescript typings of withMsal HoC

--- experimental/msal-react/src/withMsal.tsx
+++ experimental/msal-react/src/withMsal.tsx
@@ -3,16 +3,28 @@ import React from 'react';
 import { IMsalContext } from './MsalContext';
 import { useMsal } from './MsalProvider';
 
+// Utility types
+// Reference: https://github.com/piotrwitek/utility-types
+type SetDifference<A, B> = A extends B ? never : A;
+type SetComplement<A, A1 extends A> = SetDifference<A, A1>;
+type Subtract<T extends T1, T1 extends object> = Pick<
+    T,
+    SetComplement<keyof T, keyof T1>
+>;
+
 export interface IWithMsalProps {
     msalContext: IMsalContext;
 }
 
-export const withMsal = <P extends IWithMsalProps = IWithMsalProps>(
+export const withMsal = <P extends IWithMsalProps>(
     Component: React.ComponentType<P>
 ) => {
-    const ComponentWithMsal: React.FunctionComponent<P> = props => {
+    const ComponentWithMsal: React.FunctionComponent<Subtract<
+        P,
+        IWithMsalProps
+    >> = props => {
         const msal = useMsal();
-        return <Component {...props} msalContext={msal} />;
+        return <Component {...(props as P)} msalContext={msal} />;
     };
 
     const componentName =
