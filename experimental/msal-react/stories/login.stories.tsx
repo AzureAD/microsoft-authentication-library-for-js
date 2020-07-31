678741e52600b9d0bc0f3619e81947138332a0dd
Andrew Craswell <ajcraswell@gmail.com>
2020-07-30 22:03:30 -0700

    Update the login stories with a logout scenario

--- experimental/msal-react/stories/login.stories.tsx
+++ experimental/msal-react/stories/login.stories.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { MsalProvider, MsalConsumer, useMsal } from '../src';
+import { MsalProvider, MsalConsumer, useMsal, useIsAuthenticated, AuthenticatedTemplate, UnauthenticatedTemplate } from '../src';
 
 import { msalInstance } from './msalInstance';
 
@@ -13,23 +13,48 @@ export const LoginPopup = () => (
     </MsalProvider>
 );
 
+export const Logout = () => (
+    <MsalProvider instance={msalInstance}>
+        <UnauthenticatedTemplate>
+            <p>You must be logged in to be able to logout.</p>
+        </UnauthenticatedTemplate>
+        <LogoutExample />
+    </MsalProvider>
+);
+
+
 const PopupExample = () => {
     const { instance, state } = useMsal();
-    const accounts = state.accounts;
-    const isAuthenticated = accounts.length > 0;
 
-    if (isAuthenticated) {
-        return (
-            <React.Fragment>
+    const accounts = state.accounts;
+
+    return (
+        <React.Fragment>
+            <AuthenticatedTemplate>
                 <p>Accounts: {accounts.map(a => a.username).join(', ')}</p>
                 <button onClick={() => instance.logout()}>Logout</button>
-            </React.Fragment>
-        );
-    } else {
-        return (
-            <React.Fragment>
+            </AuthenticatedTemplate>
+            <UnauthenticatedTemplate>
                 <button onClick={() => instance.loginPopup({ scopes: [] })}>Login</button>
-            </React.Fragment>
-        );
-    }
+            </UnauthenticatedTemplate>
+        </React.Fragment>
+    );
+};
+
+
+const LogoutExample = () => {
+    const { instance, state } = useMsal();
+    
+    const accounts = state.accounts;
+
+    return (
+        <React.Fragment>
+            {accounts.map((account) => (
+                <React.Fragment key={account.homeAccountId}>
+                    <span>{account.username}</span>
+                    <button onClick={() => instance.logout({ account })} style={{ marginLeft: 20 }}>Logout</button>
+                </React.Fragment>
+            ))}
+        </React.Fragment>
+    );
 };
