f0e733a693802fdfb75821d49fb37e2bd8b47612
HectorMg <hectormgdev@gmail.com>
2020-07-31 12:16:40 -0700

    De-couple auth code sample configuration from application code

--- samples/msal-node-samples/auth-code/config/clientApplication.js
+++ samples/msal-node-samples/auth-code/config/clientApplication.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+require('dotenv').config({ path: './AAD.env' });
 
 const msal = require('@azure/msal-node');
 const { promises: fs } = require("fs");
 
+const cacheLocation = process.env.CACHE_LOCATION || "./data/cache.json";
+
 const readFromStorage = () => {
-    return fs.readFile("./data/cache.json", "utf-8");
+    return fs.readFile(cacheLocation, "utf-8");
 };
 
 const writeToStorage = (getMergedState) => {
     return readFromStorage().then(oldFile =>{
         const mergedState = getMergedState(oldFile);
-        return fs.writeFile("./data/cacheAfterWrite.json", mergedState);
+        return fs.writeFile(cacheLocation, mergedState);
     })
 };
 
@@ -22,7 +24,7 @@ const cachePlugin = {
 module.exports = {
     auth: {
         clientId: process.env.APP_CLIENT_ID || "99cab759-2aab-420b-91d8-5e3d8d4f063b",
-        authority: "https://login.microsoftonline.com/common",
+        authority: process.env.AUTHORITY || "https://login.microsoftonline.com/90b8faa8-cc95-460e-a618-ee770bee1759",
     },
     cache: {
         cachePlugin
