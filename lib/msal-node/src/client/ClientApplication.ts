274c5074262c2588e1d0cd424c865019e4d686ae
Santiago <Santiago.Gonzalez@microsoft.com>
2020-08-03 16:48:14 -0700

    Add comments. Delete unnecessary loggin

--- lib/msal-node/src/client/ClientApplication.ts
+++ lib/msal-node/src/client/ClientApplication.ts
@@ -229,7 +229,6 @@ export abstract class ClientApplication {
             return this._authority;
         }
 
-        this.logger.verbose("No authority set on application object. Defaulting to common authority");
         this._authority = AuthorityFactory.createInstance(
             this.config.auth.authority || Constants.DEFAULT_AUTHORITY,
             this.config.system!.networkClient!
