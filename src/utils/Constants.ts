348e6c307800b2b0b24118e1f6f2b347abc15ad0
Santiago <Santiago.Gonzalez@microsoft.com>
2020-08-10 17:40:21 -0700

    Add support for on-behalf-of flow

--- src/utils/Constants.ts
+++ src/utils/Constants.ts
@@ -102,7 +102,10 @@ export enum AADServerParamKeys {
     CLIENT_SECRET = "client_secret",
     CLIENT_ASSERTION = "client_assertion",
     CLIENT_ASSERTION_TYPE = "client_assertion_type",
-}
+    OBO_ASSERTION = "assertion",
+    REQUESTED_TOKEN_USE = "requested_token_use",
+    ON_BEHALF_OF = "on_behalf_of",
+ }
 
 /**
  * IdToken claim string constants
@@ -193,7 +196,8 @@ export enum GrantType {
     CLIENT_CREDENTIALS_GRANT = "client_credentials",
     RESOURCE_OWNER_PASSWORD_GRANT = "password",
     REFRESH_TOKEN_GRANT = "refresh_token",
-    DEVICE_CODE_GRANT = "device_code"
+    DEVICE_CODE_GRANT = "device_code",
+    JWT_BEARER = "urn:ietf:params:oauth:grant-type:jwt-bearer" 
 }
 
 /**
