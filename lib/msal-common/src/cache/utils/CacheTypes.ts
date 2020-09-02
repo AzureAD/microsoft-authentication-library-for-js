21d84a29ed154b0dce45617331b5f373a5007eb2
sameerag <sameera.gajjarapu@microsoft.com>
2020-09-02 11:18:20 -0700

    update linter

--- lib/msal-common/src/cache/utils/CacheTypes.ts
+++ lib/msal-common/src/cache/utils/CacheTypes.ts
@@ -8,7 +8,7 @@ import { IdTokenEntity } from "../entities/IdTokenEntity";
 import { AccessTokenEntity } from "../entities/AccessTokenEntity";
 import { RefreshTokenEntity } from "../entities/RefreshTokenEntity";
 import { AppMetadataEntity } from "../entities/AppMetadataEntity";
-import { ServerTelemetryEntity } from '../entities/ServerTelemetryEntity';
+import { ServerTelemetryEntity } from "../entities/ServerTelemetryEntity";
 
 export type AccountCache = Record<string, AccountEntity>;
 export type IdTokenCache = Record<string, IdTokenEntity>;
