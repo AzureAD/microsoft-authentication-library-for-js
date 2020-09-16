591a2074bc8c0261ff19bf4d2e79220f5981baa5
sameerag <sameera.gajjarapu@microsoft.com>
2020-09-16 13:41:42 -0700

    Scopes comparison should be case insensitive, scopes storage case sensitive

--- test/utils/StringConstants.ts
+++ test/utils/StringConstants.ts
@@ -109,7 +109,7 @@ export const TEST_CONFIG = {
     CODE_CHALLENGE_METHOD: "S256",
     TOKEN_TYPE_BEARER: "Bearer",
     DEFAULT_SCOPES: ["openid", "profile", "offline_access"],
-    DEFAULT_GRAPH_SCOPE: ["user.read"],
+    DEFAULT_GRAPH_SCOPE: ["User.Read"],
     LOGIN_HINT: "user@test.com",
     DOMAIN_HINT: "test.com",
     SID: "session-id",
