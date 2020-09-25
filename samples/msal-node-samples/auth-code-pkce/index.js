2546e2ae9ad4681e1f2789f7c231619e5b70a389
HectorMg <hemoral@microsoft.com>
2020-09-25 14:32:23 -0700

    Update PKCE sample with comments

--- samples/msal-node-samples/auth-code-pkce/index.js
+++ samples/msal-node-samples/auth-code-pkce/index.js
@@ -13,7 +13,6 @@ const config = {
     auth: {
         clientId: "89e61572-2f96-47ba-b571-9d8c8f96b69d",
         authority: "https://login.microsoftonline.com/5d97b14d-c396-4aee-b524-c86d33e9b660"
-
     },
     system: {
         loggerOptions: {
@@ -29,7 +28,7 @@ const config = {
 /**
  * PKCE Setup
  * 
- * MSAL enables PKCE by including the codeChallenge and codeChallengeMethod parameters
+ * MSAL enables PKCE in the Authorization Code Grant Flow by including the codeChallenge and codeChallengeMethod parameters
  * in the request passed into getAuthCodeUrl() API, as well as the codeVerifier parameter in the
  * second leg (acquireTokenByCode() API).
  * 
@@ -37,12 +36,14 @@ const config = {
  * For this sample, you can either implement your own PKCE code generation logic or use an existing tool
  * to manually generate a Code Verifier and Code Challenge, plugging them into the pkceCodes object below.
  * 
+ * For details on implementing your own PKCE code generation logic, consult the 
+ * PKCE specification https://tools.ietf.org/html/rfc7636#section-4
  */
 
-const pkceCodes = {
-    challengeMethod: "S256", // Use SHA256 Algorithm
-    codeVerifier: "", // Generate a code verifier for the Auth Code Request
-    codeChallenge: "" // Generate a code challenge from the code verifier
+const PKCE_CODES = {
+    CHALLENGE_METHOD: "S256", // Use SHA256 Algorithm
+    VERIFIER: "", // Generate a code verifier for the Auth Code Request first
+    CHALLENGE: "" // Generate a code challenge from the previously generated code verifier
 };
 
 // Create msal application object
@@ -55,13 +56,12 @@ app.get('/', (req, res) => {
     const authCodeUrlParameters = {
         scopes: ["user.read"],
         redirectUri: "http://localhost:3000/redirect",
-        codeChallenge: pkceCodes.challenge,
-        codeChallengeMethod: "S256"
+        codeChallenge: PKCE_CODES.CHALLENGE, // PKCE Code Challenge
+        codeChallengeMethod: PKCE_CODES.CHALLENGE_METHOD // PKCE Code Challenge Method
     };
 
     // get url to sign user in and consent to scopes needed for applicatio
     pca.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
-        console.log(response);
         res.redirect(response);
     }).catch((error) => console.log(JSON.stringify(error)));
 });
@@ -71,10 +71,8 @@ app.get('/redirect', (req, res) => {
         code: req.query.code,
         scopes: ["user.read"],
         redirectUri: "http://localhost:3000/redirect",
-        codeVerifier: pkceCodes.verifier
+        codeVerifier: PKCE_CODES.VERIFIER // PKCE Code Verifier
     };
-
-    console.log(tokenRequest);
 
     pca.acquireTokenByCode(tokenRequest).then((response) => {
         console.log("\nResponse: \n:", response);
