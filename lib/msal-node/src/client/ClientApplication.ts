4d345691453bc6295ec42b1dd991fc7e7a87ac26
Jake McKennon <jamckenn@microsoft.com>
2020-10-07 15:34:56 -0700

    Fix node createInstance calls

--- lib/msal-node/src/client/ClientApplication.ts
+++ lib/msal-node/src/client/ClientApplication.ts
@@ -246,7 +246,7 @@ export abstract class ClientApplication {
         let authority: Authority;
         if (authorityString) {
             this.logger.verbose("Authority passed in, creating authority instance");
-            authority = AuthorityFactory.createInstance(authorityString, this.config.system!.networkClient!);
+            authority = AuthorityFactory.createInstance(authorityString, this.config.system!.networkClient!, this.config.auth.protocolMode);
         } else {
             this.logger.verbose("No authority passed in request, defaulting to authority set on application object");
             authority = this.authority;
@@ -271,7 +271,8 @@ export abstract class ClientApplication {
 
         this._authority = AuthorityFactory.createInstance(
             this.config.auth.authority || Constants.DEFAULT_AUTHORITY,
-            this.config.system!.networkClient!
+            this.config.system!.networkClient!,
+            this.config.auth.protocolMode
         );
 
         return this._authority;
--- lib/msal-node/src/config/Configuration.ts
+++ lib/msal-node/src/config/Configuration.ts
@@ -5,7 +5,7 @@
 import {
     LoggerOptions,
     INetworkModule,
-    LogLevel
+    LogLevel, ProtocolMode
 } from "@azure/msal-common";
 import { NetworkUtils } from "../utils/NetworkUtils";
 import debug from "debug";
@@ -18,6 +18,7 @@ import { ICachePlugin } from "../cache/ICachePlugin";
  * - clientSecret           - Secret string that the application uses when requesting a token. Only used in confidential client applications. Can be created in the Azure app registration portal. 
  * - clientAssertion        - Assertion string that the application uses when requesting a token. Only used in confidential client applications. Assertion should be of type urn:ietf:params:oauth:client-assertion-type:jwt-bearer.
  * - clientCertificate      - Certificate that the application uses when requesting a token. Only used in confidential client applications. Requires hex encoded X.509 SHA-1 thumbprint of the certificiate, and the PEM encoded private key (string should contain -----BEGIN PRIVATE KEY----- ... -----END PRIVATE KEY----- )
+ * - protocolMode               - Enum that represents the protocol that msal follows. Used for configuring proper endpoints.
  */
 export type NodeAuthOptions = {
     clientId: string;
@@ -30,7 +31,8 @@ export type NodeAuthOptions = {
     };
     knownAuthorities?: Array<string>;
     cloudDiscoveryMetadata?: string;
-    clientCapabilities?: []
+    clientCapabilities?: [],
+    protocolMode: ProtocolMode
 };
 
 /**
@@ -77,7 +79,8 @@ const DEFAULT_AUTH_OPTIONS: NodeAuthOptions = {
     },
     knownAuthorities: [],
     cloudDiscoveryMetadata: "",
-    clientCapabilities: []
+    clientCapabilities: [],
+    protocolMode: ProtocolMode.AAD
 };
 
 const DEFAULT_CACHE_OPTIONS: CacheOptions = {};
