b88c64302e3ba37e12e11e8a2f5618d32c875999
Jason Nutter <janutter@microsoft.com>
2020-10-14 16:14:11 -0700

    Ensure only 1 set of events if emitted for each API call

--- src/app/PublicClientApplication.ts
+++ src/app/PublicClientApplication.ts
@@ -53,7 +53,6 @@ export class PublicClientApplication extends ClientApplication implements IPubli
      * @param {@link (RedirectRequest:type)}
      */
     async loginRedirect(request?: RedirectRequest): Promise<void> {
-        this.emitEvent(EventType.LOGIN_START, InteractionType.REDIRECT, request);
         return this.acquireTokenRedirect(request || DEFAULT_REQUEST);
     }
 
@@ -65,18 +64,15 @@ export class PublicClientApplication extends ClientApplication implements IPubli
      * @returns {Promise.<AuthenticationResult>} - a promise that is fulfilled when this function has completed, or rejected if an error was raised. Returns the {@link AuthResponse} object
      */
     loginPopup(request?: PopupRequest): Promise<AuthenticationResult> {
-        this.emitEvent(EventType.LOGIN_START, InteractionType.POPUP, request);
-        return this.acquireTokenPopup(request || DEFAULT_REQUEST)
-            .then((result) => {
-                this.emitEvent(EventType.LOGIN_SUCCESS, InteractionType.POPUP, result);
-                return result;
-            })
-            .catch((e) => {
-                this.emitEvent(EventType.LOGIN_FAILURE, InteractionType.POPUP, null, e);
-                throw e;
-            });
+        return this.acquireTokenPopup(request || DEFAULT_REQUEST);
     }
 
+    /**
+     * Silently acquire an access token for a given set of scopes. Will use cached token if available, otherwise will attempt to acquire a new token from the network via refresh token.
+     * 
+     * @param {@link (SilentRequest:type)} 
+     * @returns {Promise.<AuthenticationResult>} - a promise that is fulfilled when this function has completed, or rejected if an error was raised. Returns the {@link AuthResponse} object
+     */
     async acquireTokenSilent(request: SilentRequest): Promise<AuthenticationResult> {
         this.preflightBrowserEnvironmentCheck();
         const silentRequest: SilentFlowRequest = {
@@ -92,7 +88,14 @@ export class PublicClientApplication extends ClientApplication implements IPubli
             this.emitEvent(EventType.ACQUIRE_TOKEN_SUCCESS, InteractionType.SILENT, cachedToken);
             return cachedToken;
         } catch (e) {
-            return this.acquireTokenByRefreshToken(request);
+            try {
+                const tokenRenewalResult = await this.acquireTokenByRefreshToken(request);
+                this.emitEvent(EventType.ACQUIRE_TOKEN_SUCCESS, InteractionType.SILENT, tokenRenewalResult);
+                return tokenRenewalResult;
+            } catch (tokenRenewalError) {
+                this.emitEvent(EventType.ACQUIRE_TOKEN_FAILURE, InteractionType.SILENT, null, tokenRenewalError);
+                throw tokenRenewalError;
+            }
         }
     }
 }
