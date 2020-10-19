d1b51806bb157e399aa73daa8c2acc5d5e1949b3
Thomas Norling <thomas.norling@microsoft.com>
2020-10-19 12:55:27 -0700

    Fix lint

--- lib/msal-react/src/utilities.ts
+++ lib/msal-react/src/utilities.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
 import { IMsalContext } from "./MsalContext";
 import { AuthenticationResult } from "@azure/msal-browser";
 
