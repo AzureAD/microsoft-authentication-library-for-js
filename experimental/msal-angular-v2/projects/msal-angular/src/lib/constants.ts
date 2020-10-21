c99a8e7eec008edd87558d78642417d01f5456bf
Jo Arroyo <joarroyo@microsoft.com>
2020-10-21 11:35:44 -0700

    Update v2 to use events from msal-browser

--- experimental/msal-angular-v2/projects/msal-angular/src/lib/constants.ts
+++ experimental/msal-angular-v2/projects/msal-angular/src/lib/constants.ts
@@ -5,18 +5,3 @@ export const MSAL_INSTANCE = new InjectionToken<string>("MSAL_INSTANCE");
 export const MSAL_GUARD_CONFIG = new InjectionToken<string>("MSAL_GUARD_CONFIG");
 
 export const MSAL_INTERCEPTOR_CONFIG = new InjectionToken<string>("MSAL_INTERCEPTOR_CONFIG");
-
-export enum InteractionType {
-    REDIRECT = "redirect",
-    POPUP = "popup",
-    SILENT = "silent"
-}
-
-export enum MsalBroadcastEvent {
-    LOGIN_SUCCESS = "msal:loginSuccess",
-    LOGIN_FAILURE = "msal:loginFailure",
-    ACQUIRE_TOKEN_SUCCESS = "msal:acquireTokenSuccess",
-    ACQUIRE_TOKEN_FAILURE = "msal:acquireTokenFailure",
-    SSO_SILENT_SUCCESS = "msal:ssoSilentSuccess",
-    SSO_SILENT_FAILURE = "msal:ssoSilentFailure"
-}
--- experimental/msal-angular-v2/projects/msal-angular/src/lib/msal.broadcast.service.ts
+++ experimental/msal-angular-v2/projects/msal-angular/src/lib/msal.broadcast.service.ts
@@ -1,25 +1,20 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
 import { Observable, Subject } from 'rxjs';
-import { MsalBroadcastEvent } from './constants';
-import { AuthenticationResult, AuthError } from '@azure/msal-browser';
-
-export interface MsalBroadcastMessage {
-  type: MsalBroadcastEvent;
-  payload: AuthenticationResult | AuthError;
-}
+import { MSAL_INSTANCE } from './constants';
+import { EventMessage, IPublicClientApplication } from '@azure/msal-browser';
 
 @Injectable()
 export class MsalBroadcastService {
   private _msalSubject: Subject<any>;
   public msalSubject$: Observable<any>;
 
-  constructor() {
-    this._msalSubject = new Subject<MsalBroadcastMessage>();
+  constructor(
+    @Inject(MSAL_INSTANCE) private msalInstance: IPublicClientApplication
+  ) {
+    this._msalSubject = new Subject<EventMessage>();
     this.msalSubject$  = this._msalSubject.asObservable();
-  }
-
-  broadcast(type: MsalBroadcastEvent, payload: AuthenticationResult | AuthError) {
-
-    this._msalSubject.next({type , payload});
+    this.msalInstance.addEventCallback((message: EventMessage) => {
+      this._msalSubject.next(message);
+    });
   }
 }
