17d66740e836b7c1e4a6431ef850b907f8599b51
HectorMg <hemoral@microsoft.com>
2020-10-23 16:27:59 -0700

    Add test logger message to confirm test app is using local build instead of npm package

--- lib/msal-node/src/client/ClientApplication.ts
+++ lib/msal-node/src/client/ClientApplication.ts
@@ -56,7 +56,6 @@ export abstract class ClientApplication {
         );
         this.cryptoProvider = new CryptoProvider();
         TrustedAuthority.setTrustedAuthoritiesFromConfig(this.config.auth.knownAuthorities!, this.config.auth.cloudDiscoveryMetadata!);
-        console.log(this);
     }
 
     /**
@@ -74,6 +73,7 @@ export abstract class ClientApplication {
             request.authority
         );
         this.logger.verbose("Auth client config generated");
+        this.logger.verbose("TEST LOGGER MESSAGE");
         const authorizationCodeClient = new AuthorizationCodeClient(
             authClientConfig
         );
