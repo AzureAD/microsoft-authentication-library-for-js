679f593fbc5d00e1474392e678d3835da97119c2
Prithvi Kanherkar <prkanher@microsoft.com>
2020-10-27 11:37:22 -0600

    Update MemoryStorage.ts

--- lib/msal-browser/src/cache/MemoryStorage.ts
+++ lib/msal-browser/src/cache/MemoryStorage.ts
@@ -2,6 +2,7 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
+
 import { IWindowStorage } from "./IWindowStorage";
 
 export class MemoryStorage implements IWindowStorage {
