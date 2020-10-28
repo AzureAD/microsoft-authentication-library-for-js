94c6228f10a44882a328114663e71e3cd6676fa6
Prithvi Kanherkar <prkanher@microsoft.com>
2020-10-28 15:56:12 -0600

    Update browserMemStorage.spec.ts

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserMemStorage.spec.ts
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserMemStorage.spec.ts
@@ -10,7 +10,7 @@ import { msalConfig as memStorageConfig, request as memStorageTokenRequest } fro
 import { clickLoginPopup, clickLoginRedirect, enterCredentials, waitForReturnToApp } from "./testUtils";
 import fs from "fs";
 
-const SCREENSHOT_BASE_FOLDER_NAME = `${__dirname}/screenshots`;
+const SCREENSHOT_BASE_FOLDER_NAME = `${__dirname}/screenshots/memStorage`;
 const SAMPLE_HOME_URL = "http://localhost:30662/";
 
 async function verifyTokenStore(BrowserCache: BrowserCacheUtils, scopes: string[]): Promise<void> {
@@ -93,96 +93,6 @@ describe("Browser tests", function () {
                 const [popupPage, popupWindowClosed] = await clickLoginPopup(screenshot, page);
                 await enterCredentials(popupPage, screenshot, username, accountPwd);
                 await waitForReturnToApp(screenshot, page, popupPage, popupWindowClosed);
-
-                // Verify browser cache contains Account, idToken, AccessToken and RefreshToken
-                await verifyTokenStore(BrowserCache, memStorageTokenRequest.scopes);
-            });
-        });
-
-        describe("acquireToken Tests", () => {
-            let testName: string;
-            let screenshot: Screenshot;
-            
-            before(async () => {
-                context = await browser.createIncognitoBrowserContext();
-                page = await context.newPage();
-                BrowserCache = new BrowserCacheUtils(page, memStorageConfig.cache.cacheLocation);
-                await page.goto(SAMPLE_HOME_URL);
-
-                testName = "acquireTokenBaseCase";
-                screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-                const [popupPage, popupWindowClosed] = await clickLoginPopup(screenshot, page);
-                await enterCredentials(popupPage, screenshot, username, accountPwd);
-                await waitForReturnToApp(screenshot, page, popupPage, popupWindowClosed);
-            });
-
-            beforeEach(async () => {
-                await page.reload();
-                await page.waitForSelector("#WelcomeMessage");
-            });
-        
-            after(async () => {
-                await page.evaluate(() =>  Object.assign({}, window.sessionStorage.clear()));
-                await page.evaluate(() =>  Object.assign({}, window.localStorage.clear()));
-                await page.close();
-            });
-
-            it("acquireTokenRedirect", async () => {
-                await page.waitForSelector("#acquireTokenRedirect");
-                
-                // Remove access_tokens from cache so we can verify acquisition
-                const tokenStore = await BrowserCache.getTokens();
-                await BrowserCache.removeTokens(tokenStore.refreshTokens);
-                await BrowserCache.removeTokens(tokenStore.accessTokens);
-                await page.click("#acquireTokenRedirect");
-                await page.waitForSelector("#scopes-acquired");
-                await screenshot.takeScreenshot(page, "acquireTokenRedirectGotTokens");
-
-                // Verify browser cache contains Account, idToken, AccessToken and RefreshToken
-                await verifyTokenStore(BrowserCache, memStorageTokenRequest.scopes);
-            });
-
-            it("acquireTokenPopup", async () => {
-                await page.waitForSelector("#acquireTokenPopup");
-
-                // Remove access_tokens from cache so we can verify acquisition
-                const tokenStore = await BrowserCache.getTokens();
-                await BrowserCache.removeTokens(tokenStore.refreshTokens);
-                await BrowserCache.removeTokens(tokenStore.accessTokens);
-                await page.click("#acquireTokenPopup");
-                await page.waitForSelector("#scopes-acquired");
-                await screenshot.takeScreenshot(page, "acquireTokenPopupGotTokens");
-
-                // Verify browser cache contains Account, idToken, AccessToken and RefreshToken
-                await verifyTokenStore(BrowserCache, memStorageTokenRequest.scopes);
-            });
-
-            it("acquireTokenSilent from Cache", async () => {
-                await page.waitForSelector("#acquireTokenSilent");
-                await page.click("#acquireTokenSilent");
-                await page.waitForSelector("#scopes-acquired");
-                await screenshot.takeScreenshot(page, "acquireTokenSilent-fromCache-GotTokens");
-
-                const telemetryCacheEntry = await BrowserCache.getTelemetryCacheEntry(memStorageConfig.auth.clientId);
-                expect(telemetryCacheEntry).to.not.be.null;
-                expect(telemetryCacheEntry["cacheHits"]).to.be.eq(1);
-                // Remove Telemetry Cache entry for next test
-                await BrowserCache.removeTokens([BrowserCacheUtils.getTelemetryKey(memStorageConfig.auth.clientId)]);
-
-                // Verify browser cache contains Account, idToken, AccessToken and RefreshToken
-                await verifyTokenStore(BrowserCache, memStorageTokenRequest.scopes);
-            });
-
-            it("acquireTokenSilent via RefreshToken", async () => {
-                await page.waitForSelector("#acquireTokenSilent");
-
-                // Remove access_tokens from cache so we can verify acquisition
-                const tokenStore = await BrowserCache.getTokens();
-                await BrowserCache.removeTokens(tokenStore.accessTokens);
-
-                await page.click("#acquireTokenSilent");
-                await page.waitForSelector("#scopes-acquired");
-                await screenshot.takeScreenshot(page, "acquireTokenSilent-viaRefresh-GotTokens");
 
                 // Verify browser cache contains Account, idToken, AccessToken and RefreshToken
                 await verifyTokenStore(BrowserCache, memStorageTokenRequest.scopes);
