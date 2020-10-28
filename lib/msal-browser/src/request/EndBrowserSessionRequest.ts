e94355419bf6519c2f604e95c74d474b15a64ed9
Thomas Norling <thomas.norling@microsoft.com>
2020-10-28 16:44:21 -0700

    update typedocs

--- lib/msal-browser/src/request/EndBrowserSessionRequest.ts
+++ lib/msal-browser/src/request/EndBrowserSessionRequest.ts
@@ -5,6 +5,13 @@
 
 import { EndSessionRequest } from "@azure/msal-common";
 
+/**
+ * EndSessionRequest
+ * - account                - Account object that will be logged out of. All tokens tied to this account will be cleared.
+ * - postLogoutRedirectUri  - URI to navigate to after logout page.
+ * - authority              - Authority to send logout request to.
+ * - correlationId          - Unique GUID set per request to trace a request end-to-end for telemetry purposes.
+ */
 export type EndBrowserSessionRequest = Partial<EndSessionRequest> & {
     authority?: string;
 };
