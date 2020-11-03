dcedd22ee859317774d5a5fcc2f7e8f9e65c9d1e
Jo Arroyo <joarroyo@microsoft.com>
2020-11-02 16:39:39 -0800

    Removed angular-v1 folder from repo

--- lib/msal-angular-v1/src/constants.ts
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved.
- * Licensed under the MIT License.
- */
-
-import { InjectionToken } from "@angular/core";
-
-export const MSAL_CONFIG = new InjectionToken<string>("MSAL_CONFIG");
-export const MSAL_CONFIG_ANGULAR = new InjectionToken<string>("MSAL_CONFIG_ANGULAR");
