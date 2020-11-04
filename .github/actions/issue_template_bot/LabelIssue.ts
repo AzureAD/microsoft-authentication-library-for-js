c6952f040738e2d1613585b778c0cff5d07577a4
Thomas Norling <thomas.norling@microsoft.com>
2020-11-04 11:30:36 -0800

    Update config structure

--- .github/actions/issue_template_bot/LabelIssue.ts
+++ .github/actions/issue_template_bot/LabelIssue.ts
@@ -6,7 +6,12 @@ export type RepoParamsType = {
     repo: string
 }
 
-export type IssueLabelConfigType = Record<string, Record<string, string[]>>;
+export type IssueLabelConfigType = Record<string, HeaderConfigType>;
+export type HeaderConfigType = {
+    labels: Record<string, string[]>,
+    enforceSelection: boolean,
+    message: string
+};
 
 export class LabelIssue {
     private issueNo: number;
@@ -46,15 +51,14 @@ export class LabelIssue {
         Object.entries(this.issueLabelConfig).forEach(([header, value]) => {
             const issueContent = this.issueContent.get(header) || "";
             core.info(`${header} Content: ${issueContent}`);
-            Object.entries(value).forEach(([label, searchStrings]) => {
+            const labels = value.labels;
+            Object.entries(labels).forEach(([label, searchStrings]) => {
                 core.info(`Checking label: ${label}`);
                 let labelMatched = false;
                 searchStrings.every(searchString => {
                     core.info(`Searching string: ${searchString}`);
                     const libraryRegEx = RegExp("-\\s*\\[\\s*[xX]\\s*\\]\\s*(.*)", "g");
                     while((match = libraryRegEx.exec(issueContent)) !== null) {
-                        core.info(`Match found: ${match}`);
-                        core.info(`Match group 1: ${match[1]}`);
                         if (match[1].includes(searchString)) {
                             labelMatched = true;
                             break;
--- .github/actions/issue_template_bot/dist/index.js
+++ .github/actions/issue_template_bot/dist/index.js
@@ -5825,15 +5825,14 @@ class LabelIssue {
         Object.entries(this.issueLabelConfig).forEach(([header, value]) => {
             const issueContent = this.issueContent.get(header) || "";
             core.info(`${header} Content: ${issueContent}`);
-            Object.entries(value).forEach(([label, searchStrings]) => {
+            const labels = value.labels;
+            Object.entries(labels).forEach(([label, searchStrings]) => {
                 core.info(`Checking label: ${label}`);
                 let labelMatched = false;
                 searchStrings.every(searchString => {
                     core.info(`Searching string: ${searchString}`);
                     const libraryRegEx = RegExp("-\\s*\\[\\s*[xX]\\s*\\]\\s*(.*)", "g");
                     while ((match = libraryRegEx.exec(issueContent)) !== null) {
-                        core.info(`Match found: ${match}`);
-                        core.info(`Match group 1: ${match[1]}`);
                         if (match[1].includes(searchString)) {
                             labelMatched = true;
                             break;
