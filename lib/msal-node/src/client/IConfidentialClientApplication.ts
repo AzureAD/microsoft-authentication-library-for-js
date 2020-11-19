b619a44b5dfcc95db028034dd1f9a05b859f28ba
sameerag <sameera.gajjarapu@microsoft.com>
2020-11-19 11:11:00 -0800

    Add authcode, silent and refreshtoken flows to CCA

--- lib/msal-node/src/client/IConfidentialClientApplication.ts
+++ lib/msal-node/src/client/IConfidentialClientApplication.ts
@@ -3,20 +3,48 @@
  * Licensed under the MIT License.
  */
 
-import { AuthenticationResult, ClientCredentialRequest, OnBehalfOfRequest } from "@azure/msal-common";
+import { AuthenticationResult, AuthorizationCodeRequest, AuthorizationUrlRequest, ClientCredentialRequest, Logger, OnBehalfOfRequest, RefreshTokenRequest, SilentFlowRequest } from "@azure/msal-common";
 import { NodeConfigurationAuthError } from "../error/NodeConfigurationAuthError";
-
+import { TokenCache } from "../cache/TokenCache";
 export interface IConfidentialClientApplication {
+    getAuthCodeUrl(request: AuthorizationUrlRequest): Promise<string>;
+    acquireTokenByCode(request: AuthorizationCodeRequest): Promise<AuthenticationResult>;
+    acquireTokenSilent(request: SilentFlowRequest): Promise<AuthenticationResult>;
+    acquireTokenByRefreshToken(request: RefreshTokenRequest): Promise<AuthenticationResult>;
     acquireTokenByClientCredential(request: ClientCredentialRequest): Promise<AuthenticationResult>;
     acquireTokenOnBehalfOf(request: OnBehalfOfRequest): Promise<AuthenticationResult>;
+    getTokenCache(): TokenCache;
+    getLogger(): Logger;
+    setLogger(logger: Logger): void;
 }
 
 export const stubbedConfidentialClientApplication: IConfidentialClientApplication = {
+    getAuthCodeUrl: () => {
+        return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
+    },
+    acquireTokenByCode: () => {
+        return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
+    },
+    acquireTokenSilent: () => {
+        return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
+    },
+    acquireTokenByRefreshToken: () => {
+        return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
+    },
     acquireTokenByClientCredential: () => {
         return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
     },
     acquireTokenOnBehalfOf: () => {
         return Promise.reject(NodeConfigurationAuthError.createStubPcaInstanceCalledError);
+    },
+    getTokenCache: () => {
+        throw NodeConfigurationAuthError.createStubTokenCacheCalledError();
+    },
+    getLogger: () => {
+        throw NodeConfigurationAuthError.createStubPcaInstanceCalledError();
+    },
+    setLogger: () => {
+        throw NodeConfigurationAuthError.createStubPcaInstanceCalledError();
     }
 };
 
