ea5ad9e1623976e5f48447e61d205d09d2289b06
HectorMg <hemoral@microsoft.com>
2020-12-02 17:54:16 -0800

    Enable executing a single scenario end-to-end test suite in msal-node e2e testRunner

--- samples/msal-node-samples/EndToEndTestApp/testRunner.ts
+++ samples/msal-node-samples/EndToEndTestApp/testRunner.ts
@@ -1,5 +1,18 @@
 import { runCLI } from "jest";
 import { Config } from "@jest/types";
+import yargs from "yargs";
+
+interface Arguments {
+    [x:string]: unknown;
+    s: string;
+    p: number | undefined;
+}
+
+// Command line argument config
+const argv = yargs(process.argv).options({
+    s: { type: "string", alias: "scenario"},
+    p: { type: "number", alias: "port"}
+}).argv;
 
 const { readScenarioNames, readTestFiles } = require("./sampleUtils.js");
 const { runSample } = require("./index.js");
@@ -8,30 +21,43 @@ const scenarios = readScenarioNames();
 const tests = readTestFiles();
 
 // Filter so only scenarios that have tests are executed
-const testScenarios = scenarios.filter((scenario: string) => tests.includes(scenario));
+let testScenarios = scenarios.filter((scenario: string) => tests.includes(scenario));
+
+const selectedScenario = argv.s;
+
+if (selectedScenario) {
+    testScenarios = testScenarios.filter((scenario: string) => scenario === selectedScenario);
+    if (testScenarios.length === 1) {
+        console.log(`Test Runner executed with scenario flag, executing scenario ${selectedScenario} only.`);
+    } else if (testScenarios.length < 0) {
+        console.log(`Selected test scenario ${selectedScenario} not found, aborting.`);
+    }
+}
 
 async function runE2ETests() {
+    let globalResults: Array<any> = [];
     // Using reduce instead of map to chain each test scenario execution in serial, initial accumulator is a dummy Promise
-    const globalResults = await testScenarios.reduce(
-        (currentScenarioPromise: Promise<string>, nextScenario: string) => {
-            return currentScenarioPromise.then(() => {
-                return testScenario(nextScenario);
-            });
-        }, Promise.resolve(null));
-
-  Promise.all(globalResults).then(globalResults => {
-      const globalFailedTests = globalResults.reduce((totalFailedTests: number, scenarioResults: any) => {
-          return totalFailedTests + scenarioResults.results.numFailedTests;
-      }, 0);
-      // If any tests fail, exit with code 1 so CI/CD check fails
-      process.exitCode = (globalFailedTests > 0) ? 1 : 0;
-  })
+    const executionResult = await testScenarios.reduce((currentScenarioPromise: Promise<string>, nextScenario: string) => {
+        const currentScenarioResults = currentScenarioPromise.then(() => {
+            return testScenario(nextScenario);
+        });
+        globalResults = [...globalResults, currentScenarioResults];
+        return currentScenarioResults;
+    }, Promise.resolve(null));
+    
+    Promise.all(globalResults).then((globalResults: Array<any>) => {
+        const globalFailedTests = globalResults.reduce((totalFailedTests: number, scenarioResults: any) => {
+            return totalFailedTests + scenarioResults.results.numFailedTests;
+        }, 0);
+        // If any tests fail, exit with code 1 so CI/CD check fails
+        process.exitCode = (globalFailedTests > 0) ? 1 : 0;
+    });
 }
 
 async function testScenario (scenario: string): Promise<any> {
     const testCacheLocation = `${__dirname}/app/test/${scenario}/data/testCache.json`;
     const testLocation = `./app/test/${scenario}`;
-    
+
     // Execute sample application under scenario configuration
     return await runSample(scenario, 3000, testCacheLocation).then(async (server: any) => {
         const args = {
