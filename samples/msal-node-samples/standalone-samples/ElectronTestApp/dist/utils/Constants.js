311ead8c7a1de648acb29531ac9f75f1a9b9d823
HectorMg <hemoral@microsoft.com>
2020-12-03 10:35:04 -0800

    Remove ElectronTestApp dist directory

--- samples/msal-node-samples/standalone-samples/ElectronTestApp/dist/utils/Constants.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CACHE_LOCATION = void 0;
-exports.CACHE_LOCATION = "./data/cache.json";
-//# sourceMappingURL=Constants.js.map
