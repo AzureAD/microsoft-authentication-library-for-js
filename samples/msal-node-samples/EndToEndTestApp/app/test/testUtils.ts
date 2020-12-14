b269bd2197a89bf19c0a7a2505c527d08c194a30
HectorMg <hemoral@microsoft.com>
2020-12-14 15:10:09 -0800

    Don't await final click

--- samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
+++ samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
@@ -15,7 +15,7 @@ export async function enterCredentials(page: Page, screenshot: Screenshot, usern
     await screenshot.takeScreenshot(page, "pwdInputPage");
     await page.type("#i0118", accountPwd);
     await screenshot.takeScreenshot(page, "pwdTyped");
-    await page.click("#idSIButton9");
+    page.click("#idSIButton9");
     await screenshot.takeScreenshot(page, "pwdSubmitted");
 }
 
