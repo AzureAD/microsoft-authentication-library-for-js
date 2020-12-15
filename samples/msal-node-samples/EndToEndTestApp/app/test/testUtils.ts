f095637d9702a307a16c93fbebf5401bcb642323
HectorMg <hemoral@microsoft.com>
2020-12-14 16:57:16 -0800

    Revert to node-specific enterCredentials

--- samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
+++ samples/msal-node-samples/EndToEndTestApp/app/test/testUtils.ts
@@ -7,7 +7,6 @@ import { Screenshot } from "../../../../e2eTestUtils/TestUtils";
 import { Page } from "puppeteer";
 
 export async function enterCredentials(page: Page, screenshot: Screenshot, username: string, accountPwd: string): Promise<void> {
-    await page.waitForNavigation({ waitUntil: "networkidle0"});
     await page.waitForSelector("#i0116");
     await screenshot.takeScreenshot(page, "loginPage");
     await page.type("#i0116", username);
@@ -15,7 +14,9 @@ export async function enterCredentials(page: Page, screenshot: Screenshot, usern
     await page.waitForSelector("#idA_PWD_ForgotPassword");
     await screenshot.takeScreenshot(page, "pwdInputPage");
     await page.type("#i0118", accountPwd);
+    await screenshot.takeScreenshot(page, "pwdTyped");
     await page.click("#idSIButton9");
+    await screenshot.takeScreenshot(page, "pwdSubmitted");
 }
 
 export async function clickSignIn(page: Page, screenshot: Screenshot): Promise<void> {
