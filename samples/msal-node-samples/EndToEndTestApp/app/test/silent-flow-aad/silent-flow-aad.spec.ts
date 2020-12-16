1e6205ed3417c1ef80a8e10a37fab7f9cd4a2b0d
HectorMg <hemoral@microsoft.com>
2020-12-16 14:57:43 -0800

    Add done parameter to async tests

--- samples/msal-node-samples/EndToEndTestApp/app/test/silent-flow-aad/silent-flow-aad.spec.ts
+++ samples/msal-node-samples/EndToEndTestApp/app/test/silent-flow-aad/silent-flow-aad.spec.ts
@@ -28,22 +28,8 @@ describe("Silent Flow AAD PPE Tests", () => {
     let context: puppeteer.BrowserContext;
     let page: puppeteer.Page;
 
-    beforeAll(async () => {
-        createFolder(SCREENSHOT_BASE_FOLDER_NAME);
-      
-        browser = await puppeteer.launch({
-            headless: true,
-            ignoreDefaultArgs: ["--no-sandbox", "-disable-setuid-sandbox"]
-        });
-    });
-
-    afterAll(async () => {
-        await context.close();
-        await browser.close();
-    });
-
-    describe("Authenticated", () => {
-        beforeAll(async () => {
+    beforeAll(async (done) => {
+        try {
             const labApiParms: LabApiQueryParams = {
                 azureEnvironment: AzureEnvironments.PPE,
                 appType: AppTypes.CLOUD
@@ -52,111 +38,104 @@ describe("Silent Flow AAD PPE Tests", () => {
             const labClient = new LabClient();
             const envResponse = await labClient.getVarsByCloudEnvironment(labApiParms);
             [username, accountPwd] = await setupCredentials(envResponse[0], labClient);
-        });
-        
-        beforeEach(async () => {
-            context = await browser.createIncognitoBrowserContext();
-            page = await context.newPage();
-            page.setDefaultNavigationTimeout(0);
-            await page.goto(SAMPLE_HOME_URL);
-        });
-    
-        afterEach(async () => {
-            await page.close();
-            NodeCacheTestUtils.resetCache(TEST_CACHE_LOCATION);
-        });
+            createFolder(SCREENSHOT_BASE_FOLDER_NAME);
+          
+            browser = await puppeteer.launch({
+                headless: true,
+                ignoreDefaultArgs: ["--no-sandbox", "-disable-setuid-sandbox"]
+            });
 
-        it("Performs acquire token with Auth Code flow", async () => {
-            const testName = "AADAuthCodeSignIn";
-            const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-            await clickSignIn(page, screenshot);
-            await enterCredentials(page, screenshot, username, accountPwd);
-            await screenshot.takeScreenshot(page, "WaitingForATS");
-            await page.waitForSelector("#acquireTokenSilent");
-            await page.click("#acquireTokenSilent");
-            const cachedTokens = NodeCacheTestUtils.getTokens(TEST_CACHE_LOCATION);
-            expect(cachedTokens.accessTokens.length).toBe(1);
-            expect(cachedTokens.idTokens.length).toBe(1);
-            expect(cachedTokens.refreshTokens.length).toBe(1);
-        });
-
-        it("Performs acquire token silent", async () => {
-            const testName = "AADAcquireTokenSilent";
-            const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-            await clickSignIn(page, screenshot);
-            await enterCredentials(page, screenshot, username, accountPwd);
-            await page.waitForSelector("#acquireTokenSilent");
-            await page.click("#acquireTokenSilent");
-            await page.waitForSelector("#graph-called-successfully");
-            await screenshot.takeScreenshot(page, "acquireTokenSilentGotTokens");
-            const htmlBody = await page.evaluate(() => document.body.innerHTML);
-            expect(htmlBody).toContain(SUCCESSFUL_GRAPH_CALL_ID);
-        });
-
-        it("Refreshes an expired access token", async () => {
-            const testName = "AADRefreshExpiredToken";
-            const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-            await clickSignIn(page, screenshot);
-            await enterCredentials(page, screenshot, username, accountPwd);
-            await page.waitForSelector("#acquireTokenSilent");
-            const originalAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
-            NodeCacheTestUtils.expireAccessTokens(TEST_CACHE_LOCATION);
-            const expiredAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
-            await page.click("#acquireTokenSilent");
-            await page.waitForSelector(`#${SUCCESSFUL_GRAPH_CALL_ID}`);
-            const refreshedAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
-            await screenshot.takeScreenshot(page, "acquireTokenSilentGotTokens");
-            const htmlBody = await page.evaluate(() => document.body.innerHTML);
-            expect(htmlBody).toContain(SUCCESSFUL_GRAPH_CALL_ID);
-            expect(Number(originalAccessToken.expiresOn)).toBeGreaterThan(0);
-            expect(Number(expiredAccessToken.expiresOn)).toBe(0);
-            expect(Number(refreshedAccessToken.expiresOn)).toBeGreaterThan(0);
-            expect(refreshedAccessToken.secret).not.toEqual(originalAccessToken.secret);
-        });
-
-        it("Gets all accounts", async () => {
-            const testName = "AADGetAllAccounts";
-            const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-            await clickSignIn(page, screenshot);
-            await enterCredentials(page, screenshot, username, accountPwd);
-            await page.waitForSelector("#getAllAccounts");
-            await page.click("#getAllAccounts");
-            await page.waitForSelector(`#${SUCCESSFUL_GET_ALL_ACCOUNTS_ID}`);
-            await screenshot.takeScreenshot(page, "gotAllAccounts");
-            const accounts  = await page.evaluate(() => JSON.parse(document.getElementById("nav-tabContent").children[0].innerHTML));
-            const htmlBody = await page.evaluate(() => document.body.innerHTML);
-            expect(htmlBody).toContain(SUCCESSFUL_GET_ALL_ACCOUNTS_ID);
-            expect(htmlBody).not.toContain("No accounts found in the cache.");
-            expect(htmlBody).not.toContain("Failed to get accounts from cache.");
-            expect(accounts.length).toBe(1);
-        });
+            done();
+        } catch(error) {
+            done(error);
+        }
     });
 
-    describe("Unauthenticated", () => {
-        beforeEach(async () => {
-            context = await browser.createIncognitoBrowserContext();
-            page = await context.newPage();
-            page.setDefaultNavigationTimeout(0);
-            await page.goto(SAMPLE_HOME_URL);
-        });
-    
-        afterEach(async () => {
-            await page.close();
-            NodeCacheTestUtils.resetCache(TEST_CACHE_LOCATION);
-        });
+    afterAll(async (done) => {
+        await context.close();
+        await browser.close();
+        done();
+    });
+    beforeEach(async (done) => {
+        context = await browser.createIncognitoBrowserContext();
+        page = await context.newPage();
+        page.setDefaultNavigationTimeout(0);
+        await page.goto(SAMPLE_HOME_URL);
+        done();
+    });
 
-        it("Returns empty account array", async () => {
-            const testName = "AADNoCachedAccounts";
-            const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
-            await page.goto(`${SAMPLE_HOME_URL}/allAccounts`);
-            await page.waitForSelector("#getAllAccounts");
-            await page.click("#getAllAccounts");
-            await screenshot.takeScreenshot(page, "gotAllAccounts");
-            const accounts  = await page.evaluate(() => JSON.parse(document.getElementById("nav-tabContent").children[0].innerHTML));
-            const htmlBody = await page.evaluate(() => document.body.innerHTML);
-            expect(htmlBody).toContain("No accounts found in the cache.");
-            expect(htmlBody).not.toContain("Failed to get accounts from cache.");
-            expect(accounts.length).toBe(0);
-        });
+    afterEach(async (done) => {
+        await page.close();
+        NodeCacheTestUtils.resetCache(TEST_CACHE_LOCATION);
+        done();
+    });
+
+    it("Performs acquire token silent", async (done) => {
+        const testName = "AADAcquireTokenSilent";
+        const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
+        await clickSignIn(page, screenshot);
+        await enterCredentials(page, screenshot, username, accountPwd);
+        await page.waitForSelector("#acquireTokenSilent");
+        await page.click("#acquireTokenSilent");
+        await page.waitForSelector("#graph-called-successfully");
+        await screenshot.takeScreenshot(page, "acquireTokenSilentGotTokens");
+        const htmlBody = await page.evaluate(() => document.body.innerHTML);
+        expect(htmlBody).toContain(SUCCESSFUL_GRAPH_CALL_ID);
+        done();
+    });
+
+    it("Refreshes an expired access token", async (done) => {
+        const testName = "AADRefreshExpiredToken";
+        const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
+        await clickSignIn(page, screenshot);
+        await enterCredentials(page, screenshot, username, accountPwd);
+        await page.waitForSelector("#acquireTokenSilent");
+        const originalAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
+        NodeCacheTestUtils.expireAccessTokens(TEST_CACHE_LOCATION);
+        const expiredAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
+        await page.click("#acquireTokenSilent");
+        await page.waitForSelector(`#${SUCCESSFUL_GRAPH_CALL_ID}`);
+        const refreshedAccessToken = NodeCacheTestUtils.getAccessTokens(TEST_CACHE_LOCATION)[0].token;
+        await screenshot.takeScreenshot(page, "acquireTokenSilentGotTokens");
+        const htmlBody = await page.evaluate(() => document.body.innerHTML);
+        expect(htmlBody).toContain(SUCCESSFUL_GRAPH_CALL_ID);
+        expect(Number(originalAccessToken.expiresOn)).toBeGreaterThan(0);
+        expect(Number(expiredAccessToken.expiresOn)).toBe(0);
+        expect(Number(refreshedAccessToken.expiresOn)).toBeGreaterThan(0);
+        expect(refreshedAccessToken.secret).not.toEqual(originalAccessToken.secret);
+        done();
+    });
+
+    it("Gets all accounts", async (done) => {
+        const testName = "AADGetAllAccounts";
+        const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
+        await clickSignIn(page, screenshot);
+        await enterCredentials(page, screenshot, username, accountPwd);
+        await page.waitForSelector("#getAllAccounts");
+        await page.click("#getAllAccounts");
+        await page.waitForSelector(`#${SUCCESSFUL_GET_ALL_ACCOUNTS_ID}`);
+        await screenshot.takeScreenshot(page, "gotAllAccounts");
+        const accounts  = await page.evaluate(() => JSON.parse(document.getElementById("nav-tabContent").children[0].innerHTML));
+        const htmlBody = await page.evaluate(() => document.body.innerHTML);
+        expect(htmlBody).toContain(SUCCESSFUL_GET_ALL_ACCOUNTS_ID);
+        expect(htmlBody).not.toContain("No accounts found in the cache.");
+        expect(htmlBody).not.toContain("Failed to get accounts from cache.");
+        expect(accounts.length).toBe(1);
+        done();
+    });
+
+    it("Returns empty account array", async () => {
+        const testName = "AADNoCachedAccounts";
+        const screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
+        await page.goto(`${SAMPLE_HOME_URL}/allAccounts`);
+        await page.waitForSelector("#getAllAccounts");
+        await page.click("#getAllAccounts");
+        await screenshot.takeScreenshot(page, "gotAllAccounts");
+        const accounts  = await page.evaluate(() => JSON.parse(document.getElementById("nav-tabContent").children[0].innerHTML));
+        const htmlBody = await page.evaluate(() => document.body.innerHTML);
+        expect(htmlBody).toContain("No accounts found in the cache.");
+        expect(htmlBody).not.toContain("Failed to get accounts from cache.");
+        expect(accounts.length).toBe(0);
+        done();
     });
 });
