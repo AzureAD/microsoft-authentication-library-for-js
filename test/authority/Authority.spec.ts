29d6f21a58ccb1ecbf1c7249bcd88b99945232f4
Thomas Norling <thomas.norling@microsoft.com>
2020-12-21 16:54:46 -0800

    common tests passing

--- test/authority/Authority.spec.ts
+++ test/authority/Authority.spec.ts
@@ -6,14 +6,26 @@ import { Constants } from "../../src/utils/Constants";
 import {
     TEST_URIS,
     RANDOM_TEST_GUID,
-    DEFAULT_OPENID_CONFIG_RESPONSE
+    DEFAULT_OPENID_CONFIG_RESPONSE,
+    TEST_CONFIG
 } from "../utils/StringConstants";
 import { ClientConfigurationErrorMessage, ClientConfigurationError } from "../../src/error/ClientConfigurationError";
-import { ClientAuthErrorMessage, ProtocolMode } from "../../src";
-import { ClientTestUtils } from "../client/ClientTestUtils";
-import { TrustedAuthority } from "../../src/authority/TrustedAuthority";
+import { AuthorityOptions, ClientAuthErrorMessage, ProtocolMode } from "../../src";
+import { MockStorageClass, mockCrypto } from "../client/ClientTestUtils";
+
+let mockStorage: MockStorageClass;
+
+const authorityOptions: AuthorityOptions = {
+    protocolMode: ProtocolMode.AAD,
+    knownAuthorities: [Constants.DEFAULT_AUTHORITY],
+    cloudDiscoveryMetadata: "",
+    authorityMetadata: ""
+}
 
 describe("Authority.ts Class Unit Tests", () => {
+    beforeEach(() => {
+        mockStorage = new MockStorageClass(TEST_CONFIG.MSAL_CLIENT_ID, mockCrypto);
+    });
     afterEach(() => {
         sinon.restore();
     });
@@ -29,7 +41,7 @@ describe("Authority.ts Class Unit Tests", () => {
                     return null;
                 }
             };
-            const authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, ProtocolMode.AAD);
+            const authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
             expect(authority.canonicalAuthority).to.be.eq(`${Constants.DEFAULT_AUTHORITY}`);
         });
 
@@ -43,9 +55,9 @@ describe("Authority.ts Class Unit Tests", () => {
                 }
             };
 
-            expect(() => new Authority("http://login.microsoftonline.com/common", networkInterface, ProtocolMode.AAD)).to.throw(ClientConfigurationErrorMessage.authorityUriInsecure.desc);
-            expect(() => new Authority("This is not a URI", networkInterface, ProtocolMode.AAD)).to.throw(ClientConfigurationErrorMessage.urlParseError.desc);
-            expect(() => new Authority("", networkInterface, ProtocolMode.AAD)).to.throw(ClientConfigurationErrorMessage.urlEmptyError.desc);
+            expect(() => new Authority("http://login.microsoftonline.com/common", networkInterface, mockStorage, authorityOptions)).to.throw(ClientConfigurationErrorMessage.authorityUriInsecure.desc);
+            expect(() => new Authority("This is not a URI", networkInterface, mockStorage, authorityOptions)).to.throw(ClientConfigurationErrorMessage.urlParseError.desc);
+            expect(() => new Authority("", networkInterface, mockStorage, authorityOptions)).to.throw(ClientConfigurationErrorMessage.urlEmptyError.desc);
         });
     });
 
@@ -60,7 +72,7 @@ describe("Authority.ts Class Unit Tests", () => {
         };
         let authority: Authority;
         beforeEach(() => {
-            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, ProtocolMode.AAD);
+            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
         });
 
         it("Gets canonical authority that ends in '/'", () => {
@@ -95,8 +107,7 @@ describe("Authority.ts Class Unit Tests", () => {
         describe("OAuth Endpoints", () => {
 
             beforeEach(async () => {
-                sinon.stub(Authority.prototype, <any>"discoverEndpoints").resolves(DEFAULT_OPENID_CONFIG_RESPONSE);
-                ClientTestUtils.setCloudDiscoveryMetadataStubs();
+                sinon.stub(Authority.prototype, <any>"getEndpointMetadataFromNetwork").resolves(DEFAULT_OPENID_CONFIG_RESPONSE.body);
                 await authority.resolveEndpointsAsync();
             });
 
@@ -117,7 +128,7 @@ describe("Authority.ts Class Unit Tests", () => {
             });
 
             it("Throws error if endpoint discovery is incomplete for authorizationEndpoint, tokenEndpoint, endSessionEndpoint and selfSignedJwtAudience", () => {
-                authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, ProtocolMode.AAD);
+                authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
                 expect(() => authority.authorizationEndpoint).to.throw(ClientAuthErrorMessage.endpointResolutionError.desc);
                 expect(() => authority.tokenEndpoint).to.throw(ClientAuthErrorMessage.endpointResolutionError.desc);
                 expect(() => authority.endSessionEndpoint).to.throw(ClientAuthErrorMessage.endpointResolutionError.desc);
@@ -139,7 +150,7 @@ describe("Authority.ts Class Unit Tests", () => {
         };
         let authority: Authority;
         beforeEach(() => {
-            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, ProtocolMode.AAD);
+            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
         });
 
         it("discoveryComplete returns false if endpoint discovery has not been completed", () => {
@@ -147,8 +158,7 @@ describe("Authority.ts Class Unit Tests", () => {
         });
 
         it("discoveryComplete returns true if resolveEndpointsAsync resolves successfully", async () => {
-            ClientTestUtils.setCloudDiscoveryMetadataStubs();
-            sinon.stub(Authority.prototype, <any>"discoverEndpoints").resolves(DEFAULT_OPENID_CONFIG_RESPONSE);
+            sinon.stub(Authority.prototype, <any>"getEndpointMetadataFromNetwork").resolves(DEFAULT_OPENID_CONFIG_RESPONSE.body);
             await authority.resolveEndpointsAsync();
             expect(authority.discoveryComplete()).to.be.true;
         });
@@ -157,8 +167,7 @@ describe("Authority.ts Class Unit Tests", () => {
             networkInterface.sendGetRequestAsync = (url: string, options?: NetworkRequestOptions): any => {
                 return DEFAULT_OPENID_CONFIG_RESPONSE;
             };
-            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, ProtocolMode.AAD);
-            ClientTestUtils.setCloudDiscoveryMetadataStubs();
+            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
             await authority.resolveEndpointsAsync();
 
             expect(authority.discoveryComplete()).to.be.true;
@@ -169,56 +178,51 @@ describe("Authority.ts Class Unit Tests", () => {
             expect(authority.selfSignedJwtAudience).to.be.eq(DEFAULT_OPENID_CONFIG_RESPONSE.body.issuer.replace("{tenant}", "common"));
         });
 
-        it("Attempts to set instance metadata from network if not set", async () => {
-            sinon.restore();
-            let setFromNetwork = false;
-            sinon.stub(TrustedAuthority, "getTrustedHostList").returns([]);
-            sinon.stub(TrustedAuthority, "IsInTrustedHostList").returns(true);
-            sinon.stub(TrustedAuthority, "setTrustedAuthoritiesFromNetwork").callsFake(async () => {
-                setFromNetwork = true;
-            });
-            sinon.stub(TrustedAuthority, "getCloudDiscoveryMetadata").callsFake(() => {
+        it("Attempts to set instance metadata from network if not set", (done) => {
+            const authorityOptions: AuthorityOptions = {
+                protocolMode: ProtocolMode.AAD,
+                knownAuthorities: [],
+                cloudDiscoveryMetadata: "",
+                authorityMetadata: ""
+            }
+            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
+            sinon.stub(Authority.prototype, <any>"getCloudDiscoveryMetadataFromNetwork").callsFake(() => {
+                done();
                 return {
-                    preferred_cache: Constants.DEFAULT_AUTHORITY_HOST, 
-                    preferred_network: Constants.DEFAULT_AUTHORITY_HOST, 
-                    aliases: [Constants.DEFAULT_AUTHORITY_HOST]}
+                    aliases: [Constants.DEFAULT_AUTHORITY],
+                    preferred_network: Constants.DEFAULT_AUTHORITY,
+                    preferred_cache: Constants.DEFAULT_AUTHORITY
+                }
             });
 
-            await authority.resolveEndpointsAsync();
-            expect(setFromNetwork).to.be.true;
+            authority.resolveEndpointsAsync();
         });
 
-        it("throws untrustedAuthority error if host is not in TrustedHostList", async () => {
-            sinon.restore();
-            sinon.stub(TrustedAuthority, "getTrustedHostList").returns(["testAuthority"]);
-            sinon.stub(TrustedAuthority, "IsInTrustedHostList").returns(false);
-
-            let err = null;
-            try {
-                await authority.resolveEndpointsAsync();
-            } catch (e) {
-                expect(e).to.be.instanceOf(ClientConfigurationError);
-                err = e;
+        it("throws untrustedAuthority error if host is not in TrustedHostList", (done) => {
+            const authorityOptions: AuthorityOptions = {
+                protocolMode: ProtocolMode.AAD,
+                knownAuthorities: [],
+                cloudDiscoveryMetadata: "",
+                authorityMetadata: ""
             }
-            expect(err.errorMessage).to.equal(ClientConfigurationErrorMessage.untrustedAuthority.desc);
-            expect(err.errorCode).to.equal(ClientConfigurationErrorMessage.untrustedAuthority.code);
+            authority = new Authority(Constants.DEFAULT_AUTHORITY, networkInterface, mockStorage, authorityOptions);
+            sinon.stub(Authority.prototype, <any>"getCloudDiscoveryMetadataFromNetwork").returns(null);
+
+            authority.resolveEndpointsAsync().catch(e => {
+                expect(e).to.be.instanceOf(ClientConfigurationError);
+                expect(e.errorMessage).to.equal(ClientConfigurationErrorMessage.untrustedAuthority.desc);
+                expect(e.errorCode).to.equal(ClientConfigurationErrorMessage.untrustedAuthority.code);
+                done();
+            });
         });
 
         it("ADFS authority uses v1 well-known endpoint", async () => {
             const authorityUrl = "https://login.microsoftonline.com/adfs/"
             let endpoint = "";
-            authority = new Authority(authorityUrl, networkInterface, ProtocolMode.AAD);
-            sinon.stub(TrustedAuthority, "getTrustedHostList").returns(["login.microsoftonline.com"]);
-            sinon.stub(TrustedAuthority, "IsInTrustedHostList").returns(true);
-            sinon.stub(TrustedAuthority, "getCloudDiscoveryMetadata").callsFake(() => {
-                return {
-                    preferred_cache: Constants.DEFAULT_AUTHORITY_HOST, 
-                    preferred_network: Constants.DEFAULT_AUTHORITY_HOST, 
-                    aliases: [Constants.DEFAULT_AUTHORITY_HOST]}
-            });
-            sinon.stub(Authority.prototype, <any>"discoverEndpoints").callsFake((openIdEndpoint) => {
-                endpoint = openIdEndpoint;
-                return DEFAULT_OPENID_CONFIG_RESPONSE; // Response is required but is not important for this test
+            authority = new Authority(authorityUrl, networkInterface, mockStorage, authorityOptions);
+            sinon.stub(networkInterface, <any>"sendGetRequestAsync").callsFake((openIdConfigEndpoint) => {
+                endpoint = openIdConfigEndpoint;
+                return DEFAULT_OPENID_CONFIG_RESPONSE;
             });
 
             await authority.resolveEndpointsAsync();
@@ -228,18 +232,16 @@ describe("Authority.ts Class Unit Tests", () => {
         it("OIDC ProtocolMode does not append v2 to endpoint", async () => {
             const authorityUrl = "https://login.microsoftonline.com/"
             let endpoint = "";
-            authority = new Authority(authorityUrl, networkInterface, ProtocolMode.OIDC);
-            sinon.stub(TrustedAuthority, "getTrustedHostList").returns(["login.microsoftonline.com"]);
-            sinon.stub(TrustedAuthority, "IsInTrustedHostList").returns(true);
-            sinon.stub(TrustedAuthority, "getCloudDiscoveryMetadata").callsFake(() => {
-                return {
-                    preferred_cache: Constants.DEFAULT_AUTHORITY_HOST, 
-                    preferred_network: Constants.DEFAULT_AUTHORITY_HOST, 
-                    aliases: [Constants.DEFAULT_AUTHORITY_HOST]}
-            });
-            sinon.stub(Authority.prototype, <any>"discoverEndpoints").callsFake((openIdEndpoint) => {
-                endpoint = openIdEndpoint;
-                return DEFAULT_OPENID_CONFIG_RESPONSE; // Response is required but is not important for this test
+            const options = {
+                protocolMode: ProtocolMode.OIDC,
+                knownAuthorities: [Constants.DEFAULT_AUTHORITY],
+                cloudDiscoveryMetadata: "",
+                authorityMetadata: ""
+            }
+            authority = new Authority(authorityUrl, networkInterface, mockStorage, options);
+            sinon.stub(networkInterface, <any>"sendGetRequestAsync").callsFake((openIdConfigEndpoint) => {
+                endpoint = openIdConfigEndpoint;
+                return DEFAULT_OPENID_CONFIG_RESPONSE;
             });
 
             await authority.resolveEndpointsAsync();
