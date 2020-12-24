c4aa261f8902ce0325181999e4b0d989bfd40033
Samuel Kubai <samuel.kamau@microsoft.com>
2020-12-24 16:26:56 +0300

    feat(msal-node): extend the msal-node e2e test suite
    - add the auth code test suite for the aad environment
    - add the auth code test suite for the adfs environment

---
+++ samples/msal-node-samples/EndToEndTestApp/app/test/auth-code-adfs/auth-code-adfs.spec.ts
@@ -0,0 +1,85 @@
+import "jest";
+import puppeteer from "puppeteer";
+import { ChildProcessWithoutNullStreams, spawn } from "child_process";
+import { Screenshot, createFolder, setupCredentials } from "../../../../../e2eTestUtils/TestUtils";
+import { NodeCacheTestUtils } from "../../../../../e2eTestUtils/NodeCacheTestUtils";
+import { LabClient } from "../../../../../e2eTestUtils/LabClient";
+import { LabApiQueryParams } from "../../../../../e2eTestUtils/LabApiQueryParams";
+import { AppTypes, AzureEnvironments, FederationProviders, UserTypes } from "../../../../../e2eTestUtils/Constants";
+import { 
+    enterCredentialsADFS, 
+    SCREENSHOT_BASE_FOLDER_NAME,
+ } from "../testUtils";
+
+const TEST_CACHE_LOCATION = `${__dirname}/data/testCache.json`;
+const SUCCESSFUL_SIGNED_IN_MESSAGE = "OK";
+const HOME_ROUTE="http://localhost:3000";
+
+let username: string;
+let accountPwd: string;
+
+describe('Auth Code ADFS PPE Tests', () => {
+    jest.setTimeout(60000);
+    let browser: puppeteer.Browser;
+    let context: puppeteer.BrowserContext;
+    let page: puppeteer.Page;
+    let device: ChildProcessWithoutNullStreams;
+    const stream: Array<any> = [];
+    
+    beforeAll(async() => {
+        createFolder(SCREENSHOT_BASE_FOLDER_NAME);
+
+        const labApiParms: LabApiQueryParams = {
+            azureEnvironment: AzureEnvironments.CLOUD,
+            appType: AppTypes.CLOUD,
+            federationProvider: FederationProviders.ADFS2019,
+            userType: UserTypes.FEDERATED
+        }; 
+
+        const labClient = new LabClient();
+        const envResponse = await labClient.getVarsByCloudEnvironment(labApiParms);
+        [username, accountPwd] = await setupCredentials(envResponse[0], labClient);
+
+        browser = await puppeteer.launch({
+            headless: true,
+            ignoreDefaultArgs: ['--no-sandbox', '-disable-setuid-sandbox', '--disable-extensions']
+        });
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    describe("Acquire Token", () => {
+        let testName: string;
+        let screenshot: Screenshot;
+
+        beforeAll(() => {
+            testName = "authCodeFlowBaseCase";
+            screenshot = new Screenshot(`${SCREENSHOT_BASE_FOLDER_NAME}/${testName}`);
+        });
+
+        beforeEach(async () => {
+            context = await browser.createIncognitoBrowserContext();
+            page = await context.newPage();
+            page.setDefaultNavigationTimeout(0);
+        });
+
+        afterEach(async () => {
+            await page.close();
+            await context.close();
+            NodeCacheTestUtils.resetCache(TEST_CACHE_LOCATION);
+        });
+
+        it("Performs acquire token with Auth Code flow", async () => {
+            await page.goto(HOME_ROUTE);
+            await enterCredentialsADFS(page, screenshot, username, accountPwd);
+            const htmlBody = await page.evaluate(() => document.body.innerHTML);
+            expect(htmlBody).toContain(SUCCESSFUL_SIGNED_IN_MESSAGE);
+            const cachedTokens = NodeCacheTestUtils.getTokens(TEST_CACHE_LOCATION);
+            expect(cachedTokens.accessTokens.length).toBe(1);
+            expect(cachedTokens.idTokens.length).toBe(1);
+            expect(cachedTokens.refreshTokens.length).toBe(1);
+         });
+    });
+});
