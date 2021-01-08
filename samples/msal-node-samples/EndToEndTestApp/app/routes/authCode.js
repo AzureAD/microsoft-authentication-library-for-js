0efed225ef17fe5dcb5809435b0f52ef84fec25b
Samuel Kubai <samuel.kamau@microsoft.com>
2021-01-08 15:10:53 +0300

    feat(auth-code): fix the auth code url parameters object

--- samples/msal-node-samples/EndToEndTestApp/app/routes/authCode.js
+++ samples/msal-node-samples/EndToEndTestApp/app/routes/authCode.js
@@ -4,41 +4,34 @@
 */
 
 module.exports = function(app, clientApplication, msalTokenCache, scenarioConfig) {
+
     const requestConfig = scenarioConfig.request;
+
     app.get('/', (req, res) => {
-        let authCodeUrlParameters = requestConfig.authCodeUrlParameters;
+
+        let { authCodeUrlParameters } = requestConfig;
+        
+        // Check for the state parameter
+        if (req.query && req.query.state) authCodeUrlParameters.state = req.query.state;
 
         // Check for the prompt parameter
-        if (req.query && req.query.prompt) {
-            authCodeUrlParameters.prompt = req.query.prompt;
-        }
-
-        // Check for the state parameter
-        if (req.query && req.query.state) {
-            authCodeUrlParameters.state = req.query.state;
-        }
+        if (req.query && req.query.prompt) authCodeUrlParameters.prompt = req.query.prompt;
 
         // Check for the loginHint parameter
-        if (req.query && req.query.loginHint) {
-            authCodeUrlParameters.loginHint = req.query.loginHint;
-        }
+        if (req.query && req.query.loginHint) authCodeUrlParameters.loginHint = req.query.loginHint;
 
         // Check for the domainHint parameter
-        if (req.query && req.query.domainHint) {
-            authCodeUrlParameters.domainHint = req.query.domainHint;
-        }
+        if (req.query && req.query.domainHint) authCodeUrlParameters.domainHint = req.query.domainHint;
         
         // get url to sign user in and consent to scopes needed for applicatio
-        clientApplication.getAuthCodeUrl(requestConfig.authCodeUrlParameters).then((response) => {
+        clientApplication.getAuthCodeUrl(authCodeUrlParameters).then((response) => {
             res.redirect(response);
         }).catch((error) => console.log(JSON.stringify(error)));
     });
     
     app.get('/redirect', (req, res) => {
-        const tokenRequest = {
-            ...requestConfig.tokenRequest,
-            code: req.query.code
-        };
+
+        const tokenRequest = { ...requestConfig.tokenRequest, code: req.query.code };
     
         clientApplication.acquireTokenByCode(tokenRequest).then((response) => {
             res.sendStatus(200);
