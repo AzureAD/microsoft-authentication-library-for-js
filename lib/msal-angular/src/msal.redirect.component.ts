e1f222ed31baaf137848444a795c5bfd86920839
Jo Arroyo <joarroyo@microsoft.com>
2021-01-14 16:54:48 -0800

    Add msal.redirect.component to msal-angular

---
+++ lib/msal-angular/src/msal.redirect.component.ts
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+/**
+ * This is a dedicated redirect component to be added to Angular apps to 
+ * handle redirects when using @azure/msal-angular.
+ * Uncomment the following comment to use it in your app.
+ */
+
+/**
+ *import { Component, OnInit } from "@angular/core";
+ *import { MsalService } from "@azure/msal-angular";
+ *
+ *@Component({
+ *  template: ""
+ *})
+ *export class MsalRedirectComponent implements OnInit {
+ *  
+ *  constructor(private authService: MsalService) { }
+ *  
+ *  ngOnInit(): void {    
+ *      this.authService.handleRedirectObservable().subscribe();
+ *  }
+ *  
+ *}
+ */
