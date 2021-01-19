32d7af21e685e3f86b02028a771e4ca15ee53333
sameerag <sameera.gajjarapu@microsoft.com>
2021-01-19 09:01:36 -0800

    Remove node errors - no longer needed

--- lib/msal-node/src/cache/ITokenCache.ts
+++ lib/msal-node/src/cache/ITokenCache.ts
@@ -4,25 +4,9 @@
  */
 
 import { AccountInfo } from "@azure/msal-common";
-
 export interface ITokenCache {
     getAllAccounts(): Promise<AccountInfo[]>;
     getAccountByHomeId(homeAccountId: string): Promise<AccountInfo | null>;
     getAccountByLocalId(localAccountId: string): Promise<AccountInfo | null>;
     removeAccount(account: AccountInfo): Promise<void>;
 }
-
-export const stubbedTokenCache: ITokenCache = {
-    getAllAccounts: () => {
-        return Promise.reject(NodeConfigurationAuthError.createStubTokenCacheCalledError());
-    },
-    getAccountByHomeId: () => {
-        return Promise.reject(NodeConfigurationAuthError.createStubTokenCacheCalledError());
-    },
-    getAccountByLocalId: () => {
-        return Promise.reject(NodeConfigurationAuthError.createStubTokenCacheCalledError());
-    },
-    removeAccount: () => {
-        return Promise.reject(NodeConfigurationAuthError.createStubTokenCacheCalledError());
-    },
-};
