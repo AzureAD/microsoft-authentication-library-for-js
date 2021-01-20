c1ea8b4882dbb6633d48326060a728d965b94641
Samuel Kubai <samuel.kamau@microsoft.com>
2021-01-20 18:08:10 +0300

    feat: reduce jest timeout in the auth-code-aad test suite

--- samples/msal-node-samples/EndToEndTestApp/app/test/auth-code-aad/auth-code-aad.spec.ts
+++ samples/msal-node-samples/EndToEndTestApp/app/test/auth-code-aad/auth-code-aad.spec.ts
@@ -18,7 +18,7 @@ let username: string;
 let accountPwd: string;
 
 describe('Auth Code AAD PPE Tests', () => {
-    jest.setTimeout(120000);
+    jest.setTimeout(95000);
     let browser: puppeteer.Browser;
     let context: puppeteer.BrowserContext;
     let page: puppeteer.Page;
