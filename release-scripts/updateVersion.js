1dd8ee09287a95920e758b2a5031ac4b894e8728
Thomas Norling <thomas.norling@microsoft.com>
2021-01-28 13:21:36 -0800

    UpdateVersion script runs after beachball bump

--- release-scripts/updateVersion.js
+++ release-scripts/updateVersion.js
@@ -4,14 +4,44 @@
  */
 
 const path = require("path");
-const packageJsonPath = path.resolve(process.cwd(), "package.json");
-const { name, version } = require(packageJsonPath);
 const fs = require("fs");
 
-const data = {
-    name: name,
-    version: version
-};
+const exclude = ["msal-angularjs"]; // Libs that should be excluded
+const failures = 0;
 
-const outputFilepath = path.resolve(process.cwd(), "src", "version.json");
-fs.writeFileSync(outputFilepath, JSON.stringify(data));
+const LIB_DIR = path.resolve(process.cwd(), "lib");
+const libFolders = fs.readdirSync(LIB_DIR, { withFileTypes: true }).filter(function(file) {
+    return file.isDirectory() && exclude.indexOf(file.name) === -1 && fs.existsSync(path.resolve(LIB_DIR, file.name, "package.json"));
+}).map(function(file) {
+    return file.name;
+});
+
+libFolders.forEach((lib) => {
+    const packageJsonPath = path.resolve(LIB_DIR, lib, "package.json");
+    const outputFilepath = path.resolve(LIB_DIR, lib, "src", "version.json");
+    const packageJson = require(packageJsonPath);
+    const versionJson = require(outputFilepath);
+
+    if (packageJson.version !== versionJson.version) {
+        console.log(`Updating ${packageJson.name} to version ${packageJson.version} from ${versionJson.version}`);
+        const data = {
+            name: packageJson.name,
+            version: packageJson.version
+        };
+
+        try {
+            fs.writeFileSync(outputFilepath, JSON.stringify(data));
+        } catch {
+            failures += 1;
+            console.error(`Failed to update version for ${packageJson.name}`);
+        }
+    } else {
+        console.log(`Update not needed for: ${packageJson.name}`);
+    }
+});
+
+if (failedPackages.length > 0) {
+    console.error(`Failed to update versions on ${failedPackages.length} packages`);
+} else {
+    console.log("All package versions updated successfully!");
+}
