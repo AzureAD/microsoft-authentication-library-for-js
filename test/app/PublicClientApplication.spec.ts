31efb975c4c52dab2a47544d27d888e9708cdae9
Prithvi Kanherkar <prkanher@microsoft.com>
2021-02-03 13:21:58 -0700

    Update PublicClientApplication.spec.ts

--- test/app/PublicClientApplication.spec.ts
+++ test/app/PublicClientApplication.spec.ts
@@ -1021,10 +1021,8 @@ describe("PublicClientApplication.ts Class Unit Tests", () => {
             });
 
             it("passes onRedirectNavigate callback", (done) => {
-                const expectedUrl = `https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=0813e1d1-ad72-46a9-8665-399bba48c201&scope=user.read%20openid%20profile&redirect_uri=https%3A%2F%2Flocalhost%3A8081%2Findex.html&client-request-id=11553a9b-7116-48b1-9d48-f6d4a8ff8371&response_mode=fragment&response_type=code&x-client-SKU=msal.js.browser&x-client-VER=${pkg.version}&x-client-OS=&x-client-CPU=&client_info=1&code_challenge=JsjesZmxJwehdhNY9kvyr0QOeSMEvryY_EHZo3BKrqg&code_challenge_method=S256&nonce=11553a9b-7116-48b1-9d48-f6d4a8ff8371&state=eyJpZCI6IjExNTUzYTliLTcxMTYtNDhiMS05ZDQ4LWY2ZDRhOGZmODM3MSIsIm1ldGEiOnsiaW50ZXJhY3Rpb25UeXBlIjoicmVkaXJlY3QifX0%3D%7CuserState`;
-
-                const onRedirectNavigate = (url) => {
-                    expect(url).to.equal(expectedUrl)
+                const onRedirectNavigate = (url: string) => {
+                    expect(url).to.equal(testNavUrl)
                     done();
                 };
 
@@ -1032,8 +1030,8 @@ describe("PublicClientApplication.ts Class Unit Tests", () => {
                     redirectTimeout: timeout, redirectStartPage, onRedirectNavigate: onRedirectNavigateCb
                 }): Promise<void> => {
                     expect(onRedirectNavigateCb).to.be.eq(onRedirectNavigate);
-                    expect(navigateUrl).to.eq(expectedUrl);
-                    onRedirectNavigateCb(navigateUrl);
+                    expect(navigateUrl).to.eq(testNavUrl);
+                    onRedirectNavigate(navigateUrl);
                     return Promise.resolve();
                 });
                 sinon.stub(CryptoOps.prototype, "generatePkceCodes").resolves({
