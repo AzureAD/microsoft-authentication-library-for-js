1695404023f53b2970750d641f5e9452e17a57a5
Thomas Norling <thomas.norling@microsoft.com>
2021-02-03 17:10:40 -0800

    Add optional timeout and fix server teardown

--- samples/e2eTestUtils/jest-puppeteer-utils/jestTeardown.js
+++ samples/e2eTestUtils/jest-puppeteer-utils/jestTeardown.js
@@ -1,17 +1,5 @@
 const serverUtils = require("./serverUtils");
-const path = require("path");
 
-function kill(rootDir) {
-    serverUtils.killServer(rootDir);
-};
-
-module.exports = (jestOptions) => {
-    if(jestOptions.projects && jestOptions.projects.length > 0) {
-        jestOptions.projects.forEach((project) => {
-            const jestConfig = require(path.resolve(project, "jest.config.js"));
-            kill(jestConfig.rootDir);
-        });
-    } else {
-        kill(jestOptions.rootDir);
-    }
+module.exports = async (jestOptions) => {
+    await serverUtils.killServers(jestOptions);
 }
--- samples/msal-angular-v2-samples/angular9-v2-sample-app/jest.config.js
+++ samples/msal-angular-v2-samples/angular9-v2-sample-app/jest.config.js
@@ -2,7 +2,8 @@ module.exports = {
     displayName: "angular9-v2-sample-app",
     globals: {
         __PORT__: 4200,
-        __STARTCMD__: "npm start -- --port 4200"
+        __STARTCMD__: "npm start -- --port 4200",
+        __TIMEOUT__: 45000
     },
     preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
 };
