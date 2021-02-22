b18cb405765aae9f06729dc21a030db70acbf4a8
Thomas Norling <thomas.norling@microsoft.com>
2021-02-22 13:45:11 -0800

    Add popup open event

--- lib/msal-browser/src/event/EventType.ts
+++ lib/msal-browser/src/event/EventType.ts
@@ -16,6 +16,7 @@ export enum EventType {
     SSO_SILENT_FAILURE = "msal:ssoSilentFailure",
     HANDLE_REDIRECT_START = "msal:handleRedirectStart",
     HANDLE_REDIRECT_END = "msal:handleRedirectEnd",
+    POPUP_OPENED = "msal:popupOpened",
     LOGOUT_START = "msal:logoutStart",
     LOGOUT_SUCCESS = "msal:logoutSuccess",
     LOGOUT_FAILURE = "msal:logoutFailure"
