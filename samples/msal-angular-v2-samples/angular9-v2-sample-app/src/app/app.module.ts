b46f229ae492178cb1e265b4e19799a53387207a
Jo Arroyo <joarroyo@microsoft.com>
2021-04-13 13:55:28 -0700

    Add e2e tests to angular9-v2-sample

--- samples/msal-angular-v2-samples/angular9-v2-sample-app/src/app/app.module.ts
+++ samples/msal-angular-v2-samples/angular9-v2-sample-app/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -25,7 +26,8 @@ export function loggerCallback(logLevel: LogLevel, message: string) {
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
     auth: {
-      clientId: '6226576d-37e9-49eb-b201-ec1eeb0029b6',
+      clientId: '3fba556e-5d4a-48e3-8e1a-fd57c12cb82e',
+      authority: 'https://login.windows-ppe.net/common',
       redirectUri: '/',
       postLogoutRedirectUri: '/'
     },
@@ -45,7 +47,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
 
 export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
   const protectedResourceMap = new Map<string, Array<string>>();
-  protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']);
+  protectedResourceMap.set('https://graph.microsoft-ppe.com/v1.0/me', ['user.read']);
 
   return {
     interactionType: InteractionType.Redirect,
@@ -70,6 +72,7 @@ export function MSALGuardConfigFactory(): MsalGuardConfiguration {
     MatButtonModule,
     MatToolbarModule,
     MatListModule,
+    MatMenuModule,
     HttpClientModule,
     MsalModule
   ],
