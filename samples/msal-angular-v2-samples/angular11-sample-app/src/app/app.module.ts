b2805eca21b0d5d09a233ec7d7566315f00986f7
Jo Arroyo <joarroyo@microsoft.com>
2021-04-14 14:12:41 -0700

    Update prod vs ppe

--- samples/msal-angular-v2-samples/angular11-sample-app/src/app/app.module.ts
+++ samples/msal-angular-v2-samples/angular11-sample-app/src/app/app.module.ts
@@ -22,11 +22,14 @@ const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigato
 export function loggerCallback(logLevel: LogLevel, message: string) {
   console.log(message);
 }
+
 export function MSALInstanceFactory(): IPublicClientApplication {
   return new PublicClientApplication({
     auth: {
-      clientId: '3fba556e-5d4a-48e3-8e1a-fd57c12cb82e',
-      authority: 'https://login.windows-ppe.net/common',
+      // clientId: '6226576d-37e9-49eb-b201-ec1eeb0029b6', // Prod enviroment. Uncomment to use. 
+      clientId: '3fba556e-5d4a-48e3-8e1a-fd57c12cb82e', // PPE testing environment
+      // authority: "https://login.windows-ppe.net/common", // Prod environment. Uncomment to use.
+      authority: 'https://login.windows-ppe.net/common', // PPE testing environment.
       redirectUri: '/',
       postLogoutRedirectUri: '/'
     },
@@ -46,6 +49,7 @@ export function MSALInstanceFactory(): IPublicClientApplication {
 
 export function MSALInterceptorConfigFactory(): MsalInterceptorConfiguration {
   const protectedResourceMap = new Map<string, Array<string>>();
+  // protectedResourceMap.set('https://graph.microsoft.com/v1.0/me', ['user.read']); // Prod environment. Uncomment to use.
   protectedResourceMap.set('https://graph.microsoft-ppe.com/v1.0/me', ['user.read']);
 
   return {
