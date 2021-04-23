d207674dd6ccd9148bdb4a7e63eb0d763d61a764
Thomas Norling <thomas.norling@microsoft.com>
2021-04-23 16:45:52 -0700

    Add claims comparison to accountInfoIsEqual

--- lib/msal-react/src/hooks/useAccount.ts
+++ lib/msal-react/src/hooks/useAccount.ts
@@ -4,7 +4,7 @@
  */
 
 import { useState, useEffect } from "react";
-import { AccountInfo, IPublicClientApplication, InteractionStatus } from "@azure/msal-browser";
+import { AccountInfo, IPublicClientApplication, InteractionStatus, AccountEntity } from "@azure/msal-browser";
 import { useMsal } from "./useMsal";
 import { AccountIdentifiers } from "../types/AccountIdentifiers";
 
@@ -42,7 +42,10 @@ export function useAccount(accountIdentifiers: AccountIdentifiers): AccountInfo 
     const [account, setAccount] = useState<AccountInfo|null>(initialStateValue);
 
     useEffect(() => {
-        setAccount(getAccount(instance, accountIdentifiers));
+        const currentAccount = getAccount(instance, accountIdentifiers);
+        if (!AccountEntity.accountInfoIsEqual(account, currentAccount, true)) {
+            setAccount(currentAccount);
+        }
     }, [inProgress, accountIdentifiers, instance]);
 
     return account;
