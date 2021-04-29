4eb270a6f11ca3acde4fa77eddcd9d6581686c54
HectorMg <hemoral@microsoft.com>
2021-04-29 14:59:57 -0700

    Revert changes to sample applications

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/default/authConfig.js
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/default/authConfig.js
@@ -35,7 +35,7 @@ const msalConfig = {
 
 // Add here scopes for id token to be used at MS Identity Platform endpoints.
 const loginRequest = {
-    scopes: []
+    scopes: ["User.Read"]
 };
 
 // Add here the endpoints for MS Graph API services you would like to use.
@@ -51,7 +51,7 @@ const tokenRequest = {
 };
 
 const silentRequest = {
-    scopes: ["openid", "profile"]
+    scopes: ["openid", "profile", "User.Read", "Mail.Read"]
 };
 
 const logoutRequest = {}
