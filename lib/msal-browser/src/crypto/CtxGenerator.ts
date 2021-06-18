34c74666e3de58d273e8c544b4e0311726c0aa62
technical-boy <hectormgdev@gmail.com>
2021-06-18 18:00:37 -0500

    Add CtxGenerator class

---
+++ lib/msal-browser/src/crypto/CtxGenerator.ts
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import { BrowserStringUtils } from "../utils/BrowserStringUtils";
+import { BrowserCrypto } from "./BrowserCrypto";
+import { Base64Encode } from "../encode/Base64Encode";
+
+export class CtxGenerator {
+
+    // browser crypto object used to generate random values
+    private cryptoObj: BrowserCrypto;
+    private base64Encode: Base64Encode;
+
+    constructor(cryptoObj: BrowserCrypto) {
+        this.cryptoObj = cryptoObj;
+        this.base64Encode = new Base64Encode();
+    }
+
+    generateCtx(): Uint8Array {
+        const ctxBuffer = new Uint8Array(32);
+        this.cryptoObj.getRandomValues(ctxBuffer);
+        return ctxBuffer;
+    }
+}
