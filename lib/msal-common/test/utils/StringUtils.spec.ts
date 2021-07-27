23e80732f4d5b1fb3c95006a1d54117b490eb9c1
Balraj Mahal <bmahal@microsoft.com>
2021-07-27 18:28:28 -0400

    double decoding issue- tests added

--- lib/msal-common/test/utils/StringUtils.spec.ts
+++ lib/msal-common/test/utils/StringUtils.spec.ts
@@ -138,6 +138,14 @@ describe("StringUtils.ts Class Unit Tests", () => {
 
     });
 
+    it("queryStringToObject correctly deserializes query string into object", () => {
+        const serializedObj = "param1=test%2525u00f1";
+        const deserializedObj = {
+            "param1": "test%25u00f1"
+        };
+        expect(StringUtils.queryStringToObject(serializedObj)).toEqual(deserializedObj);        
+    });
+
     describe("jsonParseHelper", () => {
         it("parses json", () => {
             const test = { test: "json" };
