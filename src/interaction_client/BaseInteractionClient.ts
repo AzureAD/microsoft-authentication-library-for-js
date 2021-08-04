9388f98b2b9b31fc8393bcb151791509ebc55eee
Thomas Norling <thomas.norling@microsoft.com>
2021-08-04 15:05:08 -0700

    Add correlationId to errors

--- src/interaction_client/BaseInteractionClient.ts
+++ src/interaction_client/BaseInteractionClient.ts
@@ -11,6 +11,8 @@ import { EndSessionRequest } from "../request/EndSessionRequest";
 import { RedirectRequest } from "../request/RedirectRequest";
 import { PopupRequest } from "../request/PopupRequest";
 import { SsoSilentRequest } from "../request/SsoSilentRequest";
+import { version } from "../packageMetadata";
+import { BrowserConstants } from "../utils/BrowserConstants";
 
 export abstract class BaseInteractionClient {
 
@@ -20,14 +22,16 @@ export abstract class BaseInteractionClient {
     protected networkClient: INetworkModule;
     protected logger: Logger;
     protected eventHandler: EventHandler;
+    protected correlationId: string;
 
-    constructor(config: BrowserConfiguration, storageImpl: BrowserCacheManager, browserCrypto: ICrypto, logger: Logger, eventHandler: EventHandler) {
+    constructor(config: BrowserConfiguration, storageImpl: BrowserCacheManager, browserCrypto: ICrypto, logger: Logger, eventHandler: EventHandler, correlationId?: string) {
         this.config = config;
         this.browserStorage = storageImpl;
         this.browserCrypto = browserCrypto;
         this.networkClient = this.config.system.networkClient;
-        this.logger = logger;
         this.eventHandler = eventHandler;
+        this.correlationId = correlationId || this.browserCrypto.createNewGuid();
+        this.logger = logger.clone(BrowserConstants.MSAL_SKU, version, this.correlationId);
     }
 
     abstract acquireToken(request: RedirectRequest|PopupRequest|SsoSilentRequest): Promise<AuthenticationResult|void>;
