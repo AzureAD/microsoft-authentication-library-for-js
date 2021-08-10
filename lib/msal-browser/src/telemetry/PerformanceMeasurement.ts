6affb79700c6d9404b9750251eb5255dd66df0d1
HectorMg <hemoral@microsoft.com>
2021-08-10 15:28:42 -0700

    Add PerformanceManager and PerformanceMeasurement

---
+++ lib/msal-browser/src/telemetry/PerformanceMeasurement.ts
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export class PerformanceMeasurement {
+    private measureName: string;
+    private startMark: string;
+    private endMark: string;
+
+    constructor(measureName: string) {
+        this.measureName = `msal.measure.${measureName}`;
+        this.startMark = `msal.start.${measureName}`;
+        this.endMark = `msal.end.${measureName}`;
+    }
+
+    static supportsBrowserPerformance(): boolean {
+        return typeof window !== "undefined" &&
+            "performance" in window &&
+            !!window.performance.mark && 
+            !!window.performance.measure;
+    }
+
+    startMeasurement(): void {
+        if (PerformanceMeasurement.supportsBrowserPerformance()) {
+            window.performance.mark(this.startMark);
+        }
+    }
+
+    endMeasurement():void {
+        if (PerformanceMeasurement.supportsBrowserPerformance()) {
+            window.performance.mark(this.endMark);
+            window.performance.measure(this.measureName, this.startMark, this.endMark);
+        }
+    }
+
+    flushMeasurement(): number {
+        if (PerformanceMeasurement.supportsBrowserPerformance()) {
+            const duration = window.performance.getEntriesByName(this.measureName, "measure")[0].duration;
+            window.performance.clearMeasures(this.measureName);
+            window.performance.clearMarks(this.startMark);
+            window.performance.clearMarks(this.endMark);
+            return duration;
+        }
+        return 0;
+    }
+}
