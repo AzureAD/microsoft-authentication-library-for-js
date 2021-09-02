b3b18bc86a5949905be265c9b892e288551ac894
Hector Morales <hemoral@microsoft.com>
2021-09-02 11:51:13 -0700

    Add PoP params to acquireTokenSilent request thumbprint (#3973)
    * Add POP params to acquireTokenSilent request thumbprint to enable parallel ATS requests
    
    * Add shrClaims to request thumbprint
    
    * Change files
    
    * Update ThrottlingUtils to support new RequestThumbprint parameters by taking in full request instead of specific request thumbprint elements
    
    * Add request as optional parameter at the end of ThrottlingUtils.removeThrottling to avoid a breaking change
    
    * Make request a required parameter in ThrottlingUtils.removeThrottle
    
    * Change files
    
    * Remove outdated changefile
    
    * Remove redundant undefined value for homeAccountIdentifier in removeThrottle calls
    
    * Update lib/msal-common/src/network/ThrottlingUtils.ts
    
    Co-authored-by: Thomas Norling <thomas.l.norling@gmail.com>
    
    Co-authored-by: Thomas Norling <thomas.l.norling@gmail.com>

--- lib/msal-common/src/client/RefreshTokenClient.ts
+++ lib/msal-common/src/client/RefreshTokenClient.ts
@@ -139,7 +139,11 @@ export class RefreshTokenClient extends BaseClient {
         const thumbprint: RequestThumbprint = {
             clientId: this.config.authOptions.clientId,
             authority: authority.canonicalAuthority,
-            scopes: request.scopes
+            scopes: request.scopes,
+            authenticationScheme: request.authenticationScheme,
+            resourceRequestMethod: request.resourceRequestMethod,
+            resourceRequestUri: request.resourceRequestUri,
+            shrClaims: request.shrClaims
         };
 
         const endpoint = UrlString.appendQueryString(authority.tokenEndpoint, queryParameters);
