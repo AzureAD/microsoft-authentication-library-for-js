c484147534876e9a3c2a5b7edc5497e5f2a74067
Thomas Norling <thomas.norling@microsoft.com>
2021-09-23 11:04:47 -0700

    Fix error

--- lib/msal-browser/src/app/ClientApplication.ts
+++ lib/msal-browser/src/app/ClientApplication.ts
@@ -28,7 +28,7 @@ import { TokenCache } from "../cache/TokenCache";
 import { ITokenCache } from "../cache/ITokenCache";
 import { WamInteractionClient } from "../interaction_client/WamInteractionClient";
 import { WamMessageHandler } from "../broker/wam/WamMessageHandler";
-import { BrowserAuthError, BrowserAuthErrorMessage } from "../error/BrowserAuthError";
+import { BrowserAuthError } from "../error/BrowserAuthError";
 import { SilentRequest } from "../request/SilentRequest";
 import { WamAuthError } from "../error/WamAuthError";
 import { SilentCacheClient } from "../interaction_client/SilentCacheClient";
@@ -268,7 +268,7 @@ export abstract class ClientApplication {
 
         if (this.config.system.platformSSO && this.wamExtensionProvider) {
             result = this.acquireTokenNative(request).catch((e: AuthError) => {
-                if (this.fallbackToStandardFlow(e)) {
+                if (e instanceof WamAuthError && e.isExtensionError()) {
                     this.wamExtensionProvider = undefined; // If extension gets uninstalled during session prevent future requests from continuing to attempt 
                     const popupClient = this.createPopupClient(request.correlationId);
                     return popupClient.acquireToken(request);
