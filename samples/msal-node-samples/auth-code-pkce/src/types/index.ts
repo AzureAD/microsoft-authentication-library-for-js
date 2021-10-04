7b6126284c574bca93e06dcb488f08da849091ff
Samuel Kubai <kamausamuel11@gmail.com>
2021-10-04 15:44:17 +0300

    refactor: format the type file in the auth-code-pkce sample

--- samples/msal-node-samples/auth-code-pkce/src/types/index.ts
+++ samples/msal-node-samples/auth-code-pkce/src/types/index.ts
@@ -1,4 +1,12 @@
 import { Request} from "express";
 import { Session } from "express-session";
 
-export type RequestWithPKCE = Request & { session: Session & { pkceCodes: { challengeMethod: string, challenge?: string, verifier?: string } }};
+export type RequestWithPKCE = Request & { 
+    session: Session & {
+        pkceCodes: {
+            challengeMethod: string,
+            challenge?: string,
+            verifier?: string
+        }
+    }
+};
