3283d36f0e56922643efa91c825b8562cf582921
joannaarroyo <joliuac@gmail.com>
2021-10-08 23:30:14 -0700

    Initial commit. Set up ms-identity-node package

---
+++ experimental/ms-identity-node/index.js
@@ -0,0 +1,6 @@
+/*! @azure/ms-identity-node v1.0.0-alpha.0 2021-10-08 */
+'use strict';
+'use strict';
+
+console.log("something");
+//# sourceMappingURL=index.js.map
---
+++ experimental/ms-identity-node/packageMetadata.d.ts
@@ -0,0 +1,3 @@
+export declare const name = "@azure/ms-identity-node";
+export declare const version = "1.0.0-alpha.0";
+//# sourceMappingURL=packageMetadata.d.ts.map
