688016d39504b20b430990cf7e19a8d3fc058453
Thomas Norling <thomas.norling@microsoft.com>
2022-01-06 12:14:41 -0800

    Use getAccountByIdentifiers helper

--- lib/msal-react/src/hooks/useAccount.ts
+++ lib/msal-react/src/hooks/useAccount.ts
@@ -4,9 +4,10 @@
  */
 
 import { useState, useEffect } from "react";
-import { AccountInfo, IPublicClientApplication, InteractionStatus, AccountEntity } from "@azure/msal-browser";
+import { AccountInfo, IPublicClientApplication, AccountEntity } from "@azure/msal-browser";
 import { useMsal } from "./useMsal";
 import { AccountIdentifiers } from "../types/AccountIdentifiers";
+import { getAccountByIdentifiers } from "../utils/utilities";
 
 function getAccount(instance: IPublicClientApplication, accountIdentifiers?: AccountIdentifiers): AccountInfo | null {
     if (!accountIdentifiers || (!accountIdentifiers.homeAccountId && !accountIdentifiers.localAccountId && !accountIdentifiers.username)) {
@@ -14,26 +15,7 @@ function getAccount(instance: IPublicClientApplication, accountIdentifiers?: Acc
         return instance.getActiveAccount();
     }
 
-    const allAccounts = instance.getAllAccounts();
-    if (allAccounts.length > 0) {
-        const matchedAccounts = allAccounts.filter(accountObj => {
-            if (accountIdentifiers.username && accountIdentifiers.username.toLowerCase() !== accountObj.username.toLowerCase()) {
-                return false;
-            }
-            if (accountIdentifiers.homeAccountId && accountIdentifiers.homeAccountId.toLowerCase() !== accountObj.homeAccountId.toLowerCase()) {
-                return false;
-            }
-            if (accountIdentifiers.localAccountId && accountIdentifiers.localAccountId.toLowerCase() !== accountObj.localAccountId.toLowerCase()) {
-                return false;
-            }
-
-            return true;
-        });
-
-        return matchedAccounts[0] || null;
-    } else {
-        return null;
-    }
+    return getAccountByIdentifiers(instance.getAllAccounts(), accountIdentifiers);
 }
 
 /**
@@ -43,10 +25,7 @@ function getAccount(instance: IPublicClientApplication, accountIdentifiers?: Acc
 export function useAccount(accountIdentifiers?: AccountIdentifiers): AccountInfo | null {
     const { instance, inProgress } = useMsal();
 
-    const [account, setAccount] = useState<AccountInfo|null>(() => {
-        // Lazy intialization ensures getAccount is only called on the first render
-        return inProgress === InteractionStatus.Startup ? null : getAccount(instance, accountIdentifiers);
-    });
+    const [account, setAccount] = useState<AccountInfo|null>(() => getAccount(instance, accountIdentifiers));
 
     useEffect(() => {
         const currentAccount = getAccount(instance, accountIdentifiers);
