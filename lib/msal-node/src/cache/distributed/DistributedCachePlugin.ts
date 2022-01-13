8fa9e9878cf6e09f2770a05c3ab72fd9664b4fd0
Samuel Kubai <kamausamuel11@gmail.com>
2022-01-13 14:03:21 +0300

    refactor: implement suggested changes in the DistributedCachePlugin

--- lib/msal-node/src/cache/distributed/DistributedCachePlugin.ts
+++ lib/msal-node/src/cache/distributed/DistributedCachePlugin.ts
@@ -10,27 +10,26 @@ import { ICacheClient } from "./ICacheClient";
 
 export class DistributedCachePlugin implements ICachePlugin {
     private client: ICacheClient;
-    private partitionManager?: IPartitionManager;
+    private partitionManager: IPartitionManager;
 
-    constructor(client: ICacheClient, partitionManager?: IPartitionManager) {
+    constructor(client: ICacheClient, partitionManager: IPartitionManager) {
         this.client = client;
         this.partitionManager = partitionManager;
     }
   
     public async beforeCacheAccess(cacheContext: TokenCacheContext): Promise<void> {
-        if (this.partitionManager) {
-            const partitionKey = await this.partitionManager.getKey();
-            const cacheData = await this.client.get(partitionKey);
-            cacheContext.tokenCache.deserialize(cacheData);
-        }
+        const partitionKey = await this.partitionManager.getKey();
+        const cacheData = await this.client.get(partitionKey);
+        cacheContext.tokenCache.deserialize(cacheData);
     }
   
     public async afterCacheAccess(cacheContext: TokenCacheContext): Promise<void> {
-        if (cacheContext.cacheHasChanged && this.partitionManager) {
+        if (cacheContext.cacheHasChanged) {
             const kvStore = (cacheContext.tokenCache as TokenCache).getKVStore();
-  
-            if (Object.keys(kvStore).length >= 2) {
-                const accountEntity = Object.values(kvStore)[1] as AccountEntity; // the second entity is the account
+            const accountEntities = Object.values(kvStore).filter(value => AccountEntity.isAccountEntity(value as object));
+
+            if (accountEntities.length > 0) {
+                const accountEntity = accountEntities[0] as AccountEntity;
                 const partitionKey = await this.partitionManager.extractKey(accountEntity);
                           
                 await this.client.set(partitionKey, cacheContext.tokenCache.serialize());           
