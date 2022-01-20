6a4007e7680ffcdd2f933b31493e8ebb1dcb9d07
Balraj Mahal <bmahal@microsoft.com>
2022-01-20 16:49:22 -0500

    Revert "Node testing mock overhaul" (#4427)

--- lib/msal-node/test/utils/MockUtils.ts
+++
@@ -1,4 +0,0 @@
-import * as msalCommon from '@azure/msal-common';
-type MSALCommonModule = typeof msalCommon;
-
-export const getMsalCommonAutoMock = (): MSALCommonModule => jest.genMockFromModule('@azure/msal-common')
