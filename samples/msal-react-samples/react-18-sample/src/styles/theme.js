95465a80f32866286df51fefae00e8b3e7bde454
Thomas Norling <thomas.l.norling@gmail.com>
2022-03-03 14:13:12 -0800

    Add sample for React 18 (#4546)
    * Support React 18
    
    * react 18 sample
    
    * Change files
    
    * Update samples/msal-react-samples/react-18-sample/README.md
    
    Co-authored-by: Jo Arroyo <joarroyo@microsoft.com>
    
    Co-authored-by: Jo Arroyo <joarroyo@microsoft.com>

---
+++ samples/msal-react-samples/react-18-sample/src/styles/theme.js
@@ -0,0 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+import { red } from '@mui/material/colors';
+
+// Create a theme instance.
+export const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+  },
+});
