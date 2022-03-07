8827910377868b04e8a352c02fa16d156a28220a
Thomas Norling <thomas.norling@microsoft.com>
2022-03-07 13:54:57 -0800

    update test

--- lib/msal-browser/test/interaction_client/WamInteractionClient.spec.ts
+++ lib/msal-browser/test/interaction_client/WamInteractionClient.spec.ts
@@ -3,16 +3,13 @@
  * Licensed under the MIT License.
  */
 
-import { Logger, AuthError, AuthErrorMessage, AuthenticationResult, AuthenticationScheme, AccountInfo } from "@azure/msal-common";
+import { AuthenticationScheme, AccountInfo } from "@azure/msal-common";
 import sinon from "sinon";
 import { WamMessageHandler } from "../../src/broker/wam/WamMessageHandler";
-import { BrowserAuthError, BrowserAuthErrorMessage } from "../../src/error/BrowserAuthError";
-import { ApiId, WamConstants, WamExtensionMethod } from "../../src/utils/BrowserConstants";
-import { WamAuthError } from "../../src/error/WamAuthError";
+import { ApiId } from "../../src/utils/BrowserConstants";
 import { WamInteractionClient } from "../../src/interaction_client/WamInteractionClient";
 import { PublicClientApplication } from "../../src/app/PublicClientApplication";
 import { ID_TOKEN_CLAIMS, RANDOM_TEST_GUID, TEST_CONFIG, TEST_DATA_CLIENT_INFO, TEST_TOKENS } from "../utils/StringConstants";
-import { CryptoOps } from "../../src/crypto/CryptoOps";
 
 describe("WamInteractionClient Tests", () => {
     globalThis.MessageChannel = require("worker_threads").MessageChannel; // jsdom does not include an implementation for MessageChannel
@@ -25,13 +22,12 @@ describe("WamInteractionClient Tests", () => {
     const wamProvider = new WamMessageHandler(pca.getLogger());
 
     // @ts-ignore
-    const wamInteractionClient = new WamInteractionClient(pca.config, pca.browserStorage, pca.browserCrypto, pca.getLogger(), pca.eventHandler, pca.navigationClient, ApiId.acquireTokenRedirect, wamProvider);
+    const wamInteractionClient = new WamInteractionClient(pca.config, pca.browserStorage, pca.browserCrypto, pca.getLogger(), pca.eventHandler, pca.navigationClient, ApiId.acquireTokenRedirect, wamProvider, RANDOM_TEST_GUID);
     let postMessageSpy: sinon.SinonSpy;
     let mcPort: MessagePort;
 
     beforeEach(() => {
         postMessageSpy = sinon.spy(window, "postMessage");
-        sinon.stub(CryptoOps.prototype, "createNewGuid").returns(RANDOM_TEST_GUID);
         sinon.stub(MessageEvent.prototype, "source").get(() => window); // source property not set by jsdom window messaging APIs
     });
 
@@ -54,28 +50,12 @@ describe("WamInteractionClient Tests", () => {
             };
 
             const testAccount: AccountInfo = {
-                homeAccountId: TEST_DATA_CLIENT_INFO.TEST_HOME_ACCOUNT_ID,
+                homeAccountId: `${TEST_DATA_CLIENT_INFO.TEST_UID}.${TEST_DATA_CLIENT_INFO.TEST_UTID}`,
                 localAccountId: TEST_DATA_CLIENT_INFO.TEST_UID,
                 environment: "login.windows.net",
                 tenantId: ID_TOKEN_CLAIMS.tid,
                 username: ID_TOKEN_CLAIMS.preferred_username
             };
-
-            const authenticationResult: AuthenticationResult = {
-                accessToken: mockWamResponse.access_token,
-                idToken: mockWamResponse.id_token,
-                uniqueId: ID_TOKEN_CLAIMS.oid,
-                tenantId: ID_TOKEN_CLAIMS.tid,
-                idTokenClaims: ID_TOKEN_CLAIMS,
-                authority: TEST_CONFIG.validAuthority,
-                scopes: ["User.Read"],
-                fromCache: false,
-                correlationId: RANDOM_TEST_GUID,
-                expiresOn: new Date(Date.now() + (mockWamResponse.expires_in * 1000)),
-                account: testAccount,
-                state: undefined,
-                tokenType: AuthenticationScheme.BEARER
-            }
             sinon.stub(WamMessageHandler.prototype, "sendMessage").callsFake((): Promise<object> => {
                 return Promise.resolve(mockWamResponse);
             });
