32d3c23d8699445c194aa77c2a3ac5ffe5ae5e92
Samuel Kubai <kamausamuel11@gmail.com>
2022-03-09 10:34:06 +0300

    feat: adding the metadata npm commands

---
+++ lib/msal-common/src/utils/Metadata.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+import metadataJson from "../utils/metadata.json";
+
+export const EndpointMetadata = metadataJson.endpointMetadata;
+export const InstanceDiscoveryMetadata = metadataJson.instanceDiscoveryMetadata
