a2d1c32b6250952ff29b042643a7f40be3777939
Jason Nutter <janutter@microsoft.com>
2022-03-29 10:40:13 -0700

    Ensure BrowserPerformanceMeasurements dont bubble up exceptions from window.performance APIs

--- src/telemetry/BrowserPerformanceMeasurement.ts
+++ src/telemetry/BrowserPerformanceMeasurement.ts
@@ -30,26 +30,38 @@ export class BrowserPerformanceMeasurement implements IPerformanceMeasurement {
 
     startMeasurement(): void {
         if (BrowserPerformanceMeasurement.supportsBrowserPerformance()) {
-            window.performance.mark(this.startMark);
+            try {
+                window.performance.mark(this.startMark);
+            } catch (e) {
+                // Silently catch
+            }
         }
     }
 
     endMeasurement():void {
         if (BrowserPerformanceMeasurement.supportsBrowserPerformance()) {
-            window.performance.mark(this.endMark);
-            window.performance.measure(this.measureName, this.startMark, this.endMark);
+            try {
+                window.performance.mark(this.endMark);
+                window.performance.measure(this.measureName, this.startMark, this.endMark);
+            } catch (e) {
+                // Silently catch
+            }
         }
     }
 
     flushMeasurement(): number | null {
         if (BrowserPerformanceMeasurement.supportsBrowserPerformance()) {
-            const entriesForMeasurement = window.performance.getEntriesByName(this.measureName, "measure");
-            if (entriesForMeasurement.length > 0) {
-                const durationMs = entriesForMeasurement[0].duration;
-                window.performance.clearMeasures(this.measureName);
-                window.performance.clearMarks(this.startMark);
-                window.performance.clearMarks(this.endMark);
-                return durationMs;
+            try {
+                const entriesForMeasurement = window.performance.getEntriesByName(this.measureName, "measure");
+                if (entriesForMeasurement.length > 0) {
+                    const durationMs = entriesForMeasurement[0].duration;
+                    window.performance.clearMeasures(this.measureName);
+                    window.performance.clearMarks(this.startMark);
+                    window.performance.clearMarks(this.endMark);
+                    return durationMs;
+                }
+            } catch (e) {
+                // Silently catch and return null
             }
         }
         return null;
--- test/telemetry/BrowserPerformanceMeasurement.spec.ts
+++ test/telemetry/BrowserPerformanceMeasurement.spec.ts
@@ -95,4 +95,73 @@ describe("BrowserPerformanceMeasurement.ts", () => {
 
         expect(BrowserPerformanceMeasurement.supportsBrowserPerformance()).toBe(false);
     });
+
+    describe("measurement APIs dont throw if performance APIs throw", () => {
+        describe("startMeasurement", () => {
+            it("mark", () => {
+                jest.spyOn(window.performance, "mark").mockImplementation(() => {
+                    throw "markError"
+                });
+
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+
+                expect(() => measurement.startMeasurement()).not.toThrow("markError");
+
+            });
+        });
+
+        describe("endMeasurement", () => {
+            it("mark", () => {
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+                
+                jest.spyOn(window.performance, "mark").mockImplementation(() => {
+                    throw "markError"
+                });
+
+                expect(() => measurement.endMeasurement()).not.toThrow("markError");
+            });
+
+            it("measure", () => {
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+                
+                jest.spyOn(window.performance, "measure").mockImplementation(() => {
+                    throw "measureError"
+                });
+    
+                expect(() => measurement.endMeasurement()).not.toThrow("measureError");
+            });
+        });
+
+        describe("flushMeasurements", () => {
+            it("getEntriesByName", () => {
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+                
+                jest.spyOn(window.performance, "getEntriesByName").mockImplementation(() => {
+                    throw "getEntriesByNameError"
+                });
+
+                expect(() => measurement.flushMeasurement()).not.toThrow("getEntriesByNameError");
+            });
+
+            it("clearMeasures", () => {
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+
+                jest.spyOn(window.performance, "clearMeasures").mockImplementation(() => {
+                    throw "clearMeasuresError"
+                });
+
+                expect(() => measurement.flushMeasurement()).not.toThrow("clearMeasuresError");
+            });
+
+            it("clearMarks", () => {
+                const measurement = new BrowserPerformanceMeasurement(eventName, correlationId);
+
+                jest.spyOn(window.performance, "clearMarks").mockImplementation(() => {
+                    throw "clearMarksError"
+                });
+
+                expect(() => measurement.flushMeasurement()).not.toThrow("clearMarksError");
+            });
+        });
+    });
 });
