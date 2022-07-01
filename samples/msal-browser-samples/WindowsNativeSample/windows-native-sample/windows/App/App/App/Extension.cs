64dcf731d8c13e448f8d03776ee51986ddb6f6a0
shylasummers <shylasummers@users.noreply.github.com>
2022-07-01 13:08:34 -0700

    Added bi-directional communication between C# extension code and the WebView2 window

--- samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App/Extension.cs
+++ samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App/Extension.cs
@@ -1,12 +1,17 @@
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
-using Microsoft.Web.WebView2.Core;
+using Microsoft.Web.WebView2;
 using System;
 using System.Threading.Tasks;
 public class Extension
 {
     public async Task ExecuteExtensionAsync(WebView2 WebView)
     {
-        await WebView.ExecuteScriptAsync("alert(window.document.URL);");
+        //await WebView.ExecuteScriptAsync("alert(window.document.URL);");
+        await WebView.ExecuteScriptAsync("chrome.webview.postMessage(window.document.URL)");
+    }
+    public void HandleWebMessage(Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs args)
+    {
+        Console.WriteLine(args.ToString());
     }
 }
--- samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App/MainWindow.xaml.cs
+++ samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App/MainWindow.xaml.cs
@@ -34,6 +34,10 @@ namespace App {
         private void CapacitorWebView_WebMessageReceived(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2WebMessageReceivedEventArgs args)
         {
             //Console.WriteLine(args.TryGetWebMessageAsString());
+            if (args.Source == sender.Source.ToString()) // validate source
+            {
+                this.Extension.HandleWebMessage(args);
+            }
         }
 
         private void CapacitorWebView_NavigationCompleted(WebView2 sender, Microsoft.Web.WebView2.Core.CoreWebView2NavigationCompletedEventArgs args)
