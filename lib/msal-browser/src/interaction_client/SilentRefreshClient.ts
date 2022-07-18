72b7b2543f954a7055159aec913ac74a911b7393
Balraj Mahal <bmahal@microsoft.com>
2022-07-18 09:26:58 -0700

    Update SilentRefreshClient.ts

--- lib/msal-browser/src/interaction_client/SilentRefreshClient.ts
+++ lib/msal-browser/src/interaction_client/SilentRefreshClient.ts
@@ -8,7 +8,6 @@ import { CommonSilentFlowRequest, AuthenticationResult, ServerTelemetryManager, 
 import { ApiId } from "../utils/BrowserConstants";
 import { BrowserAuthError } from "../error/BrowserAuthError";
 
-
 export class SilentRefreshClient extends StandardInteractionClient {
     /**
      * Exchanges the refresh token for new tokens
