57d25d2814af084d007d810d41f2f40765ab8434
shylasummers <shylasummers@users.noreply.github.com>
2022-07-21 09:56:19 -0700

    Update app.js

--- samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App (Package)/Public/app.js
+++ samples/msal-browser-samples/WindowsNativeSample/windows-native-sample/windows/App/App/App (Package)/Public/app.js
@@ -1,16 +1,9 @@
-function onClick(method)
-{
-    _method = "popup";
-    if(method === "redirect_i" || method === "redirect_o") {
-        _method = "redirect";
-    }
-    if(method === "redirect_o" || method === "popup_o") {
+function onClick(method) {
+    _method = "redirect";
+    if (method === "redirect_o") {
         promise = signOut(_method);
         promise.then((response) => {
             document.getElementById("r_btn").innerHTML = '<ion-button id = redirect_i onclick ="onClick(this.id)">Log in with redirect</ion-button>'
-        });
-        promise.then((response) => {
-            document.getElementById("p_btn").innerHTML = '<ion-button id = popup_i onclick ="onClick(this.id)">Log in with popup</ion-button>'
         });
         promise.then((response) => {
             document.getElementById("text").innerHTML = '<ion-text><h1>Please log in.</h1></ion-text>'
@@ -22,14 +15,10 @@ function onClick(method)
             acc = myMSALObj.getAllAccounts();
         });
         promise.then((response) => {
-            if(acc.length !== 0) {
+            if (acc.length !== 0) {
                 document.getElementById("r_btn").innerHTML = '<ion-button id = redirect_o onclick ="onClick(this.id)">Log out with redirect</ion-button>'
-            }
-        });
-        promise.then((response) => {
-            if(acc.length !== 0) {
-                document.getElementById("p_btn").innerHTML = '<ion-button id = popup_o onclick ="onClick(this.id)">Log out with popup</ion-button>'
             }
         });
     }
 }
+
