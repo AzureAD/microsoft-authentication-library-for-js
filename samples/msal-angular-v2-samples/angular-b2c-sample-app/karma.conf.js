d7cb1f9d16c0701cc5fc422d2cc2a1db48484767
derisen <dogan.ersen@gmal.com>
2022-08-04 15:11:14 -0700

    fix incorrect folder name

--- samples/msal-angular-v2-samples/angular-b2c-sample-app/karma.conf.js
+++ samples/msal-angular-v2-samples/angular-b2c-sample-app/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/angular14-b2c-sample'),
+      dir: require('path').join(__dirname, './coverage/angular-b2c-sample-app'),
       subdir: '.',
       reporters: [
         { type: 'html' },
