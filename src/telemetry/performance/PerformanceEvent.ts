4453009cccba7c29960fcdd543cf5acf034bf650
Robbie-Microsoft <rginsburg@microsoft.com>
2022-09-01 18:47:12 -0400

    Added silentTokenRetrievalStrategy to PerformanceEvents, for telemetry

--- src/telemetry/performance/PerformanceEvent.ts
+++ src/telemetry/performance/PerformanceEvent.ts
@@ -291,5 +291,12 @@ export type PerformanceEvent = {
      *
      * @type {?boolean}
      */
-    isNativeBroker?: boolean
+    isNativeBroker?: boolean,
+
+    /**
+     * The Silent Token Retrieval Strategy
+     *
+     * @type {?(number | undefined)}
+     */
+    silentTokenRetrievalStrategy?: number | undefined
 };
