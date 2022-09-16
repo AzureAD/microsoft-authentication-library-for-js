abba32d1f84a5fcc93c28796729b5b400ca7f359
Thomas Norling <thomas.norling@microsoft.com>
2022-09-16 16:25:54 -0700

    migrate to napi

--- extensions/msal-node-extensions/src/dpapi-addon/Dpapi.ts
+++ extensions/msal-node-extensions/src/dpapi-addon/Dpapi.ts
@@ -8,10 +8,5 @@ export interface DpapiBindings{
     unprotectData(encryptData: Uint8Array, optionalEntropy: Uint8Array, scope: string): Uint8Array
 }
 /* eslint-disable-next-line @typescript-eslint/no-var-requires, no-var, import/no-commonjs */
-export var Dpapi: DpapiBindings = require("bindings")({
-    bindings: "dpapi",
-    userDefinedTries: [
-        ["module_root", "node_modules", "@azure", "msal-node-extensions", "build", "Release",  "bindings"],
-    ]
-});
+export var Dpapi: DpapiBindings = require("bindings")("dpapi");
 export default Dpapi;
