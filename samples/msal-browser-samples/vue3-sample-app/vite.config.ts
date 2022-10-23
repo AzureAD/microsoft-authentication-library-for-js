29282988fd6054b1f510d07088d0c04c886ea589
Doğan Erişen <dogan.erisen@gmail.com>
2022-10-22 18:46:35 -0700

    update vue sample

--- samples/msal-browser-samples/vue3-sample-app/vite.config.ts
+++ samples/msal-browser-samples/vue3-sample-app/vite.config.ts
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+    server: {
+        host: 'localhost',
+        port: 3000,
+    },
 })
