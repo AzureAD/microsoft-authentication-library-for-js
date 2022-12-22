b102e32ec150af0a8f1d0b9359e5945f7d2814d7
konstantin-msft <kshabelko@microsoft.com>
2022-12-22 09:39:27 -0500

    - Split `customizable-e2e-test/browser.spec.ts` into `AAD` and `B2C` tests to mitigate concurrency. - Fix port number for `onPageLoad` test.

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/onPageLoad/authConfig.js
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/onPageLoad/authConfig.js
@@ -3,7 +3,7 @@ const msalConfig = {
     auth: {
         clientId: "3fba556e-5d4a-48e3-8e1a-fd57c12cb82e",
         authority: "https://login.windows-ppe.net/common/",
-        redirectUri: `http://localhost:30666`
+        redirectUri: `http://localhost:30663`
     },
     cache: {
         cacheLocation: "sessionStorage", // This configures where your cache will be stored
