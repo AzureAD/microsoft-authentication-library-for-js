87d49eda10f2f6dc4bb87964ca7fbdcd3ef44a94
Jo Arroyo <joarroyo@microsoft.com>
2023-01-25 11:48:57 -0800

    Fix tests for typo

--- test/interaction_handler/InteractionHandler.spec.ts
+++ test/interaction_handler/InteractionHandler.spec.ts
@@ -122,6 +122,7 @@ const performanceClient = {
     addPerformanceCallback: jest.fn(),
     emitEvents: jest.fn(),
     startPerformanceMeasurement: jest.fn(),
+    startPerformanceMeasuremeant: jest.fn(),
     generateId: jest.fn(),
     calculateQueuedTime: jest.fn(),
     addQueueMeasurement: jest.fn(),
