ec34b7a569454f12144311bc1858d51b251832a6
Hector Morales <hemoral@microsoft.com>
2023-02-07 12:33:44 -0800

    Undo puppeteer config change

--- samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
+++ samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
@@ -11,7 +11,7 @@ class PuppeteerEnvironment extends NodeEnvironment {
 
 		// connect to puppeteer
 		this.global.__BROWSER__ = await puppeteer.launch({
-            headless: false,
+            headless: true,
             ignoreDefaultArgs: ["--no-sandbox", "–disable-setuid-sandbox"]
         });
 	}
