83cc06eb79926e6c20993edb732933202a97228a
Pratik Manandhar <59083817+pratikfuse@users.noreply.github.com>
2023-02-08 09:35:34 +0545

    fixes

--- lib/msal-node/src/network/LoopbackClient.ts
+++ lib/msal-node/src/network/LoopbackClient.ts
@@ -3,56 +3,100 @@
  * Licensed under the MIT License.
  */
 
-import { Constants as CommonConstants, ServerAuthorizationCodeResponse, UrlString } from "@azure/msal-common";
+import {
+    Constants as CommonConstants,
+    ServerAuthorizationCodeResponse,
+    UrlString
+} from "@azure/msal-common";
 import { createServer, IncomingMessage, Server, ServerResponse } from "http";
 import { NodeAuthError } from "../error/NodeAuthError";
-import { Constants, HttpStatus, LOOPBACK_SERVER_CONSTANTS } from "../utils/Constants";
+import {
+    Constants,
+    HttpStatus,
+    LOOPBACK_SERVER_CONSTANTS
+} from "../utils/Constants";
 
+console.log("here");
 export class LoopbackClient {
     private server: Server;
+    private port: number = 64000;
+
+    async startServer() {
+        let error = null;
+        try {
+            this.server.listen(this.port, Constants.LOCALHOST);
+        } catch (e) {
+            error = e;
+        }
+        return error;
+    }
 
     /**
      * Spins up a loopback server which returns the server response when the localhost redirectUri is hit
-     * @param successTemplate 
-     * @param errorTemplate 
-     * @returns 
+     * @param successTemplate
+     * @param errorTemplate
+     * @returns
      */
-    async listenForAuthCode(successTemplate?: string, errorTemplate?: string): Promise<ServerAuthorizationCodeResponse> {
+    async listenForAuthCode(
+        successTemplate?: string,
+        errorTemplate?: string
+    ): Promise<ServerAuthorizationCodeResponse> {
         if (!!this.server) {
             throw NodeAuthError.createLoopbackServerAlreadyExistsError();
         }
 
-        const authCodeListener = new Promise<ServerAuthorizationCodeResponse>((resolve, reject) => {
-            this.server = createServer(async (req: IncomingMessage, res: ServerResponse) => {
-                const url = req.url;
-                if (!url) {
-                    res.end(errorTemplate || "Error occurred loading redirectUrl");
-                    reject(NodeAuthError.createUnableToLoadRedirectUrlError());
-                    return;
-                } else if (url === CommonConstants.FORWARD_SLASH) {
-                    res.end(successTemplate || "Auth code was successfully acquired. You can close this window now.");
-                    return;
-                }
-    
-                const authCodeResponse = UrlString.getDeserializedQueryString(url);
-                if (authCodeResponse.code) {
-                    const redirectUri = await this.getRedirectUri();
-                    res.writeHead(HttpStatus.REDIRECT, { location: redirectUri }); // Prevent auth code from being saved in the browser history
-                    res.end();
-                }
-                resolve(authCodeResponse);
-            });
-            this.server.listen(0); // Listen on any available port
-        });
+        const authCodeListener = new Promise<ServerAuthorizationCodeResponse>(
+            (resolve, reject) => {
+                this.server = createServer(
+                    async (req: IncomingMessage, res: ServerResponse) => {
+                        const url = req.url;
+                        if (!url) {
+                            res.end(
+                                errorTemplate ||
+                                    "Error occurred loading redirectUrl"
+                            );
+                            reject(
+                                NodeAuthError.createUnableToLoadRedirectUrlError()
+                            );
+                            return;
+                        } else if (url === CommonConstants.FORWARD_SLASH) {
+                            res.end(
+                                false
+                                    ? successTemplate
+                                    : "asfea" ||
+                                          "Auth code was successfully acquired. You can close this window now."
+                            );
+                            return;
+                        }
+
+                        const authCodeResponse = UrlString.getDeserializedQueryString(
+                            url
+                        );
+                        if (authCodeResponse.code) {
+                            const redirectUri = this.getRedirectUri();
+                            res.writeHead(HttpStatus.REDIRECT, {
+                                location: redirectUri
+                            }); // Prevent auth code from being saved in the browser history
+                            res.end();
+                        }
+                        resolve(authCodeResponse);
+                    }
+                );
+            }
+        );
 
         // Wait for server to be listening
-        await new Promise<void>((resolve) => {
+        await new Promise<void>(resolve => {
             let ticks = 0;
             const id = setInterval(() => {
-                if ((LOOPBACK_SERVER_CONSTANTS.TIMEOUT_MS / LOOPBACK_SERVER_CONSTANTS.INTERVAL_MS) < ticks) {
+                if (
+                    LOOPBACK_SERVER_CONSTANTS.TIMEOUT_MS /
+                        LOOPBACK_SERVER_CONSTANTS.INTERVAL_MS <
+                    ticks
+                ) {
                     throw NodeAuthError.createLoopbackServerTimeoutError();
                 }
-                
+
                 if (this.server.listening) {
                     clearInterval(id);
                     resolve();
@@ -66,22 +110,14 @@ export class LoopbackClient {
 
     /**
      * Get the port that the loopback server is running on
-     * @returns 
+     * @returns
      */
     getRedirectUri(): string {
         if (!this.server) {
             throw NodeAuthError.createNoLoopbackServerExistsError();
         }
-                
-        const address = this.server.address();
-        if (!address || typeof address === "string" || !address.port) {
-            this.closeServer();
-            throw NodeAuthError.createInvalidLoopbackAddressTypeError();
-        } 
 
-        const port = address && address.port;
-
-        return `${Constants.HTTP_PROTOCOL}${Constants.LOCALHOST}:${port}`;
+        return `${Constants.HTTP_PROTOCOL}${Constants.LOCALHOST}:${this.port}`;
     }
 
     /**
