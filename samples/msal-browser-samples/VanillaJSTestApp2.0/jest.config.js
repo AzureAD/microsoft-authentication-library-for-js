1e7bda54af9a9090c704ee4ca505ce020bec98de
Doğan Erişen <dogan.erisen@gmail.com>
2023-02-18 13:54:24 -0800

    Use matrix for running browser tests

--- samples/msal-browser-samples/VanillaJSTestApp2.0/jest.config.js
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/jest.config.js
@@ -3,13 +3,19 @@ const path = require("path");
 
 const APP_DIR = path.join(__dirname, 'app');
 
-const sampleFolders = fs.readdirSync(APP_DIR, { withFileTypes: true }).filter(function(file) {
-    return file.isDirectory() && file.name !== "node_modules" && fs.existsSync(path.resolve(APP_DIR, file.name, "jest.config.js"));
-}).map(function(file) {
-    return path.join(APP_DIR, file.name);
-});
+let sampleFolders;
+
+if (process.argv.find((x) => x.startsWith('--sample'))) {
+    sampleFolders = [APP_DIR + '/' + process.argv.find((x) => x.startsWith('--sample')).split('=')[1]];
+} else {
+    sampleFolders = fs.readdirSync(APP_DIR, { withFileTypes: true }).filter(function(file) {
+        return file.isDirectory() && file.name !== "node_modules" && fs.existsSync(path.resolve(APP_DIR, file.name, "jest.config.js"));
+    }).map(function(file) {
+        return path.join(APP_DIR, file.name);
+    });
+}
 
 module.exports = {
-    projects : sampleFolders,
+    projects: sampleFolders,
     testTimeout: 30000
 };
