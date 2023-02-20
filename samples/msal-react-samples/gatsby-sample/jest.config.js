dc3d4920c7e83da1f5eb7973e5facbc21fc76d93
Doğan Erişen <dogan.erisen@gmail.com>
2023-02-20 15:43:18 -0800

    fix failing test

--- samples/msal-react-samples/gatsby-sample/jest.config.js
+++ samples/msal-react-samples/gatsby-sample/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     displayName: "Gatsby",
     globals: {
         __PORT__: 3001,
-        __STARTCMD__: "npm run develop -- -p 3001"
+        __STARTCMD__: "NODE_ENV=development npm run develop -- -p 3001"
     },
     preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
 };
