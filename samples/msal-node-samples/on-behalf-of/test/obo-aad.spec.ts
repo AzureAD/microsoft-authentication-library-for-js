11a532e569da6f195cd14392e0cda3c2e81e750c
Robert Ginsburg <rgins16@gmail.com>
2023-02-22 14:07:48 -0500

    Added dotenv back to index.js

--- samples/msal-node-samples/on-behalf-of/test/obo-aad.spec.ts
+++ samples/msal-node-samples/on-behalf-of/test/obo-aad.spec.ts
@@ -15,6 +15,7 @@ import path from "path";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: path.resolve(__dirname, "../../../../.env") });
+console.log(path.resolve(__dirname, "../../../../.env"));
 
 const { acquireTokenByCode, acquireTokenObo } = require("../index");
 
