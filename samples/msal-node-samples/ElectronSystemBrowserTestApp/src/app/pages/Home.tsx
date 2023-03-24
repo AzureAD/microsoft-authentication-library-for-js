bcca459e62b699cb476291676b32c1eb140e55fe
Salman Salem <v-ssalem@microsoft.com>
2023-03-24 12:06:25 -0700

    Update Electron Samples (#5785)
    I've added React to the [Electron system browser
    Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/ElectronSystemBrowserTestApp)
    and Retired the [React Electron
    sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/ElectronReactTestApp).
    I've also added a warning message to the
    [ElectronTestApp](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-node-samples/ElectronTestApp)
    sample and removed the e2e tests due to the sample not following the
    [authentication best practices for native
    apps](https://www.rfc-editor.org/rfc/rfc8252).
    
    ---------
    
    Co-authored-by: Doğan Erişen <v-derisen@microsoft.com>

---
+++ samples/msal-node-samples/ElectronSystemBrowserTestApp/src/app/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Home = () => {
+    return (
+        <div>
+            <h5 className="text-center">
+                Microsoft Authentication Library For Node Electron Sample
+            </h5>
+        </div>
+    );
+}
