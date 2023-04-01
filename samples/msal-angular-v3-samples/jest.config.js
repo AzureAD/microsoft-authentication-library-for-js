988534c96d30c834b2b81c52c7ee70f2a61b647e
Jo Arroyo <joarroyo@microsoft.com>
2023-03-31 17:24:59 -0700

    Update jest.config for v3 samples

--- samples/msal-angular-v3-samples/jest.config.js
+++ samples/msal-angular-v3-samples/jest.config.js
@@ -1,12 +1,16 @@
 const fs = require("fs");
 const path = require("path");
 
-const sampleFolders = fs.readdirSync(__dirname, { withFileTypes: true }).filter(function(file) {
-  return file.isDirectory() && file.name !== "node_modules" && fs.existsSync(path.resolve(__dirname, file.name, "jest.config.js"));
-}).map(function(file) {
-  return `<rootDir>/${file.name}`;
-});
+const APP_DIR = path.join(__dirname, 'app');
+
+// if a sample is specified, only run tests for that sample
+const sampleFolders = process.argv.find(arg => arg.startsWith('--sample='))
+    ? [path.join(APP_DIR, process.argv.find(arg => arg.startsWith('--sample='))?.split('=')[1])]
+    : fs.readdirSync(APP_DIR, { withFileTypes: true })
+        .filter(file => file.isDirectory() && file.name !== "node_modules" && fs.existsSync(path.resolve(APP_DIR, file.name, "jest.config.js")))
+        .map(file => path.join(APP_DIR, file.name));
 
 module.exports = {
-  projects : sampleFolders
+    projects: sampleFolders,
+    testTimeout: 30000
 };
