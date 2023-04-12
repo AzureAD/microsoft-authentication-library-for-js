0f5df36d90f9aa2403e13d7de9fc64d1d03f2fc9
Salman Salem <v-ssalem@microsoft.com>
2023-04-12 08:58:33 -0700

    Retiring React 18 sample (#5841)
    Since we upgraded our React samples to version 18, we will no longer
    need to maintain the React 18 sample.
    
    Co-authored-by: Doğan Erişen <v-derisen@microsoft.com>

--- samples/msal-react-samples/react-18-sample/src/ui-components/ErrorComponent.jsx
+++
@@ -1,5 +0,0 @@
-import Typography from "@mui/material/Typography";
-
-export const ErrorComponent = ({error}) => {
-    return <Typography variant="h6">An Error Occurred: {error.errorCode}</Typography>;
-}
