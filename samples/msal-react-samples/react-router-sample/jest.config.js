60930931160a1af533002340d4d3ab82767b63e0
Salman Salem <v-ssalem@microsoft.com>
2023-04-17 09:29:36 -0700

    Add Redirecting to an empty page.  (#5840)
    Add redirecting to an empty page to the react-router and the default
    vanillaJs samples to showcase MSAL.js best performance practices.
    
    ---------
    
    Co-authored-by: Doğan Erişen <v-derisen@microsoft.com>

--- samples/msal-react-samples/react-router-sample/jest.config.js
+++ samples/msal-react-samples/react-router-sample/jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
     displayName: "React Router",
     globals: {
         __PORT__: 3000,
-        __STARTCMD__: "npm start"
+        __STARTCMD__: "env-cmd -f .env.e2e npm start",
     },
-    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
+    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js",
 };
