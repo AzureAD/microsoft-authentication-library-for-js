48a039939639196f3323558d4558dde0ff6adfd4
Jo Arroyo <joarroyo@microsoft.com>
2023-05-10 11:34:30 -0700

    Remove angularjs, angular v1 and v2 samples

--- samples/msal-angular-v2-samples/angular10-browser-sample/src/app/msal/index.ts
+++
@@ -1,6 +0,0 @@
-export { MsalService } from './msal.service';
-export { MsalGuard } from './msal.guard';
-export { MsalInterceptor } from './msal.interceptor';
-export { MSAL_INSTANCE } from './constants';
-export { MsalBroadcastService } from './msal.broadcast.service';
-export { IMsalService } from './IMsalService';
