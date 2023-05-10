f76d1d53c14d3a21ff2e704bf035985901e48e96
Jo Arroyo <joarroyo@microsoft.com>
2023-05-10 11:34:30 -0700

    Remove angularjs, angular v1 and v2 samples

--- samples/msal-angular-v2-samples/angular14-rxjs6-sample-app/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    displayName: "angular14-rxjs6-sample-app",
-    globals: {
-        __PORT__: 4211,
-        __STARTCMD__: "npm start -- --port 4211",
-        __TIMEOUT__: 90000
-    },
-    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
-};
