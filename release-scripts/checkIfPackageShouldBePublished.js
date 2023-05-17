143f37c9133fc48358a96737bafe9f2dff6d2503
Hector Morales <hemoral@microsoft.com>
2023-05-17 11:44:32 -0700

    Fix check package availability (#6029)

--- release-scripts/checkIfPackageShouldBePublished.js
+++ release-scripts/checkIfPackageShouldBePublished.js
@@ -6,13 +6,14 @@
 const execSync = require("child_process").execSync;
 
 function checkVersion(packageName, version) {
-	return execSync(`npm view ${packageName}@${version}`).toString().trim();
+	// It is expected npm view will fail for packages that are about to be published. 2> dev/null/ is used to suppress the error.
+	return execSync(`npm view ${packageName}@${version} 2> /dev/null`).toString().trim();
 }
 
 const path = require("path");
 const libPath = path.join(__dirname, '..', process.argv[2]);
 
-const {name, version} = require(`${libPath}/package.json`);
+const { name, version } = require(`${libPath}/package.json`);
 
 const versionIsPublished = checkVersion(name, version);
 
