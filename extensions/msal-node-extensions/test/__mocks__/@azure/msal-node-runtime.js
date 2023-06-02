fd71319241d6fc65e91bfc624521d0ac06e80534
Thomas Norling <thomas.norling@microsoft.com>
2023-06-02 16:27:00 +0000

    update mock

--- extensions/msal-node-extensions/test/__mocks__/@azure/msal-node-runtime.js
+++ extensions/msal-node-extensions/test/__mocks__/@azure/msal-node-runtime.js
@@ -5,21 +5,24 @@
 
 const mock = jest.requireActual("@azure/msal-node-runtime");
 
-const asyncHandle = {
-    CancelAsyncOperation: () => { }
-}
-mock.msalNodeRuntime = {
-    ReadAccountByIdAsync: (accountId, correlationId, callback) => asyncHandle,
-    SignInAsync: (windowHandle, authParams, correlationId, accountHint, callback) => asyncHandle,
-    SignInSilentlyAsync: (authParams, correlationId, callback) => asyncHandle,
-    SignInInteractivelyAsync: (windowHandle, authParams, correlationId, accountHint, callback) => asyncHandle,
-    AcquireTokenSilentlyAsync: (authParams, correlationId, account, callback) => asyncHandle,
-    AcquireTokenInteractivelyAsync: (windowHandle, authParams, correlationId, account, callback) => asyncHandle,
-    SignOutSilentlyAsync: (clientId, correlationId, account, callback) => asyncHandle,
-    RegisterLogger: (callback, isPiiEnabled) => { },
-    DiscoverAccountsAsync: (clientId, correlationId, callback) => asyncHandle,
-    StartupError: undefined,
-    AuthParameters: mock.msalNodeRuntime.AuthParameters
+// Only mock on Windows so that we can test that non supported systems properly report that the broker is unavailable
+if (process.platform === "win32") {
+    const asyncHandle = {
+        CancelAsyncOperation: () => { }
+    }
+    mock.msalNodeRuntime = {
+        ReadAccountByIdAsync: (accountId, correlationId, callback) => asyncHandle,
+        SignInAsync: (windowHandle, authParams, correlationId, accountHint, callback) => asyncHandle,
+        SignInSilentlyAsync: (authParams, correlationId, callback) => asyncHandle,
+        SignInInteractivelyAsync: (windowHandle, authParams, correlationId, accountHint, callback) => asyncHandle,
+        AcquireTokenSilentlyAsync: (authParams, correlationId, account, callback) => asyncHandle,
+        AcquireTokenInteractivelyAsync: (windowHandle, authParams, correlationId, account, callback) => asyncHandle,
+        SignOutSilentlyAsync: (clientId, correlationId, account, callback) => asyncHandle,
+        RegisterLogger: (callback, isPiiEnabled) => { },
+        DiscoverAccountsAsync: (clientId, correlationId, callback) => asyncHandle,
+        StartupError: undefined,
+        AuthParameters: mock.msalNodeRuntime.AuthParameters
+    }
 }
 
 module.exports = mock;
