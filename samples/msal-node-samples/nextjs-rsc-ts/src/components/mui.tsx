4d8fcfa525b819fdd9e1d11b30a7199b43486e6b
Herman Jensen <herman.jensen@intility.no>
2023-06-03 10:14:28 +0200

    add mui re-export comment

--- samples/msal-node-samples/nextjs-rsc-ts/src/components/mui.tsx
+++ samples/msal-node-samples/nextjs-rsc-ts/src/components/mui.tsx
@@ -1,5 +1,9 @@
 "use client";
 
+// https://nextjs.org/docs/getting-started/react-essentials#third-party-packages
+// this file re-exports modules from Material UI explicitly marked as client components
+// so that they can be imported and used in server components
+
 export {
   ButtonGroup,
   Button,
