1da836e48f8272b0978fe5131bbe448c4a45361a
Robert Ginsburg <rgins16@gmail.com>
2023-06-07 15:54:26 -0400

    updated docs

--- lib/msal-node/src/request/ClientCredentialRequest.ts
+++ lib/msal-node/src/request/ClientCredentialRequest.ts
@@ -7,7 +7,7 @@ import { CommonClientCredentialRequest } from "@azure/msal-common";
 
 /**
  * CommonClientCredentialRequest
- * - scopes                  - Array of scopes the application is requesting access to.
+ * - scopes                  - An array containing one (string) scope the application is requesting access to.
  * - authority               - URL of the authority, the security token service (STS) from which MSAL will acquire tokens.
  * - correlationId           - Unique GUID set per request to trace a request end-to-end for telemetry purposes.
  * - skipCache               - Skip token cache lookup and force request to authority to get a a new token. Defaults to false.
@@ -18,13 +18,11 @@ import { CommonClientCredentialRequest } from "@azure/msal-common";
 export type ClientCredentialRequest = Partial<
     Omit<
         CommonClientCredentialRequest,
-        | "scopes"
         | "resourceRequestMethod"
         | "resourceRequestUri"
         | "requestedClaimsHash"
         | "clientAssertion"
     >
 > & {
-    scopes: Array<string>;
     clientAssertion?: string;
 };
