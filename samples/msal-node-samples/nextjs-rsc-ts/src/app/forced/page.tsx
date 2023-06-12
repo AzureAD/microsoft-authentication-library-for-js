6042ee216395aa064156b348b4d9ed9d472bba2c
Herman Jensen <herman.jensen@intility.no>
2023-06-12 19:34:04 +0200

    style: fix formatting

--- samples/msal-node-samples/nextjs-rsc-ts/src/app/forced/page.tsx
+++ samples/msal-node-samples/nextjs-rsc-ts/src/app/forced/page.tsx
@@ -1,12 +1,11 @@
 import { authProvider } from "~/services/auth";
 
 export default async function ForcedPage() {
-    const account = await authProvider.getAccount();
+  const account = await authProvider.getAccount();
 
-    if (!account) {
-        throw new Error("How did this happen?");
-    }
+  if (!account) {
+    throw new Error("How did this happen?");
+  }
 
-    return <div>Hello {account.name}</div>;
+  return <div>Hello {account.name}</div>;
 }
-
