ba337826a995f16c6d46ffe60166a423913827c6
Herman Jensen <herman.jensen@intility.no>
2023-06-15 13:11:37 +0200

    fix: use calendarRequest in eventConsent route

--- samples/msal-node-samples/nextjs-rsc-ts/src/app/auth/eventConsent/route.ts
+++ samples/msal-node-samples/nextjs-rsc-ts/src/app/auth/eventConsent/route.ts
@@ -1,10 +1,10 @@
 import { redirect } from "next/navigation";
 import { NextRequest } from "next/server";
-import { loginRequest } from "~/serverConfig";
+import { calendarRequest } from "~/serverConfig";
 import { authProvider } from "~/services/auth";
 
 export async function GET(request: NextRequest) {
   const returnTo = new URL("/event", request.url).toString();
 
-  redirect(await authProvider.getAuthCodeUrl(loginRequest, returnTo));
+  redirect(await authProvider.getAuthCodeUrl(calendarRequest, returnTo));
 }
