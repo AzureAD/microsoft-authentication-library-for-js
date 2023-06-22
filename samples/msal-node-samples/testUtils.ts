e1b27616046293431c85594a598c4e81449d24c7
Thomas Norling <thomas.norling@microsoft.com>
2023-06-22 15:42:53 -0700

    Fix imports

--- samples/msal-node-samples/testUtils.ts
+++ samples/msal-node-samples/testUtils.ts
@@ -3,7 +3,7 @@
  * Licensed under the MIT License.
  */
 
-import { Screenshot } from "e2eTestUtils/TestUtils";
+import { Screenshot } from "e2eTestUtils/src/TestUtils";
 import { Page, HTTPResponse } from "puppeteer";
 import fs from "fs";
 
