f714b51f03eb87ccd0102d886c814b3dab2b5cc9
Thomas Norling <thomas.norling@microsoft.com>
2023-06-27 12:07:02 -0700

    Update dependencies (#6159)
    - Updates Jest, ts-jest, types to latest
    - Removes tslint and husky
    - Updates sample dependencies to use beta releases of MSAL libs

--- lib/msal-node/src/index.ts
+++ lib/msal-node/src/index.ts
@@ -112,7 +112,7 @@ export {
     // IAppTokenProvider
     IAppTokenProvider,
     AppTokenProviderParameters,
-    AppTokenProviderResult,
+    AppTokenProviderResult
 } from "@azure/msal-common";
 
 export { version } from "./packageMetadata";
