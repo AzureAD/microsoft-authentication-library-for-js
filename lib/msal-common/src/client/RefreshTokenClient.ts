396f5b2ff4bfbd56a991e2b0b706c90e5f98fdec
shylasummers <shylasummers@users.noreply.github.com>
2023-07-28 15:55:00 -0700

    Disable telemetry parameters in the token request when using OIDC protocol mode (#6262)
    This PR automatically removes the `x-client-current-telemetry` and
    `x-client-last-telemetry` parameters from the token request when using
    OIDC protocol mode.
    
    [Relevant
    issue](https://github.com/AzureAD/microsoft-authentication-library-for-js/issues/4361)

--- lib/msal-common/src/client/RefreshTokenClient.ts
+++ lib/msal-common/src/client/RefreshTokenClient.ts
@@ -3,7 +3,10 @@
  * Licensed under the MIT License.
  */
 
-import { ClientConfiguration } from "../config/ClientConfiguration";
+import {
+    ClientConfiguration,
+    isOidcProtocolMode,
+} from "../config/ClientConfiguration";
 import { BaseClient } from "./BaseClient";
 import { CommonRefreshTokenRequest } from "../request/CommonRefreshTokenRequest";
 import { Authority } from "../authority/Authority";
@@ -356,7 +359,7 @@ export class RefreshTokenClient extends BaseClient {
         );
         parameterBuilder.addThrottling();
 
-        if (this.serverTelemetryManager) {
+        if (this.serverTelemetryManager && !isOidcProtocolMode(this.config)) {
             parameterBuilder.addServerTelemetry(this.serverTelemetryManager);
         }
 
