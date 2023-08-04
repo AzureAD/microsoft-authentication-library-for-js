73f3d4e96a3020c9a7b4f047336ad3de2c2c2775
Jo Arroyo <joarroyo@microsoft.com>
2023-08-04 12:44:58 -0700

    Update unit tests

--- samples/msal-angular-v3-samples/angular-standalone-sample/src/app/app.component.spec.ts
+++ samples/msal-angular-v3-samples/angular-standalone-sample/src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalService } from '@azure/msal-angular';
 import { AppComponent } from './app.component';
+import { MSALGuardConfigFactory, MSALInstanceFactory } from 'src/main';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +11,20 @@ describe('AppComponent', () => {
         RouterTestingModule,
         AppComponent
     ],
-}).compileComponents();
+    providers: [
+        MsalService,
+        MsalGuard,
+        MsalBroadcastService,
+        {
+          provide: MSAL_INSTANCE,
+          useFactory: MSALInstanceFactory
+        },
+        {
+          provide: MSAL_GUARD_CONFIG,
+          useFactory: MSALGuardConfigFactory
+        }
+      ]
+    }).compileComponents();
   });
 
   it('should create the app', () => {
@@ -18,16 +33,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-standalone-sample'`, () => {
+  it(`should have as title 'Angular Standalone Sample - MSAL Angular v3'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-standalone-sample');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-standalone-sample app is running!');
+    expect(app.title).toEqual('Angular Standalone Sample - MSAL Angular v3');
   });
 });
