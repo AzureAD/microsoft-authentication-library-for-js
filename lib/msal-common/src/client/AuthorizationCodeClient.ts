56cda94f998a291ed2801ae5cdb653221a18aa38
Sameera Gajjarapu <sameera.gajjarapu@microsoft.com>
2023-08-17 11:46:54 -0700

    Update lib/msal-common/src/client/AuthorizationCodeClient.ts
    Co-authored-by: Thomas Norling <thomas.norling@microsoft.com>

--- lib/msal-common/src/client/AuthorizationCodeClient.ts
+++ lib/msal-common/src/client/AuthorizationCodeClient.ts
@@ -400,9 +400,8 @@ export class AuthorizationCodeClient extends BaseClient {
                 PerformanceEvents.PopTokenGenerateCnf,
                 request.correlationId
             );
-            const reqCnfData = request.reqCnf
-                ? request.reqCnf
-                : await popTokenGenerator.generateCnf(request);
+            const reqCnfData = request.reqCnf ||
+                await popTokenGenerator.generateCnf(request);
             // SPA PoP requires full Base64Url encoded req_cnf string (unhashed)
             parameterBuilder.addPopToken(reqCnfData.reqCnfString);
         } else if (request.authenticationScheme === AuthenticationScheme.SSH) {
