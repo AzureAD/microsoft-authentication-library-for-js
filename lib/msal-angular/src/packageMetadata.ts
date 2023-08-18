2c03b3d7a024cd607e4bc502d9d1406fa9164925
Hector Morales <hectormg@hey.com>
2023-08-18 11:43:43 -0700

    Bump package versions

--- lib/msal-angular/src/packageMetadata.ts
+++ lib/msal-angular/src/packageMetadata.ts
@@ -1,3 +1,3 @@
 /* eslint-disable header/header */
-export const name = '@azure/msal-angular';
-export const version = '3.0.1';
+export const name = "@azure/msal-angular";
+export const version = "3.0.2";
