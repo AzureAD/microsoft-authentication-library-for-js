cf0d67d1eda847756e259bad236ce16e9e90b612
Sameera Gajjarapu <sameera.gajjarapu@microsoft.com>
2023-08-23 22:41:09 -0700

    Adding telemetry

--- lib/msal-browser/src/app/ClientApplication.ts
+++ lib/msal-browser/src/app/ClientApplication.ts
@@ -188,7 +188,9 @@ export abstract class ClientApplication {
 
         if(!this.config.cache.claimsBasedCachingEnabled) {
             this.logger.verbose("Claims-based caching is disabled. Clearing the previous cache with claims");
+            const claimsTokensRemovalMeasurement = this.performanceClient.startMeasurement(PerformanceEvents.ClearTokensAndKeysWithClaims);
             await this.browserStorage.clearTokensAndKeysWithClaims();
+            claimsTokensRemovalMeasurement.endMeasurement(PerformanceEvents.ClearTokensAndKeysWithClaims);
         }
 
         this.initialized = true;
