1bf349411c6b2785a4e16216862ed413152e9fb7
Thomas Norling <thomas.norling@microsoft.com>
2023-08-25 10:10:01 -0700

    Add tests

--- lib/msal-common/src/telemetry/performance/StubPerformanceClient.ts
+++ lib/msal-common/src/telemetry/performance/StubPerformanceClient.ts
@@ -3,10 +3,9 @@
  * Licensed under the MIT License.
  */
 
-import { IPerformanceClient } from "./IPerformanceClient";
+import { IPerformanceClient, InProgressPerformanceEvent } from "./IPerformanceClient";
 import { IPerformanceMeasurement } from "./IPerformanceMeasurement";
-import { PerformanceClient } from "./PerformanceClient";
-import { PerformanceEvent } from "./PerformanceEvent";
+import { PerformanceEvent, PerformanceEventStatus } from "./PerformanceEvent";
 
 export class StubPerformanceMeasurement implements IPerformanceMeasurement {
     startMeasurement(): void {
@@ -21,11 +20,31 @@ export class StubPerformanceMeasurement implements IPerformanceMeasurement {
 }
 
 export class StubPerformanceClient
-    extends PerformanceClient
     implements IPerformanceClient
 {
     generateId(): string {
         return "callback-id";
+    }
+
+    startMeasurement(measureName: string, correlationId?: string | undefined): InProgressPerformanceEvent {
+        return {
+            end: () => null,
+            discard: () => {},
+            add: () => {},
+            increment: () => {},
+            event: {
+                eventId: this.generateId(),
+                status: PerformanceEventStatus.InProgress,
+                authority: "",
+                libraryName: "",
+                libraryVersion: "",
+                clientId: "",
+                name: measureName,
+                startTimeMs: Date.now(),
+                correlationId: correlationId || "",
+            },
+            measurement: new StubPerformanceMeasurement(),
+        }
     }
 
     startPerformanceMeasurement(): IPerformanceMeasurement {
