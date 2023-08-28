cec79a28e71c1983c8f03375d9869f83abe1e4d7
Lalima Sharda <lalimasharda@microsoft.com>
2023-08-28 15:25:11 -0700

    formatting

--- lib/msal-browser/src/request/ClearCacheRequest.ts
+++ lib/msal-browser/src/request/ClearCacheRequest.ts
@@ -7,8 +7,8 @@ import { AccountInfo } from "@azure/msal-common";
 
 /**
  * ClearCacheRequest
- * - account                - Account object that will be logged out of. All tokens tied to this account will be cleared.
  * - correlationId          - Unique GUID set per request to trace a request end-to-end for telemetry purposes.
+ * - account                - Account object that will be logged out of. All tokens tied to this account will be cleared.
  */
 export type ClearCacheRequest = {
     correlationId?: string;
