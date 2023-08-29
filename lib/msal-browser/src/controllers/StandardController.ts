ca09bbd8c4ea6848d95a552b08dce2d7ccd8fcae
Thomas Norling <thomas.norling@microsoft.com>
2023-08-29 11:38:50 -0700

    Add wrapper functions for telemetry & logging (#6383)
    - Introduces `invoke` and `invokeAsync` helper functions to wrap APIs
    with telemetry measurements and trace logging.
    - Adds perf measurements for ssoSilent/iframed flows
    - Moves `httpVerToken` and `refreshTokenSize` to more central place
    right after POST
    - Remove `httpVerAuthority` is it represents the same thing as
    `httpVerToken` but with a different name
    
    ### Pros
    
    - Makes it easier and less verbose to add logging & take perf
    measurements
    - In theory should be less expensive from a package size perspective
    
    ### Cons
    
    - Makes code a bit harder to read
    - If a function is called from multiple places each callsite must wrap
    the function (in contrast to being baked in today)

--- lib/msal-browser/src/controllers/StandardController.ts
+++ lib/msal-browser/src/controllers/StandardController.ts
@@ -26,6 +26,7 @@ import {
     AccountEntity,
     ServerResponseType,
     UrlString,
+    invokeAsync,
 } from "@azure/msal-common";
 import {
     BrowserCacheManager,
@@ -1068,11 +1069,13 @@ export class StandardController implements IController {
             request.correlationId
         );
 
-        this.performanceClient.setPreQueueTime(
+        return invokeAsync(
+            silentIframeClient.acquireToken.bind(silentIframeClient),
             PerformanceEvents.SilentIframeClientAcquireToken,
+            this.logger,
+            this.performanceClient,
             request.correlationId
-        );
-        return silentIframeClient.acquireToken(request);
+        )(request);
     }
 
     // #endregion
--- lib/msal-common/src/client/BaseClient.ts
+++ lib/msal-common/src/client/BaseClient.ts
@@ -25,6 +25,7 @@ import { buildClientInfoFromHomeAccountId } from "../account/ClientInfo";
 import { IPerformanceClient } from "../telemetry/performance/IPerformanceClient";
 import { RequestParameterBuilder } from "../request/RequestParameterBuilder";
 import { BaseAuthRequest } from "../request/BaseAuthRequest";
+import { PerformanceEvents } from "../telemetry/performance/PerformanceEvent";
 
 /**
  * Base application class which will construct requests to send to and handle responses from the Microsoft STS using the authorization code flow.
@@ -139,14 +140,27 @@ export abstract class BaseClient {
         tokenEndpoint: string,
         queryString: string,
         headers: Record<string, string>,
-        thumbprint: RequestThumbprint
+        thumbprint: RequestThumbprint,
+        correlationId: string
     ): Promise<NetworkResponse<ServerAuthorizationTokenResponse>> {
+        this.performanceClient?.addQueueMeasurement(
+            PerformanceEvents.BaseClientCreateTokenRequestHeaders,
+            correlationId
+        );
         const response =
             await this.networkManager.sendPostRequest<ServerAuthorizationTokenResponse>(
                 thumbprint,
                 tokenEndpoint,
                 { body: queryString, headers: headers }
             );
+        this.performanceClient?.addFields(
+            {
+                refreshTokenSize: response.body.refresh_token?.length || 0,
+                httpVerToken:
+                    response.headers?.[HeaderNames.X_MS_HTTP_VERSION] || "",
+            },
+            correlationId
+        );
 
         if (
             this.config.serverTelemetryManager &&
