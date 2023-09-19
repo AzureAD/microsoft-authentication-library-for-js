700dddb34265d8b7c037dceb39f58a9180411787
Konstantin <kshabelko@microsoft.com>
2023-09-19 13:30:17 -0400

    - Resolve `e2e-test-utils` package from workspaces.

---
+++ samples/e2eTestUtils/src/index.ts
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export { Screenshot, setupCredentials, enterCredentials } from "./TestUtils";
+export { LabClient } from "./LabClient";
+export type { LabApiQueryParams } from "./LabApiQueryParams";
+export { AzureEnvironments, AppTypes } from "./Constants";
+export { BrowserCacheUtils } from "./BrowserCacheTestUtils";
+export { Browser, Page, BrowserContext } from "puppeteer";
