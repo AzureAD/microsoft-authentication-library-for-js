5e79b3c60772bb640716c971049b063ad67c8212
Thomas Norling <thomas.norling@microsoft.com>
2023-09-19 13:07:21 -0700

    refactor remaining errors

--- lib/msal-browser/src/error/NativeAuthError.ts
+++ lib/msal-browser/src/error/NativeAuthError.ts
@@ -14,6 +14,10 @@ import {
     BrowserAuthErrorCodes,
 } from "./BrowserAuthError";
 
+import * as NativeAuthErrorCodes from "./NativeAuthErrorCodes";
+export { NativeAuthErrorCodes };
+import * as NativeStatusCodes from "../broker/nativeBroker/NativeStatusCodes";
+
 export type OSError = {
     error?: number;
     protocol_error?: string;
@@ -24,125 +28,86 @@ export type OSError = {
 
 const INVALID_METHOD_ERROR = -2147186943;
 
-export const NativeStatusCode = {
-    USER_INTERACTION_REQUIRED: "USER_INTERACTION_REQUIRED",
-    USER_CANCEL: "USER_CANCEL",
-    NO_NETWORK: "NO_NETWORK",
-    TRANSIENT_ERROR: "TRANSIENT_ERROR",
-    PERSISTENT_ERROR: "PERSISTENT_ERROR",
-    DISABLED: "DISABLED",
-    ACCOUNT_UNAVAILABLE: "ACCOUNT_UNAVAILABLE",
-} as const;
-export type NativeStatusCode =
-    (typeof NativeStatusCode)[keyof typeof NativeStatusCode];
-
-export const NativeAuthErrorMessage = {
-    extensionError: {
-        code: "ContentError",
-    },
-    userSwitch: {
-        code: "user_switch",
-        desc: "User attempted to switch accounts in the native broker, which is not allowed. All new accounts must sign-in through the standard web flow first, please try again.",
-    },
-    tokensNotFoundInCache: {
-        code: "tokens_not_found_in_internal_memory_cache",
-        desc: "Tokens not cached in MSAL JS internal memory, please make the WAM request",
-    },
+export const NativeAuthErrorMessages = {
+    [NativeAuthErrorCodes.userSwitch]:
+        "User attempted to switch accounts in the native broker, which is not allowed. All new accounts must sign-in through the standard web flow first, please try again.",
 };
 
 export class NativeAuthError extends AuthError {
     ext: OSError | undefined;
 
-    constructor(errorCode: string, description: string, ext?: OSError) {
+    constructor(errorCode: string, description?: string, ext?: OSError) {
         super(errorCode, description);
 
         Object.setPrototypeOf(this, NativeAuthError.prototype);
         this.name = "NativeAuthError";
         this.ext = ext;
     }
+}
 
-    /**
-     * These errors should result in a fallback to the 'standard' browser based auth flow.
-     */
-    isFatal(): boolean {
-        if (
-            this.ext &&
-            this.ext.status &&
-            (this.ext.status === NativeStatusCode.PERSISTENT_ERROR ||
-                this.ext.status === NativeStatusCode.DISABLED)
-        ) {
+/**
+ * These errors should result in a fallback to the 'standard' browser based auth flow.
+ */
+export function isFatalNativeAuthError(error: NativeAuthError): boolean {
+    if (
+        error.ext &&
+        error.ext.status &&
+        (error.ext.status === NativeStatusCodes.PERSISTENT_ERROR ||
+            error.ext.status === NativeStatusCodes.DISABLED)
+    ) {
+        return true;
+    }
+
+    if (
+        error.ext &&
+        error.ext.error &&
+        error.ext.error === INVALID_METHOD_ERROR
+    ) {
+        return true;
+    }
+
+    switch (error.errorCode) {
+        case NativeAuthErrorCodes.contentError:
             return true;
-        }
+        default:
+            return false;
+    }
+}
 
-        if (
-            this.ext &&
-            this.ext.error &&
-            this.ext.error === INVALID_METHOD_ERROR
-        ) {
-            return true;
-        }
-
-        switch (this.errorCode) {
-            case NativeAuthErrorMessage.extensionError.code:
-                return true;
-            default:
-                return false;
+/**
+ * Create the appropriate error object based on the WAM status code.
+ * @param code
+ * @param description
+ * @param ext
+ * @returns
+ */
+export function createNativeAuthError(
+    code: string,
+    description?: string,
+    ext?: OSError
+): AuthError {
+    if (ext && ext.status) {
+        switch (ext.status) {
+            case NativeStatusCodes.ACCOUNT_UNAVAILABLE:
+                return createInteractionRequiredAuthError(
+                    InteractionRequiredAuthErrorCodes.nativeAccountUnavailable
+                );
+            case NativeStatusCodes.USER_INTERACTION_REQUIRED:
+                return new InteractionRequiredAuthError(code, description);
+            case NativeStatusCodes.USER_CANCEL:
+                return createBrowserAuthError(
+                    BrowserAuthErrorCodes.userCancelled
+                );
+            case NativeStatusCodes.NO_NETWORK:
+                return createBrowserAuthError(
+                    BrowserAuthErrorCodes.noNetworkConnectivity
+                );
         }
     }
 
-    /**
-     * Create the appropriate error object based on the WAM status code.
-     * @param code
-     * @param description
-     * @param ext
-     * @returns
-     */
-    static createError(
-        code: string,
-        description: string,
-        ext?: OSError
-    ): AuthError {
-        if (ext && ext.status) {
-            switch (ext.status) {
-                case NativeStatusCode.ACCOUNT_UNAVAILABLE:
-                    return createInteractionRequiredAuthError(
-                        InteractionRequiredAuthErrorCodes.nativeAccountUnavailable
-                    );
-                case NativeStatusCode.USER_INTERACTION_REQUIRED:
-                    return new InteractionRequiredAuthError(code, description);
-                case NativeStatusCode.USER_CANCEL:
-                    return createBrowserAuthError(
-                        BrowserAuthErrorCodes.userCancelled
-                    );
-                case NativeStatusCode.NO_NETWORK:
-                    return createBrowserAuthError(
-                        BrowserAuthErrorCodes.noNetworkConnectivity
-                    );
-            }
-        }
-
-        return new NativeAuthError(code, description, ext);
-    }
-
-    /**
-     * Creates user switch error when the user chooses a different account in the native broker prompt
-     * @returns
-     */
-    static createUserSwitchError(): NativeAuthError {
-        return new NativeAuthError(
-            NativeAuthErrorMessage.userSwitch.code,
-            NativeAuthErrorMessage.userSwitch.desc
-        );
-    }
-
-    /**
-     * Creates a tokens not found error when the internal cache look up fails
-     * @returns NativeAuthError: tokensNotFoundInCache
-     */
-    static createTokensNotFoundInCacheError(): NativeAuthError {
-        return new NativeAuthError(
-            NativeAuthErrorMessage.tokensNotFoundInCache.code,
-            NativeAuthErrorMessage.tokensNotFoundInCache.desc
-        );
-    }
+    return new NativeAuthError(
+        code,
+        NativeAuthErrorMessages[code] || description,
+        ext
+    );
 }
