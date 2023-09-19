0b4a9a2955f9d510ff8188aad61514be84bc1cfa
Konstantin <kshabelko@microsoft.com>
2023-09-19 15:36:45 -0400

    - Remove `msal` dependencies in `e2e-test-utils`.

--- samples/e2eTestUtils/src/NodeCacheTestUtils.ts
+++ samples/e2eTestUtils/src/NodeCacheTestUtils.ts
@@ -1,37 +1,40 @@
 import fs from "fs";
-import { IdTokenEntity, AccessTokenEntity, RefreshTokenEntity } from "@azure/msal-common";
-import { InMemoryCache, internals } from "@azure/msal-node";
+import { Deserializer } from "./Deserializer";
+import { Serializer } from "./Serializer";
 
 export type tokenMap = {
-    idTokens: IdTokenEntity[],
-    accessTokens: AccessTokenEntity[],
-    refreshTokens: RefreshTokenEntity[]
+    idTokens: object[];
+    accessTokens: object[];
+    refreshTokens: object[];
 };
 
 export class NodeCacheTestUtils {
     static async getTokens(cacheLocation: string): Promise<tokenMap> {
-        const deserializedCache = await NodeCacheTestUtils.readCacheFile(cacheLocation);
+        const deserializedCache = await NodeCacheTestUtils.readCacheFile(
+            cacheLocation
+        );
         const tokenCache: tokenMap = {
             idTokens: [],
             accessTokens: [],
-            refreshTokens: []
+            refreshTokens: [],
         };
 
         Object.keys(tokenCache).forEach((cacheSectionKey: string) => {
             Object.keys(deserializedCache[cacheSectionKey]).map((cacheKey) => {
                 const cacheSection = deserializedCache[cacheSectionKey];
                 tokenCache[cacheSectionKey].push(cacheSection[cacheKey]);
-            })
+            });
         });
-
 
         return Promise.resolve(tokenCache);
     }
 
     static async getAccounts(cacheLocation: string): Promise<Object> {
-        const deserializedCache = await NodeCacheTestUtils.readCacheFile(cacheLocation);
+        const deserializedCache = await NodeCacheTestUtils.readCacheFile(
+            cacheLocation
+        );
         return Promise.resolve(deserializedCache.accounts || {});
-    };
+    }
 
     static async readCacheFile(cacheLocation: string): Promise<any> {
         return new Promise((resolve, reject) => {
@@ -40,20 +43,25 @@ export class NodeCacheTestUtils {
                     console.log("Error getting tokens from cache: ", err);
                     reject(err);
                 }
-                const cache = (data) ? data : this.getCacheTemplate();
-                const deserializedCache = internals.Deserializer.deserializeAllCache(JSON.parse(cache));
+                const cache = data ? data : this.getCacheTemplate();
+                const deserializedCache = Deserializer.deserializeAllCache(
+                    JSON.parse(cache)
+                );
                 resolve(deserializedCache);
             });
         });
     }
 
-    static async waitForTokens(cacheLocation: string, interval: number): Promise<tokenMap> {
+    static async waitForTokens(
+        cacheLocation: string,
+        interval: number
+    ): Promise<tokenMap> {
         let tokenCache = await this.getTokens(cacheLocation);
         if (tokenCache.idTokens.length) {
             return tokenCache;
         }
 
-        return new Promise(resolve => {
+        return new Promise((resolve) => {
             const intervalId = setInterval(async () => {
                 tokenCache = await this.getTokens(cacheLocation);
 
@@ -62,23 +70,35 @@ export class NodeCacheTestUtils {
                     resolve(tokenCache);
                 }
             }, interval);
-        })
+        });
     }
 
-    static async writeToCacheFile(cacheLocation: string, deserializedCache: InMemoryCache): Promise<void> {
+    static async writeToCacheFile(
+        cacheLocation: string,
+        deserializedCache: object
+    ): Promise<void> {
         return new Promise((resolve, reject) => {
-            fs.writeFile(cacheLocation, JSON.stringify(deserializedCache, null, 1), (error) => {
-                if (error) {
-                    console.error("Error writing to cache file in resetCache: ", error);
-                    reject(error);
+            fs.writeFile(
+                cacheLocation,
+                JSON.stringify(deserializedCache, null, 1),
+                (error) => {
+                    if (error) {
+                        console.error(
+                            "Error writing to cache file in resetCache: ",
+                            error
+                        );
+                        reject(error);
+                    }
+                    resolve();
                 }
-                resolve();
-            });
+            );
         });
     }
 
     static async expireAccessTokens(cacheLocation: string): Promise<void> {
-        const deserializedCache = await NodeCacheTestUtils.readCacheFile(cacheLocation);
+        const deserializedCache = await NodeCacheTestUtils.readCacheFile(
+            cacheLocation
+        );
         const atKeys = Object.keys(deserializedCache.accessTokens);
 
         atKeys.forEach((atKey: string) => {
@@ -86,16 +106,20 @@ export class NodeCacheTestUtils {
             deserializedCache.accessTokens[atKey].extendedExpiresOn = "0";
         });
 
-        const serializedCache = internals.Serializer.serializeAllCache(deserializedCache);
+        const serializedCache = Serializer.serializeAllCache(deserializedCache);
 
         return new Promise((resolve, reject) => {
-            fs.writeFile(cacheLocation, JSON.stringify(serializedCache, null, 1), (error) => {
-                if(error) {
-                    reject(error);
-                }
+            fs.writeFile(
+                cacheLocation,
+                JSON.stringify(serializedCache, null, 1),
+                (error) => {
+                    if (error) {
+                        reject(error);
+                    }
 
-                resolve();
-            })
+                    resolve();
+                }
+            );
         });
     }
 
@@ -110,7 +134,7 @@ export class NodeCacheTestUtils {
             IdToken: {},
             AccessToken: {},
             RefreshToken: {},
-            AppMetadata: {}
+            AppMetadata: {},
         };
     }
 
---
+++ samples/e2eTestUtils/src/Serializer.ts
@@ -0,0 +1,146 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export class Serializer {
+    /**
+     * serialize the JSON blob
+     * @param data
+     */
+    static serializeJSONBlob(data: object): string {
+        return JSON.stringify(data);
+    }
+
+    /**
+     * Serialize Accounts
+     * @param accCache
+     */
+    static serializeAccounts(accCache: object): Record<string, object> {
+        const accounts: Record<string, object> = {};
+        Object.keys(accCache).map(function (key) {
+            const accountEntity = accCache[key];
+            accounts[key] = {
+                home_account_id: accountEntity.homeAccountId,
+                environment: accountEntity.environment,
+                realm: accountEntity.realm,
+                local_account_id: accountEntity.localAccountId,
+                username: accountEntity.username,
+                authority_type: accountEntity.authorityType,
+                name: accountEntity.name,
+                client_info: accountEntity.clientInfo,
+                last_modification_time: accountEntity.lastModificationTime,
+                last_modification_app: accountEntity.lastModificationApp,
+            };
+        });
+
+        return accounts;
+    }
+
+    /**
+     * Serialize IdTokens
+     * @param idTCache
+     */
+    static serializeIdTokens(idTCache: object): Record<string, object> {
+        const idTokens: Record<string, object> = {};
+        Object.keys(idTCache).map(function (key) {
+            const idTEntity = idTCache[key];
+            idTokens[key] = {
+                home_account_id: idTEntity.homeAccountId,
+                environment: idTEntity.environment,
+                credential_type: idTEntity.credentialType,
+                client_id: idTEntity.clientId,
+                secret: idTEntity.secret,
+                realm: idTEntity.realm,
+            };
+        });
+
+        return idTokens;
+    }
+
+    /**
+     * Serializes AccessTokens
+     * @param atCache
+     */
+    static serializeAccessTokens(atCache: object): Record<string, object> {
+        const accessTokens: Record<string, object> = {};
+        Object.keys(atCache).map(function (key) {
+            const atEntity = atCache[key];
+            accessTokens[key] = {
+                home_account_id: atEntity.homeAccountId,
+                environment: atEntity.environment,
+                credential_type: atEntity.credentialType,
+                client_id: atEntity.clientId,
+                secret: atEntity.secret,
+                realm: atEntity.realm,
+                target: atEntity.target,
+                cached_at: atEntity.cachedAt,
+                expires_on: atEntity.expiresOn,
+                extended_expires_on: atEntity.extendedExpiresOn,
+                refresh_on: atEntity.refreshOn,
+                key_id: atEntity.keyId,
+                token_type: atEntity.tokenType,
+                requestedClaims: atEntity.requestedClaims,
+                requestedClaimsHash: atEntity.requestedClaimsHash,
+                userAssertionHash: atEntity.userAssertionHash,
+            };
+        });
+
+        return accessTokens;
+    }
+
+    /**
+     * Serialize refreshTokens
+     * @param rtCache
+     */
+    static serializeRefreshTokens(rtCache: object): Record<string, object> {
+        const refreshTokens: Record<string, object> = {};
+        Object.keys(rtCache).map(function (key) {
+            const rtEntity = rtCache[key];
+            refreshTokens[key] = {
+                home_account_id: rtEntity.homeAccountId,
+                environment: rtEntity.environment,
+                credential_type: rtEntity.credentialType,
+                client_id: rtEntity.clientId,
+                secret: rtEntity.secret,
+                family_id: rtEntity.familyId,
+                target: rtEntity.target,
+                realm: rtEntity.realm,
+            };
+        });
+
+        return refreshTokens;
+    }
+
+    /**
+     * Serialize amdtCache
+     * @param amdtCache
+     */
+    static serializeAppMetadata(amdtCache: object): Record<string, object> {
+        const appMetadata: Record<string, object> = {};
+        Object.keys(amdtCache).map(function (key) {
+            const amdtEntity = amdtCache[key];
+            appMetadata[key] = {
+                client_id: amdtEntity.clientId,
+                environment: amdtEntity.environment,
+                family_id: amdtEntity.familyId,
+            };
+        });
+
+        return appMetadata;
+    }
+
+    /**
+     * Serialize the cache
+     * @param jsonContent
+     */
+    static serializeAllCache(inMemCache: any): object {
+        return {
+            Account: this.serializeAccounts(inMemCache.accounts),
+            IdToken: this.serializeIdTokens(inMemCache.idTokens),
+            AccessToken: this.serializeAccessTokens(inMemCache.accessTokens),
+            RefreshToken: this.serializeRefreshTokens(inMemCache.refreshTokens),
+            AppMetadata: this.serializeAppMetadata(inMemCache.appMetadata),
+        };
+    }
+}
