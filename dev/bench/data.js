30244e4d11ba5b7ecc05b045e623ab861967010e
github-action-benchmark <github@users.noreply.github.com>
2023-09-20 16:16:22 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 0cad734bb8ded0a17c9eeedfd8d25c7717bbf6a7

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1695145910152,
+  "lastUpdate": 1695226581928,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -608,6 +608,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.24%",
             "unit": "ops/sec",
             "extra": "229 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "thomas.norling@microsoft.com",
+            "name": "Thomas Norling",
+            "username": "tnorling"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "0cad734bb8ded0a17c9eeedfd8d25c7717bbf6a7",
+          "message": "Refactor remaining errors for reduced size (#6497)\n\nCompletes the error refactor for NativeAuthError, JoseHeaderError and\r\nAuthError classes",
+          "timestamp": "2023-09-20T09:11:35-07:00",
+          "tree_id": "a99fc3cc1c464f5565e21baf5f052b54c443e791",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-js/commit/0cad734bb8ded0a17c9eeedfd8d25c7717bbf6a7"
+        },
+        "date": 1695226580967,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 144495,
+            "range": "±1.40%",
+            "unit": "ops/sec",
+            "extra": "232 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 142655,
+            "range": "±1.36%",
+            "unit": "ops/sec",
+            "extra": "231 samples"
           }
         ]
       }
