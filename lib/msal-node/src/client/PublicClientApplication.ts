2f35887759a71a3b5b762c78d7dbf92faadb290f
Hector Morales <hemoral@microsoft.com>
2023-10-03 14:27:49 -0700

    Load local authority metadata in MSAL Node PCA

--- lib/msal-node/src/client/PublicClientApplication.ts
+++ lib/msal-node/src/client/PublicClientApplication.ts
@@ -22,6 +22,8 @@ import {
     AccountInfo,
     INativeBrokerPlugin,
     ServerAuthorizationCodeResponse,
+    AuthorityOptions,
+    AuthorityFactory,
 } from "@azure/msal-common";
 import { Configuration } from "../config/Configuration.js";
 import { ClientApplication } from "./ClientApplication.js";
@@ -66,6 +68,7 @@ export class PublicClientApplication
      */
     constructor(configuration: Configuration) {
         super(configuration);
+        this.loadLocalAuthorityMetadata();
         if (this.config.broker.nativeBrokerPlugin) {
             if (this.config.broker.nativeBrokerPlugin.isBrokerAvailable) {
                 this.nativeBrokerPlugin = this.config.broker.nativeBrokerPlugin;
@@ -78,6 +81,25 @@ export class PublicClientApplication
                 );
             }
         }
+    }
+
+    private loadLocalAuthorityMetadata(): void {
+        const authorityOptions: AuthorityOptions = {
+            protocolMode: this.config.auth.protocolMode,
+            knownAuthorities: this.config.auth.knownAuthorities,
+            cloudDiscoveryMetadata: this.config.auth.cloudDiscoveryMetadata,
+            authorityMetadata: this.config.auth.authorityMetadata,
+        };
+
+        const locallyDiscoveredAuthorityInstance =
+            AuthorityFactory.createInstance(
+                this.config.auth.authority,
+                this.config.system.networkClient,
+                this.storage,
+                authorityOptions,
+                this.logger
+            );
+        locallyDiscoveredAuthorityInstance.resolveEndpointsFromLocalSources();
     }
 
     /**
