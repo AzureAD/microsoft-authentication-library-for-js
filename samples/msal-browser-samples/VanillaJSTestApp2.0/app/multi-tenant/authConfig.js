276d2bdf0899ae6fa90e26d0c3021160c54ba56c
Hector Morales <hemoral@microsoft.com>
2023-10-31 12:41:53 -0700

    Set sample to localstorage

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/multi-tenant/authConfig.js
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/multi-tenant/authConfig.js
@@ -9,7 +9,7 @@ const msalConfig = {
         authority: `${baseAuthority}/${homeTenant}`
     },
     cache: {
-        cacheLocation: "sessionStorage", // This configures where your cache will be stored
+        cacheLocation: "localStorage", // This configures where your cache will be stored
         storeAuthStateInCookie: false, // Set this to "true" if you are having issues on IE11 or Edge
     },
     system: {
