9225d42c915af330895373e05213aed842d3eec5
Thomas Norling <thomas.norling@microsoft.com>
2023-11-08 14:27:03 -0800

    Add E2E test for AcquireTokenInteractive (#6669)
    E2E test for AcquireTokenInteractive, including verification of the
    success template. Closes out the work for #6666

---
+++ samples/msal-node-samples/auth-code-cli-app/jest.config.js
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+module.exports = {
+    displayName: "Authorization Code CLI",
+    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset-no-setup.js",
+};
