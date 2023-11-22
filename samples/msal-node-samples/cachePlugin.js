f47c68fbef13822f25a729c6c8e4cfb550a02f4d
Robin <hi@rablet.dev>
2023-11-22 14:32:38 +0000

    Fix cachePlugin not resolving when cache file doesn't exist
    Signed-off-by: Robin <hi@rablet.dev>

--- samples/msal-node-samples/cachePlugin.js
+++ samples/msal-node-samples/cachePlugin.js
@@ -25,6 +25,7 @@ module.exports = function (cacheLocation) {
                     if (err) {
                         reject(err);
                     }
+                    resolve();
                 });
             }
         });
