42665061140e1d45285c004bf604a2b58e47b57c
Thomas Norling <thomas.norling@microsoft.com>
2024-01-08 11:44:35 -0800

    Restore isAppMetadataEntity helper

--- lib/msal-common/src/cache/utils/CacheHelpers.ts
+++ lib/msal-common/src/cache/utils/CacheHelpers.ts
@@ -374,6 +374,22 @@ export function generateAppMetadataKey({
         .toLowerCase();
 }
 
+/*
+ * Validates an entity: checks for all expected params
+ * @param entity
+ */
+export function isAppMetadataEntity(key: string, entity: object): boolean {
+    if (!entity) {
+        return false;
+    }
+
+    return (
+        key.indexOf(APP_METADATA) === 0 &&
+        entity.hasOwnProperty("clientId") &&
+        entity.hasOwnProperty("environment")
+    );
+}
+
 /**
  * Validates an entity: checks for all expected params
  * @param entity
