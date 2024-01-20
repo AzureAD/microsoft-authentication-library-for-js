d63e3181137e9b3771cd7de6c2a3544fc91e72e0
github-action-benchmark <github@users.noreply.github.com>
2024-01-20 22:33:16 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 54b4253e3848dc985529c77036bab8d692309bf5

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1705621066350,
+  "lastUpdate": 1705789996335,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -4520,6 +4520,44 @@ window.BENCHMARK_DATA = {
             "range": "±1.86%",
             "unit": "ops/sec",
             "extra": "221 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "54b4253e3848dc985529c77036bab8d692309bf5",
+          "message": "Bump vite from 3.2.7 to 3.2.8 (#6832)\n\nBumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite)\r\nfrom 3.2.7 to 3.2.8.\r\n<details>\r\n<summary>Changelog</summary>\r\n<p><em>Sourced from <a\r\nhref=\"https://github.com/vitejs/vite/blob/v3.2.8/packages/vite/CHANGELOG.md\">vite's\r\nchangelog</a>.</em></p>\r\n<blockquote>\r\n<h2><!-- raw HTML omitted -->3.2.8 (2024-01-19)<!-- raw HTML omitted\r\n--></h2>\r\n<ul>\r\n<li>fix: fs deny for case insensitive (<a\r\nhref=\"https://github.com/vitejs/vite/commit/a26c87d\">a26c87d</a>)</li>\r\n</ul>\r\n</blockquote>\r\n</details>\r\n<details>\r\n<summary>Commits</summary>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/7e3a86638e1585702e7fb8eaf8ede43740a6a44e\"><code>7e3a866</code></a>\r\nrelease: v3.2.8</li>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/a26c87d20f9af306b5ce3ff1648be7fa5146c278\"><code>a26c87d</code></a>\r\nfix: fs deny for case insensitive</li>\r\n<li>See full diff in <a\r\nhref=\"https://github.com/vitejs/vite/commits/v3.2.8/packages/vite\">compare\r\nview</a></li>\r\n</ul>\r\n</details>\r\n<br />\r\n\r\n\r\n[![Dependabot compatibility\r\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=vite&package-manager=npm_and_yarn&previous-version=3.2.7&new-version=3.2.8)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\r\n\r\nDependabot will resolve any conflicts with this PR as long as you don't\r\nalter it yourself. You can also trigger a rebase manually by commenting\r\n`@dependabot rebase`.\r\n\r\n[//]: # (dependabot-automerge-start)\r\n[//]: # (dependabot-automerge-end)\r\n\r\n---\r\n\r\n<details>\r\n<summary>Dependabot commands and options</summary>\r\n<br />\r\n\r\nYou can trigger Dependabot actions by commenting on this PR:\r\n- `@dependabot rebase` will rebase this PR\r\n- `@dependabot recreate` will recreate this PR, overwriting any edits\r\nthat have been made to it\r\n- `@dependabot merge` will merge this PR after your CI passes on it\r\n- `@dependabot squash and merge` will squash and merge this PR after\r\nyour CI passes on it\r\n- `@dependabot cancel merge` will cancel a previously requested merge\r\nand block automerging\r\n- `@dependabot reopen` will reopen this PR if it is closed\r\n- `@dependabot close` will close this PR and stop Dependabot recreating\r\nit. You can achieve the same result by closing it manually\r\n- `@dependabot show <dependency name> ignore conditions` will show all\r\nof the ignore conditions of the specified dependency\r\n- `@dependabot ignore this major version` will close this PR and stop\r\nDependabot creating any more for this major version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this minor version` will close this PR and stop\r\nDependabot creating any more for this minor version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this dependency` will close this PR and stop\r\nDependabot creating any more for this dependency (unless you reopen the\r\nPR or upgrade to it yourself)\r\nYou can disable automated security fix PRs for this repo from the\r\n[Security Alerts\r\npage](https://github.com/AzureAD/microsoft-authentication-library-for-js/network/alerts).\r\n\r\n</details>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-01-20T14:27:49-08:00",
+          "tree_id": "c882e83c8167a62a14b0430604e8f3d300f884c6",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-js/commit/54b4253e3848dc985529c77036bab8d692309bf5"
+        },
+        "date": 1705789995383,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 189365,
+            "range": "±1.87%",
+            "unit": "ops/sec",
+            "extra": "220 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 189452,
+            "range": "±1.65%",
+            "unit": "ops/sec",
+            "extra": "224 samples"
           }
         ]
       }
