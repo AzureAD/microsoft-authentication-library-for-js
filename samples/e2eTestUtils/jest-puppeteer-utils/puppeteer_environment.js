3460df6a6f29376ff240cd3157309c266b573a8d
Hector Morales <hemoral@microsoft.com>
2024-02-10 12:20:27 -0800

    Add debugging statements to puppeteer config

--- samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
+++ samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
@@ -7,6 +7,7 @@ class PuppeteerEnvironment extends NodeEnvironment {
 	}
 
 	async setup() {
+		process.stdout.write("SETUP STARGING");
 		await super.setup();
 
 		// connect to puppeteer
@@ -14,10 +15,14 @@ class PuppeteerEnvironment extends NodeEnvironment {
 			headless: true,
 			ignoreDefaultArgs: ["--no-sandbox", "–disable-setuid-sandbox"]
 		});
+
+		process.stdout.write(JSON.parse(this.global.__BROWSER__) + "SETUP FINISHED");
 	}
 
 	async teardown() {
+		process.stdout.write("TEARDOWN STARGING");
 		await super.teardown();
+		process.stdout.write(JSON.parse(this.global.__BROWSER__) + "TEARDOWN FINISHED");
 		this.global.__BROWSER__.close();
 	}
 
