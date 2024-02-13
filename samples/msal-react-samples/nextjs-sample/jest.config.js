071518e3fd6823cb35eef38fba62b52a77a79870
Hector Morales <hemoral@microsoft.com>
2024-02-13 15:42:34 -0800

    Add junit configs to browser and node samples

--- samples/msal-react-samples/nextjs-sample/jest.config.js
+++ samples/msal-react-samples/nextjs-sample/jest.config.js
@@ -4,5 +4,12 @@ module.exports = {
         __PORT__: 3200,
         __STARTCMD__: "npm run start -- -p 3200"
     },
-    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
+    preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js",
+    reporters: [
+        "jest-junit", {
+            "suiteName": "Next.js E2E Tests",
+            "outputDirectory": ".",
+            "outputName": "junit.xml"
+        }
+    ]
 };
