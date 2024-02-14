b6b0d71fa13e7afda908bdd5dcebc36acea312ec
Hector Morales <hemoral@microsoft.com>
2024-02-13 18:14:18 -0800

    Reset puppeteer utils

--- samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
+++ samples/e2eTestUtils/jest-puppeteer-utils/puppeteer_environment.js
@@ -7,27 +7,18 @@ class PuppeteerEnvironment extends NodeEnvironment {
 	}
 
 	async setup() {
-		process.stdout.write("SETUP STARTING");
 		await super.setup();
 
-		const puppeteerConfig = {
-			headless: "new",
-			ignoreDefaultArgs: (process.env.CI) ? [] : ["--no-sandbox", "–disable-setuid-sandbox"],
-		};
-
 		// connect to puppeteer
-		this.global.__BROWSER__ = await puppeteer.launch(puppeteerConfig);
-
-		process.stdout.write(JSON.stringify(this.global.__BROWSER__) + "SETUP FINISHED");
+		this.global.__BROWSER__ = await puppeteer.launch({
+			headless: true,
+			ignoreDefaultArgs: ["--no-sandbox", "–disable-setuid-sandbox"]
+		});
 	}
 
 	async teardown() {
-		process.stdout.write("TEARDOWN STARTING");
 		await super.teardown();
-		if(this.global.__BROWSER__) {
-		process.stdout.write(JSON.stringify(this.global.__BROWSER__) + "TEARDOWN FINISHED");
-			this.global.__BROWSER__.close();
-		}
+		this.global.__BROWSER__.close();
 	}
 
 	runScript(script) {
