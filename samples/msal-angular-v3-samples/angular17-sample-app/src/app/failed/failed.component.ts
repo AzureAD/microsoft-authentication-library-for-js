07098018ccf77b928dd76497ac04cb0c7bf18536
Jo Arroyo <joarroyo@microsoft.com>
2024-04-02 13:49:21 -0700

    Update component selector

--- samples/msal-angular-v3-samples/angular17-sample-app/src/app/failed/failed.component.ts
+++ samples/msal-angular-v3-samples/angular17-sample-app/src/app/failed/failed.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-orders',
+  selector: 'app-failed',
   templateUrl: './failed.component.html',
-  styleUrls: ['./failed.component.css']
+  styleUrls: ['./failed.component.css'],
 })
 export class FailedComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
