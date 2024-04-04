efd0e5c499cfe5ff25b5ce0626589a611caf73bf
Jo Arroyo <joarroyo@microsoft.com>
2024-04-04 16:21:30 -0700

    Update standalone jest port

--- samples/msal-angular-v3-samples/angular-standalone-sample/jest.config.js
+++ samples/msal-angular-v3-samples/angular-standalone-sample/jest.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     displayName: "angular-standalone-sample",
     globals: {
-        __PORT__: 4217,
-        __STARTCMD__: "npm start -- --port 4217",
+        __PORT__: 4201,
+        __STARTCMD__: "npm start -- --port 4201",
         __TIMEOUT__: 90000
     },
     preset: "../../e2eTestUtils/jest-puppeteer-utils/jest-preset.js"
