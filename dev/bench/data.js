f0e8ddde1e3b324bb92367b49ccc305bdc7f031e
github-action-benchmark <github@users.noreply.github.com>
2024-04-04 00:30:06 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for 4848823559291842bacb5b05d5385c58af50a3ff

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1712186193829,
+  "lastUpdate": 1712190606521,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -6712,6 +6712,44 @@ window.BENCHMARK_DATA = {
             "range": "±2.05%",
             "unit": "ops/sec",
             "extra": "216 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "4848823559291842bacb5b05d5385c58af50a3ff",
+          "message": "Bump vite from 3.2.8 to 3.2.10 (#7003)\n\nBumps [vite](https://github.com/vitejs/vite/tree/HEAD/packages/vite)\r\nfrom 3.2.8 to 3.2.10.\r\n<details>\r\n<summary>Changelog</summary>\r\n<p><em>Sourced from <a\r\nhref=\"https://github.com/vitejs/vite/blob/v3.2.10/packages/vite/CHANGELOG.md\">vite's\r\nchangelog</a>.</em></p>\r\n<blockquote>\r\n<h2><!-- raw HTML omitted -->3.2.10 (2024-03-24)<!-- raw HTML omitted\r\n--></h2>\r\n<h2><!-- raw HTML omitted -->3.2.9 (2024-03-24)<!-- raw HTML omitted\r\n--></h2>\r\n<ul>\r\n<li>fix: port <a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/15653\">#15653</a>\r\nto v3 (<a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/15655\">#15655</a>)\r\n(<a href=\"https://github.com/vitejs/vite/commit/99080ca\">99080ca</a>),\r\ncloses <a\r\nhref=\"https://redirect.github.com/vitejs/vite/issues/15653\">#15653</a>\r\n<a\r\nhref=\"https://redirect.github.com/vitejs/vite/issues/15655\">#15655</a></li>\r\n<li>fix: port <a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/16250\">#16250</a>\r\nto v3 (<a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/16253\">#16253</a>)\r\n(<a href=\"https://github.com/vitejs/vite/commit/89c7c64\">89c7c64</a>),\r\ncloses <a\r\nhref=\"https://redirect.github.com/vitejs/vite/issues/16250\">#16250</a>\r\n<a\r\nhref=\"https://redirect.github.com/vitejs/vite/issues/16253\">#16253</a></li>\r\n<li>release: v3.2.8 (<a\r\nhref=\"https://github.com/vitejs/vite/commit/8352b75\">8352b75</a>)</li>\r\n</ul>\r\n</blockquote>\r\n</details>\r\n<details>\r\n<summary>Commits</summary>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/1a8728f8dfff9d62ebcd67d65ef1cc4b82042de4\"><code>1a8728f</code></a>\r\nrelease: v3.2.10</li>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/5910f13ee2b6eac1d458f65d721c08ca600b18ab\"><code>5910f13</code></a>\r\nrelease: v3.2.9</li>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/89c7c645f09d16a38f146ef4a1528f218e844d67\"><code>89c7c64</code></a>\r\nfix: port <a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/16250\">#16250</a>\r\nto v3 (<a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/16253\">#16253</a>)</li>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/8352b7546369b6ec958fe0b828e86eaca4e5cf88\"><code>8352b75</code></a>\r\nrelease: v3.2.8</li>\r\n<li><a\r\nhref=\"https://github.com/vitejs/vite/commit/99080ca3e6c2dcdc3c2d67bb2d143c59e0a329f2\"><code>99080ca</code></a>\r\nfix: port <a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/15653\">#15653</a>\r\nto v3 (<a\r\nhref=\"https://github.com/vitejs/vite/tree/HEAD/packages/vite/issues/15655\">#15655</a>)</li>\r\n<li>See full diff in <a\r\nhref=\"https://github.com/vitejs/vite/commits/v3.2.10/packages/vite\">compare\r\nview</a></li>\r\n</ul>\r\n</details>\r\n<br />\r\n\r\n\r\n[![Dependabot compatibility\r\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=vite&package-manager=npm_and_yarn&previous-version=3.2.8&new-version=3.2.10)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\r\n\r\nDependabot will resolve any conflicts with this PR as long as you don't\r\nalter it yourself. You can also trigger a rebase manually by commenting\r\n`@dependabot rebase`.\r\n\r\n[//]: # (dependabot-automerge-start)\r\n[//]: # (dependabot-automerge-end)\r\n\r\n---\r\n\r\n<details>\r\n<summary>Dependabot commands and options</summary>\r\n<br />\r\n\r\nYou can trigger Dependabot actions by commenting on this PR:\r\n- `@dependabot rebase` will rebase this PR\r\n- `@dependabot recreate` will recreate this PR, overwriting any edits\r\nthat have been made to it\r\n- `@dependabot merge` will merge this PR after your CI passes on it\r\n- `@dependabot squash and merge` will squash and merge this PR after\r\nyour CI passes on it\r\n- `@dependabot cancel merge` will cancel a previously requested merge\r\nand block automerging\r\n- `@dependabot reopen` will reopen this PR if it is closed\r\n- `@dependabot close` will close this PR and stop Dependabot recreating\r\nit. You can achieve the same result by closing it manually\r\n- `@dependabot show <dependency name> ignore conditions` will show all\r\nof the ignore conditions of the specified dependency\r\n- `@dependabot ignore this major version` will close this PR and stop\r\nDependabot creating any more for this major version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this minor version` will close this PR and stop\r\nDependabot creating any more for this minor version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this dependency` will close this PR and stop\r\nDependabot creating any more for this dependency (unless you reopen the\r\nPR or upgrade to it yourself)\r\nYou can disable automated security fix PRs for this repo from the\r\n[Security Alerts\r\npage](https://github.com/AzureAD/microsoft-authentication-library-for-js/network/alerts).\r\n\r\n</details>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-04-03T17:24:43-07:00",
+          "tree_id": "0210c2b45970aea026300d97d34d579fe62e7cf5",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-js/commit/4848823559291842bacb5b05d5385c58af50a3ff"
+        },
+        "date": 1712190605405,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 190718,
+            "range": "±2.02%",
+            "unit": "ops/sec",
+            "extra": "214 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 228494,
+            "range": "±1.23%",
+            "unit": "ops/sec",
+            "extra": "212 samples"
           }
         ]
       }
