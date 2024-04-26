6872191eeaa6ea8adf9dcbff8040d6f0d437647f
Jo Arroyo <joarroyo@microsoft.com>
2024-04-26 16:57:18 -0700

    Add MSAL to sample

--- samples/msal-angular-v3-samples/angular18-standalone-sample/src/app/app.component.spec.ts
+++ samples/msal-angular-v3-samples/angular18-standalone-sample/src/app/app.component.spec.ts
@@ -1,10 +1,60 @@
+// import { TestBed } from '@angular/core/testing';
+// import { AppComponent } from './app.component';
+
+// describe('AppComponent', () => {
+//   beforeEach(async () => {
+//     await TestBed.configureTestingModule({
+//       imports: [AppComponent],
+//     }).compileComponents();
+//   });
+
+//   it('should create the app', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app).toBeTruthy();
+//   });
+
+//   it(`should have the 'angular18-standalone-sample' title`, () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     const app = fixture.componentInstance;
+//     expect(app.title).toEqual('angular18-standalone-sample');
+//   });
+
+//   it('should render title', () => {
+//     const fixture = TestBed.createComponent(AppComponent);
+//     fixture.detectChanges();
+//     const compiled = fixture.nativeElement as HTMLElement;
+//     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular18-standalone-sample');
+//   });
+// });
+
 import { TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalService } from '@azure/msal-angular';
 import { AppComponent } from './app.component';
+import { MSALInstanceFactory, MSALGuardConfigFactory } from './app.config';
+import { routes } from './app.routes';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent
+      ],
+      providers: [
+        provideRouter(routes),
+        MsalService,
+        MsalGuard,
+        MsalBroadcastService,
+        {
+          provide: MSAL_INSTANCE,
+          useFactory: MSALInstanceFactory
+        },
+        {
+          provide: MSAL_GUARD_CONFIG,
+          useFactory: MSALGuardConfigFactory
+        }
+      ]
     }).compileComponents();
   });
 
@@ -14,16 +64,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular18-standalone-sample' title`, () => {
+  it(`should have the 'Angular 17 Sample - MSAL Angular v3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular18-standalone-sample');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular18-standalone-sample');
+    expect(app.title).toEqual('Angular 17 Sample - MSAL Angular v3');
   });
 });
