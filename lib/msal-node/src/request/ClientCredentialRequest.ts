d0e6c95f1c42eb60a388b54f95c2b0147e7488ba
Robbie Ginsburg <rginsburg@microsoft.com>
2024-04-30 16:28:37 -0400

    updated comment

--- lib/msal-node/src/request/ClientCredentialRequest.ts
+++ lib/msal-node/src/request/ClientCredentialRequest.ts
@@ -14,7 +14,7 @@ import {
  * - authority               - URL of the authority, the security token service (STS) from which MSAL will acquire tokens.
  * - correlationId           - Unique GUID set per request to trace a request end-to-end for telemetry purposes.
  * - skipCache               - Skip token cache lookup and force request to authority to get a a new token. Defaults to false.
- * - clientAssertion         - A callback function that returns a Base64Url-encoded signed JWT assertion string used in the Client Credential flow
+ * - clientAssertion         - An assertion string or a callback function that returns an assertion string (both are Base64Url-encoded signed JWTs) used in the Client Credential flow
  * - tokenQueryParameters    - String to string map of custom query parameters added to the /token call
  * @public
  */
