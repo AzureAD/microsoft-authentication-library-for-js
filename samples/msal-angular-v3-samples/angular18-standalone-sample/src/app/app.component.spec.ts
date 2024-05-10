58e1d505075a8183795c11d79c4403b7586bcde9
Jo Arroyo <joarroyo@microsoft.com>
2024-05-10 10:48:51 -0700

    Update unit tests

--- samples/msal-angular-v3-samples/angular18-standalone-sample/src/app/app.component.spec.ts
+++ samples/msal-angular-v3-samples/angular18-standalone-sample/src/app/app.component.spec.ts
@@ -1,33 +1,3 @@
-// import { TestBed } from '@angular/core/testing';
-// import { AppComponent } from './app.component';
-
-// describe('AppComponent', () => {
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       imports: [AppComponent],
-//     }).compileComponents();
-//   });
-
-//   it('should create the app', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app).toBeTruthy();
-//   });
-
-//   it(`should have the 'angular18-standalone-sample' title`, () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     const app = fixture.componentInstance;
-//     expect(app.title).toEqual('angular18-standalone-sample');
-//   });
-
-//   it('should render title', () => {
-//     const fixture = TestBed.createComponent(AppComponent);
-//     fixture.detectChanges();
-//     const compiled = fixture.nativeElement as HTMLElement;
-//     expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular18-standalone-sample');
-//   });
-// });
-
 import { TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { MSAL_GUARD_CONFIG, MSAL_INSTANCE, MsalBroadcastService, MsalGuard, MsalService } from '@azure/msal-angular';
@@ -64,9 +34,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular 17 Sample - MSAL Angular v3' title`, () => {
+  it(`should have the 'Angular 18 Sample - MSAL Angular v3' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular 17 Sample - MSAL Angular v3');
+    expect(app.title).toEqual('Angular 18 Sample - MSAL Angular v3');
   });
 });
