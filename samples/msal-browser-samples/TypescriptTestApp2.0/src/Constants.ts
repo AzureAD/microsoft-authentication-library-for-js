cc18b42d4ffaf80b72262d0efee2d157ab9f7a1c
Hector Morales <hemoral@microsoft.com>
2024-06-06 13:13:59 -0700

    Remove outdated TS Sample (#7149)

--- samples/msal-browser-samples/TypescriptTestApp2.0/src/Constants.ts
+++
@@ -1,5 +0,0 @@
-// Add here the endpoints for MS Graph API services you would like to use.
-export const GRAPH_CONFIG = {
-    GRAPH_ME_ENDPT: "https://graph.microsoft.com/v1.0/me",
-    GRAPH_MAIL_ENDPT: "https://graph.microsoft.com/v1.0/me/messages"
-};
