2b6041320cc8bcf007dd2e6bab41745c81057aec
Hector Morales <hemoral@microsoft.com>
2024-06-26 09:46:24 -0700

    Update package-lock (#7176)
    This PR updates package-lock to fix build issues

--- samples/msal-react-samples/gatsby-sample/src/components/ErrorComponent.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Typography } from "@mui/material";
-
-export const ErrorComponent = ({error}) => {
-    return <Typography variant="h6">An Error Occurred: {error.errorCode}</Typography>;
-}
