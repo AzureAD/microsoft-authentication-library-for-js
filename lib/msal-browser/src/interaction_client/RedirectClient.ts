db7d36fd9d36df71f37363e6c3afd06b77039297
Jo Arroyo <joarroyo@microsoft.com>
2024-08-05 14:36:47 -0700

    Move redirect request clearing

--- lib/msal-browser/src/interaction_client/RedirectClient.ts
+++ lib/msal-browser/src/interaction_client/RedirectClient.ts
@@ -386,14 +386,14 @@ export class RedirectClient extends StandardInteractionClient {
                 return null;
             }
 
-            this.browserStorage.removeRequestRetried();
-            throw e;
-        } finally {
             this.browserStorage.removeTemporaryItem(
                 this.browserStorage.generateCacheKey(
                     TemporaryCacheKeys.REDIRECT_REQUEST
                 )
             );
+            this.browserStorage.removeRequestRetried();
+            throw e;
+        } finally {
             this.browserStorage.cleanRequestByInteractionType(
                 InteractionType.Redirect
             );
