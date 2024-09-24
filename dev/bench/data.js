8c05555e6e344fff07dd7381ea0e89770635a4bf
github-action-benchmark <github@users.noreply.github.com>
2024-09-24 16:36:02 +0000

    add msal-node client-credential Regression Test (benchmarkjs) benchmark result for fafaada5ecfee546f33cbc196b71c06008ebf0a9

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1726866121453,
+  "lastUpdate": 1727195762180,
   "repoUrl": "https://github.com/AzureAD/microsoft-authentication-library-for-js",
   "entries": {
     "msal-node client-credential Regression Test": [
@@ -11932,6 +11932,44 @@ window.BENCHMARK_DATA = {
             "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
             "value": 191823,
             "range": "±2.14%",
+            "unit": "ops/sec",
+            "extra": "221 samples"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "49699333+dependabot[bot]@users.noreply.github.com",
+            "name": "dependabot[bot]",
+            "username": "dependabot[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "fafaada5ecfee546f33cbc196b71c06008ebf0a9",
+          "message": "Bump rollup from 3.29.4 to 3.29.5 (#7332)\n\nBumps [rollup](https://github.com/rollup/rollup) from 3.29.4 to 3.29.5.\r\n<details>\r\n<summary>Changelog</summary>\r\n<p><em>Sourced from <a\r\nhref=\"https://github.com/rollup/rollup/blob/master/CHANGELOG.md\">rollup's\r\nchangelog</a>.</em></p>\r\n<blockquote>\r\n<h1>rollup changelog</h1>\r\n<h2>4.22.4</h2>\r\n<p><em>2024-09-21</em></p>\r\n<h3>Bug Fixes</h3>\r\n<ul>\r\n<li>Fix a vulnerability in generated code that affects IIFE, UMD and CJS\r\nbundles when run in a browser context (<a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5671\">#5671</a>)</li>\r\n</ul>\r\n<h3>Pull Requests</h3>\r\n<ul>\r\n<li><a\r\nhref=\"https://redirect.github.com/rollup/rollup/pull/5670\">#5670</a>:\r\nrefactor: Use object.prototype to check for reserved properties (<a\r\nhref=\"https://github.com/YuHyeonWook\"><code>@​YuHyeonWook</code></a>)</li>\r\n<li><a\r\nhref=\"https://redirect.github.com/rollup/rollup/pull/5671\">#5671</a>:\r\nFix DOM Clobbering CVE (<a\r\nhref=\"https://github.com/lukastaegert\"><code>@​lukastaegert</code></a>)</li>\r\n</ul>\r\n<h2>4.22.3</h2>\r\n<p><em>2024-09-21</em></p>\r\n<h3>Bug Fixes</h3>\r\n<ul>\r\n<li>Ensure that mutations in modules without side effects are observed\r\nwhile properly handling transitive dependencies (<a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5669\">#5669</a>)</li>\r\n</ul>\r\n<h3>Pull Requests</h3>\r\n<ul>\r\n<li><a\r\nhref=\"https://redirect.github.com/rollup/rollup/pull/5669\">#5669</a>:\r\nEnsure impure dependencies of pure modules are added (<a\r\nhref=\"https://github.com/lukastaegert\"><code>@​lukastaegert</code></a>)</li>\r\n</ul>\r\n<h2>4.22.2</h2>\r\n<p><em>2024-09-20</em></p>\r\n<h3>Bug Fixes</h3>\r\n<ul>\r\n<li>Revert fix for side effect free modules until other issues are\r\ninvestigated (<a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5667\">#5667</a>)</li>\r\n</ul>\r\n<h3>Pull Requests</h3>\r\n<ul>\r\n<li><a\r\nhref=\"https://redirect.github.com/rollup/rollup/pull/5667\">#5667</a>:\r\nPartially revert <a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5658\">#5658</a>\r\nand re-apply <a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5644\">#5644</a>\r\n(<a\r\nhref=\"https://github.com/lukastaegert\"><code>@​lukastaegert</code></a>)</li>\r\n</ul>\r\n<h2>4.22.1</h2>\r\n<p><em>2024-09-20</em></p>\r\n<h3>Bug Fixes</h3>\r\n<ul>\r\n<li>Revert <a\r\nhref=\"https://redirect.github.com/rollup/rollup/issues/5644\">#5644</a>\r\n&quot;stable chunk hashes&quot; while issues are being investigated</li>\r\n</ul>\r\n<h3>Pull Requests</h3>\r\n<ul>\r\n<li><a\r\nhref=\"https://redirect.github.com/rollup/rollup/pull/5663\">#5663</a>:\r\nchore(deps): update dependency inquirer to v11 (<a\r\nhref=\"https://github.com/renovate\"><code>@​renovate</code></a>[bot], <a\r\nhref=\"https://github.com/lukastaegert\"><code>@​lukastaegert</code></a>)</li>\r\n</ul>\r\n<!-- raw HTML omitted -->\r\n</blockquote>\r\n<p>... (truncated)</p>\r\n</details>\r\n<details>\r\n<summary>Commits</summary>\r\n<ul>\r\n<li><a\r\nhref=\"https://github.com/rollup/rollup/commit/dfd233d3a9feb18aa69b49b6c1da0248a4f1c6e3\"><code>dfd233d</code></a>\r\n3.29.5</li>\r\n<li><a\r\nhref=\"https://github.com/rollup/rollup/commit/2ef77c00ec2635d42697cff2c0567ccc8db34fb4\"><code>2ef77c0</code></a>\r\nFix DOM Clobbering CVE</li>\r\n<li>See full diff in <a\r\nhref=\"https://github.com/rollup/rollup/compare/v3.29.4...v3.29.5\">compare\r\nview</a></li>\r\n</ul>\r\n</details>\r\n<br />\r\n\r\n\r\n[![Dependabot compatibility\r\nscore](https://dependabot-badges.githubapp.com/badges/compatibility_score?dependency-name=rollup&package-manager=npm_and_yarn&previous-version=3.29.4&new-version=3.29.5)](https://docs.github.com/en/github/managing-security-vulnerabilities/about-dependabot-security-updates#about-compatibility-scores)\r\n\r\nDependabot will resolve any conflicts with this PR as long as you don't\r\nalter it yourself. You can also trigger a rebase manually by commenting\r\n`@dependabot rebase`.\r\n\r\n[//]: # (dependabot-automerge-start)\r\n[//]: # (dependabot-automerge-end)\r\n\r\n---\r\n\r\n<details>\r\n<summary>Dependabot commands and options</summary>\r\n<br />\r\n\r\nYou can trigger Dependabot actions by commenting on this PR:\r\n- `@dependabot rebase` will rebase this PR\r\n- `@dependabot recreate` will recreate this PR, overwriting any edits\r\nthat have been made to it\r\n- `@dependabot merge` will merge this PR after your CI passes on it\r\n- `@dependabot squash and merge` will squash and merge this PR after\r\nyour CI passes on it\r\n- `@dependabot cancel merge` will cancel a previously requested merge\r\nand block automerging\r\n- `@dependabot reopen` will reopen this PR if it is closed\r\n- `@dependabot close` will close this PR and stop Dependabot recreating\r\nit. You can achieve the same result by closing it manually\r\n- `@dependabot show <dependency name> ignore conditions` will show all\r\nof the ignore conditions of the specified dependency\r\n- `@dependabot ignore this major version` will close this PR and stop\r\nDependabot creating any more for this major version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this minor version` will close this PR and stop\r\nDependabot creating any more for this minor version (unless you reopen\r\nthe PR or upgrade to it yourself)\r\n- `@dependabot ignore this dependency` will close this PR and stop\r\nDependabot creating any more for this dependency (unless you reopen the\r\nPR or upgrade to it yourself)\r\nYou can disable automated security fix PRs for this repo from the\r\n[Security Alerts\r\npage](https://github.com/AzureAD/microsoft-authentication-library-for-js/network/alerts).\r\n\r\n</details>\r\n\r\nSigned-off-by: dependabot[bot] <support@github.com>\r\nCo-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>",
+          "timestamp": "2024-09-24T09:30:33-07:00",
+          "tree_id": "f44c560cd45d062a5875406eb6afc9cc924ed7c3",
+          "url": "https://github.com/AzureAD/microsoft-authentication-library-for-js/commit/fafaada5ecfee546f33cbc196b71c06008ebf0a9"
+        },
+        "date": 1727195761211,
+        "tool": "benchmarkjs",
+        "benches": [
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsFirstItemInTheCache",
+            "value": 183943,
+            "range": "±1.94%",
+            "unit": "ops/sec",
+            "extra": "221 samples"
+          },
+          {
+            "name": "ConfidentialClientApplication#acquireTokenByClientCredential-fromCache-resourceIsLastItemInTheCache",
+            "value": 187715,
+            "range": "±2.17%",
             "unit": "ops/sec",
             "extra": "221 samples"
           }
