0c484f5d42bbca5006c5ea1deac55abaf559430b
nguyencuong2596 <164532638+nguyencuong2596@users.noreply.github.com>
2024-12-13 14:54:28 +0000

    Update native auth

--- lib/msal-native-auth/src/index.ts
+++ lib/msal-native-auth/src/index.ts
@@ -1,3 +1,8 @@
-export function say() {
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ */
+
+export function say(): string {
     return "Hello";
 }
