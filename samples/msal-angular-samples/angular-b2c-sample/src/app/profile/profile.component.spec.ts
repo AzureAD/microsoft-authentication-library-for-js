1f824a8fee6b645bdb25b4515e385b8586f3fa7e
Jo Arroyo <joarroyo@microsoft.com>
2025-01-17 12:10:27 -0800

    Move angular-b2c-sample-app and rename

--- samples/msal-angular-samples/angular-b2c-sample/src/app/profile/profile.component.spec.ts
+++ samples/msal-angular-samples/angular-b2c-sample/src/app/profile/profile.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AppModule } from './../app.module';
+import { AppModule } from '../app.module';
 
 import { ProfileComponent } from './profile.component';
 
