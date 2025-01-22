71916048f5d9f3ab18d3a90f1289e748b957daf6
Jo Arroyo <joarroyo@microsoft.com>
2025-01-21 16:17:07 -0800

    Update zone

--- lib/msal-angular/src/test.ts
+++ lib/msal-angular/src/test.ts
@@ -3,8 +3,8 @@
  * Licensed under the MIT License.
  */
 
-import "zone.js/dist/zone";
-import "zone.js/dist/zone-testing";
+import "zone.js";
+import "zone.js/testing"
 // eslint-disable-next-line import/no-unresolved
 import { getTestBed } from "@angular/core/testing";
 import {
