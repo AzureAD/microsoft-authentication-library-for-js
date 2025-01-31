1a9a287a09141f18502b9ea1e60abb536cc4d329
Thomas Norling <thomas.norling@microsoft.com>
2025-01-31 15:22:36 -0800

    Set samesite to None for encrypt cookie

--- lib/msal-browser/src/cache/CookieStorage.ts
+++ lib/msal-browser/src/cache/CookieStorage.ts
@@ -12,6 +12,13 @@ import { IWindowStorage } from "./IWindowStorage.js";
 // Cookie life calculation (hours * minutes * seconds * ms)
 const COOKIE_LIFE_MULTIPLIER = 24 * 60 * 60 * 1000;
 
+export const SameSiteOptions = {
+    Lax: "Lax",
+    None: "None",
+} as const;
+export type SameSiteOptions =
+    (typeof SameSiteOptions)[keyof typeof SameSiteOptions];
+
 export class CookieStorage implements IWindowStorage<string> {
     initialize(): Promise<void> {
         return Promise.resolve();
@@ -40,11 +47,12 @@ export class CookieStorage implements IWindowStorage<string> {
         key: string,
         value: string,
         cookieLifeDays?: number,
-        secure: boolean = true
+        secure: boolean = true,
+        sameSite: SameSiteOptions = SameSiteOptions.Lax
     ): void {
         let cookieStr = `${encodeURIComponent(key)}=${encodeURIComponent(
             value
-        )};path=/;SameSite=Lax;`;
+        )};path=/;SameSite=${sameSite};`;
 
         if (cookieLifeDays) {
             const expireTime = getCookieExpirationTime(cookieLifeDays);
@@ -52,6 +60,7 @@ export class CookieStorage implements IWindowStorage<string> {
         }
 
         if (secure) {
+            // SameSite None requires Secure flag
             cookieStr += "Secure;";
         }
 
