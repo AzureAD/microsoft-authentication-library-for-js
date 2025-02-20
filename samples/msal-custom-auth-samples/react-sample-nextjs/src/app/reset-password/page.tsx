3c0d349a005d66dab157208ae562fc112581174d
REDMOND\axhaferllari <axhaferllari@microsoft.com>
2025-02-20 23:20:08 +0000

    [CustomAuth] Create React JS Sample skeleton

---
+++ samples/msal-custom-auth-samples/react-sample-nextjs/src/app/reset-password/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+export default function ResetPassword() {
+  return (
+    <div className="auth-container">
+      <h2>Reset Password</h2>
+      {/* Reset Password form will be added here */}
+    </div>
+  );
+}
