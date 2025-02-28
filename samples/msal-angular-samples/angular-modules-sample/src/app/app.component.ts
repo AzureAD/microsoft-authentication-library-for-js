ddfea3f2ccf012d1ddf339be5ca66903adac58e2
Shyla Summers <shylasummers@microsoft.com>
2025-02-28 11:45:43 -1000

    unnecessary import

--- samples/msal-angular-samples/angular-modules-sample/src/app/app.component.ts
+++ samples/msal-angular-samples/angular-modules-sample/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { MsalService, MsalBroadcastService, MSAL_GUARD_CONFIG, MsalGuardConfiguration } from '@azure/msal-angular';
-import { AuthenticationResult, InteractionStatus, PopupRequest, RedirectRequest, EventMessage, EventType, PublicClientApplication } from '@azure/msal-browser';
+import { AuthenticationResult, InteractionStatus, PopupRequest, RedirectRequest, EventMessage, EventType } from '@azure/msal-browser';
 import { Subject } from 'rxjs';
 import { filter, takeUntil } from 'rxjs/operators';
 
