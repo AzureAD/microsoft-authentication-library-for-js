58d8f7e6e87cbe79ca41e2174902d21009a6bf93
Brad Kovach <bradkovach@gmail.com>
2025-03-20 10:29:41 -0600

    Isolate inject() type changes to be merged into msal-v5

--- lib/msal-angular/src/constants.ts
+++ lib/msal-angular/src/constants.ts
@@ -4,17 +4,21 @@
  */
 
 import { InjectionToken } from "@angular/core";
+import { type IPublicClientApplication } from "@azure/msal-browser";
+import { type MsalBroadcastConfiguration } from "./msal.broadcast.config";
+import { type MsalGuardConfiguration } from "./msal.guard.config";
+import { type MsalInterceptorConfiguration } from "./msal.interceptor.config";
 
-export const MSAL_INSTANCE = new InjectionToken<string>("MSAL_INSTANCE");
+export const MSAL_INSTANCE = new InjectionToken<IPublicClientApplication>(
+  "MSAL_INSTANCE"
+);
 
-export const MSAL_GUARD_CONFIG = new InjectionToken<string>(
+export const MSAL_GUARD_CONFIG = new InjectionToken<MsalGuardConfiguration>(
   "MSAL_GUARD_CONFIG"
 );
 
-export const MSAL_INTERCEPTOR_CONFIG = new InjectionToken<string>(
-  "MSAL_INTERCEPTOR_CONFIG"
-);
+export const MSAL_INTERCEPTOR_CONFIG =
+  new InjectionToken<MsalInterceptorConfiguration>("MSAL_INTERCEPTOR_CONFIG");
 
-export const MSAL_BROADCAST_CONFIG = new InjectionToken<string>(
-  "MSAL_BROADCAST_CONFIG"
-);
+export const MSAL_BROADCAST_CONFIG =
+  new InjectionToken<MsalBroadcastConfiguration>("MSAL_BROADCAST_CONFIG");
