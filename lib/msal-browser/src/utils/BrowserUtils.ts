6a9ee3540f7c777a9c4852cf49b39eefd9cb6de5
Shyla Summers <shylasummers@microsoft.com>
2025-04-03 12:53:53 -0700

    format fix

--- lib/msal-browser/src/utils/BrowserUtils.ts
+++ lib/msal-browser/src/utils/BrowserUtils.ts
@@ -3,7 +3,12 @@
  * Licensed under the MIT License.
  */
 
-import { UrlString, invoke, invokeAsync, UrlUtils } from "@azure/msal-common/browser";
+import {
+    UrlString,
+    invoke,
+    invokeAsync,
+    UrlUtils,
+} from "@azure/msal-common/browser";
 import {
     createBrowserAuthError,
     BrowserAuthErrorCodes,
