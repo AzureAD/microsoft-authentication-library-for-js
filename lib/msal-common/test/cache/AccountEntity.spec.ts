52fd1c6470c0e4ca504fd1f3ce4c2a766c910d52
Shyla Summers <shylasummers@microsoft.com>
2025-04-15 11:26:43 -0700

    format fix and api extractor

--- lib/msal-common/test/cache/AccountEntity.spec.ts
+++ lib/msal-common/test/cache/AccountEntity.spec.ts
@@ -1,11 +1,14 @@
 import * as AccountEntityUtils from "../../src/cache/utils/AccountEntityUtils.js";
-import { mockAccountEntity, mockAccessTokenEntity_1 } from "./entities/cacheConstants.js";
+import {
+    mockAccountEntity,
+    mockAccessTokenEntity_1,
+} from "./entities/cacheConstants.js";
 
 describe("AccountEntity unit tests", () => {
     it("verify if an object is an AccountEntity", () => {
-        expect(
-            AccountEntityUtils.isAccountEntity(mockAccountEntity)
-        ).toEqual(true);
+        expect(AccountEntityUtils.isAccountEntity(mockAccountEntity)).toEqual(
+            true
+        );
     });
 
     it("verify if an object is not an AccountEntity", () => {
