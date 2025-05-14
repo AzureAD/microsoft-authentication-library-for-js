2af519f6e7a730c0bbb02237effac71d0ffd8a2d
Thomas Norling <thomas.norling@microsoft.com>
2025-05-14 16:06:09 -0700

    Fix E2E Tests (#7749)
    A couple of changes to fix the broken E2E tests and make tests a bit
    more reliable going forward

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserAAD.spec.ts
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserAAD.spec.ts
@@ -295,7 +295,6 @@ describe("AAD-Prod Tests", () => {
                     .startsWith("https://login.microsoftonline.com/common/")
             ).toBeTruthy();
             expect(popupWindow.url()).toContain("logout");
-            await popupWindow.waitForNavigation();
             const tokenStore = await BrowserCache.getTokens();
 
             expect(tokenStore.idTokens.length).toEqual(0);
@@ -369,8 +368,9 @@ describe("AAD-Prod Tests", () => {
                     .startsWith("https://login.microsoftonline.com/common/")
             ).toBeTruthy();
             expect(page.url()).toContain("logout");
-
-            await page.waitForNavigation();
+            await page.waitForFunction(
+                `window.location.href.startsWith("${sampleHomeUrl}")`
+            );
 
             const tokenStore = await BrowserCache.getTokens();
             expect(tokenStore.idTokens.length).toEqual(0);
