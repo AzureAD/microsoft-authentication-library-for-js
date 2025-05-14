ebac94f95a90bc859470f01a427ad1b9ef41041d
Thomas Norling <thomas.norling@microsoft.com>
2025-05-14 16:06:09 -0700

    Pull B2C test fixes from v4 branch

--- samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserAADMultiTenant.spec.ts
+++ samples/msal-browser-samples/VanillaJSTestApp2.0/app/customizable-e2e-test/test/browserAADMultiTenant.spec.ts
@@ -149,7 +149,6 @@ describe("AAD-Prod Tests", () => {
                     )
             ).toBeTruthy();
             expect(popupWindow.url()).toContain("logout");
-            await popupWindow.waitForNavigation();
             const tokenStore = await BrowserCache.getTokens();
 
             expect(tokenStore.idTokens.length).toEqual(0);
