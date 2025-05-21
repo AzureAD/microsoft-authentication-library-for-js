c34e1e47c821da4567b2e608afc6a47e7b2db396
Robbie Ginsburg <rginsburg@microsoft.com>
2025-05-21 19:08:10 -0400

    undid code deletion

--- lib/msal-node/test/client/ManagedIdentitySources/AppService.spec.ts
+++ lib/msal-node/test/client/ManagedIdentitySources/AppService.spec.ts
@@ -126,6 +126,8 @@ describe("Acquires a token successfully via an App Service Managed Identity", ()
                     ManagedIdentityUserAssignedIdQueryParameterNames.MANAGED_IDENTITY_RESOURCE_ID_NON_IMDS
                 )
             ).toEqual(MANAGED_IDENTITY_RESOURCE_ID);
+
+            jest.restoreAllMocks();
         });
     });
 
