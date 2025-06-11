f78baf362595e508acfd89766894c927d6cf7624
Robbie Ginsburg <rginsburg@microsoft.com>
2025-06-11 17:54:03 -0400

    Finished unit tests

--- lib/msal-node/test/client/ManagedIdentitySources/ImdsV2.spec.ts
+++ lib/msal-node/test/client/ManagedIdentitySources/ImdsV2.spec.ts
@@ -32,27 +32,34 @@ describe("ImdsV2", () => {
         error_description: "credential_endpoint_probe_error_description",
     };
 
-    const mockSendPostRequestAsync = (serverHeader: string, status: number) => {
+    const mockSendPostRequestAsync = (
+        status: number,
+        serverHeader?: string
+    ) => {
         const response: NetworkResponse<CredentialEndpointProbeResponse> = {
-            headers: { server: serverHeader },
+            headers: serverHeader ? { server: serverHeader } : {},
             body: credentialEndpointProbeResponse,
             status,
         };
 
-        jest.spyOn(networkClient, "sendPostRequestAsync").mockImplementation(((
-            url: string,
-            _options?: NetworkRequestOptions
-        ) => {
-            if (url === validatedCredentialEndpoint) {
-                return Promise.resolve(response);
-            }
-            throw new Error(
-                "An invalid url was used in the tests' post request"
-            );
-        }) as typeof networkClient.sendPostRequestAsync);
+        const sendPostRequestAsyncSpy: jest.SpyInstance = jest
+            .spyOn(networkClient, "sendPostRequestAsync")
+            .mockImplementation(((
+                url: string,
+                _options?: NetworkRequestOptions
+            ) => {
+                if (url === validatedCredentialEndpoint) {
+                    return Promise.resolve(response);
+                }
+                throw new Error(
+                    "An invalid url was used in the tests' post request"
+                );
+            }) as typeof networkClient.sendPostRequestAsync);
         // Type assertion is needed because sendPostRequestAsync is a generic method.
         // Jest's mockImplementation does not infer generics, so we cast to the method's type
         // to ensure the mock matches the original signature and TypeScript type checks correctly.
+
+        return sendPostRequestAsyncSpy;
     };
 
     beforeEach(() => {
@@ -74,140 +81,115 @@ describe("ImdsV2", () => {
     });
 
     describe("isCredentialEndpointAvailable Tests", () => {
-        describe("isCredentialEndpointAvailable returns true", () => {
-            test("probe response is 400 with valid version", async () => {
-                mockSendPostRequestAsync(
-                    "IMDS/1.1.1.2222",
-                    HttpStatus.BAD_REQUEST
-                );
+        test("returns true when probe response is 400 with valid version", async () => {
+            mockSendPostRequestAsync(HttpStatus.BAD_REQUEST, "IMDS/1.1.1.2222");
 
-                const managedIdentityApplication =
-                    new ManagedIdentityApplication({
-                        system: {
-                            networkClient,
-                        },
-                    });
-
-                expect(
-                    await managedIdentityApplication.getManagedIdentitySource()
-                ).toBe(ManagedIdentitySourceNames.IMDSV2);
-            });
-        });
-
-        describe("isCredentialEndpointAvailable returns false", () => {
-            test("probe response is 400 with invalid version", async () => {
-                mockSendPostRequestAsync(
-                    "IMDS/1.1.1.1111",
-                    HttpStatus.BAD_REQUEST
-                );
-
-                const managedIdentityApplication =
-                    new ManagedIdentityApplication({
-                        system: {
-                            networkClient,
-                        },
-                    });
-
-                expect(
-                    await managedIdentityApplication.getManagedIdentitySource()
-                ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
-            });
-
-            test("probe response is 400 but the server header is missing", async () => {
-                mockSendPostRequestAsync("", HttpStatus.BAD_REQUEST);
-
-                const managedIdentityApplication =
-                    new ManagedIdentityApplication({
-                        system: {
-                            networkClient,
-                        },
-                    });
-
-                expect(
-                    await managedIdentityApplication.getManagedIdentitySource()
-                ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
-            });
-
-            test("probe response is not 400 or 500", async () => {
-                mockSendPostRequestAsync(
-                    "IMDS/1.1.1.2222",
-                    HttpStatus.NOT_FOUND
-                );
-
-                const managedIdentityApplication =
-                    new ManagedIdentityApplication({
-                        system: {
-                            networkClient,
-                        },
-                    });
-
-                expect(
-                    await managedIdentityApplication.getManagedIdentitySource()
-                ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
-            });
-        });
-        describe("isCredentialEndpointAvailable network request retries", () => {
-            test("getManagedIdentitySource returns IMDSV2 after retrying on retriable status code", async () => {
-                const timeStart = new Date();
-                let attempts = 0;
-
-                jest.spyOn(
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
                     networkClient,
-                    "sendPostRequestAsync"
-                ).mockImplementation(((
-                    url: string,
-                    _options?: NetworkRequestOptions
-                ) => {
-                    attempts++;
-                    if (attempts <= 2) {
-                        return Promise.resolve({
+                },
+            });
+
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.IMDSV2);
+        });
+
+        test("returns false when probe response is 400 with invalid version", async () => {
+            mockSendPostRequestAsync(HttpStatus.BAD_REQUEST, "IMDS/1.1.1.1111");
+
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
+                    networkClient,
+                },
+            });
+
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
+        });
+
+        test("returns false when probe response is 400 but the server header is missing", async () => {
+            mockSendPostRequestAsync(HttpStatus.BAD_REQUEST);
+
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
+                    networkClient,
+                },
+            });
+
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
+        });
+
+        test("returns false when probe response is not 400 or 500", async () => {
+            const sendPostRequestSpy: jest.SpyInstance =
+                mockSendPostRequestAsync(
+                    HttpStatus.SUCCESS, // TODO: change this to NOT_FOUND after implementing the credential endpoint probe retry policy
+                    "IMDS/1.1.1.2222"
+                );
+
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
+                    networkClient,
+                },
+            });
+
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
+            expect(sendPostRequestSpy).toHaveBeenCalledTimes(1); // initial request + 0 retries
+        });
+
+        test("returns true after retrying on retriable status code", async () => {
+            const sendPostRequestSpy: jest.SpyInstance =
+                mockSendPostRequestAsync(
+                    HttpStatus.BAD_REQUEST,
+                    "IMDS/1.1.1.2222"
+                )
+                    .mockReturnValueOnce(
+                        Promise.resolve({
                             headers: {},
                             body: credentialEndpointProbeResponse,
-                            status: HttpStatus.NOT_FOUND,
-                        });
-                    }
-                    return Promise.resolve({
-                        headers: { server: "IMDS/1.1.1.2222" },
-                        body: credentialEndpointProbeResponse,
-                        status: HttpStatus.BAD_REQUEST,
-                    });
-                }) as typeof networkClient.sendPostRequestAsync);
+                            status: HttpStatus.SERVER_ERROR,
+                        })
+                    )
+                    // second retry, will trigger third retry
+                    .mockReturnValueOnce(
+                        Promise.resolve({
+                            headers: {},
+                            body: credentialEndpointProbeResponse,
+                            status: HttpStatus.SERVER_ERROR,
+                        })
+                    );
 
-                const managedIdentityApplication =
-                    new ManagedIdentityApplication({
-                        system: {
-                            networkClient,
-                        },
-                    });
-
-                const result =
-                    await managedIdentityApplication.getManagedIdentitySource();
-                const timeEnd = new Date();
-
-                expect(result).toBe(ManagedIdentitySourceNames.IMDSV2);
-                expect(attempts).toBe(3);
-                expect(
-                    timeEnd.valueOf() - timeStart.valueOf()
-                ).toBeGreaterThanOrEqual(
-                    DefaultManagedIdentityRetryPolicy.DEFAULT_MANAGED_IDENTITY_RETRY_DELAY_MS *
-                        2 * // two retries
-                        ONE_HUNDRED_TIMES_FASTER
-                );
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
+                    networkClient,
+                },
             });
 
-            /**test("getManagedIdentitySource returns DEFAULT_TO_IMDS after maximum retry attempts", async () => {
-                mockSendPostRequestAsync("", HttpStatus.GONE);
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.IMDSV2);
+            expect(sendPostRequestSpy).toHaveBeenCalledTimes(3); // initial request + 2 retries
+        });
 
-                const managedIdentityApplication = new ManagedIdentityApplication({
-                    system: {
-                        networkClient,
-                    },
-                });
+        test("returns false after after maximum retry attempts", async () => {
+            const sendPostRequestSpy: jest.SpyInstance =
+                mockSendPostRequestAsync(HttpStatus.SERVER_ERROR);
 
-                expect(
-                    await managedIdentityApplication.getManagedIdentitySource()
-                ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
-            });*/
+            const managedIdentityApplication = new ManagedIdentityApplication({
+                system: {
+                    networkClient,
+                },
+            });
+
+            expect(
+                await managedIdentityApplication.getManagedIdentitySource()
+            ).toBe(ManagedIdentitySourceNames.DEFAULT_TO_IMDS);
+            expect(sendPostRequestSpy).toHaveBeenCalledTimes(4); // initial request + 3 retries
         });
     });
 });
