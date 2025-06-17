08196b9ea59d94a06558c66aea01b3a51612317d
Jian Shen <shen.jian@live.com>
2025-06-17 12:30:15 +0100

    Remove unused codes.

--- lib/msal-browser/src/custom_auth/UserAccountAttributes.ts
+++ lib/msal-browser/src/custom_auth/UserAccountAttributes.ts
@@ -3,9 +3,6 @@
  * Licensed under the MIT License.
  */
 
-import { UserAccountAttributeError } from "./core/error/UserAccountAttributeError.js";
-import { InvalidAttributeErrorCode } from "./core/error/UserAccountAttributeErrorCodes.js";
-
 export type UserAccountAttributes = Record<string, string> & {
     city?: string;
     country?: string;
@@ -17,63 +14,3 @@ export type UserAccountAttributes = Record<string, string> & {
     streetAddress?: string;
     surname?: string;
 };
-
-export class UserAccountAttributes1 {
-    private attributes: Record<string, string>;
-
-    constructor() {
-        this.attributes = {};
-    }
-
-    setCustomAttribute(name: string, value: string): void {
-        if (!name) {
-            throw new UserAccountAttributeError(
-                InvalidAttributeErrorCode,
-                name,
-                value
-            );
-        }
-
-        this.attributes[name] = value;
-    }
-
-    setCity(value: string): void {
-        this.attributes["city"] = value;
-    }
-
-    setCountry(value: string): void {
-        this.attributes["country"] = value;
-    }
-
-    setDisplayName(value: string): void {
-        this.attributes["displayName"] = value;
-    }
-
-    setGivenName(value: string): void {
-        this.attributes["givenName"] = value;
-    }
-
-    setJobTitle(value: string): void {
-        this.attributes["jobTitle"] = value;
-    }
-
-    setPostalCode(value: string): void {
-        this.attributes["postalCode"] = value;
-    }
-
-    setState(value: string): void {
-        this.attributes["state"] = value;
-    }
-
-    setStreetAddress(value: string): void {
-        this.attributes["streetAddress"] = value;
-    }
-
-    setSurname(value: string): void {
-        this.attributes["surname"] = value;
-    }
-
-    toRecord(): Record<string, string> {
-        return this.attributes;
-    }
-}
