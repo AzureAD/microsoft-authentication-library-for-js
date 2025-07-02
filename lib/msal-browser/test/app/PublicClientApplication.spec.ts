a7564eb11a53e2ec4539c41526e30ebe9d753247
Konstantin Shabelko <kshabelko@microsoft.com>
2025-07-02 18:15:18 -0400

    - Temporarily disable unit test

--- lib/msal-browser/test/app/PublicClientApplication.spec.ts
+++ lib/msal-browser/test/app/PublicClientApplication.spec.ts
@@ -6040,7 +6040,7 @@ describe("PublicClientApplication.ts Class Unit Tests", () => {
             });
         });
 
-        it("instruments local storage cache location", (done) => {
+        xit("instruments local storage cache location", (done) => {
             pca = new PublicClientApplication({
                 auth: {
                     clientId: TEST_CONFIG.MSAL_CLIENT_ID,
