561b7caa02cae1d0f9d0d6b285b22b22a3adc434
Thomas Norling <thomas.norling@microsoft.com>
2025-07-30 10:35:30 -0700

    Node tests passing

--- lib/msal-node/src/cache/NodeStorage.ts
+++ lib/msal-node/src/cache/NodeStorage.ts
@@ -22,8 +22,6 @@ import {
     CacheHelpers,
     CredentialEntity,
     AccountInfo,
-    CredentialType,
-    AuthenticationScheme,
 } from "@azure/msal-common/node";
 
 import { Deserializer } from "./serializer/Deserializer.js";
@@ -34,7 +32,7 @@ import {
     CacheKVStore,
 } from "./serializer/SerializerTypes.js";
 import { StubPerformanceClient } from "@azure/msal-common";
-import { CACHE } from "../utils/Constants.js";
+import { generateAccountKey, generateCredentialKey } from "./CacheHelpers.js";
 
 /**
  * This class implements Storage for node, reading cache from user specified storage location or an  extension library
@@ -208,39 +206,11 @@ export class NodeStorage extends CacheManager {
     }
 
     generateCredentialKey(credential: CredentialEntity): string {
-        const familyId =
-            (credential.credentialType === CredentialType.REFRESH_TOKEN &&
-                credential.familyId) ||
-            credential.clientId;
-        const scheme =
-            credential.tokenType &&
-            credential.tokenType.toLowerCase() !==
-                AuthenticationScheme.BEARER.toLowerCase()
-                ? credential.tokenType.toLowerCase()
-                : "";
-        const credentialKey = [
-            credential.homeAccountId,
-            credential.environment,
-            credential.credentialType,
-            familyId,
-            credential.realm || "",
-            credential.target || "",
-            credential.requestedClaimsHash || "",
-            scheme,
-        ];
-
-        return credentialKey.join(CACHE.KEY_SEPARATOR).toLowerCase();
+        return generateCredentialKey(credential);
     }
 
     generateAccountKey(account: AccountInfo): string {
-        const homeTenantId = account.homeAccountId.split(".")[1];
-        const accountKey = [
-            account.homeAccountId,
-            account.environment,
-            homeTenantId || account.tenantId || "",
-        ];
-
-        return accountKey.join(CACHE.KEY_SEPARATOR).toLowerCase();
+        return generateAccountKey(account);
     }
 
     getAccountKeys(): string[] {
--- shared-test-utils/src/CredentialGenerators.ts
+++ shared-test-utils/src/CredentialGenerators.ts
@@ -68,6 +68,7 @@ export function buildIdToken(
         secret: idTokenSecret,
         clientId: "mock_client_id",
         homeAccountId: homeAccountId,
+        lastUpdatedAt: Date.now().toString()
     };
 
     return { ...idToken, ...options };
