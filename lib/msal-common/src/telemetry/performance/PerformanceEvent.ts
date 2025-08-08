2f867393b404b1fc90f590c226b2ab65cf1b5a6c
Konstantin <kshabelko@microsoft.com>
2025-08-08 15:12:26 -0400

    Instrument timed out or cancelled pre-redirect requests

--- lib/msal-common/src/telemetry/performance/PerformanceEvent.ts
+++ lib/msal-common/src/telemetry/performance/PerformanceEvent.ts
@@ -329,6 +329,8 @@ export type PerformanceEvent = {
     msalInstanceCount?: number;
     // Number of MSAL JS instances using the same client id in the frame
     sameClientIdInstanceCount?: number;
+
+    navigateCallbackResult?: boolean;
 };
 
 export type PerformanceEventContext = {
