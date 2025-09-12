c914f853de304b7dba075a31ddbf3a7cbdef6b47
Yongdi Wang <ydi.w127@gmail.com>
2025-09-12 10:50:17 +0100

    Add extraQueryParams for api calls (#7974) (#8021)
    Add extraQueryParams for api calls
    (https://github.com/AzureAD/microsoft-authentication-library-for-js/pull/7974)

--- lib/msal-browser/src/custom_auth/core/network_client/custom_auth_api/CustomAuthApiClient.ts
+++ lib/msal-browser/src/custom_auth/core/network_client/custom_auth_api/CustomAuthApiClient.ts
@@ -18,25 +18,29 @@ export class CustomAuthApiClient implements ICustomAuthApiClient {
         customAuthApiBaseUrl: string,
         clientId: string,
         httpClient: IHttpClient,
-        capabilities?: string
+        capabilities?: string,
+        customAuthApiQueryParams?: Record<string, string>
     ) {
         this.signInApi = new SignInApiClient(
             customAuthApiBaseUrl,
             clientId,
             httpClient,
-            capabilities
+            capabilities,
+            customAuthApiQueryParams
         );
         this.signUpApi = new SignupApiClient(
             customAuthApiBaseUrl,
             clientId,
             httpClient,
-            capabilities
+            capabilities,
+            customAuthApiQueryParams
         );
         this.resetPasswordApi = new ResetPasswordApiClient(
             customAuthApiBaseUrl,
             clientId,
             httpClient,
-            capabilities
+            capabilities,
+            customAuthApiQueryParams
         );
     }
 }
