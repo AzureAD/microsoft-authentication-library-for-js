1194b2eb82596f66fe20a083c3e0e20c7d1c9f6c
Konstantin Shabelko <kshabelko@microsoft.com>
2025-09-15 14:26:07 -0400

    - Update node tests

--- lib/msal-node/test/utils/TestConstants.ts
+++ lib/msal-node/test/utils/TestConstants.ts
@@ -257,7 +257,7 @@ export const mockAccountInfo: AccountInfo = {
             } as TenantProfile,
         ],
     ]),
-    dataBoundary: undefined
+    dataBoundary: undefined,
 };
 
 export const mockNativeAccountInfo: AccountInfo = {
