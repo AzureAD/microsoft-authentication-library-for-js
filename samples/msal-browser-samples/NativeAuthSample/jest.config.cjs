58062fa00f303ffab88892ebe5d94c034e707579
yongdiw <ydi.w127@gmail.com>
2025-12-10 21:46:31 +0000

    Update

--- samples/msal-browser-samples/NativeAuthSample/jest.config.cjs
+++ samples/msal-browser-samples/NativeAuthSample/jest.config.cjs
@@ -1,5 +1,5 @@
 module.exports = {
-    displayName: "Native Auth Sample App",
+    displayName: "Native Auth Sample App E2E tests",
     globals: {
         __PORT__: 30670,
         __STARTCMD__: "npm start -- --port 30670",
