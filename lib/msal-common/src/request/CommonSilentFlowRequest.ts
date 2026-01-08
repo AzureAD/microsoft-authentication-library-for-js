a85f9462055c6869fcd3b02262d2115b08fc64e4
Jo Arroyo <joarroyo@microsoft.com>
2026-01-07 20:05:40 -0800

    format:fix

--- lib/msal-common/src/request/CommonSilentFlowRequest.ts
+++ lib/msal-common/src/request/CommonSilentFlowRequest.ts
@@ -10,19 +10,19 @@ import { BaseAuthRequest } from "./BaseAuthRequest.js";
  * SilentFlow parameters passed by the user to retrieve credentials silently
  */
 export type CommonSilentFlowRequest = BaseAuthRequest & {
-    /** 
+    /**
      * Account object to lookup the credentials
      */
     account: AccountInfo;
-    /** 
-     * Skip cache lookup and forces network call(s) to get fresh tokens 
+    /**
+     * Skip cache lookup and forces network call(s) to get fresh tokens
      */
     forceRefresh: boolean;
-    /** 
-     * RedirectUri registered on the app registration - only required in brokering scenarios 
+    /**
+     * RedirectUri registered on the app registration - only required in brokering scenarios
      */
     redirectUri?: string;
-    /** 
+    /**
      * If refresh token will expire within the configured value, consider it already expired. Used to pre-emptively invoke interaction when cached refresh token is close to expiry.
      */
     refreshTokenExpirationOffsetSeconds?: number;
