6a1c1de298c67c91be926a9a628e6df5d6983187
Jo Arroyo <joarroyo@microsoft.com>
2026-01-09 11:15:27 -0800

    [v5] Update typedoc comments for requests (#8218)
    This PR updates our comments for various requests as this helps our
    typedoc documentation.
    
    ---------
    
    Co-authored-by: Thomas Norling <thomas.norling@microsoft.com>

--- lib/msal-node/src/request/ManagedIdentityRequestParams.ts
+++ lib/msal-node/src/request/ManagedIdentityRequestParams.ts
@@ -5,13 +5,19 @@
 
 /**
  * ManagedIdentityRequest
- * - claims       - a stringified claims request which will be used to determine whether or not the cache should be skipped
- * - forceRefresh - forces managed identity requests to skip the cache and make network calls if true
- * - resource     - resource requested to access the protected API. It should be of the form "ResourceIdUri" or "ResourceIdUri/.default". For instance https://management.azure.net or, for Microsoft Graph, https://graph.microsoft.com/.default
  * @public
  */
 export type ManagedIdentityRequestParams = {
+    /**
+     * A stringified claims request which will be used to determine whether or not the cache should be skipped
+     */
     claims?: string;
+    /**
+     * Forces managed identity requests to skip the cache and make network calls if true
+     */
     forceRefresh?: boolean;
+    /**
+     * Resource requested to access the protected API. It should be of the form "ResourceIdUri" or "ResourceIdUri/.default". For instance https://management.azure.net or, for Microsoft Graph, https://graph.microsoft.com/.default
+     */
     resource: string;
 };
