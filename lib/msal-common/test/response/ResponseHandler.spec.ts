fdf87a64ef6471994153175de61830dd4fa8c7fb
Thomas Norling <thomas.norling@microsoft.com>
2026-01-09 13:59:05 -0800

    fix test

--- lib/msal-common/test/response/ResponseHandler.spec.ts
+++ lib/msal-common/test/response/ResponseHandler.spec.ts
@@ -362,7 +362,8 @@ describe("ResponseHandler.ts", () => {
                 testResponse,
                 testAuthority,
                 reqTimestamp,
-                testRequest
+                testRequest,
+                0
             );
 
             expect(addFieldsSpy).toHaveBeenCalledWith(
