e5c9911fa7e7d19c25c5aadbdf483bc2973d56f6
Thomas Norling <thomas.norling@microsoft.com>
2026-03-10 10:47:08 -0700

    fix formatting

--- lib/msal-browser/src/interaction_client/StandardInteractionClient.ts
+++ lib/msal-browser/src/interaction_client/StandardInteractionClient.ts
@@ -327,9 +327,7 @@ export async function initializeAuthorizationRequest(
         logger,
         correlationId
     );
-    if (
-        new URL(redirectUri).origin !== new URL(window.location.href).origin
-    ) {
+    if (new URL(redirectUri).origin !== new URL(window.location.href).origin) {
         logger.warning(
             "The origin of the redirect URI does not match the origin of the current page. This is likely to cause issues with authentication.",
             correlationId
