29f55faf2b29270122c0d8fe2a8588d4809f946f
Jo Arroyo <joarroyo@microsoft.com>
2026-03-25 21:23:14 -0700

    [v5] Add support for React 16 and 17 (#8461)

--- lib/msal-react/README.md
+++ lib/msal-react/README.md
@@ -40,7 +40,7 @@ The `@azure/msal-react` package described by the code in this folder uses the [`
 
 | MSAL React version    | MSAL support status | Supported React versions |
 | --------------------- | ------------------- | ------------------------ |
-| MSAL React v5         | Active development  | 18, 19                   |
+| MSAL React v5         | Active development  | 16 (16.8+), 17, 18, 19  |
 | MSAL React v3         | In maintenance      | 16, 17, 18, 19           |
 | MSAL React v1, v2     | In maintenance      | 16, 17, 18               |
 
@@ -134,6 +134,9 @@ Our [samples directory](https://github.com/AzureAD/microsoft-authentication-libr
 -   [Next.js Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/nextjs-sample)
 -   [Gatsby Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/gatsby-sample)
 -   [B2C Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/b2c-sample)
+-   [React 16 Compat Sample (MUI v4)](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/react16-sample)
+-   [React 17 Compat Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/react17-sample)
+-   [React 18 Compat Sample](https://github.com/AzureAD/microsoft-authentication-library-for-js/tree/dev/samples/msal-react-samples/react18-sample)
 
 More advanced samples backed with a tutorial can be found in the [Azure Samples](https://github.com/Azure-Samples) space on GitHub:
 
---
+++ samples/msal-react-samples/react18-sample/src/pages/Profile.jsx
@@ -0,0 +1,76 @@
+import { useEffect, useState, useCallback } from "react";
+
+// Msal imports
+import { MsalAuthenticationTemplate, useMsal } from "@azure/msal-react";
+import { EventType, InteractionType, InteractionRequiredAuthError } from "@azure/msal-browser";
+import { loginRequest } from "../authConfig";
+
+// Sample app imports
+import { ProfileData } from "../ui-components/ProfileData";
+import { Loading } from "../ui-components/Loading";
+import { ErrorComponent } from "../ui-components/ErrorComponent";
+import { callMsGraph } from "../utils/MsGraphApiCall";
+
+// Material-ui imports
+import Paper from "@mui/material/Paper";
+
+const ProfileContent = () => {
+    const { instance } = useMsal();
+    const [graphData, setGraphData] = useState(null);
+
+    const fetchProfile = useCallback(() => {
+        if (!instance.getActiveAccount()) {
+            return;
+        }
+        callMsGraph().then(response => setGraphData(response)).catch((e) => {
+            if (e instanceof InteractionRequiredAuthError) {
+                instance.acquireTokenRedirect({
+                    ...loginRequest,
+                    account: instance.getActiveAccount()
+                });
+            }
+        });
+    }, [instance]);
+
+    useEffect(() => {
+        // Attempt to fetch profile data immediately
+        fetchProfile();
+
+        // Subscribe to active account changes so the Graph call is retried
+        // once setActiveAccount has been called.
+        const callbackId = instance.addEventCallback((event) => {
+            if (event.eventType === EventType.ACTIVE_ACCOUNT_CHANGED) {
+                fetchProfile();
+            }
+        });
+
+        return () => {
+            if (callbackId) {
+                instance.removeEventCallback(callbackId);
+            }
+        };
+    }, [instance, fetchProfile]);
+  
+    return (
+        <Paper>
+            { graphData ? <ProfileData graphData={graphData} /> : null }
+        </Paper>
+    );
+};
+
+export function Profile() {
+    const authRequest = {
+        ...loginRequest
+    };
+
+    return (
+        <MsalAuthenticationTemplate 
+            interactionType={InteractionType.Popup} 
+            authenticationRequest={authRequest} 
+            errorComponent={ErrorComponent} 
+            loadingComponent={Loading}
+        >
+            <ProfileContent />
+        </MsalAuthenticationTemplate>
+      )
+};
