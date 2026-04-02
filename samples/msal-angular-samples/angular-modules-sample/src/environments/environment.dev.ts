5b27dcf12105babc1b7d3bddb24029ac37c73538
shylasummers <shylasummers@users.noreply.github.com>
2026-04-02 12:21:56 -0700

    [v5] Migrate tests to use new test tenant (#8416)
    This PR migrates tests to use the new test tenant. More specifically:
    1. It replaces deprecated lab API calls with a key vault fetch for user
    credentials, app configs, etc.
    2. It changes any client IDs in the old test tenant to its equivalent in
    ID4SLab2
    3. It enables fetching the newest certificate rather than using the
    thumbprint
    
    This PR also re-enables the ADFS tests and removes axios as a dependency
    in the MSAL JS library.

--- samples/msal-angular-samples/angular-modules-sample/src/environments/environment.dev.ts
+++ samples/msal-angular-samples/angular-modules-sample/src/environments/environment.dev.ts
@@ -2,7 +2,7 @@ export const environment = {
     production: false,
     msalConfig: {
         auth: {
-            clientId: 'b5c2e510-4a17-4feb-b219-e55aa5b74144',
+            clientId: '0845a021-afdf-4126-abdd-099c5e6948e1',
             authority: 'https://login.microsoftonline.com/common'
         }
     },
