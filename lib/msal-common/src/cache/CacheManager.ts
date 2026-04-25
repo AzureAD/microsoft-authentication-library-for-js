2d48fb05a60fb06b9ca69c70fd54a61133f7cae3
Ruijun Gao <rckgao@gmail.com>
2026-04-25 08:00:47 +0800

    fix(msal-common): use proper 2-arg comparator in getAccountInfoFilteredBy sort (#8477)
    ## Problem
    
    `getAccountInfoFilteredBy()` and the e2e test sample use
    `Array.prototype.sort()` with a single-argument comparator, which is
    incorrect. `sort()` expects two arguments and a return value of
    negative/zero/positive. The 1-arg form never returns `0`, breaking
    stable sort guarantees.
    
    **Impact:** After cross-tenant `acquireTokenSilent`, both home and guest
    tenant profiles have cached ID tokens. `getAccount({ homeAccountId })`
    may return the guest profile instead of the expected home profile.
    
    ## Fix
    
    Replace 1-arg comparators with proper 2-arg comparators that return `0`
    for equal priority, preserving insertion order.
    
    ## Changes
    
    | File | Change |
    |------|--------|
    | `lib/msal-common/src/cache/CacheManager.ts` | Fix sort comparator in
    `getAccountInfoFilteredBy` |
    | `lib/msal-common/test/cache/CacheManager.spec.ts` | Add test verifying
    stable sort when both accounts have idTokenClaims |
    | `samples/.../customizable-e2e-test/auth.js` | Fix same 1-arg sort
    pattern for home tenant priority |
    
    Co-authored-by: Ruijun Gao <ruijungao@microsoft.com>
    Co-authored-by: Hector Morales <hemoral@microsoft.com>

--- lib/msal-common/src/cache/CacheManager.ts
+++ lib/msal-common/src/cache/CacheManager.ts
@@ -4,49 +4,49 @@
  */
 
 import {
-    AccountFilter,
-    CredentialFilter,
-    ValidCredentialType,
-    AppMetadataFilter,
-    AppMetadataCache,
-    TokenKeys,
-    TenantProfileFilter,
-} from "./utils/CacheTypes.js";
-import { CacheRecord } from "./entities/CacheRecord.js";
-import * as Constants from "../utils/Constants.js";
-import { CredentialEntity } from "./entities/CredentialEntity.js";
-import { ScopeSet } from "../request/ScopeSet.js";
-import { AccountEntity } from "./entities/AccountEntity.js";
-import { AccessTokenEntity } from "./entities/AccessTokenEntity.js";
-import { IdTokenEntity } from "./entities/IdTokenEntity.js";
-import { RefreshTokenEntity } from "./entities/RefreshTokenEntity.js";
-import { ICacheManager } from "./interface/ICacheManager.js";
-import {
-    createClientAuthError,
-    ClientAuthErrorCodes,
-} from "../error/ClientAuthError.js";
-import {
     AccountInfo,
     TenantProfile,
     updateAccountTenantProfileData,
 } from "../account/AccountInfo.js";
-import { AppMetadataEntity } from "./entities/AppMetadataEntity.js";
-import { ServerTelemetryEntity } from "./entities/ServerTelemetryEntity.js";
-import { ThrottlingEntity } from "./entities/ThrottlingEntity.js";
 import { extractTokenClaims } from "../account/AuthToken.js";
-import { ICrypto } from "../crypto/ICrypto.js";
-import { AuthorityMetadataEntity } from "./entities/AuthorityMetadataEntity.js";
-import { BaseAuthRequest } from "../request/BaseAuthRequest.js";
-import { Logger } from "../logger/Logger.js";
-import { name, version } from "../packageMetadata.js";
-import { StoreInCache } from "../request/StoreInCache.js";
+import { TokenClaims } from "../account/TokenClaims.js";
 import { getAliasesFromStaticSources } from "../authority/AuthorityMetadata.js";
 import { StaticAuthorityOptions } from "../authority/AuthorityOptions.js";
-import { TokenClaims } from "../account/TokenClaims.js";
-import { IPerformanceClient } from "../telemetry/performance/IPerformanceClient.js";
-import { createCacheError } from "../error/CacheError.js";
-import * as AccountEntityUtils from "./utils/AccountEntityUtils.js";
+import { ICrypto } from "../crypto/ICrypto.js";
 import { AuthError } from "../error/AuthError.js";
+import { createCacheError } from "../error/CacheError.js";
+import {
+    ClientAuthErrorCodes,
+    createClientAuthError,
+} from "../error/ClientAuthError.js";
+import { Logger } from "../logger/Logger.js";
+import { name, version } from "../packageMetadata.js";
+import { BaseAuthRequest } from "../request/BaseAuthRequest.js";
+import { ScopeSet } from "../request/ScopeSet.js";
+import { StoreInCache } from "../request/StoreInCache.js";
+import { IPerformanceClient } from "../telemetry/performance/IPerformanceClient.js";
+import * as Constants from "../utils/Constants.js";
+import { AccessTokenEntity } from "./entities/AccessTokenEntity.js";
+import { AccountEntity } from "./entities/AccountEntity.js";
+import { AppMetadataEntity } from "./entities/AppMetadataEntity.js";
+import { AuthorityMetadataEntity } from "./entities/AuthorityMetadataEntity.js";
+import { CacheRecord } from "./entities/CacheRecord.js";
+import { CredentialEntity } from "./entities/CredentialEntity.js";
+import { IdTokenEntity } from "./entities/IdTokenEntity.js";
+import { RefreshTokenEntity } from "./entities/RefreshTokenEntity.js";
+import { ServerTelemetryEntity } from "./entities/ServerTelemetryEntity.js";
+import { ThrottlingEntity } from "./entities/ThrottlingEntity.js";
+import { ICacheManager } from "./interface/ICacheManager.js";
+import * as AccountEntityUtils from "./utils/AccountEntityUtils.js";
+import {
+    AccountFilter,
+    AppMetadataCache,
+    AppMetadataFilter,
+    CredentialFilter,
+    TenantProfileFilter,
+    TokenKeys,
+    ValidCredentialType,
+} from "./utils/CacheTypes.js";
 
 /**
  * Interface class which implement cache storage functions used by MSAL to perform validity checks, and store tokens.
@@ -315,8 +315,10 @@ export abstract class CacheManager implements ICacheManager {
         const allAccounts = this.getAllAccounts(accountFilter, correlationId);
         if (allAccounts.length > 1) {
             // If one or more accounts are found, prioritize accounts that have an ID token
-            const sortedAccounts = allAccounts.sort((account) => {
-                return account.idTokenClaims ? -1 : 1;
+            const sortedAccounts = allAccounts.sort((a, b) => {
+                const aHasClaims = a.idTokenClaims ? 1 : 0;
+                const bHasClaims = b.idTokenClaims ? 1 : 0;
+                return bHasClaims - aHasClaims;
             });
             return sortedAccounts[0];
         } else if (allAccounts.length === 1) {
