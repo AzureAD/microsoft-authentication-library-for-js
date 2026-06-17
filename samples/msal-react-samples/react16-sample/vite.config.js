f89362389a3e12bb71c2362bec8916b9da8ba39d
Shyla Summers <shylasummers@microsoft.com>
2026-06-17 14:15:03 -0700

    build errors

--- samples/msal-react-samples/react16-sample/vite.config.js
+++ samples/msal-react-samples/react16-sample/vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    build: { target: "esnext" },
     server: {
         port: 3000,
         strictPort: true,
