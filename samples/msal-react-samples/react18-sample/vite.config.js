f45143c45d66e8b84b8030c53766aaa741cf4643
Shyla Summers <shylasummers@microsoft.com>
2026-06-22 10:29:19 -0700

    resolve react sample errors

--- samples/msal-react-samples/react18-sample/vite.config.js
+++ samples/msal-react-samples/react18-sample/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
     plugins: [react()],
+    resolve: {
+        dedupe: ["react", "react-dom"],
+        alias: {
+            react: path.resolve(__dirname, "node_modules/react"),
+            "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+        },
+    },
     server: {
         port: 3000,
         strictPort: true,
